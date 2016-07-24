e6856d45fdaa1bb225de743e67d1d17a8847b8fe
David Merfield <dmerfield@gmail.com>
2016-07-24 16:33:41 -0700

    added configuration files

---
+++ config/index.js
@@ -0,0 +1,126 @@
+var fs = require('fs');
+
+function load (name) {
+  return fs.readFileSync(__dirname + '/secrets/' + name, 'utf-8');
+}
+
+var flags = require('./flags');
+
+var production = flags.production === true;
+var maintenance = flags.maintenance === true;
+var dropbox_test_app = flags.dropbox_test_app === true;
+var cache = flags.cache === true;
+var debug = flags.debug === true;
+
+var environment, host, protocol, stripe_secret;
+var dropbox_secret, dropbox_key;
+
+if (production) {
+
+  environment = 'production';
+  host = 'blot.im';
+  protocol = "https://";
+  stripe_key = load('stripe.live.key');
+  stripe_secret = load('stripe.live.secret');
+  pandoc_path = '/home/ec2-user/.local/bin/pandoc';
+
+} else {
+
+  environment = 'development';
+  host = "localhost:8080";
+  protocol = "http://";
+  stripe_key = load('stripe.test.key');
+  stripe_secret = load('stripe.test.secret');
+  pandoc_path = '/Users/David/.cabal/bin/pandoc';
+
+}
+
+// We can only use the Dropbox test app in development
+if (dropbox_test_app && production === false) {
+
+  dropbox_key = load('dropbox.test.key');
+  dropbox_secret = load('dropbox.test.secret');
+
+} else {
+
+  dropbox_key = load('dropbox.live.key');
+  dropbox_secret = load('dropbox.live.secret');
+}
+
+
+module.exports = {
+
+  "environment": environment,
+  "host": host,
+  "protocol": protocol,
+
+  "maintenance": maintenance,
+  "cache": cache,
+  "debug": debug,
+
+  "ip": "54.191.179.131",
+
+  "port": 8080,
+
+  "redis": {"port": 6379},
+
+  "admin": {
+    "uid": "2302164",
+    "email": "dmerfield@gmail.com"
+  },
+
+  "dropbox": {
+    "key": dropbox_key,
+    "secret": dropbox_secret,
+    "sandbox": true
+  },
+
+  "stripe": {
+    "key": stripe_key,
+    "secret": stripe_secret
+  },
+
+  "pandoc_path": pandoc_path,
+
+  "cdn": {
+    "bucket": "blot-blogs",
+    "host": "blotcdn.com"
+  },
+
+  "session": {
+    "secret": load('session.secret')
+  },
+
+  "youtube": {
+    "secret": load('youtube.secret')
+  },
+
+  "mailgun": {
+    "key": load('mailgun.key'),
+    "domain": "blot.im",
+    "from": "David Merfield <dmerfield@gmail.com>"
+  },
+
+  "s3": {
+    "key": load('s3.key'),
+    "secret": load('s3.secret'),
+    "buckets": {
+      "dump": "blot-dump",
+      "blogs": "blot-blogs",
+      "backups": "blot-backups"
+    }
+  },
+
+  "backup": {
+    "bucket": "blot-backups",
+    "password": load('backup.secret')
+  },
+
+  "twitter": {
+    "consumer_key": load('twitter.key'),
+    "consumer_secret": load('twitter.secret'),
+    "access_token": load('twitter.token.key'),
+    "access_token_secret": load('twitter.token.secret')
+  }
+
+};
