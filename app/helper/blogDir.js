57bf137acaeb422dd032e3f0d433bf686a682c3c
David Merfield <dmerfield@gmail.com>
2016-07-27 14:10:19 -0700

    simplified blog dir helper

--- app/helper/blogDir.js
+++ app/helper/blogDir.js
@@ -2,16 +2,10 @@ var fs = require('fs');
 var rootDir = require('./rootDir');
 var blogDir = rootDir + '/blogs';
 
-// Ensure tmp dir exists
-fs.exists(blogDir, function(exists) {
-
-  if (!exists) {
-    console.log('Made directory for blog files');
-    fs.mkdir(blogDir, function(){
-
-    });
-  }
-
-});
+try {
+  fs.mkdirSync(blogDir)
+} catch (e) {
+  if (e.code !== 'EEXIST') throw e;
+}
 
 module.exports = blogDir;
