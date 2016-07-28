13f4d58fcc82902f4c9a4f8a806c109c04ad70f9
David Merfield <dmerfield@gmail.com>
2016-07-27 18:16:43 -0700

    added experimental sort by path feature

---
+++ app/render/retrieve/sort:path.js
@@ -0,0 +1,18 @@
+module.exports = function (req, callback) {
+
+  return callback(null, function () {
+
+    this.entries = this.entries.sort(function(a, b){
+
+      var textA = a.path.toUpperCase();
+      var textB = b.path.toUpperCase();
+
+      return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+    });
+
+    return function(text, render){
+
+      return render(text);
+    };
+  });
+};
