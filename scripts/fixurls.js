80796477000f0bb12ef0b120583d72b4eff7d0a4
David Merfield <dmerfield@gmail.com>
2016-08-04 15:45:41 -0700

    updated migration scripts

--- scripts/fixurls.js
+++ scripts/fixurls.js
@@ -4,87 +4,17 @@ var urlkey = Entry.key.url;
 var helper = require('helper');
 var forEach = helper.forEach;
 var eachBlog = require('./each/blog');
-
+var normalize = helper.pathNormalizer;
 var options = require('minimist')(process.argv.slice(2));
 
-var cache = require('../app/cache');
+eachBlog(function(user, blog, nextBlog){
 
-// THIS SCRIPT NEEDS TO BE RESUMABLE
-// this script probably fucks ip blog's menus
-// anything which depends on entry.id needsd checking
+  entries(blog.id, function(){
 
-// remove all the cache keys to reduce the time
-// needed to clean the blog keys.
-
-// does this upgrade urls to latest permalink design BUT preserve redirects for old ones?
-
-throw 'NOT READY';
-
-cache.flush(function(){
-
-  clean('*', function(){
-
-    eachBlog(function(user, blog, nextBlog){
-
-      entries(blog.id, function(){
-
-        nextBlog();
-      });
-
-    }, process.exit, options);
-
+    nextBlog();
   });
-});
 
-
-function clean (blogID, callback) {
-
-  console.log('Blog:', blogID, 'Cleaning');
-
-  var patterns = [
-    'blog:' + blogID + ':entries',
-    'blog:' + blogID + ':drafts',
-    'blog:' + blogID + ':path',
-    'blog:' + blogID + ':scheduled',
-    'blog:' + blogID + ':pages',
-    'blog:' + blogID + ':deleted',
-    'blog:' + blogID + ':public:*',
-    'blog:' + blogID + ':url:*',
-    'blog:' + blogID + ':tags:all',
-    'blog:' + blogID + ':next_entry_id',
-    'blog:' + blogID + ':tags:entries:*',
-    'blog:' + blogID + ':tags:entry:*',
-    'blog:' + blogID + ':search:*',
-    'blog:' + blogID + ':tags:name:*'
-  ];
-
-  var all_keys = [];
-
-  forEach.parallel(patterns, function(pattern, nextPattern){
-
-    redis.keys(pattern, function(err, keys){
-
-      if (err) throw err;
-
-      if (!keys.length) return nextPattern();
-
-      all_keys = all_keys.concat(keys);
-
-      nextPattern();
-    });
-
-  }, function(){
-
-    if (!all_keys.length) return callback();
-
-    redis.del(all_keys, function(err){
-
-      if (err) throw err;
-
-      callback();
-    });
-  });
-}
+}, process.exit, options);
 
 function entries (blogID, callback) {
 
@@ -94,20 +24,26 @@ function entries (blogID, callback) {
 
     if (err) throw err;
 
+    keys = keys.filter(function(key){
+      return key.indexOf('/') === -1;
+    });
+
+    if (!keys.length) return callback();
+
     redis.mget(keys, function(err, entries){
 
       entries = entries.map(JSON.parse);
 
       // we should check the entry is valid
-      // we should check there are not two entries with the same path
+      // we should check there are not two entries with the same path?
 
       forEach(entries, function(entry, next){
 
-        redis.set(urlkey(blogID, entry.url), entry.path.toLowerCase(), function(err){
+        entry.id = normalize(entry.path);
+
+        redis.set(urlkey(blogID, entry.url), entry.id, function(err){
 
           if (err) throw err;
-
-          entry.id = entry.path.toLowerCase();
 
           Entry.set(blogID, entry.id, entry, next);
         });
