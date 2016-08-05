8104d5539ef652b40a7fe1a854a761ad0a402a4e
David Merfield <dmerfield@gmail.com>
2016-08-04 18:42:03 -0700

    truncate round prices

--- app/helper/prettyPrice.js
+++ app/helper/prettyPrice.js
@@ -1,3 +1,17 @@
-module.exports = function prettyPrice (cents) {
-  return '$' + (cents/100).toFixed(2)
+function prettyPrice (cents) {
+  var price = '$' + (cents/100).toFixed(2);
+
+  if (price.slice(-2) === '00') price = price.slice(0, -3);
+
+  return price;
 }
+
+var is = require('./is')(prettyPrice);
+
+is(2134, '$21.34');
+is(2000, '$20');
+is(200, '$2');
+is(210, '$2.10');
+is(03, '$0.03');
+
+module.exports = prettyPrice;
