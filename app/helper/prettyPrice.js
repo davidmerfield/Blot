960f3ed3498e9be31dd7eafd627be58897917d69
David Merfield <dmerfield@gmail.com>
2016-08-04 18:57:48 -0700

    tweaked copy on create blog page for existing users
    thanks massimo for your feedback about this page.

--- app/helper/prettyPrice.js
+++ app/helper/prettyPrice.js
@@ -1,4 +1,5 @@
 function prettyPrice (cents) {
+
   var price = '$' + (cents/100).toFixed(2);
 
   if (price.slice(-2) === '00') price = price.slice(0, -3);
@@ -13,5 +14,6 @@ is(2000, '$20');
 is(200, '$2');
 is(210, '$2.10');
 is(03, '$0.03');
+is(30, '$0.30');
 
 module.exports = prettyPrice;
