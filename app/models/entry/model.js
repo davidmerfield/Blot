3f1c7f8dd88dc5e595ebf0a0fe3a25ac0d457aea
David Merfield <dmerfield@gmail.com>
2016-08-05 17:20:12 -0700

    comments and ws

--- app/models/entry/model.js
+++ app/models/entry/model.js
@@ -22,60 +22,11 @@ module.exports =  {
       draft: 'boolean',
       scheduled: 'boolean',
       thumbnail: 'object',
-
       dateStamp: 'number', // UTC timestamp for resolved date
       created: 'number', // UTC timestamp for when the entry was added to Blot
       updated: 'number', // UTC timestamp for file mtime
-
       render: 'boolean',
       metadata: 'object',
       retrieve: 'object',
       partials: 'array'
 };
-
-// module.exports =  {
-
-//       // body: 'string', // html excluding the HTML tag for its title
-//       // teaserBody: 'string', // teaser excluding titleTag
-//       // html: 'string',
-
-//       id: 'number',
-//       url: 'string',
-//       permalink: 'string',
-
-//       // TEXT
-
-//       title: 'string',
-//       summary: 'string', // plain text summary of article
-//       slug: 'string',
-
-//       // HTML
-
-//       titleTag: 'string', // The HTML tag containing the title text
-//       teaser: 'string', // stuff before <!-- more -->, used before read more linkss..
-//       remainder: 'string', // stuff after the rest
-
-//       tags: 'array',
-//       menu: 'boolean',
-//       page: 'boolean',
-//       deleted: 'boolean',
-//       draft: 'boolean',
-//       scheduled: 'boolean',
-
-//       dateStamp: 'number', // UTC timestamp for resolved date
-//       created: 'number', // UTC timestamp for when the entry was added to Blot
-//       updated: 'number', // UTC timestamp for when entry was modified on Blot
-
-//       render: 'boolean',
-//       metadata: 'object',
-//       retrieve: 'object',
-//       partials: 'array',
-
-
-//       // FILE INFO
-
-//       name: 'string',
-//       path: 'string',
-//       size: 'number',
-//       mtime: 'number' // UTC timestamp for entry's file's mtime
-// };
