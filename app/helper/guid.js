2d25a6cd3f5e3eef239c33b6a029fca45124ebd9
David Merfield <dmerfield@gmail.com>
2016-08-05 17:04:37 -0700

    use time-based guid generator

--- app/helper/guid.js
+++ app/helper/guid.js
@@ -1,24 +1,25 @@
 /**
- * @author Jeff Ward (jcward.com).
+ * @author Briguy37
  * @license MIT license
- * @link http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/21963136#21963136
+ * @link http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
  **/
 
-var lut = [];
-
-for (var i=0; i<256; i++) {
-  lut[i] = (i<16?'0':'')+(i).toString(16);
-}
-
 function generate () {
-  var d0 = Math.random()*0xffffffff|0;
-  var d1 = Math.random()*0xffffffff|0;
-  var d2 = Math.random()*0xffffffff|0;
-  var d3 = Math.random()*0xffffffff|0;
-  return lut[d0&0xff]+lut[d0>>8&0xff]+lut[d0>>16&0xff]+lut[d0>>24&0xff]+'-'+
-    lut[d1&0xff]+lut[d1>>8&0xff]+'-'+lut[d1>>16&0x0f|0x40]+lut[d1>>24&0xff]+'-'+
-    lut[d2&0x3f|0x80]+lut[d2>>8&0xff]+'-'+lut[d2>>16&0xff]+lut[d2>>24&0xff]+
-    lut[d3&0xff]+lut[d3>>8&0xff]+lut[d3>>16&0xff]+lut[d3>>24&0xff];
+
+  var d = Date.now();
+
+   //use high-precision timer if available
+  if (process.hrtime && typeof process.hrtime === "function") {
+    d += process.hrtime()[0];
+  }
+
+  var guid = 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = (d + Math.random()*16)%16 | 0;
+    d = Math.floor(d/16);
+    return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+  });
+
+  return guid;
 }
 
 module.exports = generate;
