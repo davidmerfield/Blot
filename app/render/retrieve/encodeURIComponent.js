92d379f3a63a4b02487bfd5ceeb3345a0e885817
David Merfield <dmerfield@gmail.com>
2016-08-09 12:18:38 -0700

    added encodeURIComponent lambda

---
+++ app/render/retrieve/encodeURIComponent.js
@@ -0,0 +1,24 @@
+// This is useful for forming urls from entry properties
+// e.g href="https://example.com?text={{#encodeURIComponent}}{{title}}{{/encodeURIComponent}}""
+// and should be used by the social sharing buttons plugin when it exists
+
+module.exports = function (req, callback) {
+
+  return callback(null, function () {
+
+    return function(text, render){
+
+      var encoded_text = '';
+
+      text = render(text);
+
+      try {
+        encoded_text = encodeURIComponent(text);
+      } catch (e) {
+        return text;
+      }
+
+      return encoded_text;
+    };
+  });
+};
