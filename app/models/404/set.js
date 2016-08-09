a0992768abe42209f448e8b68eab3ed1178570dd
David Merfield <dmerfield@gmail.com>
2016-08-09 11:41:25 -0700

    trim the list of 404s

--- app/models/404/set.js
+++ app/models/404/set.js
@@ -1,7 +1,9 @@
 var client = require('../client');
 var helper = require('../../helper');
 var ensure = helper.ensure;
-var key = require('./key');
+var Key = require('./key');
+
+var MAX_404s = 500;
 
 module.exports = function (blogID, url, callback) {
 
@@ -11,11 +13,25 @@ module.exports = function (blogID, url, callback) {
     .and(url, 'string')
     .and(callback, 'function');
 
-  var everythingKey = key.everything(blogID);
-  var score = Date.now();
+  var key = Key.everything(blogID);
+  var now = Date.now();
+  var thirtyDaysAgo = now - 1000*60*60*24*30;
+  var multi = client.multi();
 
-  ensure(everythingKey, 'string')
-    .and(score, 'number');
+  // Add the new 404
+  multi.ZADD(key, now, url);
 
-  client.ZADD(everythingKey, score, url, callback);
+  // Remove any entries which are older than 30 days
+  // -inf is to avoid looking up the lowest score in the sorted set.
+  multi.ZREMRANGEBYSCORE(key, '-inf', thirtyDaysAgo);
+
+  // Trim the list of 404s
+  multi.ZREMRANGEBYRANK(key, 0, -MAX_404s);
+
+  multi.exec(function(err){
+
+    if (err) throw err;
+
+    callback();
+  });
 };
