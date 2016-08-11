94c34cd4990881c4cdeb41a8e45b342dc85ee506
David Merfield <dmerfield@gmail.com>
2016-08-11 12:40:38 -0700

    handle 503 file lock error for writing preview files

--- app/modules/preview/write.js
+++ app/modules/preview/write.js
@@ -17,9 +17,21 @@ module.exports = function (blogID, client, path) {
       return;
     }
 
-    client.writeFile(remotePath, contents, function (err) {
+    client.writeFile(remotePath, contents, function then (err) {
 
-      if (err && err.status && err.status !== 404) console.log(err);
+      // Happens when the user removes the parent directory for
+      // the draft. Don't bother writing again.
+      if (err && err.status === 404)
+        return;
+
+      // Happens when the user makes a series of overlapping updates
+      // to draft file, so Blot issues simultaneous requests to write
+      // the preview file. Not a big deal, since the preview file's
+      // contents don't change (just an iframe to Blot).
+      if (err && err.status === 503)
+        return;
+
+      if (err) console.log(err);
     });
   });
 };
