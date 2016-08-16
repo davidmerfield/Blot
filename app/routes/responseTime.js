8f26d29f891959c445c47aaa3450aca14a90ec3e
David Merfield <dmerfield@gmail.com>
2016-08-15 18:37:20 -0700

    added response timing middleware for dev use

---
+++ app/routes/responseTime.js
@@ -0,0 +1,11 @@
+module.exports = function(req, res, next) {
+
+  var start = Date.now();
+
+  res.on('finish', function() {
+      var duration = Date.now() - start;
+      console.log(duration + 'ms', req.protocol + '://' + req.get('host') + req.originalUrl);
+  });
+
+  next();
+};
