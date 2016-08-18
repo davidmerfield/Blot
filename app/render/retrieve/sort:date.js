77f089ebc2d9d4e7132e1ea3e7465c338202cf96
David Merfield <dmerfield@gmail.com>
2016-08-18 16:10:15 -0700

    added sort:date helper
    useful for search results page. thanks Frank!

---
+++ app/render/retrieve/sort:date.js
@@ -0,0 +1,14 @@
+module.exports = function (req, callback) {
+
+  return callback(null, function () {
+
+    this.entries = this.entries.sort(function(a, b){
+      return (a.dateStamp > b.dateStamp) ? -1 : (a.dateStamp < b.dateStamp) ? 1 : 0;
+    });
+
+    return function(text, render){
+
+      return render(text);
+    };
+  });
+};
