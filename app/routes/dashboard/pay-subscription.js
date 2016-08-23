9c5bfea4596f238ad22232bde80277474a113529
David Merfield <dmerfield@gmail.com>
2016-08-22 19:16:53 -0700

    hide nav for overdue subscription page

--- app/routes/dashboard/pay-subscription.js
+++ app/routes/dashboard/pay-subscription.js
@@ -56,6 +56,9 @@ module.exports = function(server){
         invoiceID: invoiceID
       });
 
+      // Remove navigation (it's blank otherwise)
+      response.setPartials({nav: ''});
+
       response.render('dashboard/_wrapper');
     });
   });
