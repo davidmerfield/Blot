ab93a067741457dc584c7460d812a3c8440673c8
David Merfield <dmerfield@gmail.com>
2016-08-30 20:31:55 -0700

    improved set subscription script

--- scripts/user/setSubscription.js
+++ scripts/user/setSubscription.js
@@ -1,46 +1,42 @@
 var from = process.argv[2];
 var newSubscriptionID = process.argv[3];
 var newCustomerID = process.argv[4];
+var config = require('config');
+var stripe = require('stripe')(config.stripe.secret);
 
-var get = require('./blog/get');
+var get = require('../blog/get');
 
-var User = require('../app/models/user');
-var Subscription = require('../app/models/subscription');
+var User = require('../../app/models/user');
+var Subscription = require('../../app/models/subscription');
 
 get(from, function(user){
 
   if (!user) throw 'No user';
-
   if (!newSubscriptionID) throw 'No Subscription id';
   if (!newCustomerID) throw 'No Customer id';
 
-  var subscription = user.subscription;
+  console.log('Subscription is ', user.subscription);
 
-  console.log(subscription);
-
-  console.log('Customer ID', subscription.customer);
-
-  console.log('Subscription was ', subscription.id);
-
-  subscription.id = newSubscriptionID;
-  subscription.customer = newCustomerID;
-  subscription.status = 'active';
-
-  console.log('Subscription is ', subscription.id);
-
-  console.log(subscription);
-
-  throw 'HERE';
-
-  User.set(user.uid, {subscription: subscription}, function(err){
+  stripe.customers.retrieveSubscription(newCustomerID, newSubscriptionID, function(err, subscription) {
 
     if (err) throw err;
 
-    Subscription.bind(subscription.customer, user.uid, function(err){
+    if (!subscription) throw 'No subscription found from Stripe';
+
+    console.log(subscription);
+
+    throw 'HERE';
+
+    User.set(user.uid, {subscription: subscription}, function(err){
 
       if (err) throw err;
 
-      process.exit();
+      Subscription.bind(subscription.customer, user.uid, function(err){
+
+        if (err) throw err;
+
+        process.exit();
+      });
     });
   });
 });
