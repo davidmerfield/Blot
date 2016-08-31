87af9519a1d8c6e1da57dd81c520b3d5b82300fa
David Merfield <dmerfield@gmail.com>
2016-08-30 20:26:25 -0700

    updated script

--- scripts/user/setSubscription.js
+++ scripts/user/setSubscription.js
@@ -1,17 +1,18 @@
 var from = process.argv[2];
-var subscriptionID = process.argv[3];
+var newSubscriptionID = process.argv[3];
+var newCustomerID = process.argv[4];
 
 var get = require('./blog/get');
 
 var User = require('../app/models/user');
 var Subscription = require('../app/models/subscription');
 
-
 get(from, function(user){
 
   if (!user) throw 'No user';
 
-  if (!subscriptionID) throw 'No Subscription id';
+  if (!newSubscriptionID) throw 'No Subscription id';
+  if (!newCustomerID) throw 'No Customer id';
 
   var subscription = user.subscription;
 
@@ -21,7 +22,9 @@ get(from, function(user){
 
   console.log('Subscription was ', subscription.id);
 
-  subscription.id = subscriptionID;
+  subscription.id = newSubscriptionID;
+  subscription.customer = newCustomerID;
+  subscription.status = 'active';
 
   console.log('Subscription is ', subscription.id);
 
