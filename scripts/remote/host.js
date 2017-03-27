c79f0f8f0fba3ce541ec7a1f90a4609f98789e84
David Merfield <dmerfield@gmail.com>
2017-03-27 15:06:11 -0300

    updated remote script

--- scripts/remote/host.js
+++ scripts/remote/host.js
@@ -1,1 +1,5 @@
-module.exports = 'ec2-user@blot.im';
+module.exports = 'blot';
+
+// this works as a host because I have aliased
+// blot to ec2-user@blot.im ~/.ssh/config with
+// appropriate path to public key
