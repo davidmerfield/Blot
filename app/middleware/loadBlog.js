ced2ddddcadbb09db0718906e3d1901a78509047
David Merfield <dmerfield@gmail.com>
2017-04-08 00:04:19 -0300

    middleware to load account info

---
+++ app/middleware/loadBlog.js
@@ -0,0 +1,58 @@
+var forEach = require('helper').forEach.parallel;
+var Blog = require('blog');
+var User = require('user');
+
+module.exports = function (req, res, next) {
+
+  if (!req.session || !req.user || !req.user.blogs.length) return next();
+
+  var blogs = [];
+  var activeBlog = null;
+
+  forEach(req.user.blogs, function(blogID, nextBlog){
+
+    Blog.get({id: blogID}, function(err, blog){
+
+      if (req.session.blogID === blog.id) {
+        blog.isCurrent = true;
+        activeBlog = blog;
+      }
+
+      blogs.push(blog);
+      nextBlog();
+    });
+
+  }, function(){
+
+    // The blog active in the users session
+    // no longer exists, redirect them to one
+    if (!activeBlog && req.session.blogID) {
+
+      var candidates = blogs.slice();
+
+      candidates = candidates.filter(function(id){
+        return id !== req.session.blogID;
+      });
+
+      if (candidates.length > 0) {
+        activeBlog = candidates.pop();
+        req.session.blogID = activeBlog.id;
+        User.set(req.user.uid, {lastSession: activeBlog.id}, function(){});
+      } else {
+        req.session.blogID = null;
+        User.set(req.user.uid, {lastSession: ''}, function(){});
+        console.log('THERES NOTHING HERE');
+      }
+    }
+
+    req.blog = Blog.extend(activeBlog);
+    req.blogs = blogs;
+
+    res.addLocals({
+      blog: activeBlog,
+      blogs: blogs
+    });
+
+    return next();
+  });
+};
---
+++ app/middleware/loadUser.js
@@ -0,0 +1,36 @@
+var User = require('user');
+var Csurf = require('csurf');
+
+module.exports = function (req, res, next) {
+
+  if (!req.session || !req.session.uid) return next();
+
+  var uid = req.session.uid;
+
+  User.getBy({uid: uid}, function(err, user){
+
+    if (err) return next(err);
+
+    if (!user) {
+      req.user = null;
+      req.session.uid = null;
+      return next();
+    }
+
+    // Lets append the user and
+    // set the partials to 'logged in mode'
+    req.user = user;
+    res.addLocals({user: user});
+
+    Csurf()(req, res, function(){
+
+      // Load the CSRF protection since we're
+      // inside the app,
+      res.addLocals({
+        csrftoken: req.csrfToken()
+      });
+
+      return next();
+    });
+  });
+};
