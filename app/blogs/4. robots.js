af1c0d78503d930202064a9514db531374737caa
David Merfield <dmerfield@gmail.com>
2017-04-10 10:16:20 -0300

    fix broken link to robots.txt

--- app/blogs/4. robots.js
+++ app/blogs/4. robots.js
@@ -1,19 +1,16 @@
-var path = require('path'),
-    viewDir = path.resolve(__dirname + '/../') + '/views/';
-
 module.exports = function(server){
 
+  // Prevent robots from indexing
+  // preview subdomains to ward off
+  // accusations of farming content
+
+  // do the same in case the user
+  // has a custom domain as well.
   server.get('/robots.txt', function(request, response, next){
 
-    // Prevent robots from indexing
-    // preview subdomains to ward off
-    // accusations of farming content
-
-    // do the same in case the user
-    // has a custom domain as well.
     if (request.previewSubdomain || (request.blog.domain && request.originalHost !== request.blog.domain)) {
       response.header("Content-type", 'text/plain');
-      return response.sendFile(viewDir + 'robots_deny.txt');
+      return response.sendFile(__dirname + '/robots_deny.txt');
     }
 
     return next();
