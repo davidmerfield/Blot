3fc7a4fc5d2290cfc66002c9aba34331e88b9f01
David Merfield <dmerfield@gmail.com>
2017-04-11 19:26:29 -0300

    added remove blog to model

---
+++ app/models/blog/remove.js
@@ -0,0 +1,55 @@
+var get = require('./get');
+var helper = require('helper');
+var ensure = helper.ensure;
+var forEach = helper.forEach;
+var client = require('../client');
+
+var START_CURSOR = '0';
+var SCAN_SIZE = 1000;
+
+module.exports = function (blogID, callback) {
+
+  ensure(blogID, 'string')
+    .and(callback, 'function');
+
+  get({id: blogID}, function(err, blog){
+
+    if (err) return callback(err);
+
+    var patterns = [
+      'template:' + blogID + ':*',
+      'blog:' + blogID + ':*'
+    ];
+
+    var remove = [
+      'template:owned_by:' + blogID,
+      'handle:' + blog.handle,
+    ];
+
+    if (blog.domain) remove.push('domain:' + blog.domain);
+
+    forEach.parallel(patterns, function(pattern, next){
+
+      var args = [START_CURSOR, 'MATCH', pattern, 'COUNT', SCAN_SIZE];
+
+      client.scan(args, function then (err, res){
+
+        if (err) throw err;
+
+        // the cursor for the next pass
+        args[0] = res[0];
+
+        // Append the keys we matched in the last pass
+        remove = remove.concat(res[1]);
+
+        // There are more keys to check, so keep going
+        if (res[0] !== START_CURSOR) return client.scan(args, then);
+
+        next();
+      });
+    }, function(){
+
+      client.del(remove, callback);
+    });
+  });
+};
