f0bef433f7827c784b09cad86f2ecc74168cf6f5
David Merfield <dmerfield@gmail.com>
2017-04-11 17:19:15 -0300

    updated tests

--- tests/remove-s3-folder/build.js
+++ tests/remove-s3-folder/build.js
@@ -1,12 +1,12 @@
 var files = __dirname + '/files';
 var write = require('fs').writeFileSync;
 
-var i = 0;
-var MAX = 1200;
+var i = 1;
+var MAX = parseInt(process.argv[2]) || 10;
 
 console.log('Building', MAX, 'test files to upload from', files);
 
-while (i < MAX) {
+while (i <= MAX) {
   write(files + '/' + i + '.txt', i, 'utf-8');
   i++;
 }
--- tests/remove-s3-folder/index.js
+++ tests/remove-s3-folder/index.js
@@ -1,11 +1,9 @@
 var removeFolder = require('../../app/upload/removeFolder');
+var blogFolder = require('../../app/upload/blogFolder');
 var files = __dirname + '/files';
 var blogID = require('./blogID');
 
-console.log('uploading contents of', files);
-
-
-  console.log('removing s3 upload folder for', blogID);
+console.log('removing s3 upload folder for', blogID, blogFolder(blogID));
 
 removeFolder(blogID, function(err){
 
