d4dd2b0d022f7f15be48df1c0761883972174c63
David Merfield <dmerfield@gmail.com>
2017-04-14 09:35:38 -0300

    removed logging that shouldn't have made it to production

--- app/middleware/messenger.js
+++ app/middleware/messenger.js
@@ -17,19 +17,15 @@ module.exports = function (req, res, next) {
 
   if (!req.session.message) return next();
 
-  console.log('HERE', req.url, req.session.message);
-
   if (req.session.message.url === req.path) {
 
     res.addLocals(req.session.message);
 
     if (req.session.message.error) res.status(400);
 
-    console.log(res.locals);
-
   } else {
 
-    console.log('ignoring message', req.session.message.url, req.path);
+    // console.log('ignoring message', req.session.message.url, req.path);
 
   }
 
