12e5dfb9f26163056eecc878e42bc76ba788877a
David Merfield <dmerfield@gmail.com>
2017-08-12 15:35:15 -0400

    removed name info

--- app/render/retrieve/name.js
+++
@@ -1,9 +0,0 @@
-var User = require('../../models/user');
-
-module.exports = function (req, callback) {
-
-  User.getBy({uid: req.blog.owner}, function(err, user){
-
-    return callback(err, user.name);
-  });
-};
