28aee7d600a3a1f78e8b1bbb4d5ad81f3fb3a8be
David Merfield <dmerfield@gmail.com>
2017-08-31 23:17:52 -0400

    fix set bug

--- app/clients/dropbox/database/set.js
+++ app/clients/dropbox/database/set.js
@@ -19,12 +19,13 @@ module.exports = function (blog_id, account, callback) {
       multi.sadd(key.blogs(account.id), blog_id);
     }
 
-    if (existing_account && existing_account.id && existing_account.id !== account.id) {
+    // The user's account has changed,
+    // remove the old one and add the new one
+    if (existing_account && account.id && existing_account.id && existing_account.id !== account.id) {
       multi.srem(key.blogs(existing_account.id), blog_id);
     }
 
     multi.hmset(key.account(blog_id), account);
-
     multi.exec(callback);
   });
 };
