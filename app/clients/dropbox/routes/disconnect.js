0d9b3780f0c81791aa0a8b6aabc931cdbd9899a8
David Merfield <dmerfield@gmail.com>
2017-09-01 21:11:11 -0400

    added route to disconnect dropbox

---
+++ app/clients/dropbox/routes/disconnect.js
@@ -0,0 +1,28 @@
+var Blog = require('blog');
+var database = require('database');
+var router = require('express').Router();
+var disconnect = router.route('/');
+
+disconnect.get(function(req, res) {
+
+  if (!req.account) return res.redirect(req.baseUrl);
+
+  res.dashboard('disconnect');
+});
+
+disconnect.post(function(req, res, next){
+
+  database.drop(req.blog.id, function(err){
+
+    if (err) return next(err);
+
+    Blog.set(req.blog.id, {client: ''}, function(err){
+
+      if (err) return next(err);
+
+      res.redirect('/clients');
+    });
+  });
+});
+
+module.exports = router;
