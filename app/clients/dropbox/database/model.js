213e72ad607cfc1919838734b84a1be63e4c2b00
David Merfield <dmerfield@gmail.com>
2017-09-02 22:48:16 -0400

    added error to account

--- app/clients/dropbox/database/model.js
+++ app/clients/dropbox/database/model.js
@@ -5,9 +5,11 @@ module.exports = {
 
   // either a unix datestamp for last successful request to dropbox api
   // or 0, in which case the last successful request was invalid
+  // HTTP error code from the dropbox api
   valid: 'number',
+  error: 'number',
 
   cursor: 'string',
-  folder: 'string', // path to Blot's folder inside user's Dropbox
+  folder_id: 'string', // path to Blot's folder inside user's Dropbox
   full: 'boolean' // whether Blot has full folder access
 };
