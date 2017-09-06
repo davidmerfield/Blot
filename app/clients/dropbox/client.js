643a1cb3c7df1cb39f36654a4265594c739078ad
David Merfield <dmerfield@gmail.com>
2017-09-06 17:36:08 -0400

    added read and write abstract properties

---
+++ app/clients/dropbox/client.js
@@ -0,0 +1,63 @@
+var join = require('path').join;
+var Dropbox = require('dropbox');
+var database = require('database');
+var helper = require('helper');
+var ensure = helper.ensure;
+
+module.exports = {
+
+  write: function (blogID, path, contents, callback) {
+
+    ensure(blogID, 'string')
+      .and(path, 'string')
+      .and(contents, 'string')
+      .and(callback, 'function');
+
+    database.get(blogID, function(err, account){
+
+      var client = new Dropbox({accessToken: account.access_token});
+
+      client.filesUpload({
+        contents: contents,
+        autorename: false,
+        mode: {'.tag': 'overwrite'},
+        path: join(account.folder || '/', path)
+      })
+        .then(function(res){
+          if (!res) return callback(new Error('No response from Dropbox'));
+          callback();
+        })
+        .catch(function(err){
+
+          callback(err);
+        });
+    });
+  },
+
+  remove: function (blogID, path, callback) {
+
+    ensure(blogID, 'string')
+      .and(path, 'string')
+      .and(callback, 'function');
+
+    database.get(blogID, function(err, account){
+
+      var client = new Dropbox({accessToken: account.access_token});
+
+      client.filesDelete({
+        path: join(account.folder || '/', path)
+      })
+        .then(function(res){
+          if (!res) return callback(new Error('No response from Dropbox'));
+          callback(null);
+        })
+        .catch(function(err){
+
+          // The file does not exist
+          if (err.status === 409) return callback();
+
+          callback(err);
+        });
+    });
+  }
+};
