79fc95f88f045146bfde3e5dca6865038cbdc172
David Merfield <dmerfield@gmail.com>
2017-09-06 17:42:11 -0400

    updated set account

--- app/clients/dropbox/routes/authenticate/set_account.js
+++ app/clients/dropbox/routes/authenticate/set_account.js
@@ -1,6 +1,6 @@
 var database = require('database');
+var reset = require('sync').reset;
 var sync = require('../sync');
-var fs = require('fs-extra');
 
 module.exports = function (req, res, next) {
 
@@ -11,15 +11,20 @@ module.exports = function (req, res, next) {
     if (err) return next(err);
 
     res.locals.account = account;
-
     res.message({new_folder: true, url: '/clients/dropbox', migration: req.migration});
     res.redirect('/clients/dropbox');
 
-    // we also need to delete all the posts
-    fs.emptyDir(localPath(req.blog.id), function(err){
+    // In future we can probably do better than a hard reset for
+    // every case. We might not be changing the folder if we are
+    // upgrading from partial to full folder permissions for instance.
+    reset(req.blog.id, function(err){
 
-      sync(req.blog.id, function(){});
+      if (err) console.log('Blog:', req.blog.id, 'Reset error', err);
 
+      sync(req.blog.id, function(err){
+
+        if (err) console.log('Blog:', req.blog.id, 'Sync error', err);
+      });
     });
   });
 };
