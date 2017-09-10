2e571615961e7c00bc0d237418ed60b8130bf69d
David Merfield <dmerfield@gmail.com>
2017-09-10 11:08:20 -0400

    final fix

--- app/clients/dropbox/routes/authenticate/index.js
+++ app/clients/dropbox/routes/authenticate/index.js
@@ -33,6 +33,7 @@ authenticate.get('/redirect', function (req, res) {
 
   authentication_url = client.getAuthenticationUrl(encodeURIComponent(callback), null, 'code');
   authentication_url = authentication_url.replace('response_type=token', 'response_type=code');
+  authentication_url = authentication_url.split('\n').join('');
 
   console.log(authentication_url);
   res.redirect(authentication_url);
