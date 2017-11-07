d4cbdc8b775c0f3108f41f29647d1b9c6cd3d963
David Merfield <dmerfield@gmail.com>
2017-11-07 17:36:26 -0500

    moved nginx conf into subfolder

--- scripts/production/nginx_reboot.sh
+++ scripts/production/nginx_reboot.sh
@@ -2,5 +2,5 @@
 set -e
 set -x
 
-sudo /usr/local/openresty/bin/openresty -t -c /var/www/blot/config/nginx.conf
+sudo /usr/local/openresty/bin/openresty -t -c /var/www/blot/config/nginx/server.conf
 sudo /usr/local/openresty/bin/openresty -s reload
