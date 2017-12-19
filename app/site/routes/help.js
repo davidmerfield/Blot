c4ba7900b31755df60012bfb8667ea7e753d2614
David Merfield <dmerfield@gmail.com>
2017-12-18 23:24:05 -0500

    Updated documentation

--- app/site/routes/help.js
+++ app/site/routes/help.js
@@ -1,53 +1,44 @@
 module.exports = function(server){
 
+  var help = require('../views/help/help.json');
+
   server.get('/help', function(req, res){
-
-    res.setLocals({
-      title: 'Help',
-      selected: {help: 'selected'},
-      tab: {help: 'selected'},
-    });
-
-    res.addPartials({yield: 'help/overview'});
-
-    if (req.blog)
-      return res.render('help/wrapper');
-
-    res.render('help/wrapper');
+    console.log('redirecting',req.url,' to /help/' + help.sections[0].slug);
+    return res.redirect('/help/' + help.sections[0].slug);
   });
-
-  function capitalize (str) {
-    return str[0].toUpperCase() + str.slice(1);
-  }
-
-  function deslug (slug) {
-    return capitalize(slug.split('-').join(' '));
-  }
 
   server.get('/help/:section', function(req, res){
 
+    var sidebar = help.sidebar.slice();
+    var title = 'Help';
+    var section = help[req.params.section];
+
+    sidebar = sidebar.map(function(section){
+
+      if (section.slug === req.params.section) {
+        section.selected = 'selected';
+      } else {
+        section.selected = ''
+      }
+
+      return section;
+    });
+
+    console.log('HERe!');
+
     res.addPartials({
-      yield: 'help/sections/' + req.params.section,
       sidebar: 'help/sidebar'
     });
 
     res.setLocals({
-      subsection: true,
-      title: deslug(req.params.section),
+      section: section,
+      sidebar: sidebar,
+      title: section.title,
       selected: {help: 'selected'},
       tab: {help: 'selected'}
     });
 
-    if (req.blog)
-      return res.render('help/wrapper');
-
-    res.render('help/wrapper');
-  });
-
-  server.use('/help', function(err, req, res, next){
-
-    console.log(req.url, err.message);
-
-    return res.redirect('/help');
+    console.log('rendering', req.url);
+    return res.render('help/wrapper');
   });
 };
