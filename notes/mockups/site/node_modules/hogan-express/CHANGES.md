0daefb696a8e85ef9c04c984380fad3de750fc2d
David Merfield <dmerfield@gmail.com>
2018-01-30 15:43:53 -0500

    Added new marketing site prototyping server

---
+++ notes/mockups/site/node_modules/hogan-express/CHANGES.md
@@ -0,0 +1,13 @@
+### 0.5.1
+  - partials didn't work in yield tags [fixed by @summerisgone]
+### 0.5.1
+
+  - fix bugs with lambdas
+
+### 0.5.0
+
+  - add better lambda/filter support
+
+### 0.4.0
+
+  - add cutstom yield tags support
---
+++ notes/mockups/site/routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var site = express.Router();
+
+var pages = [
+  ['about', 'About'],
+  ['contact', 'Contact'],
+  ['sign-up', 'Sign up'],
+  ['log-in', 'Log in'],
+  ['help', 'Help'],
+  ['privacy', 'Privacy'],
+  ['terms', 'Terms'],
+  ['', 'Home']
+];
+
+pages.forEach(function(page){
+
+  var slug = page[0];
+  var title = page[1];
+
+  site.get('/' + slug, function(req, res){
+    res.locals.menu = {};
+    res.locals.menu[slug || 'index'] = 'selected';
+    res.locals.title = res.locals.version + ': Blot ' + title;
+    res.renders(slug || 'index');
+  });
+
+});
+
+module.exports = site;
