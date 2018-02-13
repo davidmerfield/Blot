a32d4fdedfd928f7633adefab3a5471890791a08
David Merfield <dmerfield@gmail.com>
2018-02-13 18:34:42 -0500

    Updated help route

--- app/site/routes/help.js
+++ app/site/routes/help.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var help = express.Router();
+var finder = require('finder');
+
+help.use(finder.middleware);
 
 help.use(function(req, res, next){
   res.locals.menu = {'help': 'selected'};
@@ -8,32 +11,23 @@ help.use(function(req, res, next){
 
 help.get('/account', function(req, res){
   res.locals.menu.account = 'selected';
-  res.render('help/account');
+  res.special_render('help/account');
 });
 
 help.get(['/guides', '/guides/:guide'], function(req, res){
-
-  // We wouldn't need to do this bullshit
-  // if the template rendering library had
-  // some sensible way to add an additional
-  // partial for a given request.
-
-  // Shallow copy the global app partials, then
-  // append the specific content to populate
-  // the general layout for the guides pages
   res.locals.partials.yield = 'help/guides/' + (req.params.guide || 'index');
   res.locals.menu.guides = 'selected';
-  res.render('help/guides/wrapper');
+  res.special_render('help/guides/wrapper');
 });
 
 help.get('/templates', function(req, res){
   res.locals.menu.templates = 'selected';
-  res.render('help/templates');
+  res.special_render('help/templates');
 });
 
 help.get('/', function(req, res){
   res.locals.menu.started = 'selected';
-  res.render('help/getting-started');
+  res.special_render('help/getting-started');
 });
 
 module.exports = help;
