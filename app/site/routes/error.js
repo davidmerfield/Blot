fcb4e2c488742f5d6440541e0a0822169fdb0ab9
David Merfield <dmerfield@gmail.com>
2018-03-03 15:45:07 -0500

    Fixed error route

--- app/site/routes/error.js
+++ app/site/routes/error.js
@@ -5,7 +5,7 @@ function production (err, req, res, next) {
   // This reponse was partially finished
   // end it now and get over it...
   if (res.headersSent) return res.end();
-  
+      
   var status = 500;
   var title = err.message || 'Error';
 
@@ -18,17 +18,18 @@ function production (err, req, res, next) {
     status = 404;
   }
 
-  res.locals.title = title;
-  res.status(500);
-  res.render('error');
+  // For some reason i can't render a template here
+  // I should fix this in future.
+  res.status(status);
+  res.send('Error: ' + status);
 };
 
 function dev (err, req, res, next) {
   next(err);
 }
 
-if (config.environment === 'development') {
-  module.exports = dev;
-} else {
+// if (config.environment === 'development') {
+//   module.exports = dev;
+// } else {
   module.exports = production;
-}
+// }
