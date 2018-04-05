4a4021c04d3b495867d238c64342889533aa32af
David Merfield <dmerfield@gmail.com>
2018-04-05 19:21:53 -0400

    Added penultimate and index info to lists

--- app/render/load/list.js
+++ app/render/load/list.js
@@ -1,5 +1,21 @@
 module.exports = function (list) {
-  list[0].first = true;
-  list[list.length -1].last = true;
+    
+  if (list.length) {
+    list[list.length-1].last = true;
+    list[0].first = true;
+  }
+
+  if (list.length > 2) {
+    list[list.length-2].penultimate = true;
+  }
+
+  // Should I do a zero-based index or not?
+  // Decide before documenting this. Beware
+  // if you use this in your theme before then.
+  list = list.map(function(el, i){
+    el.index = i + 1;
+    return el;
+  });
+
   return list;
-}
+};
