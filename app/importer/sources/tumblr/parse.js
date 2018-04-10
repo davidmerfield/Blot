2ce16f99c7fd9940b58d75a7563c481099374b7e
David Merfield <dmerfield@gmail.com>
2018-04-10 19:32:23 -0400

    Updated tumblr importer

--- app/importer/sources/tumblr/parse.js
+++ app/importer/sources/tumblr/parse.js
@@ -1,38 +1,121 @@
 var fs = require('fs-extra');
-var string = require('string');
-var API_KEY = fs.readFileSync(__dirname + '/secrets/api.key', 'utf-8');
-var URL_TEMPLATE = "http://api.tumblr.com/v2/blog/{{url}}/{{resource}}?api_key={{API_KEY}}";
-var request = require('request');
 var helper = require('../../helper');
 var for_each = helper.for_each;
-var parse = require('url').parse;
+var download_images = helper.download_images;
+var to_markdown = helper.to_markdown;
+var determine_path = helper.determine_path;
+var insert_metadata = helper.insert_metadata;
+var join = require('path').join;
 
-var 
+function photo (post) {
+  var content = '';
 
-if (not.function(callback)) throw not.function(callback);
 
-if (not.object(blog)) return callback(not.object(blog));
+  post.photos.forEach(function(photo){
+
+    content += '<img src="' + photo.original_size.url + '" alt="'+ photo.caption + '">\n\n';
+  });
+
+
+  return content;
+}
+
+function text (post) {
+  var content = '<img src="">';
+  return post.body;
+}
 
 function main (blog, output_directory, callback) {
 
-  var blog = {};
+  for_each(blog.posts, function(post, next){
 
-  source_url = tidy_source_url(source_url);
+    var created, updated, path_without_extension;
+    var dateStamp, tags, draft, page, path, metadata;
+    var content, title, html, url;
 
-  get_info(source_url, function(err, info){
+    if (post.type === 'photo') {
+      content = photo(post);
+    } else if (post.type === 'text') {
+      content = text(post);
+    } else {
+      console.log(post);
+      throw '';
+    }
 
-    if (err) return callback(err);
+    title = post.title || post.source_title || post.photos[0].caption || post.id.toString();
 
-    get_posts(source_url, info.total_posts, function(err, posts){
+    if (!title) {
+      console.log(post.photos);
+      throw '';
+    }
 
-      if (err) return callback(err);
+    html = post.html;
+    url = post.url;
+    
+    created = updated = dateStamp = post.timestamp * 1000;
+    draft = page = false;
+    metadata = {};
+    tags = post.tags;
+    path_without_extension = join(output_directory, determine_path(title, page, draft, dateStamp));
 
-      blog.title = info.title;
-      blog.posts = posts;
-      blog.host = parse(info.url).host;
+    download_images(content, path_without_extension, function(err, content, has_images){
 
-      return callback(null, blog);
+      if (err) throw err;
+
+      if (has_images) {
+        path = path_without_extension + '/post.txt';
+      } else {
+        path = path_without_extension + '.txt';
+      }
+
+      try {
+        content = to_markdown(content);
+      } catch (e) {
+        throw e;
+      }
+
+      // Add the new post to the list of posts!
+      post = {
+
+        draft: false,
+        page: false,
+
+        // We don't know any of these properties
+        // as far as I can tell.
+        name: '',
+        permalink: '',
+        summary: '',
+        path: path,
+
+        title: title,
+        
+        dateStamp: dateStamp,
+        created: created,
+        updated: updated,
+        tags: tags,
+        metadata: metadata,
+        
+        // Clean up the contents of the <content>
+        // tag. Evernote has quite a lot of cruft.
+        // Then convert into Markdown!
+        content: content
+      };
+
+      post = insert_metadata(post);
+
+      // console.log(content);
+
+      console.log('...', post.path);
+      fs.outputFile(post.path, post.content, function(err){
+        
+        if (err) return callback(err);
+
+        next();
+      });
     });
+  }, function(){
+
+    callback();
   });
 }
 
@@ -45,6 +128,28 @@ if (require.main === module) {
   if (!output_directory) throw new Error('Please pass output directory to write blog to as second argument');
 
   var blog = fs.readJsonSync(source_file);
+
+  blog.posts = blog.posts.slice(0, 20);
+
+  blog.posts = blog.posts.map(function(post){
+    delete post.reblog;
+    delete post.can_reply;
+    delete post.date;
+    delete post.blog_name;
+    delete post.reblog_key;
+    delete post.short_url;
+    delete post.can_reblog;
+    delete post.is_blocks_post_format;
+    delete post.recommended_source;
+    delete post.recommended_color;
+    delete post.note_count;
+    delete post.can_send_in_message;
+    delete post.can_like;
+    delete post.display_avatar;
+    delete post.trail;
+    return post;
+  });
+
 
   main(blog, output_directory, function(err){
 
