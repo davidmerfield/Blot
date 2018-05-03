6741c10ededc602fdfeb0e3c4251d772579d569f
David Merfield <dmerfield@gmail.com>
2018-05-03 17:14:50 -0400

    Refactored cache emptying script

--- scripts/cache/empty.js
+++ scripts/cache/empty.js
@@ -1,26 +1,43 @@
 var fs = require('fs-extra');
 var config = require('config');
+var helper = require('../../app/helper');
+var forEach = helper.forEach;
 
-flush();
-
-function flush () {
-
-  fs.emptyDir(config.cache_directory, function(err){
-    
-    if (err) throw err;
-
+if (require.main === module) {
+  
+  flush(function(){
     console.log('... CACHE CLEARED');
+  });
+
+  var readline = require('readline');
+  readline.emitKeypressEvents(process.stdin);
+  process.stdin.setRawMode(true);
+  process.stdin.on('keypress', (str, key) => {
+    if (key.ctrl && key.name === 'c') {
+      process.exit();
+    } else {
+      flush(function(){
+        console.log('... CACHE CLEARED');
+      });
+    }
   });
 
 }
 
-var readline = require('readline');
-readline.emitKeypressEvents(process.stdin);
-process.stdin.setRawMode(true);
-process.stdin.on('keypress', (str, key) => {
-  if (key.ctrl && key.name === 'c') {
-    process.exit();
-  } else {
-    flush();
-  }
-});
+function flush (cb) {
+
+  fs.readdir(config.cache_directory, function(err, items){
+    
+    if (err) throw err;
+
+    forEach.parallel(items, function(item, next){
+
+      // Ignore dotfiles and .tmp directory
+      if (item[0] === '.') return next();
+
+      fs.remove(item, next);
+    }, cb);
+  });
+}
+
+module.exports = flush;
