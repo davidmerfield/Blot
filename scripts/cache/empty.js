26a45e5c4ff3ca6ac2f032da7599b92491727678
David Merfield <dmerfield@gmail.com>
2018-05-03 18:33:23 -0400

    Fixed bug in cache buster

--- scripts/cache/empty.js
+++ scripts/cache/empty.js
@@ -2,6 +2,8 @@ var fs = require('fs-extra');
 var config = require('config');
 var helper = require('../../app/helper');
 var forEach = helper.forEach;
+var each_blog = require('../each/blog');
+var Blog = require('blog');
 
 if (require.main === module) {
   
@@ -26,17 +28,24 @@ if (require.main === module) {
 
 function flush (cb) {
 
-  fs.readdir(config.cache_directory, function(err, items){
+  each_blog(function(user, blog, next){
+
+    Blog.set(blog.id, {cacheID: Date.now()}, next);
+
+  }, function(){
     
-    if (err) throw err;
+    fs.readdir(config.cache_directory, function(err, items){
+      
+      if (err) throw err;
 
-    forEach.parallel(items, function(item, next){
+      forEach.parallel(items, function(item, next){
 
-      // Ignore dotfiles and .tmp directory
-      if (item[0] === '.') return next();
+        // Ignore dotfiles and .tmp directory
+        if (item[0] === '.') return next();
 
-      fs.remove(item, next);
-    }, cb);
+        fs.remove(item, next);
+      }, cb);
+    });
   });
 }
 
