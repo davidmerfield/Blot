858085ca5de3361fa641b3c07ec781cd597c2983
David Merfield <dmerfield@gmail.com>
2018-05-06 17:48:37 -0400

    Added are.na importer

--- app/importer/sources/arena/index.js
+++ app/importer/sources/arena/index.js
@@ -1,5 +1,40 @@
-https://www.are.na/james-hicks/vaporwave
+// https://www.are.na/alex-singh/assorted-design-references
+// https://www.are.na/james-hicks/vaporwave
+// https://www.are.na/steve-k/neon-plants-and-lava-lamps
+// https://www.are.na/michael-duong/urban-outfitters-tarkovsky
 
-https://www.are.na/steve-k/neon-plants-and-lava-lamps
+if (require.main === module) {
 
-https://www.are.na/michael-duong/urban-outfitters-tarkovsky
+  var url = process.argv[2];
+  var output_directory = process.argv[3];
+  
+  if (!url) throw 'No url';
+  if (!output_directory) throw 'No output_directory';
+  
+  var slug = parse_slug(url);
+
+  main(slug, output_directory, function(err){
+
+    if (err) throw err;
+
+    console.log('Done!');
+    process.exit();
+  });
+}
+
+function main (slug, output_directory, callback) {
+
+  fetch(slug, function(err, posts){
+
+    fs.emptyDir(output_directory, function(err){
+  
+      parse(posts, callback);
+    });
+  });
+}
+
+function parse_slug (url) {
+  return require('url').parse(url).path.split('/').pop();
+}
+
+module.exports = main;
---
+++ app/importer/sources/arena/parse.js
@@ -0,0 +1,109 @@
+var fs = require('fs-extra');
+var join = require('path').join;
+var helper = require('../../helper');
+
+var determine_path = helper.determine_path;
+var download_images = helper.download_images;
+var insert_metadata = helper.insert_metadata;
+var to_markdown = helper.to_markdown;
+var for_each = helper.for_each;
+
+function main (output_directory, posts, callback) {
+
+  var done = 0;
+
+  for_each(posts, function(post, next){
+
+    // We'll only use image posts for now, would be nice to remove this in future...
+    if (!post.image) return next();
+
+    var created, updated, path_without_extension;
+    var dateStamp, draft, page, metadata, summary;
+    var content, title, html, url;
+    
+    content = (post.content_html || '') + (post.description_html || '');
+
+
+    if (post.image) content += '<img src="' + post.image.original.url + '">';
+    
+    title = post.title || post.generated_title || 'Untitled';
+    html = post.html;
+    url = post.url;
+    created = updated = dateStamp = (new Date(post.created_at)).valueOf();
+    draft = page = false;
+    path_without_extension = join(output_directory, determine_path(title, page, draft, dateStamp));
+
+    post = {
+
+      draft: false,
+      page: false,
+
+      // We don't know any of these properties
+      // as far as I can tell.
+      name: '',
+      permalink: '',
+      summary: summary,
+      path: path_without_extension,
+      html: content,
+      title: title,
+      
+      dateStamp: dateStamp,
+      created: created,
+      updated: updated,
+      tags: [],
+      metadata: metadata,
+      
+      // Clean up the contents of the <content>
+      // tag. Evernote has quite a lot of cruft.
+      // Then convert into Markdown!
+      content: content
+    };
+
+    download_images(post, function(err, post){
+
+      if (err) throw err;
+    
+      post.content = to_markdown(post.html);
+      post = insert_metadata(post);
+
+      console.log(++done + '/' + posts.length, '...', post.path);
+      fs.outputFile(post.path, post.content, function(err){
+        
+        if (err) return callback(err);
+
+        next();
+      });
+    });          
+  }, function(){
+    callback(null);
+  });
+}
+
+
+if (require.main === module) {
+
+  var input_file = process.argv[2];
+  var output_directory = process.argv[3];
+
+  if (!input_file) throw new Error('Please pass filename to read links to as first argument');
+  if (!output_directory) throw new Error('Please pass output directory as second argument');
+
+  var posts = fs.readJsonSync(input_file);
+
+  console.log('!!!!! generating trimmed version of site');
+  posts = posts.slice(0, 400);
+
+  fs.emptyDir(output_directory, function(err){
+
+    if (err) throw err;
+
+    main(output_directory, posts, function(err){
+
+      if (err) throw err;
+
+      console.log('Complete!');
+    });
+  });
+}
+
+module.exports = main;
