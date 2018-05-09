adbee07ed93ea7f1f061fef1b4500baae7e99bc5
David Merfield <dmerfield@gmail.com>
2018-05-09 19:34:06 -0400

    Rebuild dependent files on sync

--- app/sync/change/set/index.js
+++ app/sync/change/set/index.js
@@ -2,6 +2,7 @@ var helper = require('../../../helper');
 var ensure = helper.ensure;
 
 var normalize = helper.pathNormalizer;
+var rebuildDependents = require('./rebuildDependents');
 
 var Ignore = require('./ignore');
 var Metadata = require('../../../models/metadata');
@@ -44,49 +45,54 @@ module.exports = function (blog, path, callback){
 
     if (err) return callback(err);
 
-    isPreview(blog.id, path, function(err, is_preview){
+    rebuildDependents(blog.id, path, function(err){
 
       if (err) return callback(err);
 
-      // This is a preview file, don't create an entry
-      if (is_preview) return callback();
-
-      // The file belongs to a template and there
-      // fore should not become a blog post.
-      if (isTemplate(path)) return callback();
-
-      // The file is public. Its name begins
-      // with an underscore, or it's inside a folder
-      // whose name begins with an underscore. It should
-      // therefore not be a blog post.
-      if (isPublic(path)) 
-        return Ignore(blog.id, path, PUBLIC_FILE, callback);
-
-      // This file cannot become a blog post because it is not
-      // a type that Blot can process properly.
-      if (isWrongType(path))
-       return Ignore(blog.id, path, WRONG_TYPE, callback);
-
-      Entry.build(blog, path, function(err, entry){
+      isPreview(blog.id, path, function(err, is_preview){
 
         if (err) return callback(err);
 
-        // this checks the entry to see if a deleted entry
-        // matches it. If so, then use the deleted entry's url and created date.
-        catchRename(blog.id, entry, function(err, changes){
+        // This is a preview file, don't create an entry
+        if (is_preview) return callback();
 
-          if (err) throw err;
+        // The file belongs to a template and there
+        // fore should not become a blog post.
+        if (isTemplate(path)) return callback();
 
-          if (changes)
-            for (var key in changes)
-              entry[key] = changes[key];
+        // The file is public. Its name begins
+        // with an underscore, or it's inside a folder
+        // whose name begins with an underscore. It should
+        // therefore not be a blog post.
+        if (isPublic(path))
+          return Ignore(blog.id, path, PUBLIC_FILE, callback);
 
-          // This file is a draft, write a preview file
-          // to the users Dropbox and continue down
-          // We look up the remote path later in this module...
-          if (entry.draft) Preview.write(blog.id, path);
+        // This file cannot become a blog post because it is not
+        // a type that Blot can process properly.
+        if (isWrongType(path))
+         return Ignore(blog.id, path, WRONG_TYPE, callback);
 
-          Entry.set(blog.id, entry.path, entry, callback);
+        Entry.build(blog, path, function(err, entry){
+
+          if (err) return callback(err);
+
+          // this checks the entry to see if a deleted entry
+          // matches it. If so, then use the deleted entry's url and created date.
+          catchRename(blog.id, entry, function(err, changes){
+
+            if (err) return callback(err);
+
+            if (changes)
+              for (var key in changes)
+                entry[key] = changes[key];
+
+            // This file is a draft, write a preview file
+            // to the users Dropbox and continue down
+            // We look up the remote path later in this module...
+            if (entry.draft) Preview.write(blog.id, path);
+
+            Entry.set(blog.id, entry.path, entry, callback);
+          });
         });
       });
     });
---
+++ app/sync/change/set/rebuildDependents.js
@@ -0,0 +1,44 @@
+var helper = require('helper');
+var forEach = helper.forEach;
+var Entry = require('entry');
+var client = require('client');
+var Blog = require('blog');
+
+var dependentsKey = Entry.key.dependents;
+
+module.exports = function (blogID, path, callback) {
+
+  // for each dependency, rebuild?
+  // how do we prevent an infinite loop for mutually dependent files?
+
+  console.log('Blog:', blogID + ':', path, ':: Rebuilding the posts which depend on this post');
+
+  Blog.get({id: blogID}, function(err, blog){
+
+    client.SMEMBERS(dependentsKey(blogID, path), function(err, dependent_paths){
+
+      if (err) return callback(err);
+      
+      forEach(dependent_paths, function(dependent_path, next){
+
+        console.log('Blog:', blogID + ':', path, ':: Rebuilding dependent post', dependent_path);
+
+        Entry.build(blog, dependent_path, function(err, updated_dependent){
+
+          if (err) {
+            console.log(err);
+            return next();
+          }
+
+          Entry.set(blogID, dependent_path, updated_dependent, function(err){
+
+            if (err) return callback(err);
+
+            next();
+
+          }, false);
+        });
+      }, callback);
+    });
+  });
+};
