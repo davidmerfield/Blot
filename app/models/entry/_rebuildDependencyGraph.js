67da9b767edd756f16d22d8ed9a8cd1174970a1f
David Merfield <dmerfield@gmail.com>
2018-05-10 16:27:00 -0400

    Fixed typo

--- app/models/entry/_rebuildDependencyGraph.js
+++ app/models/entry/_rebuildDependencyGraph.js
@@ -2,32 +2,32 @@ var client = require('client');
 var dependentsKey = require('./key').dependents;
 var _ = require('lodash');
 
-module.exports = function (blogID, entry, previous, callback) {
+module.exports = function (blogID, entry, previous_dependencies, callback) {
 
   var removed_dependencies = []; 
-  var new_depdendencies = [];
+  var new_dependencies = [];
   var multi = client.multi();
 
   // Since this post is no longer available, none of its current or former
   // dependencies are still dependencies. Remove everything.
   if (entry.deleted) {
 
-    removed_dependencies = _.union(entry.dependencies, previous);
+    removed_dependencies = _.union(entry.dependencies, previous_dependencies);
 
   // Since this post exists, we need to work out which dependencies were
   // added since the last time this post was saved. We also need to work
   // out which dependencies were removed. 
   } else {
 
-    new_depdendencies = _.difference(entry.dependencies, previous);
-    removed_dependencies = _.difference(previous, entry.dependencies);
+    new_dependencies = _.difference(entry.dependencies, previous_dependencies);
+    removed_dependencies = _.difference(previous_dependencies, entry.dependencies); 
   }
 
   removed_dependencies.forEach(function(path){
     multi.SREM(dependentsKey(blogID, path), entry.path);
   });
 
-  new_depdendencies.forEach(function(path){
+  new_dependencies.forEach(function(path){
     multi.SADD(dependentsKey(blogID, path), entry.path);
   });
   
