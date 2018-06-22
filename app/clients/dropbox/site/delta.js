b76a8fa0332a4eb020ed734f7b8d228a3c05b56d
David Merfield <dmerfield@gmail.com>
2018-06-21 21:25:03 -0400

    Moved dropbox client to new metadata model

--- app/clients/dropbox/site/delta.js
+++ app/clients/dropbox/site/delta.js
@@ -1,22 +1,21 @@
-var NO_ENTRIES = 'Expected entries from Dropbox';
-var NO_CURSOR = 'Expected cursor from Dropbox';
-var Dropbox = require('dropbox');
-var Database = require('../database');
-var helper = require('helper');
+var NO_ENTRIES = "Expected entries from Dropbox";
+var NO_CURSOR = "Expected cursor from Dropbox";
+var Dropbox = require("dropbox");
+var Database = require("../database");
+var helper = require("helper");
 var ensure = helper.ensure;
 var forEach = helper.forEach;
 
-module.exports = function delta (blogID, account, callback) {
-
-  ensure(blogID, 'string')
-    .and(account, 'object')
-    .and(callback, 'function');
+module.exports = function delta(blogID, account, callback) {
+  ensure(blogID, "string")
+    .and(account, "object")
+    .and(callback, "function");
 
   var has_more, changes, latest_cursor;
-  var client = new Dropbox({accessToken: account.access_token});
+  var client = new Dropbox({ accessToken: account.access_token });
   var cursor = account.cursor;
   var folder_id = account.folder_id;
-  var folder = '';
+  var folder = "";
   var methods = [];
   var errors = [];
 
@@ -33,94 +32,95 @@ module.exports = function delta (blogID, account, callback) {
     methods.push(filesGetMetadata.bind(this, client, folder_id));
   }
 
-  forEach.parallel(methods, function(method, next){
+  forEach.parallel(
+    methods,
+    function(method, next) {
+      method(function(err, res) {
+        if (err || !res) {
+          errors.push(err || new Error("No response from Dropbox"));
+          return next();
+        }
 
-    method(function(err, res){
+        if (res.entries) changes = res.entries;
+        if (res.cursor) latest_cursor = res.cursor;
+        if (res.has_more) has_more = res.has_more;
+        if (res.path_display) folder = res.path_display;
 
-      if (err || !res) {
-        errors.push(err || new Error('No response from Dropbox'));
-        return next();
+        next();
+      });
+    },
+    function() {
+      var folder_missing = errors.length && errors[0].status === 409;
+      var folder_moved = folder_missing && folder;
+
+      // We were still able to retrieve the metadata
+      // for this folder but the cursor is invalid.
+      // This means the folder was renamed, not removed.
+      if (folder_moved) {
+        account.cursor = "";
+        return delta(blogID, account, callback);
       }
 
-      if (res.entries) changes = res.entries;
-      if (res.cursor) latest_cursor = res.cursor;
-      if (res.has_more) has_more = res.has_more;
-      if (res.path_display) folder = res.path_display;
+      if (!changes) errors.push(new Error(NO_ENTRIES));
+      if (!latest_cursor) errors.push(new Error(NO_CURSOR));
 
-      next();
-    });
+      if (errors.length) {
+        account.error_code = errors[0].status || 400;
+        return Database.set(blogID, account, function(err) {
+          callback(err || errors[0]);
+        });
+      }
 
-  }, function(){
+      account.cursor = latest_cursor;
+      account.last_sync = Date.now();
+      account.folder = folder;
+      account.error_code = 0;
 
-    var folder_missing = errors.length && errors[0].status === 409;
-    var folder_moved = folder_missing && folder;
+      changes = changes.map(function(c) {
+        c.path = folder ? c.path_lower.slice(folder.length) : c.path_lower;
+        return c;
+      });
 
-    // We were still able to retrieve the metadata
-    // for this folder but the cursor is invalid.
-    // This means the folder was renamed, not removed.
-    if (folder_moved) {
-      account.cursor = '';
-      return delta(blogID, account, callback);
-    }
+      Database.set(blogID, account, function(err) {
+        if (err) return callback(err);
 
-    if (!changes) errors.push(new Error(NO_ENTRIES));
-    if (!latest_cursor) errors.push(new Error(NO_CURSOR));
-
-    if (errors.length) {
-      account.error_code = errors[0].status || 400;
-      return Database.set(blogID, account, function(err){
-        callback(err || errors[0]);
+        // We save the state before dealing with the changes
+        // to avoid an infinite loop if one of these changes
+        // causes an exception. If sync enounters an exception
+        // it will verify the folder at a later date
+        callback(null, changes, has_more);
       });
     }
-
-    account.cursor = latest_cursor;
-    account.last_sync = Date.now();
-    account.folder = folder;
-    account.error_code = 0;
-
-    changes = changes.map(function(c){
-      c.path = folder ? c.path_display.slice(folder.length) : c.path_display;
-      return c;
-    });
-
-    Database.set(blogID, account, function(err){
-
-      if (err) return callback(err);
-
-      // We save the state before dealing with the changes
-      // to avoid an infinite loop if one of these changes
-      // causes an exception. If sync enounters an exception
-      // it will verify the folder at a later date
-      callback(null, changes, has_more);
-    });
-  });
+  );
 };
 
 // This is a dumb callback wrapper around Dropbox's
 // fucking stupid promise-based js client. FUCK PROMISES.
-function filesListFolder (client, folder_id, callback) {
-
-  client.filesListFolder({path: folder_id, include_deleted: true, recursive: true})
-    .then(callback.bind(this, null))
-    .catch(callback);
-
-}
-
-// This is a dumb callback wrapper around Dropbox's
-// fucking stupid promise-based js client. FUCK PROMISES.
-function filesListFolderContinue (client, cursor, callback) {
-
-  client.filesListFolderContinue({cursor: cursor})
+function filesListFolder(client, folder_id, callback) {
+  client
+    .filesListFolder({
+      path: folder_id,
+      include_deleted: true,
+      recursive: true
+    })
     .then(callback.bind(this, null))
     .catch(callback);
 }
 
 // This is a dumb callback wrapper around Dropbox's
 // fucking stupid promise-based js client. FUCK PROMISES.
-function filesGetMetadata (client, folder_id, callback) {
-
-  client.filesGetMetadata({path: folder_id})
+function filesListFolderContinue(client, cursor, callback) {
+  client
+    .filesListFolderContinue({ cursor: cursor })
     .then(callback.bind(this, null))
     .catch(callback);
+}
 
+// This is a dumb callback wrapper around Dropbox's
+// fucking stupid promise-based js client. FUCK PROMISES.
+function filesGetMetadata(client, folder_id, callback) {
+  client
+    .filesGetMetadata({ path: folder_id })
+    .then(callback.bind(this, null))
+    .catch(callback);
 }
