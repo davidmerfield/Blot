43b67c8edfacee1b7569a4625003f83e6b131b06
David Merfield <dmerfield@gmail.com>
2018-06-22 15:07:58 -0400

    Fixed bug in access script

--- scripts/access.js
+++ scripts/access.js
@@ -1,36 +1,30 @@
-var Blog = require('../app/models/blog');
-var User = require('../app/models/user');
-var format = require('url').format;
-var config = require('../config');
-
+var Blog = require("../app/models/blog");
+var User = require("../app/models/user");
+var format = require("url").format;
+var config = require("../config");
 
 if (require.main === module) {
-
   var identifier = process.argv[2];
 
-  main(identifier, function(err){
-
+  main(identifier, function(err) {
     if (err) throw err;
-    
+
     process.exit();
   });
 }
 
-function main (handle, callback) {
+function main(handle, callback) {
+  Blog.get({ handle: handle }, function(err, blog) {
+    if (err || !blog) return callback(err || new Error("No blog " + handle));
 
-  Blog.get({handle: handle}, function(err, blog){
-
-    if (err) throw err;
-
-    User.generateAccessToken(blog.owner, function(err, token){
-
+    User.generateAccessToken(blog.owner, function(err, token) {
       if (err) throw err;
 
       // The full one-time log-in link to be sent to the user
       var url = format({
-        protocol: 'https',
+        protocol: "https",
         host: config.host,
-        pathname: '/log-in',
+        pathname: "/log-in",
         query: {
           token: token
         }
