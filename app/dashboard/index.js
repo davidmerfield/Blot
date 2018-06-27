b3719586a80941dd90db9f8eb5e618e50a582f0a
David Merfield <dmerfield@gmail.com>
2018-06-27 14:31:28 -0400

    Prettified dashboard index

--- app/dashboard/index.js
+++ app/dashboard/index.js
@@ -1,12 +1,12 @@
-var express = require('express');
+var express = require("express");
 var dashboard = express();
-var middleware = require('middleware');
-var hogan = require('hogan-express');
-var compression = require('compression');
-var config = require('config');
+var middleware = require("middleware");
+var hogan = require("hogan-express");
+var compression = require("compression");
+var config = require("config");
 var add = middleware.add;
-var bodyParser = require('body-parser');
-var csurf = require('csurf');
+var bodyParser = require("body-parser");
+var csurf = require("csurf");
 var csrf = csurf();
 
 dashboard
@@ -17,85 +17,80 @@ dashboard
   // and use(compression()) must
   // be specified for each individual
   // app. Express considers each seperately.
-  .disable('x-powered-by')
+  .disable("x-powered-by")
 
-  .set('trust proxy', 'loopback')
-  .set('view engine', 'html')
-  .set('views', __dirname + '/views')
-  .engine('html', hogan);
+  .set("trust proxy", "loopback")
+  .set("view engine", "html")
+  .set("views", __dirname + "/views")
+  .engine("html", hogan);
 
 // For when we want to cache templates
-if (config.environment !== 'development')
-  dashboard.enable('view cache');
+if (config.environment !== "development") dashboard.enable("view cache");
 
 // Define these individually don't
 // overwrite server.locals
 dashboard.locals.protocol = config.protocol;
 dashboard.locals.host = config.host;
-dashboard.locals.title = 'Blot';
+dashboard.locals.title = "Blot";
 dashboard.locals.cacheID = Date.now();
 
-dashboard.use(function(req, res, next){
-
+dashboard.use(function(req, res, next) {
   res.title = function(title) {
     res.locals.title = title;
-  }
+  };
 
-  res.renderAccount = function (view) {
-
+  res.renderAccount = function(view) {
     delete res.locals.partials;
 
     res.locals.partials = {};
 
     res.addPartials({
-      head: 'partials/head',
-      footer: 'partials/footer',
-      nav: 'partials/nav',
-      message: 'partials/message',
-      yield: 'account/' + view
+      head: "partials/head",
+      footer: "partials/footer",
+      nav: "partials/nav",
+      message: "partials/message",
+      yield: "account/" + view
     });
 
     var tab = {};
     var tabname = view;
 
-    if (tabname.indexOf('/') > -1)
-      tabname = tabname.slice(0, tabname.indexOf('/'));
+    if (tabname.indexOf("/") > -1)
+      tabname = tabname.slice(0, tabname.indexOf("/"));
 
-    tab[tabname] = 'selected';
+    tab[tabname] = "selected";
 
-    res.addLocals({tab: tab});
+    res.addLocals({ tab: tab });
 
-    res.render('account/wrapper');
-
+    res.render("account/wrapper");
   };
 
   res.renderDashboard = function(view, wrapper) {
-
     delete res.locals.partials;
 
     res.locals.partials = {};
 
     res.addPartials({
-      head: 'partials/head',
-      header: 'partials/header',
-      footer: 'partials/footer',
-      nav: 'partials/nav',
-      message: 'partials/message',
+      head: "partials/head",
+      header: "partials/header",
+      footer: "partials/footer",
+      nav: "partials/nav",
+      message: "partials/message",
       yield: view
     });
 
     var tab = {};
-    var tabname = view;
 
-    if (tabname.indexOf('/') > -1)
-      tabname = tabname.slice(0, tabname.indexOf('/'));
+    if (view === "folder") {
+      tab.folder = "selected";
+    } else {
+      tab.settings = "selected";
+    }
 
-    tab[tabname] = 'selected';
+    res.addLocals({ tab: tab });
 
-    res.addLocals({tab: tab});
-
-    res.render(wrapper || 'partials/wrapper');
-  }
+    res.render(wrapper || "partials/wrapper");
+  };
 
   next();
 });
@@ -107,10 +102,12 @@ dashboard.use(middleware.loadUser);
 dashboard.use(middleware.loadBlog);
 dashboard.use(middleware.redirector);
 
-dashboard.post(['/theme*', '/folder*', '/clients*', '/flags', '/404s', '/account*', '/preferences*'], bodyParser.urlencoded({extended: false}));
+dashboard.post(
+  ["/theme*", "/folder*", "/clients*", "/flags", "/404s", "/account*"],
+  bodyParser.urlencoded({ extended: false })
+);
 
-dashboard.use(csrf, function(req, res, next){
-
+dashboard.use(csrf, function(req, res, next) {
   // Load the CSRF protection since we're
   // inside the app,
   res.addLocals({
@@ -120,16 +117,15 @@ dashboard.use(csrf, function(req, res, next){
   next();
 });
 
-require('./routes/clients')(dashboard);
-require('./routes/account')(dashboard);
-require('./routes/editor')(dashboard);
-require('./routes/folder')(dashboard);
-require('./routes/preferences')(dashboard);
-require('./routes/tools')(dashboard);
-require('./routes/profile')(dashboard);
-require('./routes/theme')(dashboard);
+require("./routes/clients")(dashboard);
+require("./routes/account")(dashboard);
+require("./routes/editor")(dashboard);
+require("./routes/folder")(dashboard);
+require("./routes/settings")(dashboard);
+require("./routes/tools")(dashboard);
+require("./routes/theme")(dashboard);
 
-dashboard.use(express.static(__dirname + '/views'));
+dashboard.use(express.static(__dirname + "/views"));
 
 // need to handle dashboard errors better...
 dashboard.use(function(err, req, res, next) {
@@ -137,7 +133,7 @@ dashboard.use(function(err, req, res, next) {
   console.log(err.trace);
   console.log(err.stack);
   res.status(500);
-  res.send(':( Error');
+  res.send(":( Error");
 });
 
 module.exports = dashboard;
