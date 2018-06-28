f2cd6f994a47ed251ac40afaead89a576ea035f9
David Merfield <dmerfield@gmail.com>
2018-06-28 13:32:41 -0400

    Added support for case insensitive folder lookip

--- app/dashboard/routes/folder/index.js
+++ app/dashboard/routes/folder/index.js
@@ -1,5 +1,4 @@
 var fs = require("fs");
-var helper = require("helper");
 var read = require("./read");
 var breadcrumbs = require("./breadcrumbs");
 var join = require("path").join;
@@ -11,13 +10,19 @@ module.exports = function(server) {
 
     dir = decodeURIComponent(dir);
 
-    read(req.blog, dir, function(err, contents, stat) {
+    read(req.blog, dir, function render (err, contents, stat) {
       if (err && err.code === "ENOTDIR") {
         return next();
       }
 
       if (err && err.code === "ENOENT" && dir === "/") {
         return fs.ensureDir(join(blog_folder_dir, req.blog.id), next);
+      }
+
+      // If the user has the Dropbox client, case-preserved folder
+      // is stored lowercase on disk. So we check that too.
+      if (err && err.code === "ENOENT" && dir.toLowerCase() !== dir) {
+        return read(req.blog, dir.toLowerCase(), render);
       }
 
       if (err && err.code === "ENOENT") {
