785e8df7f1ee067c9014b877046326cba58bcce9
David Merfield <dmerfield@gmail.com>
2018-07-03 17:48:13 -0400

    Updated formatForm

--- app/dashboard/routes/settings/formatForm.js
+++ app/dashboard/routes/settings/formatForm.js
@@ -4,42 +4,41 @@ var formJSON = helper.formJSON;
 
 module.exports = function(req, res, next) {
   try {
-    req.body = formJSON(req.body, Blog.scheme.TYPE);
+    req.updates = formJSON(req.body, Blog.scheme.TYPE);
   } catch (e) {
     return next(e);
   }
 
-  console.log("BODY", req.body);
 
   // If the user has specified a custom permalink format
   // then use it as the blog's permalink format.
   if (
-    req.body.permalink &&
-    !req.body.permalink.format &&
-    req.body.permalink.custom
+    req.updates.permalink &&
+    !req.updates.permalink.format &&
+    req.updates.permalink.custom
   ) {
-    req.body.permalink.format = req.body.permalink.custom;
-    req.body.permalink.isCustom = true;
-  } else if (req.body.permalink && req.body.permalink.format) {
-    req.body.permalink.isCustom = false;
+    req.updates.permalink.format = req.updates.permalink.custom;
+    req.updates.permalink.isCustom = true;
+  } else if (req.updates.permalink && req.updates.permalink.format) {
+    req.updates.permalink.isCustom = false;
   }
 
-  if (req.body.plugins) {
+  if (req.updates.plugins) {
     // this bullshit below is because I haven't properly declared
     // the model for blog.plugins so formJSON needs a little help...
-    for (var i in req.body.plugins) {
-      req.body.plugins[i].enabled = req.body.plugins[i].enabled === "on";
-      if (!req.body.plugins[i].options) req.body.plugins[i].options = {};
+    for (var i in req.updates.plugins) {
+      req.updates.plugins[i].enabled = req.updates.plugins[i].enabled === "on";
+      if (!req.updates.plugins[i].options) req.updates.plugins[i].options = {};
     }
 
-    for (var x in req.body.plugins.typeset.options)
-      req.body.plugins.typeset.options[x] =
-        req.body.plugins.typeset.options[x] === "on";
+    for (var x in req.updates.plugins.typeset.options)
+      req.updates.plugins.typeset.options[x] =
+        req.updates.plugins.typeset.options[x] === "on";
 
-    if (req.body.plugins.analytics.options.provider) {
+    if (req.updates.plugins.analytics.options.provider) {
       var provider = {};
-      provider[req.body.plugins.analytics.options.provider] = "selected";
-      req.body.plugins.analytics.options.provider = provider;
+      provider[req.updates.plugins.analytics.options.provider] = "selected";
+      req.updates.plugins.analytics.options.provider = provider;
     }
   }
   next();
