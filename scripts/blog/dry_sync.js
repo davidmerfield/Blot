5750fd56d2bf8761cbb400a027538008c8678485
David Merfield <dmerfield@gmail.com>
2018-07-09 17:45:28 -0400

    Updated sync script

--- scripts/blog/dry_sync.js
+++ scripts/blog/dry_sync.js
@@ -1,6 +1,8 @@
 var get = require("./get");
 var sync = require("../../app/sync");
 
+// This will request a sync lease, then rebuild the blog's templates, then release the lease
+
 get(process.argv[2], function(user, blog) {
   if (!blog || !blog.id) throw "no blog";
 
