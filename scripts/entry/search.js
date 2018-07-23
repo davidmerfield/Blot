cdb06bff6060c9b63b0afb2bcf35f9b84ff96e4b
David Merfield <dmerfield@gmail.com>
2018-07-23 15:53:35 -0400

    Added scripts to search for entry

---
+++ scripts/entry/search.js
@@ -0,0 +1,30 @@
+var Entries = require("../../app/models/entries");
+var get = require("../blog/get");
+var handle = process.argv[2];
+var query = process.argv[3];
+
+if (!handle) {
+  console.log("- pass a handle as the first argument");
+}
+
+if (!query) {
+  console.log("- pass a query to search entries in the blog's folder");
+}
+
+if (!handle || !query) {
+  return process.exit();
+}
+
+get(handle, function(user, blog) {
+  Entries.each(blog.id, function(entry, nextEntry) {
+    if (entry.path.toLowerCase().indexOf(query.trim().toLowerCase()) === 0) {
+      console.log(entry.path);
+    }
+
+    nextEntry();
+  }, function(){
+    console.log();
+    console.log('Search complete!');
+    process.exit();
+  });
+});
