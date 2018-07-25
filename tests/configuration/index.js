b5f513c07428a64915d67c46079b9308b7712bb7
David Merfield <dmerfield@gmail.com>
2018-07-25 13:16:56 -0400

    Updated config test

--- tests/configuration/index.js
+++ tests/configuration/index.js
@@ -1,12 +1,13 @@
-// var client = require('../../app/models/client');
+var client = require('../../app/models/client');
+var config = require('../../config');
 
 describe("configuration", function() {
 
   it("connects to redis", function(done){
 
-    // client.get('hey', function (err) {
-    //   expect(err).toBe(null);
+    client.get('hey', function (err) {
+      expect(err).toBe(null);
       done();
-    // });
+    });
   });
 });
