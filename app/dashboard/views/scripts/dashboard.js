f48ff80f55a8370bcc7209d4f1355a2ee664adc3
David Merfield <dmerfield@gmail.com>
2018-07-26 11:30:16 -0400

    Updated dashboard styling

--- app/dashboard/views/scripts/dashboard.js
+++ app/dashboard/views/scripts/dashboard.js
@@ -161,3 +161,60 @@ if (sortCreated && sortCreated.onclick) {
     entryList.innerHTML = html;
   }
 }
+
+
+var newRedirect = window.location.hash.slice(1);
+
+if (newRedirect) {
+
+  var from = 'input[placeholder="from"]';
+  var to = 'input[placeholder="to"]';
+
+  var index = $('#redirects').children().length;
+
+  var newRow = $('#new_redirect')
+                  .clone()
+                  .attr('id','')
+                  .find(from)
+                    .attr('name', 'redirects.' + index +'.from')
+                    .val(newRedirect)
+                  .end()
+                  .find(to)
+                    .attr('name', 'redirects.' + index +'.to')
+                  .end()
+                  .show();
+
+  $('#redirects').append(newRow);
+
+  newRow.find(to).focus();
+}
+
+$('#start').click(function(e){
+
+  var el = $(this);
+
+  $('#stream').html('');
+
+  e.preventDefault();
+
+  el.addClass('working')
+    .html('Verifying');
+
+  $.post("?_csrf={{csrftoken}}", function(data){
+
+    el.removeClass('working')
+      .html('Start verification');
+
+  });
+});
+
+$('button:not(.small)').each(function(){
+  $(this).click(function(){
+  });
+});
+
+  $('#toggleTextarea').click(function () {
+    $('#stage').append($('#backstage').children().first());
+    $('#backstage').append($('#stage').children().first());
+  });
+
