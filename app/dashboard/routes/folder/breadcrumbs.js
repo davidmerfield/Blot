13ab1ee5555cf7030a73464994fac6c6dce3cf79
David Merfield <dmerfield@gmail.com>
2018-07-30 17:33:05 -0400

    Fixed various bugs with new design

--- app/dashboard/routes/folder/breadcrumbs.js
+++ app/dashboard/routes/folder/breadcrumbs.js
@@ -2,19 +2,17 @@ module.exports = function breadcrumbs(req, res, next) {
   var breadcrumbs = [];
   var dir = req.dir;
 
-  if (dir === "/") {
-    return next();
-  }
-
   breadcrumbs.push({ name: "Your folder", url: "/view?path=/" });
 
   var names = dir.split("/").filter(function(name) {
     return !!name;
   });
 
+  var redirect = req.header('Referer') || "/";
+  
   names.forEach(function(name, i) {
     breadcrumbs.push({
-      url: "/view?path=" + names.slice(0, i + 1).join("/"),
+      url: "/view?redirect=" + redirect + "&path=" + names.slice(0, i + 1).join("/"),
       name: name
     });
   });
