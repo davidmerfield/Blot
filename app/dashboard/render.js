b9fc1c93a97d8c14a0f46a690ffdb0d317963cdb
David Merfield <dmerfield@gmail.com>
2018-08-02 18:44:18 -0400

    Updated dashboard

--- app/dashboard/render.js
+++ app/dashboard/render.js
@@ -1,23 +1,25 @@
 module.exports = function(req, res, next) {
   res.locals.partials = res.locals.partials || {};
 
-  res.locals.partials.head = "partials/head";
-  res.locals.partials.header = "partials/header";
-  res.locals.partials.footer = "partials/footer";
-  res.locals.partials.nav = "partials/nav";
+  res.locals.partials.head = __dirname + "/views/partials/head";
+  res.locals.partials.header = __dirname + "/views/partials/header";
+  res.locals.partials.footer = __dirname + "/views/partials/footer";
+  res.locals.partials.nav = __dirname + "/views/partials/nav";
 
   var _render = res.render;
   var wrapper;
   
   res.render = function(view, locals, callback) {
+
     if (view.indexOf("account/") > -1) {
-      wrapper = "account/wrapper";
+      wrapper = __dirname + "/views/account/wrapper.html";
     } else {
-      wrapper = "partials/wrapper";
+      wrapper = __dirname + "/views/partials/wrapper.html";
     }
 
     res.locals.partials.yield = view;
 
+    // console.log('calling render', wrapper, res.locals)
     _render.call(this, wrapper, locals, callback);
   };
 
