bf2de30dc5df8e775fa4413edaef6bfc166bedd7
David Merfield <dmerfield@gmail.com>
2018-08-06 16:07:09 -0400

    Improvements to dashboard

--- app/dashboard/routes/settings/save/finish.js
+++ app/dashboard/routes/settings/save/finish.js
@@ -14,11 +14,6 @@ module.exports = function(req, res, next) {
   Blog.set(blogID, updates, function(errors, changes) {
     if (errors) return next(errors);
 
-    // Add success message if we're going to the settings page
-    // and successful changes were made
-    if (changes && changes.length && _.isEmpty(errors)) {
-      res.message({ success: SUCCESS, url: redirect });
-    }
 
     // We now need to save every entry so that
     // changes to permalink format take effect.
@@ -35,6 +30,12 @@ module.exports = function(req, res, next) {
     // format. This should be improved but we.
     if (changes && changes.indexOf("plugins") > -1) {
       rebuild(blog.id);
+    }
+
+    // Add success message if we're going to the settings page
+    // and successful changes were made
+    if (changes && changes.length && _.isEmpty(errors)) {
+      return res.message('/', 'Saved changes to your ' + changes.join(','));
     }
 
     return res.redirect(redirect);
