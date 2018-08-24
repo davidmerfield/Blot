becc14f32f7eb92083afeda61ecad91b18da7221
David Merfield <dmerfield@gmail.com>
2018-08-24 13:45:59 -0400

    Removed sync check from worker

--- app/sync/check/index.js
+++ app/sync/check/index.js
@@ -1,9 +1,68 @@
-var helper = require('../../helper');
-var worker = helper.worker;
-var script = __dirname + '/main';
+var Lease = require('../lease');
+var helper = require('helper');
+var forEach = helper.forEach;
 
-// We only want to run this script
-// once, when the sync process is started
-module.exports = worker(script, {
-  forever: false
+// Look up users who were syncing when this
+// process was last shutdown
+console.log('Checking for syncing users during last shutdown...');
+Lease.active(function(err, blogIDs){
+
+  if (err) throw err;
+
+  blogIDs = blogIDs || [];
+
+  forEach(blogIDs, function(blogID, nextBlog){
+
+    console.log('Need to resync the folder of', blogID,'...');
+
+    // We probably need to release the least of this user
+    // since it
+    Lease.release(blogID, function(err){
+
+      if (err) throw err;
+
+      nextBlog();
+    });
+  }, function(){
+    console.log("All blogs checked...");
+  });
 });
+
+
+// // sync
+// var VerifyFolder = ''; // require('../dropbox/verify');
+// var VerifyBlog = ''; // require('../../verify');
+// var Sync = require('../start');
+// Sync(blogID, function(err){
+
+//   if (err) {
+//     console.log(err);
+//     if (err.stack) console.log(err.stack);
+//     if (err.trace) console.log(err.trace);
+//     return nextBlog();
+//   }
+
+//   // ensure folder is in sync...
+//   VerifyFolder(blogID, function(err){
+
+//     if (err) {
+//       console.log(err);
+//       if (err.stack) console.log(err.stack);
+//       if (err.trace) console.log(err.trace);
+//       return nextBlog();
+//     }
+
+//     console.log('Verifying blog data of ', blogID ,'...');
+
+//     VerifyBlog(blogID, function(err){
+
+//       if (err) {
+//         console.log(err);
+//         if (err.stack) console.log(err.stack);
+//         if (err.trace) console.log(err.trace);
+//       }
+
+//       nextBlog();
+//     });
+//   });
+// });
