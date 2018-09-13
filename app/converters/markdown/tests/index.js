fe9217bda95ade34ee12f4e8a0d3d4f85330c72a
David Merfield <dmerfield@gmail.com>
2018-09-13 18:46:55 -0400

    Added local tests

---
+++ app/converters/markdown/tests/index.js
@@ -0,0 +1,49 @@
+describe("markdown converter", function() {
+
+  var Blog = require('blog');
+  var Entry = require('entry');
+  var fs = require('fs-extra');
+
+  beforeEach(global.createUser);
+  beforeEach(global.createBlog);
+
+  afterEach(global.removeBlog);
+  afterEach(global.removeUser);
+
+  it("an entry", function(done){
+
+    var path = '/post.txt';
+
+    fs.copyFileSync(__dirname + path, process.env.BLOT_DIRECTORY + '/blogs/' + global.blog_id + path);
+
+    Blog.get({id: global.blog_id}, function(err, blog){
+
+      expect(err).toBe(null);
+
+      Entry.build(blog, path, function(err, entry){
+  
+        expect(err).toBe(null);
+        expect(entry).toEqual(jasmine.any(Object));
+
+        Entry.set(global.blog_id, path, entry, function(err){
+
+          expect(err).toBe(null);
+
+          Entry.get(global.blog_id, path, function(entry){
+
+            expect(err).toBe(null);
+            expect(entry).toEqual(jasmine.any(Object));
+
+            console.log(entry);
+            
+            Entry.drop(global.blog_id, path, function(err){
+
+              expect(err).toBe(null);
+              done();
+            });
+          });
+        });
+      });
+    });
+  });
+});
