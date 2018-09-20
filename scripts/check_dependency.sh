fec4e2e384de223f2f098a145e2cc419d30957e1
David Merfield <dmerfield@gmail.com>
2018-09-20 19:25:28 -0400

    Removed more unused packages

--- scripts/check_dependency.sh
+++ scripts/check_dependency.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# prevent exit error if grep finds no match
+set +e
+grep -rnwl app -e "require([\"'].*$1.*[\"'])" --exclude-dir=importer --include=\*.js
 set -e
 
-grep -rnwl app -e "require([\"']$1[\"'])" --exclude-dir=importer --include=\*.js
