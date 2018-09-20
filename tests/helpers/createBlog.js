fcc74ab1630928b81af2c2ed20f2df557f685f61
David Merfield <dmerfield@gmail.com>
2018-09-20 15:27:31 -0400

    make sure test blog has handle

--- tests/helpers/createBlog.js
+++ tests/helpers/createBlog.js
@@ -1,7 +1,16 @@
 var Blog = require('../../app/models/blog');
 
+var chars = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+function randomHandle () {
+  var res = '';
+  while(res.length < 16) res+= chars[Math.floor(Math.random() * chars.length)];
+  return res;
+}
+
 module.exports = function (done) {
-  Blog.create(global.user.uid, {}, function(err, blog){
+  Blog.create(global.user.uid, {handle: randomHandle()}, function(err, blog){
+    if (err) return done(err);
     global.blog = blog;
     done(err);
   });
