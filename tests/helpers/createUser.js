42c84d62e6b5c617e686cf18b4514feda7524ce0
David Merfield <dmerfield@gmail.com>
2018-09-20 16:47:21 -0400

    updated tests

--- tests/helpers/createUser.js
+++ tests/helpers/createUser.js
@@ -1,10 +1,14 @@
+var User = require('../../app/models/user');
+var randomString = require('./randomString');
+
 module.exports = function(done){
 
-  var User = require('../../app/models/user');
+  var fakePasswordHash = randomString(16);
+  var fakeEmail = randomString(20) + '@example.com';
 
-  User.create('XXXXX@gmail.com', 'XXXX', {}, function(err, user){
+  User.create(fakeEmail, fakePasswordHash, {}, function(err, user){
     if (err) return done(err);
     global.user = user;
-    done()
+    done();
   });
 };
