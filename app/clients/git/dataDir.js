3e236ffbf2cd09f328a71a73a661391315d7344d
David Merfield <dmerfield@gmail.com>
2018-09-21 16:12:10 -0400

    Updated readme

--- app/clients/git/dataDir.js
+++ app/clients/git/dataDir.js
@@ -1,3 +1,7 @@
 require("fs-extra").ensureDirSync(__dirname + "/data");
 
+// consolidated reference to location of bareRepoDirectory in
+// tests and code so we can move this in future painlessly:
+// basically look for '/data'
+
 module.exports = __dirname + "/data";
