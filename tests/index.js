d4421c2a43112c2c1a702ddfd6ce9219967744c7
David Merfield <dmerfield@gmail.com>
2018-09-25 13:20:09 -0400

    Stop testing spec on failure

--- tests/index.js
+++ tests/index.js
@@ -4,7 +4,7 @@ var config = {
   spec_dir: "",
   spec_files: ["tests/**/*.js", "app/**/tests/index.js", "app/**/tests.js"],
   helpers: [],
-  stopSpecOnExpectationFailure: false,
+  stopSpecOnExpectationFailure: true,
   random: false
 };
 
