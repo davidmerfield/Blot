7663f7087536f8514abe7fc6c2cf9e49e6f7c0b7
David Merfield <dmerfield@gmail.com>
2018-09-25 16:14:15 -0400

    Removed packages

--- tests/index.js
+++ tests/index.js
@@ -1,29 +1,32 @@
 var Jasmine = require("jasmine");
 var jasmine = new Jasmine();
+
 var config = {
   spec_dir: "",
-  spec_files: ["tests/**/*.js", "app/**/tests/index.js", "app/**/tests.js"],
+  spec_files: ["**/tests/index.js", "**/tests.js"],
   helpers: [],
   stopSpecOnExpectationFailure: false,
-  random: false
+  random: true
 };
 
 // Pass in a custom test glob for running only specific tests
-if (process.env.TEST) {
-  config.spec_files = [
-    process.env.TEST + "**/tests/index.js",
-    process.env.TEST + "**/tests.js"
-  ];
-  console.log("Running tests from", config.spec_files);
+if (process.argv[2]) {
+  console.log('Running specs inside', process.argv[2]);
+  config.spec_dir = process.argv[2];
+} else {
+  console.log('If you want to run tests from a subdirectory: "npm test {path_to_specs}"');
 }
 
+if (process.argv[3]) {
+  jasmine.seed(process.argv[3]);
+} else {
+  console.log('If you want your own seed run "npm test {path_to_specs} {seed}"');
+}
+
+beforeAll(function(done){
+console.log('Is this fun?');
+done();
+});
 
 jasmine.loadConfig(config);
-
-global.createUser = require("./helpers/createUser");
-global.createBlog = require("./helpers/createBlog");
-
-global.removeBlog = require("./helpers/removeBlog");
-global.removeUser = require("./helpers/removeUser");
-
 jasmine.execute();
