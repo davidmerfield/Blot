fe7b5225a86d6d928504af14393f9ae98c8971fc
David Merfield <dmerfield@gmail.com>
2018-09-25 11:08:42 -0400

    More logging

--- app/clients/git/tests/util/waitForSyncToFinish.js
+++ app/clients/git/tests/util/waitForSyncToFinish.js
@@ -2,6 +2,7 @@ var Sync = require('sync');
 
 module.exports = function main (done) {
 
+  console.log('Attempting to aqcuire sync lock to see if an existing sync has finished...');
   Sync(global.blog.id, function(_cb){
     _cb(null);
   }, function(err, unavailable){
