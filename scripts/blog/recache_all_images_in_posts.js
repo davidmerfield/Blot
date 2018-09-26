eb55289df9992ffb0c6c95e8b6bb584ba392ad97
David Merfield <dmerfield@gmail.com>
2018-09-26 14:35:49 -0400

    Initial work on scripts for resetting thumbnails

---
+++ scripts/blog/recache_all_images_in_posts.js
@@ -0,0 +1,2 @@
+
+// would be nice if this fetched existing image if current attempt to cache failed
---
+++ scripts/blog/regenerate_thumbnails.js
@@ -0,0 +1,5 @@
+// empty cache of existing thumbnail paths
+
+// rebuild each entry
+
+// delete thumbnails in static dir older than X
