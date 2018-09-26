fbf2254dab5340e4dc9e6a3bfd1a4b4e74458ab6
David Merfield <dmerfield@gmail.com>
2018-09-26 18:35:50 -0400

    Added script to fix case-sensitive paths in entries

---
+++ scripts/blog/fix_paths_in_entries.js
@@ -0,0 +1,92 @@
+var get = require("./get");
+var Entries = require("../../app/models/entries");
+var Entry = require("../../app/models/entry");
+var localPath = require("../../app/helper").localPath;
+var fs = require("fs");
+var dirname = require("path").dirname;
+var basename = require("path").basename;
+var async = require('async');
+var set = require('../../app/sync/change/set');
+var drop = require('../../app/sync/change/drop');
+
+if (require.main === module) {
+  get(process.argv[2], function(user, blog) {
+    main(blog, function(err) {
+      if (err) throw err;
+      process.exit();
+    });
+  });
+}
+
+function main(blog, callback) {
+  console.log("Blog " + blog.id + ":", "Fixing bad case in entries...");
+
+  var exists = [];
+  var missing = [];
+  var existsWithDifferentCase = [];
+
+  Entries.each(
+    blog.id,
+    function(_entry, next) {
+      if (_entry.deleted) return next();
+
+      fs.readdir(dirname(localPath(blog.id, _entry.path)), function(
+        err,
+        contents
+      ) {
+
+        if (err) {
+          console.log(err);
+          return next();
+        }
+
+        if (
+          contents.indexOf(basename(_entry.path)) === -1 &&
+          contents.indexOf(basename(_entry.path.toLowerCase())) !== -1
+        ) {
+          existsWithDifferentCase.push(_entry);
+        } else if (contents.indexOf(basename(_entry.path)) === -1) {
+          missing.push(_entry);
+        } else {
+          exists.push(_entry);
+        }
+
+        next();
+      });
+    },
+    function(err) {
+      if (err) return callback(err);
+      console.log(exists.length, " files exist on disk with same case");
+      console.log(missing.length, " files are missing from the disk", missing);
+      console.log(existsWithDifferentCase.length, "files exists on disk with a different case..");
+      async.eachSeries(existsWithDifferentCase, function(entryWithIncorrectCaseForPath, next){
+        var currentPath = entryWithIncorrectCaseForPath.path;
+        var candidates = fs.readdirSync(dirname(localPath(blog.id, currentPath))).filter(function(name){
+          return name.toLowerCase() === basename(currentPath).toLowerCase();
+        });
+
+        if (candidates.length > 1) {
+          console.log('!', currentPath, candidates);
+          next();
+        } else if (candidates.length === 1) {
+
+
+          console.log('calling entry set!', currentPath, currentPath.toLowerCase());
+
+          Entry.set(blog.id, currentPath, {path: currentPath.toLowerCase()}, next);
+
+          // console.log('O', basename(currentPath), '------>', candidates[0]);
+        } else {
+          console.log('X', currentPath, 'has no candidates for rename');
+          next();          
+        }
+        
+      }, function(err){
+        console.log("Blog " + blog.id + ":", "Fixed all entries!");
+        callback();      
+      });
+    }
+  );
+}
+
+module.exports = main;
