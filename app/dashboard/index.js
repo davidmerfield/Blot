9fece921ec3fcfc5cd58126d00dd3eebcad90f04
David Merfield <dmerfield@gmail.com>
2018-09-29 11:56:30 -0400

    initial commit

--- app/dashboard/index.js
+++ app/dashboard/index.js
@@ -190,7 +190,23 @@ dashboard.use(debug("done fetching"));
 
 // Send user's avatar
 dashboard.use("/_avatars/:avatar", function(req, res, next){
-  res.sendFile(config.blog_static_files_dir + '/' + req.blog.id + '/_avatars/' + req.params.avatar, function(err){
+  
+  var blogID;
+
+  if (req.query.handle) {
+    
+    blogID = req.blogs.filter(function(blog){
+      return blog.handle === req.query.handle;
+    }).map(function(blog){
+      return blog.id;
+    });
+
+  } else {
+
+    blogID = req.blog.id;
+  }
+
+  res.sendFile(config.blog_static_files_dir + '/' + blogID + '/_avatars/' + req.params.avatar, function(err){
     if (err) return next();
     // sent successfully
   });
