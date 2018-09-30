dd89647022279d5d71b2382707a701b08747ae23
David Merfield <dmerfield@gmail.com>
2018-09-29 20:04:59 -0400

    Added a test to see if sync detects renamed files

--- app/sync/update/index.js
+++ app/sync/update/index.js
@@ -7,36 +7,42 @@ var set = require("./set");
 var mkdir = require("./mkdir");
 
 module.exports = function(blog) {
-
   return function(path, options, callback) {
-    
     if (callback === undefined && typeof options === "function") {
       callback = options;
       options = {};
     }
 
-    // Blot likes leading slashes
+    // Blot likes leading slashes, the git client
+    // for instance does not have them but we
+    // are not so strict above these things...
     if (path[0] !== "/") path = "/" + path;
 
-
-    function onComplete (err) {
-      // we never let this error escape
-      if (err) console.log('SYNC ERROR:', err);
-
-      callback(null, {error: err || null});
+    function done (err) {
+      // we never let this error escape out
+      if (err) {
+        console.error("Blog", blog.id, "Caught error updating", path, err);
+      }
+      callback(null, { error: err || null });
     }
 
     fs.stat(localPath(blog.id, path), function(err, stat) {
       if (err && err.code === "ENOENT") {
-        drop(blog.id, path, options, onComplete);
-      } else if (err || !stat) {
-        onComplete(err || new Error("No stat from " + path));
-      } else if (stat.isDirectory()) {
-        mkdir(blog.id, path, options, onComplete);
-      } else if (stat.isFile()) {
-        set(blog, path, options, onComplete);
+        drop(blog.id, path, options, done);
+      } else if (stat && stat.isDirectory()) {
+        mkdir(blog.id, path, options, done);
+      } else if (stat && stat.isFile()) {
+        set(blog, path, options, done);
       } else {
-        onComplete(new Error("Not sure what to with stat", stat));
+        done(
+          new Error(
+            [
+              "Not sure what to do with" + path,
+              "Stat: " + stat,
+              "Error: " + err
+            ].join("\n")
+          )
+        );
       }
     });
   };
