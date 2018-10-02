3bafb3f54f7296ea34f2a2dbc6a543b49e0bee90
David Merfield <dmerfield@gmail.com>
2018-10-02 14:18:46 -0400

    Refactored promises

--- app/clients/dropbox/tests/util/emptyFolder.js
+++ app/clients/dropbox/tests/util/emptyFolder.js
@@ -1,46 +1,48 @@
 var Dropbox = require("dropbox");
 
 module.exports = function(done) {
-  var client = new Dropbox({
-    accessToken: process.env.BLOT_DROPBOX_TEST_ACCOUNT_APP_TOKEN
+  
+var client = new Dropbox({
+  accessToken: process.env.BLOT_DROPBOX_TEST_ACCOUNT_APP_TOKEN
+});
+
+function checkBatchStatus(result) {
+  return client
+    .filesDeleteBatchCheck(result)
+    .then(function(res) {
+      switch (res[".tag"]) {
+        case "in_progress":
+          return checkBatchStatus(result);
+        case "complete":
+          return Promise.resolve(res);
+        default:
+          return Promise.reject(new Error("Unknown response " + res));
+      }
+    });
+}
+
+function removeAllFiles(res) {
+  return client.filesDeleteBatch({
+    entries: res.entries.map(function(entry) {
+      return { path: entry.path_lower };
+    })
+  });
+}
+
+client
+  .filesListFolder({ path: "" })
+  .then(removeAllFiles)
+  .then(checkBatchStatus)
+  .then(function(res) {
+    console.log("Removed all files!", res);
+    done();
+  })
+  .catch(function(err) {
+    if (err instanceof Error) {
+      done(err);
+    } else {
+      done(new Error(err));
+    }
   });
 
-  client
-    .filesListFolder({
-      path: ""
-    })
-    .then(function(res) {
-      console.log("Removing", res.entries.length, "items...");
-      return client.filesDeleteBatch({
-        entries: res.entries.map(function(entry) {
-          return { path: entry.path_lower };
-        })
-      });
-    })
-    .then(function checkStatus(result) {
-      return client.filesDeleteBatchCheck(result).then(function(res) {
-        switch (res[".tag"]) {
-          case "in_progress":
-            return checkStatus(result);
-          case "complete":
-            return res;
-          default:
-            console.log("unknown response!", res);
-            throw new Error("Unknow response", res);
-        }
-      })
-    })
-    .then(function(res) {
-      console.log("Removed all files!");
-      done();
-    })
-    .catch(function(err) {
-      console.log(
-        "Failed to empty folder:",
-        err,
-        err.error.error_summary,
-        err.retry_after
-      );
-      done(new Error("Could not empty folder"));
-    });
 };
