371dab4a2147daf5d04011fc584149ae8670b7c8
David Merfield <dmerfield@gmail.com>
2018-10-03 19:01:17 -0400

    Refactored renames test

--- app/sync/tests/renames.js
+++ app/sync/tests/renames.js
@@ -3,6 +3,56 @@ describe("update", function() {
   var fs = require("fs-extra");
   var async = require("async");
 
+  it("detects a renamed file across multiple syncs", function(done) {
+    var path = this.fake.path(".txt");
+    var newPath = this.fake.path(".txt");
+    var content = this.fake.file();
+
+    var ctx = this;
+
+    ctx.writeAndSync(path, content, function(err) {
+      if (err) return done.fail(err);
+      ctx.writeAndSync(newPath, content, function(err) {
+        if (err) return done.fail(err);
+        ctx.removeAndSync(path, function(err) {
+          if (err) return done.fail(err);
+          ctx.checkRename(path, newPath, done);
+        });
+      });
+    });
+  });
+
+  it("detects a renamed file", function(testDone) {
+    var path = this.fake.path(".txt");
+    var newPath = this.fake.path(".txt");
+    var content = this.fake.file();
+    var checkRename = this.checkRename;
+
+    sync(this.blog.id, function(err, folder, done) {
+      if (err) return testDone.fail(err);
+
+      fs.outputFileSync(folder.path + path, content, "utf-8");
+
+      folder.update(path, function(err) {
+        if (err) return testDone.fail(err);
+
+        fs.moveSync(folder.path + path, folder.path + newPath);
+
+        async.series(
+          [folder.update.bind(this, path), folder.update.bind(this, newPath)],
+          function(err) {
+            if (err) return testDone.fail(err);
+
+            done(null, function(err) {
+              if (err) return testDone.fail(err);
+              checkRename(path, newPath, testDone);
+            });
+          }
+        );
+      });
+    });
+  });
+
   // Set up a test blog before each test
   global.test.blog();
 
@@ -11,48 +61,64 @@ describe("update", function() {
     this.fake = global.test.fake;
   });
 
+  // helper functions
   beforeEach(function() {
-    this.checkEntry = global.test.CheckEntry(this.blog.id);
-  });
+    var blog = this.blog;
+    var checkEntry = global.test.CheckEntry(blog.id);
+    this.checkEntry = checkEntry;
 
-  it("detects a renamed file", function(testDone) {
-    var path = this.fake.path(".txt");
-    var newPath = this.fake.path(".txt");
-    var content = this.fake.file();
-    var checkEntry = this.checkEntry;
+    this.checkRename = function(oldPath, newPath, callback) {
+      checkEntry({ path: oldPath, deleted: true }, function(err, entry) {
+        if (err) return callback(err);
 
-    sync(this.blog.id, function(err, folder, done) {
-      if (err) testDone.fail(err);
-
-      fs.outputFileSync(folder.path + path, content, "utf-8");
-
-      folder.update(path, function(err) {
-        if (err) testDone.fail(err);
-
-        fs.moveSync(folder.path + path, folder.path + newPath);
-
-        async.series(
-          [folder.update.bind(this, path), folder.update.bind(this, newPath)],
+        checkEntry(
+          {
+            path: newPath,
+            url: entry.permalink,
+            created: entry.created,
+            deleted: false
+          },
           function(err) {
-            if (err) testDone.fail(err);
-
-            done(null, function() {
-              // in future we might need to check this after finishing sync...
-              checkEntry({ path: path, deleted: true }, function(err, entry) {
-                if (err) testDone.fail(err);
-
-                checkEntry(
-                  { path: newPath, created: entry.created, deleted: false },
-                  function(err) {
-                    if (err) testDone.fail(err);
-                    testDone();
-                  }
-                );
-              });
-            });
+            if (err) return callback(err);
+            callback();
           }
         );
       });
-    });
+    };
+
+    this.removeAndSync = function(path, callback) {
+      sync(blog.id, function(err, folder, done) {
+        if (err) return callback(err);
+
+        fs.removeSync(folder.path + path);
+
+        folder.update(path, function(err) {
+          if (err) return callback(err);
+
+          done(null, function(err) {
+            if (err) return callback(err);
+
+            checkEntry({ path: path, deleted: true }, callback);
+          });
+        });
+      });
+    };
+    this.writeAndSync = function(path, contents, callback) {
+      sync(blog.id, function(err, folder, done) {
+        if (err) return callback(err);
+
+        fs.outputFileSync(folder.path + path, contents, "utf-8");
+
+        folder.update(path, function(err) {
+          if (err) return callback(err);
+
+          done(null, function(err) {
+            if (err) return callback(err);
+
+            checkEntry({ path: path, deleted: false }, callback);
+          });
+        });
+      });
+    };
   });
 });
