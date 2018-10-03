e769cd0ba34e9e7d50f249bb24b0d70d948dc0fa
David Merfield <dmerfield@gmail.com>
2018-10-03 12:45:13 -0400

    Modified sync

--- app/clients/dropbox/sync.js
+++ app/clients/dropbox/sync.js
@@ -9,113 +9,120 @@ var delta = require("./util/delta");
 var Download = require("./util/download");
 
 var Database = require("./database");
+var syncOptions = { retryCount: -1, retryDelay: 10, retryJitter: 10 };
 
 module.exports = function main(blog, callback) {
-  debug("Beginning sync");
+  debug("Blog:", blog.id, "Attempting to acquire lock on the blog folder.");
 
-  Database.get(blog.id, function(err, account) {
+  // redlock options to ensure we acquire a lock eventually...
+  Sync(blog.id, syncOptions, function(err, folder, done) {
     if (err) return callback(err);
 
-    // redlock options to ensure we acquire a lock eventually...
-    Sync(blog.id, { retryCount: -1, retryDelay: 10, retryJitter: 10 }, function(
-      err,
-      folder,
-      done
-    ) {
-      if (err) return callback(err);
+    debug("Blog:", blog.id, "Lock acquired successfully. Beginning sync...");
 
-      debug("Retrieving changes from Dropbox...", account);
+    // We need to look up the Dropbox account for this blog
+    // to retrieve the access token used to create a new Dropbox
+    // client to retrieve changes made to the user's Dropbox.
+    Database.get(blog.id, function(err, account) {
+      if (err) return done(err, callback);
 
+      // Delta retrieves changes to the folder on Dropbox for a given
+      // blog. It returns a list of changes. It also adds a new property
+      // to each change, relative_path. Use change.relative_path
+      // as the 'Blot' path, this is the path of the change relative to the
+      // blog folder in the user's Dropbox folder.
       delta(blog.id, account, function handle(err, changes, more, account) {
         if (err) return done(err, callback);
 
-        debug("Retrieved changes", changes);
+        // Now we attempt to apply the changes which occured in the
+        // user's folder on Dropbox to the blog folder on Blot's server.
+        // This means making any new directories, downloading any new
+        // or changed files, and removing any deleted items.
+        apply(changes, folder.path, account.access_token, function(err) {
+          if (err) return done(err, callback);
 
-        var client = createClient(account.access_token);
-
-        var deleted = changes.filter(function(item) {
-          return item[".tag"] === "deleted";
-        });
-
-        var folders = changes.filter(function(item) {
-          return item[".tag"] === "folder";
-        });
-
-        var files = changes.filter(function(item) {
-          return item[".tag"] === "file";
-        });
-
-        function remove(item, callback) {
-          debug("Removing", item.path);
-          fs.remove(join(folder.path, item.path), callback);
-        }
-
-        function mkdir(item, callback) {
-          debug("Mkdiring", item.path);
-          fs.ensureDir(join(folder.path, item.path), callback);
-        }
-
-        // Item.path_lower is the full path to the item
-        // in the user's Dropbox. Don't confuse it with the
-        // relative path to an item, since the root of the
-        // Dropbox folder might not be the root of the blog.
-        function download(item, callback) {
-          debug("Downloading", item.path);
-          Download(
-            client,
-            item.path_lower,
-            join(folder.path, item.path),
-            callback
-          );
-        }
-
-        debug("Deleted:", deleted);
-        debug("Folders:", folders);
-        debug("Files:", files);
-
-        async.parallel(
-          [
-            async.apply(async.each, deleted, remove),
-            async.apply(async.each, folders, mkdir),
-            async.apply(async.eachLimit, files, 20, download)
-          ],
-          function(err) {
+          // we have successfully applied this batch of changes
+          // to the user's Dropbox folder. Now we save the new
+          // cursor and folderID and folder path to the database.
+          // This means that future webhooks will invoke calls to
+          // delta which return changes made after this point in time.
+          debug("Storing latest cursor and folder information...", account);
+          Database.set(blog.id, account, function(err) {
             if (err) return done(err, callback);
 
+            // Now we report back to Blot about the changes made during
+            // this synchronization. We don't care about errors because
+            // those lie beyond the scope of this client. Its responsibilty
+            // is to ensure the blog folder on Blot's server is in sync.
             async.each(
               changes,
               function(item, next) {
                 debug("Updating on Blot:", item.path);
                 folder.update(item.path, { name: item.name }, next);
               },
-              function(err) {
-                if (err) return done(err, callback);
-
+              function() {
+                // If Dropbox says there are more changes
+                // we get them before returning the callback.
+                // This is important because a rename could
+                // be split across two pages of file events.
                 if (more) return delta(blog.id, account, handle);
 
-                debug("Storing latest greatest version of account...", account);
-
-                Database.set(blog.id, account, function(err) {
-                  if (err) return done(err, callback);
-
-                  // We save the state before dealing with the changes
-                  // to avoid an infinite loop if one of these changes
-                  // causes an exception. If sync enounters an exception
-                  // it will verify the folder at a later date
-
-                  // If Dropbox says there are more changes
-                  // we get them before returning the callback.
-                  // This is important because a rename could
-                  // be split across two pages of file events.
-
-                  debug("Finished sync!");
-                  done(null, callback);
-                });
+                debug("Finished sync!");
+                done(null, callback);
               }
             );
-          }
-        );
+          });
+        });
       });
     });
   });
 };
+
+function apply(changes, blogFolder, token, callback) {
+  var client = createClient(token);
+  debug("Retrieved changes", changes);
+
+  var deleted = changes.filter(function(item) {
+    return item[".tag"] === "deleted";
+  });
+
+  var folders = changes.filter(function(item) {
+    return item[".tag"] === "folder";
+  });
+
+  var files = changes.filter(function(item) {
+    return item[".tag"] === "file";
+  });
+
+  function remove(item, callback) {
+    debug("Removing", item.path);
+    fs.remove(join(blogFolder, item.path), callback);
+  }
+
+  function mkdir(item, callback) {
+    debug("Mkdiring", item.path);
+    fs.ensureDir(join(blogFolder, item.path), callback);
+  }
+
+  // Item.path_lower is the full path to the item
+  // in the user's Dropbox. Don't confuse it with the
+  // relative path to an item, since the root of the
+  // Dropbox folder might not be the root of the blog.
+  function download(item, callback) {
+    debug("Downloading", item.path);
+    Download(client, item.path_lower, join(blogFolder, item.path), callback);
+  }
+
+  debug("Deleted:", deleted);
+  debug("Folders:", folders);
+  debug("Files:", files);
+
+  async.parallel(
+    [
+      async.apply(async.each, deleted, remove),
+      async.apply(async.each, folders, mkdir),
+      async.apply(async.eachLimit, files, 20, download)
+    ],
+    callback
+  );
+}
