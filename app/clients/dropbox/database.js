68ec685ca54dd2bc0a5b2daa079bb4d80ea3f952
David Merfield <dmerfield@gmail.com>
2018-10-03 10:51:18 -0400

    Cleaned database function

--- app/clients/dropbox/database.js
+++ app/clients/dropbox/database.js
@@ -1,10 +1,139 @@
+var debug = require("debug")("clients:dropbox:database");
 var helper = require("helper");
 var redis = require("client");
 var Blog = require("blog");
-var debug = require("debug")("clients:dropbox:database");
 var async = require("async");
 var ensure = helper.ensure;
 var Model, database;
+
+function get(blogID, callback) {
+  redis.hgetall(accountKey(blogID), function(err, account) {
+    if (err) return callback(err, null);
+
+    if (!account) return callback(null, null);
+
+    // Restore the types of the properties
+    // of the account object before calling back.
+    for (var i in Model) {
+      if (Model[i] === "number") account[i] = parseInt(account[i]);
+
+      if (Model[i] === "boolean") account[i] = account[i] === "true";
+    }
+
+    return callback(null, account);
+  });
+}
+
+function listBlogs(account_id, callback) {
+  var blogs = [];
+
+  debug("Getting blogs conencted to Dropbox account", account_id);
+
+  redis.SMEMBERS(blogsKey(account_id), function(err, members) {
+    if (err) return callback(err);
+
+    debug("Found these blog IDs", members);
+
+    async.each(
+      members,
+      function(id, next) {
+        Blog.get({ id: id }, function(err, blog) {
+          if (blog && blog.client === "dropbox") {
+            blogs.push(blog);
+          } else {
+            debug(
+              id,
+              "does not match an extant blog using the Dropbox client."
+            );
+          }
+          next();
+        });
+      },
+      function(err) {
+        callback(err, blogs);
+      }
+    );
+  });
+}
+
+function set(blogID, changes, callback) {
+  var multi = redis.multi();
+
+  debug("Setting dropbox account info for blog", blogID);
+
+  get(blogID, function(err, account) {
+    if (err) return callback(err);
+
+    // When saving account for the first time,
+    // this will be null so we make a fresh object.
+    account = account || {};
+
+    // At the moment it is impossible to change
+    // an account ID in place, you need to disconnect
+    // then reconnect. If I decide to re-enable account switch, the
+    // following code is needed to ensure we don't sync
+    // the wrong blogs when a webhook appears. As it stands
+    // it doesn't do much harm so I'll leave it in place.
+    if (
+      account.account_id &&
+      changes.account_id &&
+      account.account_id !== changes.account_id
+    ) {
+      multi.srem(blogsKey(account.account_id), blogID);
+    }
+
+    // Overwrite existing properties with any changes
+    for (var i in changes) account[i] = changes[i];
+
+    // Verify that the type of new account state
+    // matches the expected types declared in Model below.
+    try {
+      ensure(account, Model, true);
+    } catch (e) {
+      return callback(e);
+    }
+
+    debug("Saving this account");
+    multi.sadd(blogsKey(changes.account_id), blogID);
+    multi.hmset(accountKey(blogID), account);
+    multi.exec(callback);
+  });
+}
+
+function drop(blogID, callback) {
+  get(blogID, function(err, account) {
+    var multi = redis.multi();
+
+    // Deregister this blog from the set containing
+    // the blog IDs associated with a particular dropbox
+    // account. If we SREM the last item from a set, redis also
+    // deletes the set. So don't worry about an additional
+    // operation to delete this set.
+    if (account && account.account_id)
+      multi.srem(blogsKey(account.account_id), blogID);
+
+    // Remove all the dangerous Dropbox account information
+    // including the OAUTH token used to interact with
+    // Dropbox's API. As I understand it, this is all we
+    // need to remove. We should also encourage the user
+    // to revoke the token on
+    multi.del(accountKey(blogID));
+    multi.exec(callback);
+  });
+}
+
+// JSON which stores useful information
+// information about this particular blog & dropbox account
+// combination, e.g. root directory and access token.
+function accountKey(blogID) {
+  return "blog:" + blogID + ":dropbox:account";
+}
+
+// A set whoses members are the blog ids
+// connected to this dropbox account.
+function blogsKey(account_id) {
+  return "clients:dropbox:" + account_id;
+}
 
 Model = {
   // We use the account ID to work out which
@@ -66,132 +195,6 @@ Model = {
   // user removes their folder.
   cursor: "string"
 };
-
-function get(blogID, callback) {
-  redis.hgetall(accountKey(blogID), function(err, account) {
-    if (err) return callback(err, null);
-
-    if (!account) return callback(null, null);
-
-    // Restore the types of the
-    // account properties before calling back.
-    for (var i in Model) {
-      if (Model[i] === "number") account[i] = parseInt(account[i]);
-
-      if (Model[i] === "boolean") account[i] = account[i] === "true";
-    }
-
-    return callback(null, account);
-  });
-}
-
-function listBlogs(account_id, callback) {
-  var blogs = [];
-
-  debug("Getting blogs conencted to Dropbox account", account_id);
-
-  redis.SMEMBERS(blogsKey(account_id), function(err, members) {
-    if (err) return callback(err);
-
-    debug("Found these blog IDs", members);
-
-    async.eachSeries(
-      members,
-      function(id, next) {
-        Blog.get({ id: id }, function(err, blog) {
-          if (err || !blog || blog.client !== "dropbox") {
-            debug("Could not find a blog in the DB for", id);
-            return next();
-          }
-
-          debug("Found a blog in the DB for", id);
-          blogs.push(blog);
-          next();
-        });
-      },
-      function(err) {
-        callback(err, blogs);
-      }
-    );
-  });
-}
-
-
-
-function set(blogID, changes, callback) {
-  var multi = redis.multi();
-
-  debug("Setting dropbox account info for blog", blogID);
-
-  get(blogID, function(err, account) {
-    if (err) return callback(err);
-
-    account = account || {};
-
-    if (
-      account.account_id &&
-      changes.account_id &&
-      account.account_id !== changes.account_id
-    ) {
-      debug(
-        "The user's account has changed, remove the old one and add the new one"
-      );
-      multi.srem(blogsKey(account.account_id), blogID);
-    }
-
-    for (var i in changes) account[i] = changes[i];
-
-    if (changes.account_id) {
-      debug("Adding blog id to the list of blogs for this Dropbox account");
-      multi.sadd(blogsKey(changes.account_id), blogID);
-    } else {
-      debug("Not adding blog id to the list of blogs for this Dropbox account");
-    }
-    
-    ensure(account, Model, true);
-
-    debug("Saving this account");
-    multi.hmset(accountKey(blogID), account);
-    multi.exec(callback);
-  });
-}
-
-function drop(blogID, callback) {
-  get(blogID, function(err, account) {
-    var multi = redis.multi();
-
-    // Deregister this blog from the set containing
-    // the blog IDs associated with a particular dropbox
-    // account. If we SREM the last item from a set, redis also
-    // deletes the set. So don't worry about an additional
-    // operation to delete this set.
-    if (account && account.account_id)
-      multi.srem(blogsKey(account.account_id), blogID);
-
-    // Remove all the dangerous Dropbox account information
-    // including the OAUTH token used to interact with
-    // Dropbox's API. As I understand it, this is all we
-    // need to remove. We should also encourage the user
-    // to revoke the token on
-    multi.del(accountKey(blogID));
-    multi.exec(callback);
-  });
-}
-
-
-// JSON which stores useful information
-// information about this particular blog & dropbox account
-// combination, e.g. root directory and access token.
-function accountKey(blogID) {
-  return "blog:" + blogID + ":dropbox:account";
-}
-
-// A set whoses members are the blog ids
-// connected to this dropbox account.
-function blogsKey(account_id) {
-  return "clients:dropbox:" + account_id;
-}
-
 
 database = {
   set: set,
