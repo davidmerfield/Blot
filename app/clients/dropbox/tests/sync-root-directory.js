b60f998f45aa20dd323f2a28c12bad76bf85de80
David Merfield <dmerfield@gmail.com>
2018-10-05 16:33:28 -0400

    Added logging

--- app/clients/dropbox/tests/sync-root-directory.js
+++ app/clients/dropbox/tests/sync-root-directory.js
@@ -6,27 +6,35 @@ describe("dropbox client", function() {
   var write = require("../write");
   var sync = require("../sync");
 
-  it(
-    "syncs changes to the root folder of a Dropbox",
-    function(done) {
-      var blogDirectory = this.blogDirectory;
-      var blog = this.blog;
+  it("syncs changes to the root folder of a Dropbox", function(done) {
+    var blogDirectory = this.blogDirectory;
+    var blog = this.blog;
 
-      var path = this.fake.path(".txt");
-      var contents = this.fake.file();
+    var path = this.fake.path(".txt");
+    var contents = this.fake.file();
+    var contentsInBlogDirectory;
 
-      write(blog.id, path, contents, function(err) {
+    write(blog.id, path, contents, function(err) {
+      if (err) return done.fail(err);
+
+      sync(blog, function(err) {
         if (err) return done.fail(err);
 
-        sync(blog, function(err) {
-          if (err) return done.fail(err);
+        try {
+          contentsInBlogDirectory = fs
+            .readFileSync(blogDirectory + path)
+            .toString("utf-8");
+        } catch (e) {
+          var message = [
+            "Error " + e.message + " " + e.code,
+            "Contents of blog folder: " + fs.readdirSync(blogDirectory)
+          ].join("\n");
+          return done.fail(new Error(message));
+        }
 
-          expect(
-            fs.readFileSync(blogDirectory + path).toString("utf-8")
-          ).toEqual(contents);
-
-          done();
-        });
+        expect(contentsInBlogDirectory).toEqual(contents);
+        done();
       });
     });
+  });
 });
