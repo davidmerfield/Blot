8df1cdadd53884964755da31bc0ac0607ff9e542
David Merfield <dmerfield@gmail.com>
2018-10-08 19:05:38 -0400

    First test

--- app/models/template/tests/index.js
+++ app/models/template/tests/index.js
@@ -1,1 +1,13 @@
-var Template = require('../index');
+var Template = require("../index");
+
+describe("template", function() {
+  require("./setup")();
+
+  it("creates a template", function(done) {
+    Template.create(this.blog.id, this.fake.random.word(), {}, function(err) {
+      if (err) return done.fail(err);
+
+      done();
+    });
+  });
+});
---
+++ app/models/template/tests/setup/index.js
@@ -0,0 +1,11 @@
+module.exports = function setup(options) {
+  options = options || {};
+
+  // Create test blog before each test and remove it after
+  global.test.blog();
+
+  // Expose methods for creating fake files, paths, etc.
+  beforeEach(function() {
+    this.fake = global.test.fake;
+  });
+};
