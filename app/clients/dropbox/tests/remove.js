4407f8dd6e4e8a80fb181bed815dee6d3d742a08
David Merfield <dmerfield@gmail.com>
2018-10-09 14:38:39 -0400

    Refactored write and remove

--- app/clients/dropbox/tests/remove.js
+++ app/clients/dropbox/tests/remove.js
@@ -6,33 +6,61 @@ describe("dropbox client", function() {
   var write = require("../write");
   var remove = require("../remove");
 
-  it("removes a file", function(done) {
-    var blogDirectory = this.blogDirectory;
-    var path = this.fake.path(".txt");
-    var contents = this.fake.file();
-    var folder = this.folder;
-    var client = this.client;
+  beforeEach(function() {
+    this.path = this.fake.path(".txt");
+  });
+
+  afterEach(function(done) {
+    expect(fs.existsSync(this.blogDirectory + this.path)).toEqual(false);
+    this.client
+      .filesGetMetadata({
+        path: this.folder + this.path,
+        include_deleted: true
+      })
+      .then(function(res) {
+        // Verify file no longer exists on Dropbox
+        expect(res[".tag"]).toEqual("deleted");
+        // Verify the file no longer exists in blog folder
+        done();
+      })
+      .catch(function(err) {
+        if (err && err.status === 409) return done();
+        done(new Error(err));
+      });
+  });
+
+  it("does not return an error when you remove a non-existent file", function(done) {
+    remove(this.blog.id, this.path, done);
+  });
+
+  it("does not return an error when you remove a file which exists on Dropbox but not on Blot", function(done) {
+    var path = this.path;
     var blog = this.blog;
+    var blogDirectory = this.blogDirectory;
+    var contents = this.fake.file();
 
     write(blog.id, path, contents, function(err) {
       if (err) return done.fail(err);
 
-      remove(blog.id, path, function(err) {
-        if (err) return done.fail(err);
+      fs.removeSync(blogDirectory + path);
+      remove(blog.id, path, done);
+    });
+  });
 
-        client
-          .filesGetMetadata({ path: folder + path, include_deleted: true })
-          .then(function(res) {
-            // Verify file no longer exists on Dropbox
-            expect(res[".tag"]).toEqual("deleted");
-            // Verify the file no longer exists in blog folder
-            expect(fs.existsSync(blogDirectory + path)).toEqual(false);
-            done();
-          })
-          .catch(function(err) {
-            done.fail(err);
-          });
-      });
+  it("does not return an error when you remove a file which exists on Blot but not on Dropbox", function(done) {
+    fs.outputFileSync(this.blogDirectory + this.path, this.fake.file());
+    remove(this.blog.id, this.path, done);
+  });
+
+  it("removes a file", function(done) {
+    var contents = this.fake.file();
+    var blog = this.blog;
+    var path = this.path;
+
+    write(blog.id, path, contents, function(err) {
+      if (err) return done.fail(err);
+
+      remove(blog.id, path, done);
     });
   });
 });
