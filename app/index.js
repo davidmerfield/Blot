c01a97e3dbca75d940dc147f657a91cc1c4bfe69
David Merfield <dmerfield@gmail.com>
2018-10-12 17:44:51 -0400

    Tidied up app index file

--- app/index.js
+++ app/index.js
@@ -1,35 +1,24 @@
-var root = require('helper').rootDir;
-var fs = require('fs-extra');
+var root = require("helper").rootDir;
+var fs = require("fs-extra");
+var config = require("config");
+var scheduler = require("./scheduler");
+var express = require("express");
+var compression = require("compression");
+var vhost = require("vhost");
+var helmet = require("helmet");
+var session = require("express-session");
+var redis = require("redis").createClient();
+var Store = require("connect-redis")(session);
+var dashboard = require("./dashboard");
+var site = require("./site");
+var blog = require("./blog");
 
-// Create empty directories if they don't exist
-fs.ensureDirSync(root + '/blogs');
-fs.ensureDirSync(root + '/tmp');
-fs.ensureDirSync(root + '/logs');
-fs.ensureDirSync(root + '/db');
-fs.ensureDirSync(root + '/static');
-
-var config = require('config');
-var scheduler = require('./scheduler');
-var express = require('express');
-var compression = require('compression');
-var vhost = require('vhost');
-var helmet = require('helmet');
-var session = require('express-session');
-var redis = require('redis').createClient();
-var Store = require('connect-redis')(session);
-
-var dashboard = require('./dashboard');
-var site = require('./site');
-var blog = require('./blog');
-
-// All together now
 var server = express();
 
 // Session settings
 // It is important that session
 // comes before the cache so we
 // know what to serve to which user
-
 var sessionOptions = {
   secret: config.session.secret,
   saveUninitialized: false,
@@ -37,7 +26,7 @@ var sessionOptions = {
   proxy: true,
   cookie: {
     httpOnly: true,
-    secure: config.environment !== 'development'
+    secure: config.environment !== "development"
   },
   store: new Store({
     client: redis,
@@ -53,32 +42,25 @@ var sessionOptions = {
 // Create directive at /crossdomain.xml
 // which prevents flash from doing shit
 // Rendering middleware
-
-var todayKey = 'analytics:today';
-var client = require('client');
-
-console.log('WARNING RENABLE CROSS DOMAINS (helmet)');
+var todayKey = "analytics:today";
+var client = require("client");
 
 server
-  .disable('x-powered-by')
+  .disable("x-powered-by")
   .use(compression())
-  .set('trust proxy', 'loopback')
+  .set("trust proxy", "loopback")
   .use(helmet.ieNoOpen())
   .use(helmet.noSniff())
-  .use(helmet.frameguard('allow-from', config.host))
-  // .use(helmet.crossdomain())
-  .use(function (req, res, next) {
-
+  .use(helmet.frameguard("allow-from", config.host))
+  .use(function(req, res, next) {
     next();
 
-    return client.incr(todayKey, function (err) {
-
+    return client.incr(todayKey, function(err) {
       if (err) console.log(err);
-
     });
   })
   .use(function(req, res, next) {
-    res.setHeader('Cache-Hit', 'false')
+    res.setHeader("Cache-Hit", "false");
     next();
   })
   .use(vhost(config.host, session(sessionOptions)))
@@ -91,24 +73,26 @@ server
   // Don't remove it unless you change monit.rc
   // It needs to be here because VHOSTS prevent
   // it from working under the sites app
-  .use('/health', function(req, res, next){
-    if (req.hostname === 'localhost') res.send('OK');
+  .use("/health", function(req, res, next) {
+    if (req.hostname === "localhost") res.send("OK");
     return next();
   })
 
   // Serve the blogs!
   .use(blog);
 
-
-
-// Create an HTTP service.
-server.listen(config.port);
-
-console.log('Server started! Now listening for requests on port ' + config.port);
-
 // Unleash the daemon for backups, syncs and emails
 scheduler();
 
-// Build template files and watch for changes
-// if (config.environment === 'development')
-//   require('../scripts/build');
+// Create empty directories if they don't exist
+fs.ensureDirSync(root + "/blogs");
+fs.ensureDirSync(root + "/tmp");
+fs.ensureDirSync(root + "/logs");
+fs.ensureDirSync(root + "/db");
+fs.ensureDirSync(root + "/static");
+
+// Create an HTTP service.
+server.listen(config.port);
+console.log("Blot server is listening on port " + config.port);
+console.log("WARNING RENABLE CROSS DOMAINS (helmet)");
+// .use(helmet.crossdomain())
