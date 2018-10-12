3c521a5a0765f447567d3c6897c063b6434a362d
David Merfield <dmerfield@gmail.com>
2018-10-12 12:00:31 -0400

    Fixed reference to build in rebuild

--- app/sync/update/rebuildDependents.js
+++ app/sync/update/rebuildDependents.js
@@ -1,7 +1,8 @@
-var async = require('async');
-var Entry = require('entry');
-var client = require('client');
-var Blog = require('blog');
+var async = require("async");
+var build = require("../../build");
+var Entry = require("entry");
+var client = require("client");
+var Blog = require("blog");
 
 var dependentsKey = Entry.key.dependents;
 
@@ -10,32 +11,38 @@ var dependentsKey = Entry.key.dependents;
 // on the contents of this particular file which was
 // just changed or removed.
 
-module.exports = function (blogID, path, callback) {
-
-  Blog.get({id: blogID}, function(err, blog){
-
-    client.SMEMBERS(dependentsKey(blogID, path), function(err, dependent_paths){
-
+module.exports = function(blogID, path, callback) {
+  Blog.get({ id: blogID }, function(err, blog) {
+    client.SMEMBERS(dependentsKey(blogID, path), function(
+      err,
+      dependent_paths
+    ) {
       if (err) return callback(err);
-      
-      async.eachSeries(dependent_paths, function(dependent_path, next){
 
-        Entry.build(blog, dependent_path, function(err, updated_dependent){
+      async.eachSeries(
+        dependent_paths,
+        function(dependent_path, next) {
+          build(blog, dependent_path, function(err, updated_dependent) {
+            if (err) {
+              console.log("Error rebuilding dependents:", path, err);
+              return next();
+            }
 
-          if (err) {
-            console.log('Error rebuilding dependents:', path, err);
-            return next();
-          }
+            Entry.set(
+              blogID,
+              dependent_path,
+              updated_dependent,
+              function(err) {
+                if (err) return callback(err);
 
-          Entry.set(blogID, dependent_path, updated_dependent, function(err){
-
-            if (err) return callback(err);
-
-            next();
-
-          }, false);
-        });
-      }, callback);
+                next();
+              },
+              false
+            );
+          });
+        },
+        callback
+      );
     });
   });
 };
