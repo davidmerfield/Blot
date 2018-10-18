c56d3c1979170059a06618d1db4e74611f81a623
David Merfield <dmerfield@gmail.com>
2018-10-18 18:44:04 -0400

    Updated local client

--- app/clients/local/controllers/write.js
+++ app/clients/local/controllers/write.js
@@ -1,7 +1,10 @@
 module.exports = function write(blogID, path, contents, callback) {
-  require("fs-extra").outputFile(
-    require("helper").localPath(blogID, path),
-    contents,
-    callback
-  );
+  require("../models/folder").get(blogID, function(err, folder) {
+    if (err) return callback(err);
+    require("fs-extra").outputFile(
+      require("path").join(folder, path),
+      contents,
+      callback
+    );
+  });
 };
