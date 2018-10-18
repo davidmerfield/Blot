c1edfd9d3a42c9d1b7d37d844985bcc43bd0d062
David Merfield <dmerfield@gmail.com>
2018-10-18 16:48:22 -0400

    Updated render tests

--- app/blog/render/tests/index.js
+++ app/blog/render/tests/index.js
@@ -4,7 +4,7 @@ describe("render", function() {
   it("renders a view", function(done) {
     var ctx = this;
 
-    ctx.setView({ name: "index", content: "{{blog.title}}" }, function(err) {
+    ctx.setView({ name: "index", content: "{{title}}" }, function(err) {
       if (err) return done.fail(err);
 
       ctx.server.get("/", function(req, res) {
