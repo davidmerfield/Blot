e6c72b77ac5275fb7c18430b17f356b5053cb053
David Merfield <dmerfield@gmail.com>
2018-10-18 19:06:13 -0400

    Updated write test

--- app/models/template/tests/write.js
+++ app/models/template/tests/write.js
@@ -5,18 +5,6 @@ describe("template", function() {
 
   require("./setup")({ createTemplate: true });
 
-  // Sets up a temporary tmp folder and cleans it up after
-  global.test.tmp();
-
-  beforeEach(function(done) {
-    var test = this;
-    require("blog").set(test.blog.id, { client: "local" }, function(err) {
-      if (err) return done(err);
-      console.log("setting up test folder for", test.tmp);
-      require("clients").local.setup(test.blog.id, test.tmp, done);
-    });
-  });
-
   it("writes a template to a folder", function(done) {
     var test = this;
     var view = {
@@ -29,13 +17,13 @@ describe("template", function() {
 
       write(test.blog.id, test.template.id, function(err) {
         if (err) return done.fail(err);
-        expect(fs.readdirSync(test.tmp)).toEqual(["Templates"]);
-        expect(fs.readdirSync(test.tmp + "/Templates")).toEqual([
+        expect(fs.readdirSync(test.blogDirectory)).toEqual(["Templates"]);
+        expect(fs.readdirSync(test.blogDirectory + "/Templates")).toEqual([
           test.template.name
         ]);
         expect(
-          fs.readdirSync(test.tmp + "/Templates/" + test.template.name)
-        ).toEqual([view.name + '.html']);
+          fs.readdirSync(test.blogDirectory + "/Templates/" + test.template.name)
+        ).toEqual([view.name + ".html"]);
         done();
       });
     });
