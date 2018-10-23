5b015dbf84477e96d8a92537d085937ab6668f57
David Merfield <dmerfield@gmail.com>
2018-10-23 14:25:57 -0400

    Updated script to iterate each blog

--- scripts/each/blog.js
+++ scripts/each/blog.js
@@ -1,34 +1,22 @@
-var User = require('../../app/models/user');
-var Blog = require('../../app/models/blog');
-var helper = require('../../app/helper');
-var ForEach = helper.forEach;
+var User = require("../../app/models/user");
+var Blog = require("../../app/models/blog");
+var helper = require("../../app/helper");
+var async = require("async");
 var type = helper.type;
-var ensure = helper.ensure;
 
-module.exports = function (doThis, allDone, options) {
-
+module.exports = function(doThis, allDone, options) {
   options = options || {};
 
-  ensure(doThis, 'function')
-    .and(allDone, 'function')
-    .and(options, 'object');
-
-  Blog.getAllIDs(function(err, blogIDs){
-
-    if (err || !blogIDs) throw err || 'No';
-
-    console.log();
+  Blog.getAllIDs(function(err, blogIDs) {
+    if (err || !blogIDs) throw err || "No";
 
     if (options.s) {
-
       options.s = parseInt(options.s);
-      console.log('Starting this script with blog which has ID', options.s);
+      console.log("Starting this script with blog which has ID", options.s);
       blogIDs = blogIDs.slice(options.s - 1);
-
     }
 
     if (options.e) {
-
       var end = parseInt(options.e);
       var start = 0;
 
@@ -39,55 +27,54 @@ module.exports = function (doThis, allDone, options) {
       }
 
       if (end) {
-        console.log('Ending this script at user with ID', options.e);
+        console.log("Ending this script at user with ID", options.e);
         blogIDs = blogIDs.slice(0, end);
       } else {
-        console.log('Warning: The end option was less than the start...');
+        console.log("Warning: The end option was less than the start...");
       }
     }
 
     if (options.o) {
-
-
-      if (type(options.o, 'array')) {
-        blogIDs = options.o.map(function(id){return id + '';});
+      if (type(options.o, "array")) {
+        blogIDs = options.o.map(function(id) {
+          return id + "";
+        });
       } else {
-        blogIDs = [options.o + ''];
+        blogIDs = [options.o + ""];
       }
 
-      console.log('Starting this for blogs with ID', blogIDs.length > 1 ? blogIDs : blogIDs[0]);
-
+      console.log(
+        "Starting this for blogs with ID",
+        blogIDs.length > 1 ? blogIDs : blogIDs[0]
+      );
     }
 
-    console.log();
-    console.log();
-
-    var forEach = ForEach;
+    var forEach = async.eachSeries;
 
     if (options.p) {
-      forEach = forEach.parallel;
+      forEach = async.each;
     }
 
-    if (options.m && type(options.m, 'number')) {
-      forEach = forEach.multi(options.m);
+    if (options.m && type(options.m, "number")) {
+      throw new Error('M i no longer supported');
     }
 
-    forEach(blogIDs, function(blogID, nextBlog){
+    forEach(
+      blogIDs,
+      function(blogID, nextBlog) {
+        Blog.get({ id: blogID }, function(err, blog) {
+          if (err || !blog) {
+            return nextBlog();
+          }
 
-      Blog.get({id: blogID}, function(err, blog){
+          User.getById(blog.owner, function(err, user) {
+            if (err || !user) throw err || "No user with uid " + blog.owner;
 
-        if (err || !blog) {
-          console.log('There is no blog with ID', blogID);
-          return nextBlog();
-        }
-
-        User.getById(blog.owner, function(err, user){
-
-          if (err || !user) throw err || 'No user with uid ' + blog.owner;
-
-          doThis(user, blog, nextBlog);
+            doThis(user, blog, nextBlog);
+          });
         });
-      });
-    }, allDone);
+      },
+      allDone
+    );
   });
 };
