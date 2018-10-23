1971bca0b6134e3f2e7536c5ef217550a79f4cee
David Merfield <dmerfield@gmail.com>
2018-10-23 14:36:36 -0400

    Added debugging to list

--- app/models/template/list.js
+++ app/models/template/list.js
@@ -2,6 +2,7 @@ var key = require("./key");
 var client = require("client");
 var get = require("./get");
 var async = require("async");
+var debug = require("debug")("template:list");
 
 // The list of possible template choices
 // for a given blog. Accepts a UID and
@@ -17,6 +18,8 @@ module.exports = function list(blogID, callback) {
     if (err) return callback(err);
 
     templateIDs = templateIDs[0].concat(templateIDs[1]);
+
+    debug(blogID, templateIDs);
 
     async.map(templateIDs, get, callback);
   });
