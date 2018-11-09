978c6f20ef32a66951a8b7140cecc0f3b49cb3a0
David Merfield <dmerfield@gmail.com>
2018-11-09 11:43:49 -0500

    Added script to reset dropbox sync

---
+++ scripts/dropbox/reset_cursor_and_sync.js
@@ -0,0 +1,20 @@
+var DropboxSync = require("../../app/clients/dropbox/sync");
+var DropboxDatabase = require("../../app/clients/dropbox/database");
+var get = require("../blog/get");
+
+get(process.argv[2], function(user, blog) {
+  console.log(
+    "Warning, this uses internal functions of Dropbox client. Syncing blog...",
+    blog.handle
+  );
+
+  DropboxDatabase.set(blog.id, { cursor: "" }, function(err) {
+    if (err) throw err;
+
+    DropboxSync(blog, function(err) {
+      if (err) throw err;
+
+      console.log("Synced blog!");
+    });
+  });
+});
