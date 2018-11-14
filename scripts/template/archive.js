3e61d297e76f0d40c18f0a3699b385a5ba765bbe
David Merfield <dmerfield@gmail.com>
2018-11-13 19:03:09 -0500

    Added comments to script to archive an existing template

--- scripts/template/archive.js
+++ scripts/template/archive.js
@@ -4,6 +4,12 @@ var Blog = require("../../app/models/blog");
 var fs = require("fs-extra");
 var templateDir = require("path").resolve(__dirname + "/../../app/templates");
 
+// This script is used to archive one of Blot's included templates
+// It will create a copy of the template for each blog on which it
+// is currently used (preserving the existing blog as-is) and then
+// drop the template from the list of available templates for new
+// users, or for existing users who were not using the template.
+
 if (require.main === module) {
   main(process.argv[2], function(err) {
     if (err) throw err;
@@ -11,15 +17,20 @@ if (require.main === module) {
     process.exit();
   });
 }
+
 function main(name, callback) {
   var templateID = "SITE:" + name;
+
   if (!fs.statSync(templateDir + "/" + name).isDirectory())
-    return callback(new Error("Could not find template " + name));
+    return callback(new Error("Could not find global template " + name));
 
   Template.getMetadata(templateID, function(err, template) {
     if (err) return callback(err);
+
     if (!template)
-      return callback(new Error("There is no template " + templateID));
+      return callback(
+        new Error("There is no template in the database with id: " + templateID)
+      );
 
     eachBlog(
       function(user, blog, next) {
@@ -39,6 +50,9 @@ function main(name, callback) {
           function(err) {
             if (err) return next(err);
 
+            // We rename the template to include the case-preserved
+            // original name. If we do this initially, there is a strange
+            // duplicate template bug for unknown reasons.
             Template.setMetadata(
               newTemplateID,
               { name: template.name },
