29100f5c3f4e07d28e387cb393a33f4eb8f9532c
David Merfield <dmerfield@gmail.com>
2018-11-19 15:36:51 -0500

    Added logging

--- app/clients/dropbox/routes/saveDropboxAccount.js
+++ app/clients/dropbox/routes/saveDropboxAccount.js
@@ -1,6 +1,15 @@
 var database = require("../database");
 module.exports = function saveDropboxAccount(req, res, next) {
+
+  console.log(
+    "Blog:",
+    req.blog.id,
+    "Saving new Dropbox account with full access:",
+    req.unsavedAccount.full_access
+  );
+
   database.set(req.blog.id, req.unsavedAccount, function(err) {
+    
     if (err) return next(err);
 
     res.message("/", "Set up Dropbox successfuly!");
