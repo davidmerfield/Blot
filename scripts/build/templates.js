0c512903cce4c0d33f5369c28b99664fa87b8842
David Merfield <dmerfield@gmail.com>
2018-11-29 19:46:08 -0500

    Initial commit

--- scripts/build/templates.js
+++ scripts/build/templates.js
@@ -1,163 +1,141 @@
-console.log("BUILDING TEMPLATES");
-
 var config = require("../../config");
-var fs = require("fs");
 var helper = require("../../app/helper");
+var Template = require("../../app/models/template");
+
+var fs = require("fs-extra");
 var _ = require("lodash");
-
 var path = require("path");
-
-var APPDIR = path.resolve(__dirname + "/../../app");
-
-var TEMPLATEDIR = APPDIR + "/templates/";
-
 var mime = require("mime");
-var Template = require("../../app/models/template.js");
-var owner = Template.siteOwner;
-var extend = helper.extend;
-var forEach = helper.forEach;
-var defaultDir = TEMPLATEDIR + "_/";
-var defaultInfo = fs.readFileSync(TEMPLATEDIR + "_/package.json", "utf-8");
-defaultInfo = JSON.parse(defaultInfo);
-
+var async = require("async");
 var watcher = require("watcher");
 
-build();
+var extend = helper.extend;
+var TEMPLATEDIR = path.resolve(__dirname + "/../../app/templates");
+var defaultDir = TEMPLATEDIR + "/_";
+var defaultInfo = fs.readJsonSync(TEMPLATEDIR + "/_/package.json");
 
-if (config.environment === "development") {
-  process.stdin.resume();
-  console.log("Watching public directory for changes");
-  watcher(TEMPLATEDIR, build);
+// Generate list of template names based on the names of
+// directories inside app/templates (e.g. ['console', 'default', ...])
+var templates = fs.readdirSync(TEMPLATEDIR).filter(function(name) {
+  return (
+    name[0] !== "." &&
+    name !== "_" &&
+    name.toLowerCase().indexOf("readme") === -1
+  );
+});
+
+// If invoked from the command line, build
+// every template and then wait for changes
+// if inside development mode. I might want
+// to export the function for building a
+// single template, I might not...
+if (require.main === module) {
+  async.each(templates, build, function(err) {
+    if (err) throw err;
+
+    require("../cache/empty")();
+    console.log("Built all templates successfully");
+
+    if (config.environment !== "development") return process.exit();
+
+    console.log("Watching public directory for changes...");
+    process.stdin.resume();
+    templates.forEach(watch);
+  });
 }
 
-function build() {
-  // Load templates from disk
-  fs.readdir(TEMPLATEDIR, function(err, templates) {
-    forEach(
-      templates,
-      function(templateName, nextTemplate) {
-        if (templateName.slice(0, 1) === ".") return nextTemplate();
-        if (templateName === "_") return nextTemplate();
-        if (templateName === "readme.txt") return nextTemplate();
-        if (templateName === "README.txt") return nextTemplate();
-        if (templateName === "README.md") return nextTemplate();
+// Breaking this up allows us to watch individual
+// template directories and only rebuilt specific
+// templates, rather than every single template.
+function watch(name) {
+  watcher(TEMPLATEDIR, function() {
+    build(name, function(err) {
+      if (err) console.error(err);
+      require("../cache/empty")();
+    });
+  });
+}
 
-        loadFromFolder(
-          TEMPLATEDIR + templateName + "/",
-          templateName,
-          owner,
-          function(err) {
-            if (err) {
-              console.log(templateName);
-              throw err;
-            }
+// Name e.g. 'default' or 'console'
+function build(name, callback) {
+  console.log("Building", name);
 
-            console.log("--", templateName, "BUILT FROM FOLDER");
-            nextTemplate();
+  var owner = "SITE";
+  var dir = TEMPLATEDIR + name + "/";
+  var info, views;
 
-            // console.log(stat);
+  try {
+    info = fs.readJsonSync(dir + "package.json");
+  } catch (e) {
+    return callback(new Error("Please create a package.json"));
+  }
+
+  extend(info).and(defaultInfo);
+  info.name = info.name || helper.capitalise(name);
+
+  views = _.cloneDeep(info.views);
+  delete info.views;
+
+  Template.update(owner, info.name, info, function(err) {
+    if (err) return callback(err);
+
+    var viewpaths = fs
+      .readdirSync(dir)
+      .map(function(n) {
+        return dir + "/" + n;
+      })
+      .concat(
+        fs.readdirSync(defaultDir).map(function(n) {
+          return defaultDir + "/" + n;
+        })
+      );
+
+    async.each(
+      viewpaths,
+      function(path, next) {
+        var viewFilename = require("path").basename(path);
+
+        if (viewFilename.slice(0, 1) === ".") return next();
+        if (viewFilename === "package.json") return next();
+
+        var viewName = viewFilename.slice(0, viewFilename.lastIndexOf("."));
+        var viewContent;
+
+        try {
+          viewContent = fs.readFileSync(dir + viewFilename, "utf-8");
+        } catch (err) {
+          if (err && err.code === "EISDIR") return next();
+          return next(err);
+        }
+
+        var view = {
+          name: viewName,
+          type: mime.lookup(viewFilename),
+          content: viewContent
+        };
+
+        if (view.name.slice(0, 1) === "_") {
+          view.name = view.name.slice(1);
+        }
+
+        if (views && views[view.name]) {
+          var newView = views[view.name];
+          extend(newView).and(view);
+          view = newView;
+        }
+
+        Template.setView(templateID, view, function onSet(err) {
+          if (err) {
+            view.content = err;
+            Template.setView(templateID, view, function() {});
+            console.log("her");
+            return callback(err);
           }
-        );
+
+          next();
+        });
       },
-      function() {
-        require("../cache/empty")();
-        console.log("ALL TEMPLATES BUILT");
-      }
+      callback
     );
   });
-
-  function loadFromFolder(dir, templateName, owner, callback) {
-    callback = helper.callOnce(callback);
-
-    var templateID = Template.makeID(owner, templateName);
-
-    Template.isOwner(owner, templateID, function(err, isOwner) {
-      if (err) return callback(err);
-
-      fs.readFile(dir + "package.json", "utf-8", function(err, info) {
-        if (err || !info)
-          return callback(
-            "Please create a package.json containing the template info"
-          );
-
-        info = JSON.parse(info);
-
-        extend(info).and(defaultInfo);
-
-        var views = _.cloneDeep(info.views);
-        delete info.views;
-
-        if (!info.name) {
-          return callback("Please specify the package name");
-        }
-
-        if (!isOwner) {
-          Template.create(owner, helper.capitalise(templateName), info, then);
-        } else {
-          Template.update(owner, helper.capitalise(templateName), info, then);
-        }
-
-        function then(err) {
-          if (err) return callback(err);
-
-          fs.readdir(dir, extractViews(dir));
-          fs.readdir(defaultDir, extractViews(defaultDir));
-
-          function extractViews(dir) {
-            return function(err, viewFiles) {
-              if (err) return callback(err);
-
-              var totalToSet = 0;
-
-              viewFiles.forEach(function(viewFilename) {
-                if (viewFilename.slice(0, 1) === ".") return;
-                if (viewFilename === "package.json") return;
-
-                totalToSet++;
-
-                fs.readFile(dir + viewFilename, "utf-8", function(
-                  err,
-                  viewContent
-                ) {
-                  if (err && err.code === "EISDIR") return;
-
-                  if (err) throw err;
-
-                  var viewName = viewFilename.slice(
-                    0,
-                    viewFilename.lastIndexOf(".")
-                  );
-
-                  var view = {
-                    name: viewName,
-                    type: mime.lookup(viewFilename),
-                    content: viewContent
-                  };
-
-                  if (view.name.slice(0, 1) === "_") {
-                    view.name = view.name.slice(1);
-                  }
-
-                  if (views && views[view.name]) {
-                    var newView = views[view.name];
-                    extend(newView).and(view);
-                    view = newView;
-                  }
-
-                  Template.setView(templateID, view, onSet);
-                });
-
-                function onSet(err) {
-                  if (err) return callback(err);
-
-                  if (!--totalToSet)
-                    callback(null, "Set template " + templateID);
-                }
-              });
-            };
-          }
-        }
-      });
-    });
-  }
 }
