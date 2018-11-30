befdc425bab4d4f978cd82b899f6add6be82a2f1
David Merfield <dmerfield@gmail.com>
2018-11-30 15:49:31 -0500

    Updated archive script

--- scripts/template/archive.js
+++ scripts/template/archive.js
@@ -22,7 +22,7 @@ function main(name, callback) {
   var templateID = "SITE:" + name;
 
   if (!fs.statSync(templateDir + "/" + name).isDirectory())
-    return callback(new Error("Could not find global template " + name));
+    console.warn("Warning: could not find existing global template directory at app/templates/" + name);
 
   Template.getMetadata(templateID, function(err, template) {
     if (err) return callback(err);
