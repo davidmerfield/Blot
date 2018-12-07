7839e883cefe9cdea48ad1328a67a7aeaf3ab8fd
David Merfield <dmerfield@gmail.com>
2018-12-07 13:05:57 -0500

    Updated script to resolve urs

--- app/blog/render/retrieve/absoluteURLs.js
+++ app/blog/render/retrieve/absoluteURLs.js
@@ -1,7 +1,24 @@
-var cheerio = require("cheerio");
+/* 
 
-// Used to resolve relative URLs
-// Useful for RSS feeds
+This function accepts some HTML and will resolve any relative
+URLs against the host for the particular request. This is only
+used in a site's RSS feed and should eventually be deprecated,
+probably, since it's expensive to do this at render time.
+
+For example, assuming the request is served from 'example.com'
+over HTTPS, this lambda will transform:
+<img src="/abc.png">    -->   <img src="https://example.com/abc.png">
+It checks anything with an 'src' or 'href' attribute.
+
+Use it like this:
+{{#absoluteURLs}}
+  {{{entry.html}}}
+{{/absoluteURLs}}
+
+*/
+
+var cheerio = require("cheerio");
+var debug = require("debug")("blot:render:absoluteURLs");
 
 function resolve($, base) {
   return function() {
@@ -21,21 +38,20 @@ function resolve($, base) {
   };
 }
 
-module.exports = function(req, callback) {
+module.exports = function absoluteURLs(req, callback) {
   return callback(null, function() {
     return function(text, render) {
       var $;
       var base = req.protocol + "://" + req.get("host");
+
       text = render(text);
 
       try {
         $ = cheerio.load(text);
-
         $("[href], [src]").each(resolve($, base));
-
         text = $.html();
       } catch (e) {
-        // do nothing
+        debug(e);
       }
 
       return text;
