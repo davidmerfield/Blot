05d9d784d32b13cbeda8d51372f1509f5bcfec05
David Merfield <dmerfield@gmail.com>
2018-12-19 15:36:41 -0500

    Made sections more general

--- app/site/routes/documentation.js
+++ app/site/routes/documentation.js
@@ -1,12 +1,59 @@
 var express = require('express');
 var Documentation = express.Router();
+
+Documentation.use(function(req, res, next){
+  res.locals.selected = {};
+  next();
+});
+
+Documentation.param('section', function(req, res, next, section){
+  res.locals.selected[section] = 'selected';
+  next();
+});
+
+Documentation.param('page', function(req, res, next, page){
+  res.locals.selected[page] = 'selected';
+  next();
+});
+
 Documentation.get('/', function(req, res, next) {
+
+  res.locals.selected.how = 'selected';
+
   res.locals.partials = {
-    sidebar: 'documentation/_sidebar',
-    header: 'documentation/_header',
-    footer: 'documentation/_footer',
+    sidebar: 'documentation/partials/sidebar',
+    header: 'documentation/partials/header',
+    footer: 'documentation/partials/footer',
     'documentation.css': 'documentation/documentation.css'
   };
+
   res.render('documentation/index');
 });
+
+Documentation.get('/:section', function(req, res, next){
+
+  res.locals.partials = {
+    sidebar: 'documentation/partials/sidebar',
+    header: 'documentation/partials/header',
+    footer: 'documentation/partials/footer',
+    yield: 'documentation/' + req.params.section,
+    'documentation.css': 'documentation/documentation.css'
+  };
+
+  res.render('documentation/partials/layout');
+})
+
+Documentation.get('/:section/:page', function(req, res, next){
+
+  res.locals.partials = {
+    sidebar: 'documentation/partials/sidebar',
+    header: 'documentation/partials/header',
+    footer: 'documentation/partials/footer',
+    yield: 'documentation/' + req.params.section + '/' + req.params.page,
+    'documentation.css': 'documentation/documentation.css'
+  };
+
+  res.render('documentation/partials/layout');
+});
+
 module.exports = Documentation;
