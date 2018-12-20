6591c1949fa9c976601eec2ce5841c6c81f821b3
David Merfield <dmerfield@gmail.com>
2018-12-20 10:32:59 -0500

    Trim leading a trailing whitespace in Wordpress importer

--- app/dashboard/routes/importer/helper/write.js
+++ app/dashboard/routes/importer/helper/write.js
@@ -5,7 +5,10 @@ module.exports = function (post, callback) {
   var atime, mtime;
 
   console.log('...', post.path, post.title);
-  
+    
+  // Remove leading and trailing whitespace
+  post.content = post.content.trim();
+
   fs.outputFile(post.path, post.content, function(err){
 
     if (err) return callback(err);
