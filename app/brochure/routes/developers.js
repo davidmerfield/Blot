a5a25926e77e394ff7ab2f5aa742cc1fc1147e10
David Merfield <dmerfield@gmail.com>
2019-01-24 19:43:45 -0500

    Improved developer site

--- app/brochure/routes/developers.js
+++ app/brochure/routes/developers.js
@@ -2,7 +2,19 @@ var Express = require("express");
 var developers = new Express.Router();
 
 developers.use(function(req, res, next){
+  res.locals.base = '/developers';
   res.locals.layout = 'developers/layout';
+  res.locals.selected = {};
+  next();
+});
+
+developers.param('section', function(req, res, next){
+  res.locals.selected[req.params.section] = 'selected';
+  next();
+});
+
+developers.param('subsection', function(req, res, next){
+  res.locals.selected[req.params.subsection] = 'selected';
   next();
 });
 
