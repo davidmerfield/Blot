526c813d11d2dff101dbd8e21eab837cb812413d
David Merfield <dmerfield@gmail.com>
2019-01-26 16:01:32 -0500

    Updated templates section

--- app/brochure/routes/templates.js
+++ app/brochure/routes/templates.js
@@ -1,6 +1,17 @@
 var Express = require("express");
 var templates = new Express.Router();
 
+templates.use(function(req, res, next) {
+  res.locals.layout = "templates/layout";
+  res.locals.selected = {};
+  next();
+});
+
+templates.param("template", function(req, res, next) {
+  res.locals.selected[req.params.template] = "selected";
+  next();
+});
+
 templates.get("/", function(req, res) {
   res.locals.title = "Blot – Templates";
   res.render("templates");
@@ -8,9 +19,7 @@ templates.get("/", function(req, res) {
 
 templates.get("/:template", function(req, res) {
   res.locals.title = "Blot – Templates";
-  res.locals.layout = 'templates/layout';
   res.render("templates/" + req.params.template);
 });
-
 
 module.exports = templates;
