942f5975b3b05d0ac8da93cb507126d16567675d
David Merfield <dmerfield@gmail.com>
2019-01-31 18:36:53 -0500

    Fixed a number of broken links on new site

--- app/brochure/routes/index.js
+++ app/brochure/routes/index.js
@@ -1,7 +1,7 @@
 var Express = require("express");
 var brochure = new Express.Router();
-var finder = require('finder');
-var tex = require('./tools/tex');
+var finder = require("finder");
+var tex = require("./tools/tex");
 
 // Renders the folders and text editors
 brochure.use(finder.middleware);
@@ -10,25 +10,22 @@ brochure.use(finder.middleware);
 brochure.use(tex);
 
 // Renders dates dynamically
-brochure.use(require('./tools/dates'));
+brochure.use(require("./tools/dates"));
 
-// Fixes basic typographic errors 
+// Fixes basic typographic errors
 // See typeset.js for more information
-brochure.use(require('./tools/typeset'));
+brochure.use(require("./tools/typeset"));
 
 // CSS required to render the windows
-brochure.get('/css/finder.css', function(req, res){
+brochure.get("/css/finder.css", function(req, res) {
   res.setHeader("Content-Type", "text/css");
   res.send(finder.css());
 });
-
-
 
 brochure.get("/about", function(req, res) {
   res.locals.title = "Blot – About";
   res.render("about");
 });
-
 
 brochure.get("/support", function(req, res) {
   res.locals.title = "Blot – Support";
@@ -42,18 +39,17 @@ brochure.get("/contact", function(req, res) {
 
 brochure.get("/terms", function(req, res) {
   res.locals.title = "Terms of use";
-  res.locals.layout = '/partials/layout-focussed.html';
+  res.locals.layout = "/partials/layout-focussed.html";
   res.render("terms");
 });
 
 brochure.get("/privacy", function(req, res) {
   res.locals.title = "Privacy policy";
-  res.locals.layout = '/partials/layout-focussed.html';
+  res.locals.layout = "/partials/layout-focussed.html";
   res.render("privacy");
 });
 
-brochure.get('/sitemap.xml', require('./sitemap'));
-
+brochure.get("/sitemap.xml", require("./sitemap"));
 
 brochure.use("/developers", require("./developers"));
 
@@ -67,41 +63,46 @@ brochure.use("/sign-up", require("./sign-up"));
 
 brochure.use("/log-in", require("./log-in"));
 
-brochure.use(function(req, res, next){
-  res.locals.base = '';
+brochure.use(function(req, res, next) {
+  res.locals.base = "";
   res.locals.selected = {};
   next();
 });
 
-brochure.param('section', function(req, res, next){
-  res.locals.selected[req.params.section] = 'selected';
+brochure.param("section", function(req, res, next) {
+  res.locals.selected[req.params.section] = "selected";
   next();
 });
 
-brochure.param('subsection', function(req, res, next){
-  res.locals.selected[req.params.subsection] = 'selected';
+brochure.param("subsection", function(req, res, next) {
+  res.locals.selected[req.params.subsection] = "selected";
   next();
 });
 
 brochure.get("/", function(req, res) {
   res.locals.title = "Blot – A blogging platform with no interface";
-  res.locals.selected.index = 'selected';
-  res.locals.featured = require('./featured');
+  res.locals.selected.index = "selected";
+  res.locals.featured = require("./featured");
+  res.locals.featured = res.locals.featured.map(function(site) {
+    if (site.template) site.templateLower = site.template.toLowerCase();
+    return site;
+  });
+
   res.render("index");
 });
 
-brochure.get('/:section', function(req, res){
+brochure.get("/:section", function(req, res) {
   res.locals.title = "Blot – " + req.params.section;
   res.render(req.params.section);
 });
 
-brochure.get('/:section/:subsection', function(req, res){
-  res.locals.title = "Blot – " + req.params.section + ' – ' + req.params.subsection;
-  res.render(req.params.section + '/' + req.params.subsection);
+brochure.get("/:section/:subsection", function(req, res) {
+  res.locals.title =
+    "Blot – " + req.params.section + " – " + req.params.subsection;
+  res.render(req.params.section + "/" + req.params.subsection);
 });
 
-
-brochure.use(function(err, req, res, next){
+brochure.use(function(err, req, res, next) {
   console.log(err);
   next();
 });
