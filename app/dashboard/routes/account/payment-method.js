8be99b9c882fd1a9dc9cab170f290191bb1e6005
David Merfield <dmerfield@gmail.com>
2019-02-01 13:00:18 -0500

    Updated comments

--- app/dashboard/routes/account/payment-method.js
+++ app/dashboard/routes/account/payment-method.js
@@ -6,18 +6,19 @@ var email = require("helper").email;
 var stripe = require("stripe")(config.stripe.secret);
 var User = require("user");
 
+// The purpose of this page is to allow the user to update the
+// card used by Stripe to charge their subscription fee
 PaymentMethod.route("/")
 
   .all(function(req, res, next) {
-    // User doesnt need to pay
-    if (req.user.isFreeForLife) return res.redirect("/account");
+    // User does not have a payment method on file, so redirect them
+    if (!req.user || !req.user.subscription || !req.user.subscription.plan)
+      return res.redirect("/account");
 
     return next();
   })
 
   .get(function(req, res) {
-    // If user is new or doesn't have a handle,
-    // let them choose one
     res.render("account/payment-method", {
       stripe_key: config.stripe.key,
       breadcrumb: "Edit payment method",
@@ -25,72 +26,85 @@ PaymentMethod.route("/")
     });
   })
 
-  // Takes a stripe token generated
-  // on the client and creates a charge
   .post(function(req, res, next) {
-    var user = req.user;
+    // Stripe generates a token for a payment method
+    // on the client. This token is passed to Blot
+    // through the update payment method form. We store
+    // this token to authenticate future charges.
+    if (!req.body.stripeToken) {
+      return next(new Error("No Stripe token"));
+    }
 
-    var stripeToken = req.body.stripeToken;
-
-    if (!stripeToken) return next(new Error("No Stripe token"));
-
+    // We now store the new card token against the existing
+    // subscription information for this customer
     stripe.customers.updateSubscription(
-      user.subscription.customer,
-      user.subscription.id,
-      { card: stripeToken, quantity: user.subscription.quantity },
+      req.user.subscription.customer,
+      req.user.subscription.id,
+      { card: req.body.stripeToken, quantity: req.user.subscription.quantity },
       function(err, subscription) {
         if (err) return next(err);
 
-        if (subscription) req.newSubscription = subscription;
+        if (subscription) req.latestSubscription = subscription;
 
         next();
       }
     );
   })
 
-  // Handle deleted customer edge case
+  // Sometimes, somehow, a customer is deleted on Stripe
+  // but the Blot account exists. Not sure exactly what
+  // causes this but it happened. This middleware will
+  // create a new subscription and customer, without
+  // charging the new payment information and then store
+  // it against this blot user.
   .post(function(err, req, res, next) {
+    // This is some other error, proceed.
     if (err.code !== "resource_missing") {
       return next(err);
     }
 
-    var card = req.body && req.body.stripeToken;
-    var email = req.user.email;
-    var plan = req.user.subscription.plan.id;
+    // Make sure we use the user's old suscription
+    // plan, in case Blot's price for new customers
+    // has changed since they signed up.
+    // Setting the quantity to zero will ensure the
+    // customer is not charged right now.
+    stripe.customers.create(
+      {
+        card: req.body.stripeToken,
+        email: req.user.email,
+        plan: req.user.subscription.plan.id,
+        quantity: 0,
+        description: "Blot subscription"
+      },
+      function(err, customer) {
+        if (err) return next(err);
 
-    if (!card) return next(new Error("No card"));
-    if (!plan) return next(new Error("No plan"));
+        // Now we ensure the new subscription has the correct
+        // quantity. We couldn't do this earlier because it
+        // would involve incorrectly charging the customer. If
+        // we set prorate to false, they won't be charged now.
+        stripe.customers.updateSubscription(
+          customer.subscription.customer,
+          customer.subscription.id,
+          { quantity: req.user.blogs.length || 1, prorate: false },
+          function(err, subscription) {
+            if (err) return next(err);
 
-    var info = {
-      card: card,
-      email: email,
-      plan: plan,
-      quantity: 0,
-      description: "Blot subscription"
-    };
+            if (subscription) req.latestSubscription = subscription;
 
-    stripe.customers.create(info, function(err, customer) {
-      if (err) return next(err);
-
-      stripe.customers.updateSubscription(
-        customer.subscription.customer,
-        customer.subscription.id,
-        { quantity: req.user.blogs.length || 1, prorate: false },
-        function(err, subscription) {
-          if (err) return next(err);
-
-          if (subscription) req.newSubscription = subscription;
-
-          next();
-        }
-      );
-    });
+            next();
+          }
+        );
+      }
+    );
   })
 
+  // This middleware will save a valid, updated
+  // subscription with the new payment method.
   .post(function(req, res, next) {
-    if (!req.newSubscription) return next(new Error("No subscription"));
+    if (!req.latestSubscription) return next(new Error("No subscription"));
 
-    User.set(req.user.uid, { subscription: req.newSubscription }, function(
+    User.set(req.user.uid, { subscription: req.latestSubscription }, function(
       err
     ) {
       if (err) return next(err);
