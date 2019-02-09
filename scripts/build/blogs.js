d1ea24074fb1d79e5eab211a4ca09b37565aa0aa
David Merfield <dmerfield@gmail.com>
2019-02-09 18:10:44 -0500

    Added script to build test blogs

---
+++ scripts/build/blogs.js
@@ -0,0 +1,105 @@
+var fs = require("fs-extra");
+var async = require("async");
+var config = require("../../config");
+var foldersDirectory = __dirname + "/../../app/folders";
+var User = require("../../app/models/user");
+var Blog = require("../../app/models/blog");
+var Sync = require("../../app/sync");
+
+var folders;
+
+// E.g. ['bjorn'] from the folders in /app/folders
+folders = fs.readdirSync(foldersDirectory).filter(function(folder) {
+  return fs.statSync(foldersDirectory + "/" + folder).isDirectory();
+});
+
+setupUser(function(err, user) {
+  if (err) throw err;
+
+  setupBlogs(user, folders, function(err, blogs) {
+    if (err) throw err;
+
+    async.eachSeries(
+      blogs,
+      function(blog, next) {
+        buildAllFilesInBlogFolder(foldersDirectory, blog, next);
+      },
+      function() {
+        console.log("BUILT ALL TEST BLOGS!");
+      }
+    );
+  });
+});
+
+function setupUser(callback) {
+  // Create user and blogs for each demo folder
+  User.create(config.admin.email, "", {}, function(err) {
+    if (err && err.code !== "EEXISTS") return callback(err);
+
+    User.getByEmail(config.admin.email, callback);
+  });
+}
+
+function setupBlogs(user, handles, callback) {
+  async.map(
+    handles,
+    function(handle, next) {
+      Blog.create(user.uid, { handle: handle }, function(err, blog) {
+        if (!err && blog) return next(err, blog);
+
+        if (err && err.handle && err.handle.code !== "EEXISTS")
+          return next(err);
+
+        Blog.get({ handle: handle }, function(err, blog) {
+          if (err) return next(err);
+
+          if (blog.owner === user.uid) return next(err, blog);
+
+          next(
+            new Error(
+              "Blog: ;" +
+                handle +
+                " already exists and it is not owned by " +
+                user.email
+            )
+          );
+        });
+      });
+    },
+    callback
+  );
+}
+
+function walk(dir) {
+  var results = [];
+  var list = fs.readdirSync(dir);
+  list.forEach(function(file) {
+    file = dir + "/" + file;
+    var stat = fs.statSync(file);
+    if (stat && stat.isDirectory()) {
+      results = results.concat(walk(file));
+    } else {
+      results.push(file);
+    }
+  });
+  return results;
+}
+
+function buildAllFilesInBlogFolder(foldersDirectory, blog, callback) {
+  Sync(blog.id, {}, function(err, folder, done) {
+    if (err) return callback(err);
+
+    fs.emptyDirSync(folder.path);
+    fs.copySync(foldersDirectory + "/" + blog.handle, folder.path);
+
+    async.eachSeries(
+      walk(folder.path).map(function(path) {
+        return path.slice(folder.path.length);
+      }),
+      folder.update,
+      function(err) {
+        done(err, callback);
+      }
+    );
+  });
+}
