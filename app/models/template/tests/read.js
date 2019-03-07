a0c08015f0baffd98314b15d912493cc2967c57f
David Merfield <dmerfield@gmail.com>
2019-03-07 18:58:06 -0500

    Improved tests for read

--- app/models/template/tests/read.js
+++ app/models/template/tests/read.js
@@ -1,6 +1,8 @@
-fdescribe("template", function() {
+describe("template", function() {
   var read = require("../read");
   var fs = require("fs-extra");
+  var getNameByUrl = require("../view/getNameByUrl");
+  var get = require("../view/get");
 
   require("./setup")({ createTemplate: true });
 
@@ -11,19 +13,30 @@ fdescribe("template", function() {
     read(this.blog.id, this.tmp, done);
   });
 
-  fit("reads a template from a folder", function(done) {
+  it("reads a template from a folder", function(done) {
     fs.outputFileSync(this.tmp + "/style.css", "body {color:pink}");
 
     fs.outputJsonSync(this.tmp + "/package.json", {
       locals: { foo: "bar" },
-      views: { style: { url: "/style.css" } }
+      views: { style: { url: "/style.css", locals: { baz: "bat" } } }
     });
 
     read(this.blog.id, this.tmp, function(err, template) {
       if (err) return done.fail(err);
 
       expect(template.locals.foo).toEqual("bar");
-      done();
+
+      getNameByUrl(template.id, "/style.css", function(err, name) {
+        if (err) return done.fail(err);
+
+        expect(name).toEqual("style");
+
+        get(template.id, name, function(err, view) {
+          if (err) return done.fail(err);
+          expect(view.locals.baz).toEqual("bat");
+          done();
+        });
+      });
     });
   });
 });
