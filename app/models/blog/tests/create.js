df671dbd7734899b6036d95458906f4062546109
David Merfield <dmerfield@gmail.com>
2019-03-26 16:23:04 -0400

    Clean up test blogs

--- app/models/blog/tests/create.js
+++ app/models/blog/tests/create.js
@@ -2,12 +2,39 @@ describe("blog creates", function() {
   // Create a test user before each spec
   global.test.user();
 
+  // Clean up blogs created during tests
+  afterEach(function(done) {
+    if (this.blog) {
+      require("../remove")(this.blog.id, done);
+    } else {
+      done();
+    }
+  });
+
   var create = require("../create");
 
   it("creates a blog", function(done) {
-    create(this.user.uid, { handle: "example" }, function(err, user) {
-      expect(err).toEqual(null);
-      expect(user).toEqual(jasmine.any(Object));
+    var ctx = this;
+
+    create(ctx.user.uid, { handle: "example" }, function(err, blog) {
+      if (err) return done.fail(err);
+
+      ctx.blog = blog;
+
+      expect(blog).toEqual(jasmine.any(Object));
+      done();
+    });
+  });
+
+  it("adds created blog to list of all blogs", function(done) {
+    var ctx = this;
+
+    create(ctx.user.uid, { handle: "example" }, function(err, blog) {
+      if (err) return done.fail(err);
+
+      ctx.blog = blog;
+
+      expect(blog).toEqual(jasmine.any(Object));
       done();
     });
   });
