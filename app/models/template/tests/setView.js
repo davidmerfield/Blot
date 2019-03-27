9aeffea3263d617e1c9faa7a4fe6c5c072aa4e88
David Merfield <dmerfield@gmail.com>
2019-03-27 13:52:56 -0400

    Added more tests to setView

--- app/models/template/tests/setView.js
+++ app/models/template/tests/setView.js
@@ -21,4 +21,66 @@ describe("template", function() {
       });
     });
   });
+
+  it("sets changes to an existing view", function(done) {
+    var test = this;
+    var view = {
+      name: test.fake.random.word(),
+      content: test.fake.random.word()
+    };
+
+    setView(test.template.id, view, function(err) {
+      if (err) return done.fail(err);
+
+      getView(test.template.id, view.name, function(err, savedView) {
+        if (err) return done.fail(err);
+        expect(savedView.name).toEqual(view.name);
+        expect(savedView.content).toEqual(view.content);
+
+        view.content = test.fake.random.word();
+
+        setView(test.template.id, view, function(err) {
+          if (err) return done.fail(err);
+
+          getView(test.template.id, view.name, function(err, savedView) {
+            if (err) return done.fail(err);
+            expect(savedView.content).toEqual(view.content);
+            done();
+          });
+        });
+      });
+    });
+  });
+
+  it("won't set a view with invalid mustache content", function(done) {
+    var test = this;
+    var view = {
+      name: test.fake.random.word(),
+      content: "{{#x}}" // without the closing {{/x}} mustache will err.
+    };
+
+    setView(test.template.id, view, function(err) {
+      expect(err instanceof Error).toBe(true);
+      done();
+    });
+  });
+
+  it("won't set a view against a template that does not exist", function(done) {
+    var test = this;
+    var view = { name: test.fake.random.word() };
+
+    setView(test.fake.random.word(), view, function(err) {
+      expect(err instanceof Error).toBe(true);
+      done();
+    });
+  });
+
+  // In future this should return an error to the callback, lol
+  it("won't set a view with a name that is not a string", function() {
+    var test = this;
+
+    expect(function() {
+      setView(test.template.id, { name: null }, function() {});
+    }).toThrow();
+  });
 });
