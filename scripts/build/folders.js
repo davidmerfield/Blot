1e4533c5e48f5bab6c7844ec91e63c6623d8ad6e
David Merfield <dmerfield@gmail.com>
2019-03-27 17:42:29 -0400

    Added spec for script to build demo blogs

---
+++ scripts/build/folders.js
@@ -0,0 +1,10 @@
+// This script generates demonstration blogs from the 
+// folders inside app/folders. These blogs are useful
+// for showing templates and explaining how Blot works
+// in the docs. This script will create a blog for each
+// folder e.g. one with the username 'bjorn' for 
+// app/folders/bjorn
+
+// It will configure the blog to use a local client
+// pointing to the corresponding folders so that future
+// changes apply immediately on the server.
