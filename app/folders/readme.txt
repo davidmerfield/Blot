270772a94743a7c5728d0db50a7c28e0dded9fd6
David Merfield <dmerfield@gmail.com>
2019-03-27 17:45:38 -0400

    Updated notes

--- app/folders/readme.txt
+++ app/folders/readme.txt
@@ -4,3 +4,8 @@ IDEAS: ephemera: https://ticketsplz.tumblr.com/
 		 ben@shelfheroes.com 
 		 https://twitter.com/tkts_plz
 		 https://www.instagram.com/ticketsplz/
+
+You will eventually be able to build these folders into blogs using 
+the script:
+
+node scripts/build/folders
--- scripts/build/folders.js
+++ scripts/build/folders.js
@@ -8,3 +8,8 @@
 // It will configure the blog to use a local client
 // pointing to the corresponding folders so that future
 // changes apply immediately on the server.
+
+// How to store links in the menu? How to encode an avatar?
+// This will be useful food for thought when we allow people
+// to edit their blog's settings from the folder, e.g. 
+// a .blot directory containing config...
