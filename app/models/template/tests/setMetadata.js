c086a4650802329fb6a8361be44dd7a343994262
David Merfield <dmerfield@gmail.com>
2019-03-27 12:37:55 -0400

    Added tests for template update method

--- app/models/template/tests/setMetadata.js
+++ app/models/template/tests/setMetadata.js
@@ -4,26 +4,16 @@ describe("template", function() {
   var setMetadata = require("../index").setMetadata;
   var getMetadata = require("../index").getMetadata;
 
-  it("updates a template", function(done) {
+  it("sets a template's metadata", function(done) {
     var test = this;
     var updates = { description: test.fake.random.word() };
-    setMetadata(test.template.id, updates, function(err, changes) {
+    setMetadata(test.template.id, updates, function(err) {
       if (err) return done.fail(err);
       getMetadata(test.template.id, function(err, template) {
         if (err) return done.fail(err);
         expect(template.description).toEqual(updates.description);
         done();
       });
-    });
-  });
-
-  // Enable this once we have decided whether or not setMetadata
-  // should be used for new templates. This might break uses of the
-  // api internally so be carefull.
-  xit("throws an error if you update a template which does not exist", function(done) {
-    setMetadata(this.fake.random.word(), {}, function(err) {
-      expect(err instanceof Error).toEqual(true);
-      done();
     });
   });
 });
