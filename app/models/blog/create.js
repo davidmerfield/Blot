e902449f0cd522957ba4ed64ef40a1b0c832e8e6
David Merfield <dmerfield@gmail.com>
2019-03-27 15:49:53 -0400

    Lint all methods of Blog

--- app/models/blog/create.js
+++ app/models/blog/create.js
@@ -1,64 +1,58 @@
-var helper = require('helper');
+var helper = require("helper");
 var ensure = helper.ensure;
 var extend = helper.extend;
 var capitalise = helper.capitalise;
-var defaults = require('./defaults');
-var client = require('client');
-var key = require('./key');
-var set = require('./set');
+var defaults = require("./defaults");
+var client = require("client");
+var key = require("./key");
+var set = require("./set");
 var fs = require("fs-extra");
 var localPath = helper.localPath;
-var User = require('../user');
-var validate = require('./validate');
+var User = require("../user");
+var validate = require("./validate");
 
-module.exports = function create (uid, info, callback) {
-
-  ensure(uid, 'string')
-    .and(info, 'object')
-    .and(callback, 'function');
+module.exports = function create(uid, info, callback) {
+  ensure(uid, "string")
+    .and(info, "object")
+    .and(callback, "function");
 
   var blogs;
   var blog = {
     owner: uid,
-    title: info.title || capitalise((info.handle || 'untitled') + '\’s') + ' blog',
-    client: '',
-    timeZone: info.timeZone || 'UTC',
-    dateFormat: info.dateFormat || 'M/D/YYYY'
+    title:
+      info.title || capitalise((info.handle || "untitled") + "’s") + " blog",
+    client: "",
+    timeZone: info.timeZone || "UTC",
+    dateFormat: info.dateFormat || "M/D/YYYY"
   };
 
   extend(blog)
     .and(info)
     .and(defaults);
 
-  validate('', blog, function(errors){
-
+  validate("", blog, function(errors) {
     if (errors) return callback(errors);
 
-    User.getById(uid, function(err, user){
+    User.getById(uid, function(err, user) {
+      if (err || !user) return callback(err || new Error("No user"));
 
-      if (err || !user) return callback(err || new Error('No user'));
-
-      client.incr(key.totalBlogs, function(err, blogID){
-
+      client.incr(key.totalBlogs, function(err, blogID) {
         if (err) return callback(err);
 
         // Cast to a string from int
-        blogID += '';
+        blogID += "";
         blog.id = blogID;
-        
+
         blogs = user.blogs || [];
         blogs.push(blogID);
 
-        User.set(uid, {blogs: blogs, lastSession: blogID}, function(err){
-
+        User.set(uid, { blogs: blogs, lastSession: blogID }, function(err) {
           if (err) return callback(err);
 
-          set(blogID, blog, function(err){
-
+          set(blogID, blog, function(err) {
             if (err) return callback(err);
 
             fs.emptyDir(localPath(blog.id, "/"), function(err) {
-              
               if (err) return callback(err);
 
               return callback(err, blog);
--- app/models/blog/get.js
+++ app/models/blog/get.js
@@ -1,39 +1,32 @@
-var client = require('client');
-var helper = require('helper');
+var client = require("client");
+var helper = require("helper");
 var ensure = helper.ensure;
-var key = require('./key');
-var serial = require('./serial');
+var key = require("./key");
+var serial = require("./serial");
 
-module.exports = function get (by, callback) {
-
-  ensure(by, 'object')
-    .and(callback, 'function');
+module.exports = function get(by, callback) {
+  ensure(by, "object").and(callback, "function");
 
   if (by.id) {
-    ensure(by.id, 'string');
+    ensure(by.id, "string");
     then(null, by.id);
-
   } else if (by.domain) {
-    ensure(by.domain, 'string');
+    ensure(by.domain, "string");
     client.get(key.domain(by.domain), then);
-
   } else if (by.handle) {
-    ensure(by.handle, 'string');
+    ensure(by.handle, "string");
     client.get(key.handle(by.handle), then);
-
   } else {
     console.log(by);
-    throw 'Please specify a by property';
+    throw "Please specify a by property";
   }
 
-  function then (err, blogID) {
-
-    if (err) return callback (err);
+  function then(err, blogID) {
+    if (err) return callback(err);
 
     if (!blogID) return callback(null);
 
-    client.hgetall(key.info(blogID), function (err, blog) {
-
+    client.hgetall(key.info(blogID), function(err, blog) {
       if (err) return callback(err);
 
       if (!blog) return callback(null);
