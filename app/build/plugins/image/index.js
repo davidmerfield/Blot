2b29ee93f7697e4eea92077547aca51605b2da61
Paul Esch-Laurent <herppfel@gmail.com>
2019-04-01 15:12:19 -0400

    Implement cache bypassing with URL parameter

--- app/build/plugins/image/index.js
+++ app/build/plugins/image/index.js
@@ -2,6 +2,7 @@ var Transformer = require('helper').transformer;
 var debug = require('debug')('entry:build:plugins:image');
 var eachEl = require('../eachEl');
 var optimize = require('./optimize');
+var url = require('url');
 
 function render ($, callback, options) {
 
@@ -14,6 +15,13 @@ function render ($, callback, options) {
     var src = $(el).attr('src');
     var width, height;
 
+    // Test for query string to skip caching
+    var query = url.parse(src, { parseQueryString: true }).query;
+    if (query['no_cache']) {
+      debug(src, 'Image marked with \'?no_cache=1\', skipping');
+      return next();
+    }
+
     debug(src, 'checking cache');
 
     cache.lookup(src, optimize(blogID), function(err, info){
@@ -23,12 +31,12 @@ function render ($, callback, options) {
         return next();
       }
 
-      // Replace the image's source with the new 
+      // Replace the image's source with the new
       // source, which is a path to an image in the
       // static assets folder for this blog.
       $(el).attr('src', info.src);
 
-      // Now we will attempt to declare the width and 
+      // Now we will attempt to declare the width and
       // height of the image to speed up page loads...
       if ($(el).attr('width') || $(el).attr('height')) {
         debug(src, 'El has width or height pre-specified dont modify');
@@ -37,7 +45,7 @@ function render ($, callback, options) {
 
       width = info.width;
       height = info.height;
-   
+
       // This is a retina image so halve its dimensions
       if ($(el).attr('data-2x') || isRetina(src)) {
         debug(src, 'retinafying the dimensions');
