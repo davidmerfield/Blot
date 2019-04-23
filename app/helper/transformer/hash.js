7b224ed5468357d0859358027f64db92fb38b39a
David Merfield <dmerfield@gmail.com>
2019-04-23 18:27:09 -0400

    Updated hashing function

--- app/helper/transformer/hash.js
+++ app/helper/transformer/hash.js
@@ -1,11 +1,16 @@
 var fs = require("fs-extra");
 var crypto = require("crypto");
+var debug = require("debug")("blot:helper:transformer:hash");
 
 module.exports = function(path, callback) {
+  var hash;
+
   fs.createReadStream(path)
     .pipe(crypto.createHash("sha1").setEncoding("hex"))
     .on("error", callback)
     .on("finish", function() {
-      callback(null, this.read());
+      hash = this.read();
+      debug(path, "hashed to", hash);
+      callback(null, hash);
     });
 };
