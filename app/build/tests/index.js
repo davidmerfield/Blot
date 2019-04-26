bcdeaedd23f341ef3d6aed492bd384bf6250f463
David Merfield <dmerfield@gmail.com>
2019-04-26 12:38:53 -0400

    Streamlined tests

--- app/build/tests/index.js
+++ app/build/tests/index.js
@@ -20,10 +20,10 @@ describe("build", function() {
     });
   });
 
-  it("handles images with accents in their filename correctly", function(done) {
+  it("handles images with accents and spaces in their filename", function(done) {
     var path = "/blog/Hello world.txt";
-    var contents = "![Best Image Ever](óåg.jpg)";
-    var pathToImage = "/blog/óåg.jpg";
+    var contents = "![Best Image Ever](óå g.jpg)";
+    var pathToImage = "/blog/óå g.jpg";
 
     fs.outputFileSync(this.blogDirectory + path, contents);
     fs.copySync(__dirname + "/small.jpg", this.blogDirectory + pathToImage);
@@ -45,62 +45,4 @@ describe("build", function() {
     });
   });
 
-  it("handles images with spaces in their filename correctly", function(done) {
-    var path = "/blog/Hello world.txt";
-    var contents = "![Best Image Ever](_hey there.jpg)";
-    var pathToImage = "/blog/_hey there.jpg";
-
-    fs.outputFileSync(this.blogDirectory + path, contents);
-    fs.copySync(__dirname + "/small.jpg", this.blogDirectory + pathToImage);
-
-    build(this.blog, path, {}, function(err, entry) {
-      if (err) return done.fail(err);
-
-      // verify the image was cached
-      expect(entry.html).toContain("/_image_cache/");
-
-      // verify a thumbnail was generated from the image
-      expect(entry.thumbnail.small).toEqual(
-        jasmine.objectContaining({
-          name: "small.jpg"
-        })
-      );
-      done();
-    });
-  });
-
-  it("handles images with spaces and accents in their filename correctly", function(done) {
-    var test = this;
-    var path = "/blog/Hello world.txt";
-    var contents = "![Best Image Ever](_på be.jpg)";
-    var pathToImage = "/blog/_på be.jpg";
-
-    console.log('writing', this.blogDirectory + path);
-    fs.outputFileSync(this.blogDirectory + path, contents);
-    console.log('copying', __dirname + "/small.jpg", this.blogDirectory + pathToImage);
-    fs.copySync(__dirname + "/small.jpg", this.blogDirectory + pathToImage);
-
-    build(this.blog, path, {}, function(err, entry) {
-      if (err) return done.fail(err);
-
-      console.log('Entry.html:', entry.html);
-      console.log("Directory state:");
-      console.log(test.blogDirectory, fs.readdirSync(test.blogDirectory));
-      console.log(
-        test.blogDirectory + "/blog",
-        fs.readdirSync(test.blogDirectory + "/blog")
-      );
-
-      // verify the image was cached
-      expect(entry.html).toContain("/_image_cache/");
-
-      // verify a thumbnail was generated from the image
-      expect(entry.thumbnail.small).toEqual(
-        jasmine.objectContaining({
-          name: "small.jpg"
-        })
-      );
-      done();
-    });
-  });
 });
