d5e284b4706a17984053138c7f6b2b6190ddc656
David Merfield <dmerfield@gmail.com>
2019-04-27 12:54:58 -0400

    Refactored build tests

--- app/build/tests/index.js
+++ app/build/tests/index.js
@@ -2,18 +2,41 @@ describe("build", function() {
   var build = require("../index");
   var fs = require("fs-extra");
 
-  global.test.server(function(server) {
-    // Only server an image at this route if 
-    // the request passes the correct query
-    server.get("/a.jpg", function(req, res) {
-      if (!req.query || req.query.user !== "foo" || req.query.pass !== "bar")
-        return res.sendStatus(400);
+  global.test.blog();
 
-      res.sendFile(__dirname + "/small.jpg");
+  // Only serve a test image when query contains valid user and password
+  global.test.server(function(server) {
+    server.get("/small.jpg", function(req, res) {
+      if (req.query && req.query.user === "x" && req.query.pass === "y")
+        res.sendFile(__dirname + "/small.jpg");
+      else res.sendStatus(400);
     });
   });
 
-  global.test.blog();
+  it("handles image URLs with query strings", function(done) {
+    // The test server defined above will only respond with an image if
+    // the query string is preserved. I was running into an ampersand
+    // encoding issue where the & was replaced with &amp;
+    var contents = "<img src='" + this.origin + "/small.jpg?user=x&pass=y'/>";
+    var path = "/hello.txt";
+
+    fs.outputFileSync(this.blogDirectory + path, contents);
+
+    build(this.blog, path, {}, function(err, entry) {
+      if (err) return done.fail(err);
+
+      // verify the image was cached
+      expect(entry.html).toContain("/_image_cache/");
+
+      // verify a thumbnail was generated from the image
+      expect(entry.thumbnail.small).toEqual(
+        jasmine.objectContaining({
+          name: "small.jpg"
+        })
+      );
+      done();
+    });
+  });
 
   it("resolves relative paths inside files", function(done) {
     var path = "/blog/foo.txt";
@@ -27,25 +50,6 @@ describe("build", function() {
 
       // It won't be cached since the image doesn't exist
       expect(entry.html).toContain(absolutePathToImage);
-      done();
-    });
-  });
-
-  it("handles image URLs with query strings", function(done) {
-    var path = "/hello.txt";
-    var contents = "Thumbnail: " + this.origin + "/a.jpg?user=foo&pass=bar";
-
-    fs.outputFileSync(this.blogDirectory + path, contents);
-
-    build(this.blog, path, {}, function(err, entry) {
-      if (err) return done.fail(err);
-
-      // verify a thumbnail was generated from the image
-      expect(entry.thumbnail.small).toEqual(
-        jasmine.objectContaining({
-          name: "small.jpg"
-        })
-      );
       done();
     });
   });
