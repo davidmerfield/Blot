07ece03862ffcd4daa3df1037ff0c86eebb5a73a
David Merfield <dmerfield@gmail.com>
2019-04-29 13:23:03 -0400

    Added spec to test suite for new static query string

--- app/build/tests/index.js
+++ app/build/tests/index.js
@@ -11,6 +11,10 @@ describe("build", function() {
         res.sendFile(__dirname + "/small.jpg");
       else res.sendStatus(400);
     });
+
+    server.get("/public.jpg", function(req, res) {
+      res.sendFile(__dirname + "/small.jpg");
+    });
   });
 
   it("handles image URLs with query strings", function(done) {
@@ -69,6 +73,29 @@ describe("build", function() {
       expect(entry.html).toContain("/_image_cache/");
 
       // verify a thumbnail was generated from the image
+      expect(entry.thumbnail.small).toEqual(
+        jasmine.objectContaining({
+          name: "small.jpg"
+        })
+      );
+
+      done();
+    });
+  });
+
+  it("will not cache image an image using the static query string", function(done) {
+    var path = "/Hello world.txt";
+    var contents = "![Image](" + this.origin + "/public.jpg?static=1)";
+
+    fs.outputFileSync(this.blogDirectory + path, contents);
+
+    build(this.blog, path, {}, function(err, entry) {
+      if (err) return done.fail(err);
+
+      // verify the image was not cached
+      expect(entry.html).not.toContain("/_image_cache/");
+
+      // verify a thumbnail was still generated from the image
       expect(entry.thumbnail.small).toEqual(
         jasmine.objectContaining({
           name: "small.jpg"
