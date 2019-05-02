4aa311067acbd00ff7893f271350b1d3906abf24
David Merfield <dmerfield@gmail.com>
2019-05-02 13:43:57 -0400

    Updated script to set blog ID

--- scripts/blog/set-blog-id.js
+++ scripts/blog/set-blog-id.js
@@ -1,6 +1,6 @@
 // This script will edit the ID of a blog
 // Run this on my blogs in production first
-// before incrementally rolling it out to 
+// before incrementally rolling it out to
 // ensure all blogs have IDs which are guuids
 
 // Things to think about:
@@ -16,41 +16,54 @@
 // rename /blogs/{id} directory
 // rename /static/{id} file directory
 
-var oldBlogID = process.argv[2];
-var newBlogID = process.argv[3];
+// 'template:owned_by:' + blogID,
+// 'handle:' + blog.handle,
+// 'domain:' // also www version...
+// "blog:" + blogID + ":dropbox:account";
+
+// git client should just work
+
+// Redis set whoses members are the blog IDs
+// connected to this dropbox account.
+// function blogsKey(account_id) {
+//   return "clients:dropbox:" + account_id;
+// }
 
 var keys = require("../redis/keys");
 var async = require("async");
 var client = require("../../app/models/client");
-
 var User = require("../../app/models/user");
 var Blog = require("../../app/models/blog");
+var debug = require("debug")("blot:scripts:set-blog-id");
+var fs = require("fs-extra");
+var localPath = require("helper").localPath;
+var staticDirectory = require("config").blog_static_files_dir;
 
 if (require.main === module) {
-  main(process.argv[2], process.argv[3], function(err) {
+  var oldBlogID = process.argv[2];
+  var newBlogID = process.argv[3];
+
+  main(oldBlogID, newBlogID, function(err) {
     if (err) throw err;
+
     console.log("Done!");
   });
 }
 
 function main(oldBlogID, newBlogID, callback) {
-  Blog.get({ id: oldBlogID }, function(err, blog) {
+  debug("Switching blog with", oldBlogID, "to", newBlogID);
+
+  Blog.get({ id: oldBlogID }, function(err, oldBlog) {
     if (err) return callback(err);
 
-    var multi = client.multi();
+    if (!oldBlog) return callback(new Error("No blog with ID: " + oldBlogID));
 
-    multi.set("handle:" + blog.handle, newBlogID);
-
-    // also set the www subdomain alternate key...
-    if (blog.domain) multi.set("domain:" + blog.domain, newBlogID);
-
-    multi.exec(function(err) {
+    renameKeys(oldBlog, newBlogID, function(err) {
       if (err) return callback(err);
-
-      updateUser(blog.owner, oldBlogID, newBlogID, function(err) {
+      updateUser(oldBlog.owner, oldBlogID, newBlogID, function(err) {
         if (err) return callback(err);
 
-        renamePatterns(oldBlogID, newBlogID, function(err) {
+        moveDirectories(oldBlogID, newBlogID, function(err) {
           if (err) return callback(err);
 
           callback();
@@ -60,32 +73,41 @@ function main(oldBlogID, newBlogID, callback) {
   });
 }
 
-function renamePatterns(oldBlogID, newBlogID, callback) {
-  var patterns = [
-    "blog:" + oldBlogID + ":*",
-    "template:" + oldBlogID + ":*",
-    "template:owned_by:" + oldBlogID
-  ];
+function renameKeys(oldBlog, newBlogID, callback) {
+  var multi = client.multi();
+  var patterns = ["template:" + oldBlog.id + ":*", "blog:" + oldBlog.id + ":*"];
 
-  async.each(
-    patterns,
-    function(pattern, next) {
-      keys(pattern, function(err, keys) {
-        async.each(
-          keys,
-          function(key, next) {
-            client.rename(
-              key,
-              key.split(":" + oldBlogID + ":").join(":" + newBlogID + ":"),
-              next
-            );
-          },
-          next
-        );
-      });
-    },
-    callback
-  );
+  async.map(patterns, keys, function(err, patterns) {
+    patterns[0].forEach(function(key) {
+      multi.RENAMENX(
+        key,
+        key
+          .split("template:" + oldBlog.id + ":")
+          .join("template:" + newBlogID + ":")
+      );
+    });
+
+    patterns[1].forEach(function(key) {
+      multi.RENAMENX(
+        key,
+        key.split("blog:" + oldBlog.id + ":").join("blog:" + newBlogID + ":")
+      );
+    });
+
+    multi.set("handle:" + oldBlog.handle, newBlogID);
+    multi.set("template:owned_by:" + oldBlog.id, newBlogID);
+
+    // also set the www subdomain alternate key...
+    if (oldBlog.domain) {
+      multi.set("domain:" + oldBlog.domain, newBlogID);
+
+      if (oldBlog.domain.indexOf("www.") === 0)
+        multi.set("domain:" + oldBlog.domain.slice("www.".length), newBlogID);
+      else multi.set("domain:" + "www." + oldBlog.domain, newBlogID);
+    }
+
+    multi.exec(callback);
+  });
 }
 
 function updateUser(uid, oldBlogID, newBlogID, callback) {
@@ -101,15 +123,18 @@ function updateUser(uid, oldBlogID, newBlogID, callback) {
   });
 }
 
-// 'template:owned_by:' + blogID,
-// 'handle:' + blog.handle,
-// 'domain:' // also www version...
-// "blog:" + blogID + ":dropbox:account";
+function moveDirectories(oldBlogID, newBlogID, callback) {
+  var oldBlogDir = localPath(oldBlogID, "/").slice(0, -1);
+  var newBlogDir = localPath(newBlogID, "/").slice(0, -1);
 
-// git client should just work
+  var oldStaticFilesDir = staticDirectory + "/" + oldBlogID;
+  var newStaticFilesDir = staticDirectory + "/" + newBlogID;
 
-// Redis set whoses members are the blog IDs
-// connected to this dropbox account.
-// function blogsKey(account_id) {
-//   return "clients:dropbox:" + account_id;
-// }
+  debug("Moving folder", oldBlogDir, "to", newBlogDir);
+  fs.move(oldBlogDir, newBlogDir, function(err) {
+    if (err) return callback(err);
+
+    debug("Moving folder", oldStaticFilesDir, "to", newStaticFilesDir);
+    fs.move(oldStaticFilesDir, newStaticFilesDir, callback);
+  });
+}
