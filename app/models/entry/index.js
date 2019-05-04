aa166c5df628d80b281506cb3ae47e9a66cd4668
David Merfield <dmerfield@gmail.com>
2019-05-04 08:48:24 -0400

    Removed dirToModule from Entry model

--- app/models/entry/index.js
+++ app/models/entry/index.js
@@ -1,1 +1,9 @@
-module.exports = require('helper').dirToModule(__dirname, require);
+module.exports = {
+  drop: require('./drop'),
+  get: require('./get'),
+  getByUrl: require('./getByUrl'),
+  instance: require('./instance'),
+  key: require('./key'),
+  model: require('./model'),
+  set: require('./set')
+};
