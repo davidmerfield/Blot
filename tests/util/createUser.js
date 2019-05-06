ba5de17824eaa42c7c9501215e6cff607c8cf94f
David Merfield <dmerfield@gmail.com>
2019-05-06 14:22:33 -0400

    Add fake subscription to test user

--- tests/util/createUser.js
+++ tests/util/createUser.js
@@ -5,13 +5,18 @@ module.exports = function(done) {
   var context = this;
   var fakePassword = "XXX-" + Date.now();
   var fakeEmail = randomString(20) + "@example.com";
+  var fakeSubscription = {
+    customer: "x",
+    status: "active",
+    plan: { amount: 1 }
+  };
 
   User.hashPassword(fakePassword, function(err, passwordHash) {
     if (err) {
       return done(err);
     }
 
-    User.create(fakeEmail, passwordHash, {}, function(err, user) {
+    User.create(fakeEmail, passwordHash, fakeSubscription, function(err, user) {
       if (err) {
         return done(err);
       }
