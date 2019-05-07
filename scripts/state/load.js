e93497d0e36f9e9743b3ad432097480642e43c43
David Merfield <dmerfield@gmail.com>
2019-05-07 11:43:00 -0400

    Updated scripts to load and save state

--- scripts/state/load.js
+++ scripts/state/load.js
@@ -1,12 +1,13 @@
 var fs = require("fs-extra");
 var exec = require("child_process").exec;
-var colors = require("colors/safe");
+var redis = require("redis");
+
 var ROOT = process.env.BLOT_DIRECTORY;
+
 var BLOG_FOLDERS_DIRECTORY = ROOT + "/blogs";
 var GIT_CLIENTS_DATA = ROOT + "/app/clients/git/data";
 var STATIC_FILES_DIRECTORY = ROOT + "/static";
 var ACTIVE_DATABASE_DUMP = ROOT + "/db/dump.rdb";
-var TMP_DIRECTORY = ROOT + "/tmp";
 
 if (!ROOT) throw new Error("Please set environment variable BLOT_DIRECTORY");
 
@@ -16,47 +17,40 @@ function main(label, callback) {
   loadDB(directory, function(err) {
     if (err) return callback(err);
 
+    fs.emptyDirSync(BLOG_FOLDERS_DIRECTORY);
     fs.copySync(directory + "/blogs", BLOG_FOLDERS_DIRECTORY);
+
+    fs.emptyDirSync(GIT_CLIENTS_DATA);
     fs.copySync(directory + "/git", GIT_CLIENTS_DATA);
+
+    fs.emptyDirSync(STATIC_FILES_DIRECTORY);
     fs.copySync(directory + "/static", STATIC_FILES_DIRECTORY);
 
-    fs.emptyDirSync(TMP_DIRECTORY);
-    printBlogs(callback);
-  });
-}
-
-function ensureRedisIsShutdown(callback) {
-  var redis = require("redis");
-  var client = redis.createClient();
-
-  client.on("error", function(err) {
-    if (err.code === "ECONNREFUSED") {
-      callback(null);
-    }
-  });
-
-  var multi = client.multi();
-
-  multi.config("SET", "appendonly", "no");
-  multi.config("SET", "save", "");
-  multi.shutdown();
-
-  multi.exec(function(err) {
-    if (err && err.code !== "ECONNREFUSED" && err.code !== "UNCERTAIN_STATE")
-      return callback(err);
-
-    callback(null);
+    require("./info")(callback);
   });
 }
 
 function loadDB(directory, callback) {
   var dump = directory + "/dump.rdb";
+  var client = redis.createClient();
+  var multi = client.multi();
 
-  if (!fs.existsSync(dump)) return callback(new Error("NOENT " + dump));
+  // If redis was already shut down, then
+  // we won't be able to execute the multi()
+  client.on("error", function(err) {
+    if (err.code === "ECONNREFUSED") {
+      then();
+    }
+  });
 
-  ensureRedisIsShutdown(function(err) {
-    if (err) return callback(err);
+  multi
+    .config("SET", "appendonly", "no")
+    .config("SET", "save", "")
+    .shutdown();
 
+  multi.exec(then);
+
+  function then() {
     fs.copySync(dump, ACTIVE_DATABASE_DUMP);
 
     exec(
@@ -68,88 +62,6 @@ function loadDB(directory, callback) {
         callback(null);
       }
     );
-  });
-}
-
-function printBlogs(callback) {
-  var Blog = require("blog");
-  var access = require("../access");
-  var config = require("config");
-  var async = require("async");
-
-  Blog.getAllIDs(function(err, ids) {
-    if (err) return callback(err);
-
-    if (ids && ids.length > 10) {
-      console.log("Too many blogs to log in");
-      return callback();
-    }
-
-    async.each(
-      ids,
-      function(id, next) {
-        Blog.get({ id: id }, function(err, blog) {
-          if (err) return next(err);
-          setupFolder(blog, function(err, folder) {
-            if (err) return next(err);
-            access(blog.handle, function(err, url) {
-              if (err) return next(err);
-
-              console.log();
-              console.log(colors.yellow(blog.title), "-", colors.dim(blog.id));
-              console.log("Dashboard:", url);
-              console.log("Blog:", "http://" + blog.handle + "." + config.host);
-              if (folder) console.log("Folder:", folder);
-              next();
-            });
-          });
-        });
-      },
-      callback
-    );
-  });
-}
-
-function setupFolder(blog, callback) {
-  var clients = require("../../app/clients");
-  var join = require("path").join;
-  var os = require("os");
-
-  if (blog.client === "dropbox") {
-
-    var getFolder = require("../../app/clients/dropbox/database").get;
-
-    getFolder(blog.id, function(err, account) {
-      var folder;
-
-      if (account.full_access && account.folder && account.folder !== "/") {
-        folder = join(os.homedir(), "Dropbox", account.folder);
-      } else {
-        folder = join(os.homedir(), "Dropbox", "Apps", "Blot test");
-      }
-
-      fs.copySync(BLOG_FOLDERS_DIRECTORY + "/" + blog.id, folder);
-      callback(null, folder);
-    });
-
-  } else if (blog.client === "local") {
-    var folder = TMP_DIRECTORY + "/" + blog.handle;
-
-    fs.copySync(BLOG_FOLDERS_DIRECTORY + "/" + blog.id, folder);
-
-    clients.local.setup(blog.id, folder, function(err) {
-      if (err) return callback(err);
-      callback(null, folder);
-    });
-
-  } else if (blog.client === "git") {
-
-    console.log(blog.handle, "uses git client");
-
-  } else {
-    
-    console.log(blog.handle, "uses unknown client");
-    callback(null);
   }
 }
 
