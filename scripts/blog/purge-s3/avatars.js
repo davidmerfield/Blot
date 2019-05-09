a5c921c5331542f9e957bc125b961a08eba52ede
David Merfield <dmerfield@gmail.com>
2019-05-09 13:47:48 -0400

    Finished avatar script

--- scripts/blog/purge-s3/avatars.js
+++ scripts/blog/purge-s3/avatars.js
@@ -5,12 +5,10 @@ var config = require("config");
 var uuid = require("uuid/v4");
 var extname = require("path").extname;
 var config = require("config");
-var folder = "_avatars";
-var request = require("request");
-var fs = require("fs-extra");
-var dirname = require("path").dirname;
-
+var download = require("./download");
+var yesno = require("yesno");
 var VALID_EXTENSIONS = [".jpg", ".jpeg", ".png", ".gif"];
+var colors = require('colors/safe');
 
 if (require.main === module)
   main(function(err) {
@@ -25,67 +23,39 @@ function main(callback) {
       blogIDs,
       function(blogID, next) {
         Blog.get({ id: blogID }, function(err, blog) {
-          if (err || !blog) return next();
-
-          if (blog.avatar.indexOf(CDN) === -1) return next();
+          if (err || !blog || blog.avatar.indexOf(CDN) === -1) return next();
 
           var extension = extname(blog.avatar).toLowerCase();
+          var name = uuid() + extension;
+          var avatar = "/_avatars/" + name;
+          var path = config.blog_static_files_dir + "/" + blog.id + avatar;
 
           if (VALID_EXTENSIONS.indexOf(extension) === -1) {
             console.log(blog.id, "Has invalid extension:", blog.avatar);
             return next();
           }
 
-          var name = uuid() + extension;
-          var path =
-            config.blog_static_files_dir +
-            "/" +
-            blog.id +
-            "/" +
-            folder +
-            "/" +
-            name;
+          download(blog.avatar, path, function(err) {
+            if (err) return next(err);
 
-          var url = "/" + folder + "/" + name;
+            var message =
+              colors.dim("Replace avatar for ") +
+              colors.yellow(blog.handle) +
+              " - " +
+              blog.id +
+              colors.dim("\nOld: ") +
+              blog.avatar +
+              colors.dim("\nNew: ") +
+              avatar +
+              colors.dim("\n     ") +
+              path;
 
-          fs.ensureDirSync(dirname(path));
+            yesno.ask(message, true, function(ok) {
+              if (!ok) return next();
 
-          var currentURL = blog.avatar;
-
-          if (currentURL[0] === "/" && currentURL[1] === "/") {
-            currentURL = "https:" + currentURL;
-          }
-
-          var ws = fs.createWriteStream(path);
-
-          ws.on("close", function() {
-            if (!fs.existsSync(path) || !fs.statSync(path).size) {
-              console.log(
-                blog.id,
-                "Error with file downlaoded",
-                path,
-                fs.statSync(path)
-              );
-              return next();
-            }
-
-            Blog.set(blogID, { avatar: url }, function(err) {
-              if (err) return next(err);
-
-              console.log(
-                blog.id,
-                "Saved avatar for http://" + blog.handle + "." + config.host
-              );
-              next();
+              Blog.set(blogID, { avatar: avatar }, next);
             });
           });
-
-          ws.on("error", next);
-
-          request
-            .get({ uri: currentURL, timeout: 5000 })
-            .on("error", next)
-            .pipe(ws);
         });
       },
       callback
