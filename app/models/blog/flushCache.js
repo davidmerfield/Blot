a4d9f2d223da4ad857f85c30a562d65bc11c572b
David Merfield <dmerfield@gmail.com>
2019-05-10 11:02:50 -0400

    Initial commit

--- app/models/blog/flushCache.js
+++ app/models/blog/flushCache.js
@@ -1,6 +1,8 @@
 var config = require("config");
 var flush = require("express-disk-cache")(config.cache_directory).flush;
 var get = require("./get");
+var fs = require("fs-extra");
+var localPath = require("helper").localPath;
 
 // This empties the cache for a blog by emptying the cache
 // for its Blot subdomain and its custom domain, if one is set
@@ -17,9 +19,31 @@ module.exports = function(blogID, callback) {
     flush(blog.handle + "." + config.host, function(err) {
       if (err) return callback(err);
 
-      if (!blog.domain) return callback();
+      var folderInCache =
+        config.cache_directory +
+        "/" +
+        blog.handle +
+        "." +
+        config.host +
+        "/folder";
+      var folderInCacheDomain =
+        config.cache_directory + "/" + blog.domain + "/folder";
+      var blogFolder = localPath(blog.id, "/").slice(0, -1);
 
-      flush(blog.domain, callback);
+      console.log("symlinking", blogFolder, folderInCache);
+
+      fs.symlink(blogFolder, folderInCache, function(err) {
+        if (err) return callback(err);
+
+        if (!blog.domain) return callback();
+
+        console.log("symlinking", blogFolder, folderInCacheDomain);
+        fs.symlink(blogFolder, folderInCacheDomain, function(err) {
+          if (err) return callback(err);
+
+          flush(blog.domain, callback);
+        });
+      });
     });
   });
 };
