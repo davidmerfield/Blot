3adf1f944c403131d08235cea5cc22df8707ebdd
David Merfield <dmerfield@gmail.com>
2019-05-13 16:05:47 -0400

    Fixed script to set up initial symlinks

--- scripts/blog/establish-symlinks.js
+++ scripts/blog/establish-symlinks.js
@@ -1,12 +1,19 @@
 var symlinks = require("../../app/models/blog/symlinks");
+var BackupDomain = require("../../app/models/blog/util/backupDomain.js");
 var config = require("config");
 
 function main(blog, callback) {
   var symlinksToAdd = [];
   var symlinksToRemove = [];
 
-  if (blog.handle) symlinksToAdd.push(blog.handle + "." + config.host);
-  if (blog.domain) symlinksToAdd.push(blog.domain);
+  if (blog.handle) {
+    symlinksToAdd.push(blog.handle + "." + config.host);
+  }
+
+  if (blog.domain) {
+    symlinksToAdd.push(blog.domain);
+    symlinksToAdd.push(BackupDomain(blog.domain));
+  }
 
   symlinks(blog.id, symlinksToAdd, symlinksToRemove, callback);
 }
