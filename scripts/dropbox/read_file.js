1d6b8ddad845a85a0853f3c0c3b983af4d6c2e2a
David Merfield <dmerfield@gmail.com>
2019-05-14 12:49:34 -0400

    Tidied up Dropbox scripts

--- scripts/dropbox/read_file.js
+++ scripts/dropbox/read_file.js
@@ -1,8 +1,10 @@
 var DropboxDatabase = require("../../app/clients/dropbox/database");
 var createClient = require("../../app/clients/dropbox/util/createClient");
-var get = require("../blog/get");
+var get = require("../get/blog");
 
-get(process.argv[2], function(user, blog) {
+get(process.argv[2], function(err, user, blog) {
+  if (err) throw err;
+
   var path = process.argv[3] || "";
 
   console.log(
@@ -25,11 +27,11 @@ get(process.argv[2], function(user, blog) {
         console.log(err);
       })
       .then(function(res) {
-        console.log('-----------------------------------------------');
+        console.log("-----------------------------------------------");
         console.log(res.fileBinary.toString());
-        console.log('-----------------------------------------------');
-        console.log('From',res.name, res.path_lower, res.path_display);
-        console.log('Client modified', res.client_modified);
+        console.log("-----------------------------------------------");
+        console.log("From", res.name, res.path_lower, res.path_display);
+        console.log("Client modified", res.client_modified);
         process.exit();
       });
   });
