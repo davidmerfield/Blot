73990224ac3efbb3d5830804e8bf1d6ac2bcd388
David Merfield <dmerfield@gmail.com>
2019-05-15 16:18:57 -0400

    Added sync check

--- scripts/blog/switch-blog-id-format/ensureOldBlogIsDisabled.js
+++ scripts/blog/switch-blog-id-format/ensureOldBlogIsDisabled.js
@@ -1,5 +1,6 @@
 var Blog = require("blog");
 var async = require("async");
+var Sync = require("sync");
 
 module.exports = function(oldBlogID, newBlogID, callback) {
   var cleanupTasks = [];
@@ -9,16 +10,22 @@ module.exports = function(oldBlogID, newBlogID, callback) {
   }
 
   Blog.get({ id: oldBlogID }, function(err, oldBlog) {
-    if (err || !oldBlog || oldBlog.isDisabled) return callback(null, renableOldBlog);
+    if (err || !oldBlog || oldBlog.isDisabled)
+      return callback(null, renableOldBlog);
 
     cleanupTasks.push(function(callback) {
       Blog.set(newBlogID, { isDisabled: false }, callback);
     });
 
-    Blog.set(oldBlogID, { isDisabled: true }, function(err) {
+    // This should fail if the blog is currently syncing
+    Sync(oldBlogID, function(err, folder, done) {
       if (err) return callback(err);
 
-      callback(null, renableOldBlog);
+      Blog.set(oldBlogID, { isDisabled: true }, function(err) {
+        done(err, function(err) {
+          callback(err, renableOldBlog);
+        });
+      });
     });
   });
 };
