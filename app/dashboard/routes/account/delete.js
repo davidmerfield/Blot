5dc67d4aa7c2bbdc4652c027933b71aa53ffccc1
David Merfield <dmerfield@gmail.com>
2019-05-16 14:06:59 -0400

    Initial commit

--- app/dashboard/routes/account/delete.js
+++ app/dashboard/routes/account/delete.js
@@ -6,16 +6,12 @@ var checkPassword = require("./util/checkPassword");
 var logout = require("./util/logout");
 var async = require("async");
 
-var helper = require("helper");
-var localPath = helper.localPath;
 var Blog = require("blog");
 var pretty = require("helper").prettyPrice;
 
 var User = require("user");
 var config = require("config");
 var stripe = require("stripe")(config.stripe.secret);
-var clients = require("clients");
-var fs = require("fs-extra");
 
 Delete.route("/blog/:handle")
 
@@ -41,7 +37,7 @@ Delete.route("/blog/:handle")
   .post(
     checkPassword,
     function(req, res, next) {
-      deleteBlog(req.blogToDelete.id, next);
+      Blog.remove(req.blogToDelete.id, next);
     },
     calculateSubscriptionChange,
     decreaseSubscription,
@@ -117,53 +113,8 @@ Delete.route("/")
     }
   );
 
-function deleteBlog(blogID, callback) {
-  Blog.get({ id: blogID }, function(err, blog) {
-    if (err) return callback(err);
-
-    // All of these functions take the blogID as
-    // first argument and callback as second.
-    var queue = [
-      function(blogID, done) {
-        fs.emptyDir(localPath(blogID, ""), done);
-      },
-      Blog.remove,
-      updateUser
-    ];
-
-    if (blog.client) {
-      queue.push(clients[blog.client].disconnect);
-    }
-
-    async.applyEach(queue, blog.id, callback);
-  });
-}
-
 function deleteBlogs(req, res, next) {
-  async.series(
-    req.user.blogs.map(function(blogID) {
-      return deleteBlog.bind(this, blogID);
-    }),
-    next
-  );
-}
-
-function updateUser(blogID, callback) {
-  Blog.get({ id: blogID }, function(err, blog) {
-    if (err) return callback(err);
-
-    User.getById(blog.owner, function(err, user) {
-      if (err) return callback(err);
-
-      var blogs = user.blogs.slice();
-
-      blogs = blogs.filter(function(otherBlogID) {
-        return otherBlogID !== blogID;
-      });
-
-      User.set(blog.owner, { blogs: blogs }, callback);
-    });
-  });
+  async.each(req.user.blogs, Blog.remove, next);
 }
 
 function deleteUser(req, res, next) {
--- app/models/blog/remove.js
+++ app/models/blog/remove.js
@@ -1,68 +1,117 @@
-var get = require("./get");
-var key = require("./key");
-var helper = require("helper");
-var ensure = helper.ensure;
 var async = require("async");
 var client = require("client");
-var START_CURSOR = "0";
-var SCAN_SIZE = 1000;
-var symlinks = require("./symlinks");
 var config = require("config");
+var fs = require("fs-extra");
+
+var get = require("./get");
+var key = require("./key");
+var symlinks = require("./symlinks");
 var BackupDomain = require("./util/backupDomain");
 
-module.exports = function(blogID, callback) {
-  var multi = client.multi();
-  var symlinksToRemove = [];
+var START_CURSOR = "0";
+var SCAN_SIZE = 1000;
 
-  ensure(blogID, "string").and(callback, "function");
+// function updateUser(blog, callback) {
+//   require("user").getById(blog.owner, function(err, user) {
+//     if (err) return callback(err);
 
-  get({ id: blogID }, function(err, blog) {
+//     var blogs = user.blogs.slice();
+
+//     blogs = blogs.filter(function(otherBlogID) {
+//       return otherBlogID !== blog.id;
+//     });
+
+//     User.set(blog.owner, { blogs: blogs }, callback);
+//   });
+// }
+
+function remove(blogID, callback) {
+  get({id: blogID}, function(err, blog) {
     if (err) return callback(err);
 
-    var patterns = ["template:" + blogID + ":*", "blog:" + blogID + ":*"];
+    var tasks = [
+      wipeFolders,
+      // updateUser,
+      disconnectClient,
+      removeSymlinks,
+      deleteKeys
+    ].map(function(task) {
+      return task.bind(null, blog);
+    });
 
-    var remove = ["template:owned_by:" + blogID, "handle:" + blog.handle];
-
-    // TODO ALSO remove alternate key with/out 'www', e.g. www.example.com
-    if (blog.domain) {
-      symlinksToRemove.push(blog.domain);
-      symlinksToRemove.push(BackupDomain(blog.domain));
-      remove.push("domain:" + blog.domain);
-      remove.push("domain:" + BackupDomain(blog.domain));
-    }
-
-    if (blog.handle) {
-      symlinksToRemove.push(blog.handle + "." + config.host);
-    }
-
-    async.each(
-      patterns,
-      function(pattern, next) {
-        var args = [START_CURSOR, "MATCH", pattern, "COUNT", SCAN_SIZE];
-
-        client.scan(args, function then(err, res) {
-          if (err) throw err;
-
-          // the cursor for the next pass
-          args[0] = res[0];
-
-          // Append the keys we matched in the last pass
-          remove = remove.concat(res[1]);
-
-          // There are more keys to check, so keep going
-          if (res[0] !== START_CURSOR) return client.scan(args, then);
-
-          next();
-        });
-      },
-      function() {
-        multi.del(remove);
-        multi.srem(key.ids, blogID);
-        multi.exec(function(err) {
-          if (err) return callback(err);
-          symlinks(blogID, [], symlinksToRemove, callback);
-        });
-      }
-    );
+    async.series(tasks, callback);
   });
-};
+}
+
+function wipeFolders(blog, callback) {
+  fs.remove(config.blog_folder_dir + "/" + blog.id, function(err) {
+    if (err) return callback(err);
+    fs.remove(config.blog_static_files_dir + "/" + blog.id, callback);
+  });
+}
+
+function removeSymlinks(blog, callback) {
+  var symlinksToRemove = [];
+
+  if (blog.domain) {
+    symlinksToRemove.push(blog.domain);
+    symlinksToRemove.push(BackupDomain(blog.domain));
+  }
+
+  if (blog.handle) {
+    symlinksToRemove.push(blog.handle + "." + config.host);
+  }
+
+  symlinks(blog.id, [], symlinksToRemove, callback);
+}
+
+function deleteKeys(blog, callback) {
+  var multi = client.multi();
+
+  var patterns = ["template:" + blog.id + ":*", "blog:" + blog.id + ":*"];
+
+  var remove = ["template:owned_by:" + blog.id, "handle:" + blog.handle];
+
+  // TODO ALSO remove alternate key with/out 'www', e.g. www.example.com
+  if (blog.domain) {
+    remove.push("domain:" + blog.domain);
+    remove.push("domain:" + BackupDomain(blog.domain));
+  }
+
+  async.each(
+    patterns,
+    function(pattern, next) {
+      var args = [START_CURSOR, "MATCH", pattern, "COUNT", SCAN_SIZE];
+
+      client.scan(args, function then(err, res) {
+        if (err) throw err;
+
+        // the cursor for the next pass
+        args[0] = res[0];
+
+        // Append the keys we matched in the last pass
+        remove = remove.concat(res[1]);
+
+        // There are more keys to check, so keep going
+        if (res[0] !== START_CURSOR) return client.scan(args, then);
+
+        next();
+      });
+    },
+    function() {
+      multi.del(remove);
+      multi.srem(key.ids, blog.id);
+      multi.exec(callback);
+    }
+  );
+}
+
+function disconnectClient(blog, callback) {
+  var clients = require("clients");
+
+  if (!blog.client || !clients[blog.client]) return callback(null);
+
+  clients[blog.client].disconnect();
+}
+
+module.exports = remove;
