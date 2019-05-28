51a06b22167023b10a7cfe8167d21086442d1f35
David Merfield <dmerfield@gmail.com>
2019-05-28 14:32:58 -0400

    Updated pretty price for large prices

--- app/helper/prettyPrice.js
+++ app/helper/prettyPrice.js
@@ -1,19 +1,22 @@
-function prettyPrice (cents) {
+var prettyNumber = require("./prettyNumber");
 
-  var price = '$' + (cents/100).toFixed(2);
+function prettyPrice(cents) {
+  var price = (cents / 100).toFixed(2);
 
-  if (price.slice(-2) === '00') price = price.slice(0, -3);
+  if (price.slice(-2) === "00") price = price.slice(0, -3);
 
-  return price;
+  price = prettyNumber(price);
+
+  return "$" + price;
 }
 
-var is = require('./is')(prettyPrice);
+var is = require("./is")(prettyPrice);
 
-is(2134, '$21.34');
-is(2000, '$20');
-is(200, '$2');
-is(210, '$2.10');
-is(03, '$0.03');
-is(30, '$0.30');
+is(2134, "$21.34");
+is(2000, "$20");
+is(200, "$2");
+is(210, "$2.10");
+is(03, "$0.03");
+is(30, "$0.30");
 
 module.exports = prettyPrice;
