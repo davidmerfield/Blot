87036c6fa0c7e5b2fc7e085b17ce422a02dc9dab
David Merfield <dmerfield@gmail.com>
2019-05-28 10:21:07 -0400

    Updated info scripts

--- scripts/info/index.js
+++ scripts/info/index.js
@@ -1,160 +1,52 @@
+var Blog = require('../../app/models/blog');
+var User = require('../../app/models/user');
+var forEach = require('../../app/helper').forEach;
+
 var eachBlog = require('../each/blog');
-var Entries = require('../../app/models/entries');
-var moment = require('moment');
 
-var totalBlogs = 0;
-var activeBlogs = 0;
-var disabledBlogs = 0;
-var MAU = 0;
+var identifier = process.argv[2] + '';
 
-var subscribers = 0;
-var cancelledSubscribers = 0;
-var freeloaders = 0;
+var verbose = !!process.argv[3];
 
-var totalEntries = 0;
+if (!identifier) throw 'Please pass the user\'s handle as an argument.';
 
-var TAX_RATE = 0.4;
-var goal = 1000000;
+eachBlog(function(user, blog, nextBlog){
 
-var monthlyCosts = {AWS: 110.00};
-var annualCosts = {domain: 29.00};
-var leaderboard = [];
+  if (blog.handle === identifier) return show(user, nextBlog);
 
-eachBlog(function (user, blog, next) {
+  if (user.uid === identifier) return show(user, nextBlog);
 
-  // console.log();
-  // console.log(blog.handle);
-  // console.log(blog.dateFormat);
+  if (blog.id === identifier) return show(user, nextBlog);
 
-  if (user.subscription) {
+  if (user.email === identifier) return show(user, nextBlog);
 
-    if (user.subscription.cancel_at_period_end) {
-      console.log(blog.handle, 'will quit at the end of the billing period.');
-      cancelledSubscribers++;
-    } else if (user.subscription.status && user.subscription.status !== 'active') {
-      console.log(blog.handle, 'has an inactive subscription');
-      cancelledSubscribers++;
-    } else if (user.subscription.status === 'active') {
+  nextBlog();
 
-      // user.blogs.length
+}, process.exit);
 
-      subscribers ++;
 
-    } else {
-      freeloaders++;
-    }
-  }
+function show (user, then) {
+  console.log('-----------------------------');
+  console.log('Name: ' + user.name);
+  console.log('Email: ' + user.email);
+  console.log('UID: ' + user.uid);
+  console.log('Country: ' + user.countryCode);
+  console.log('Blogs: ');
+  forEach(user.blogs, function(blogID, next){
 
-  totalBlogs++;
+    Blog.get({id: blogID}, function(err, blog){
 
-  if (blog.isDisabled || user.isDisabled) {
-    disabledBlogs++;
-    return next();
-  } else {
-    activeBlogs++;
-  }
+      console.log('  ID: ' + blog.id);
+      console.log('  Handle: ' + blog.handle);
+      console.log('  Domain: ' + blog.domain);
 
-  // if (blog.domain) {
-  //   console.log(blog.domain);
-  // } else {
-  //   console.log(blog.handle + '.blot.im');
-  // }
+      console.log();
 
-  Entries.getTotal(blog.id, function(err, entries){
-    totalEntries += entries;
-    leaderboard.push({handle: blog.handle, total: entries});
-
-    Entries.lastUpdate(blog.id, function(err, stamp){
-
-      var lastMonth = Date.now() - 1000*60*60*24*30;
-
-      if (moment.utc(stamp).valueOf() > lastMonth) {
-        MAU++;
-      }
-
+      if (verbose) console.log(blog);
       next();
     });
+  }, function(){
+    if (verbose) console.log(user);
+    then();
   });
-
-}, function(){
-
-  leaderboard = leaderboard.sort(function(a, b){
-    return b.total - a.total;
-  });
-
-  var none = 0;
-  var barely = 0;
-  var ok = 0;
-  var great = 0;
-  var awesome = 0;
-
-  for (var i in leaderboard) {
-
-    var total = leaderboard[i].total;
-
-    console.log(leaderboard[i].handle, total);
-
-    if (total === 1) none++;
-    if (total <= 5) barely++;
-    if (total <= 10) ok++;
-    if (total > 50) great++;
-    if (total > 100) awesome++;
-
-  }
-
-  console.log('________________________');
-  console.log(Math.round(none/totalBlogs * 100) + '% of blogs have not published anything');
-  console.log(Math.round(barely/totalBlogs * 100) + '% of blogs have published 5 or less');
-  console.log(Math.round(ok/totalBlogs * 100) + '% of blogs have published 10 or less');
-
-  console.log('________________________');
-  console.log(Math.round(great/totalBlogs * 100) + '% of blogs have published more than 50');
-  console.log(Math.round(awesome/totalBlogs * 100) + '% of blogs have published more than 100');
-
-  console.log('________________________');
-  console.log('TOTAL: ' + totalBlogs);
-  console.log('ACTIVE: ' + activeBlogs);
-  console.log('MONTHLY ACTIVE: ' + MAU);
-  console.log('DISABLED: ' + disabledBlogs);
-
-  console.log('subscribers: ' + subscribers);
-  console.log('cancelled: ' + cancelledSubscribers);
-  console.log('freeloaders: ' + freeloaders);
-
-  var costs = 0;
-
-  for (var i in annualCosts)
-    costs += annualCosts[i];
-
-  for (var x in monthlyCosts)
-    costs += (monthlyCosts[x] * 12);
-
-  var income = (subscribers * (20 - 0.88)).toFixed(2);
-  var monthlyIncome = (income/12).toFixed(2);
-
-  console.log('Total entries published:     ' + totalEntries);
-  console.log('________________________');
-  console.log('Revenue:     $' + income + ' / y');
-  console.log('             $' + monthlyIncome + ' / m');
-
-
-  console.log('________________________');
-  console.log('Costs:     $' + costs.toFixed(2) + ' / y');
-  console.log('             $' + (costs/12).toFixed(2) + ' / m');
-
-  var EBITDA = (income - costs).toFixed(2);
-
-  console.log('________________________');
-  console.log('Income:     $' + EBITDA + ' / y');
-  console.log('             $' + (EBITDA/12).toFixed(2) + ' / m');
-
-  var profit = ((income - costs)* (1 - TAX_RATE)).toFixed(2);
-
-  console.log('________________________');
-  console.log('Profit:      $' + profit + ' / y');
-  console.log('              $' + (profit/12).toFixed(2) + ' / m');
-  console.log('________________________');
-  console.log('Goal:              ' + (profit/goal * 100).toFixed(2) + '%');
-
-
-});
+}
--- scripts/info/util/numberWithCommas.js
+++
@@ -1,3 +0,0 @@
-module.exports = function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-};
