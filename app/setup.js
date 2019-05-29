199b08d0d494a9cdf73d8cac2ddc5bdd54ff3c0b
David Merfield <dmerfield@gmail.com>
2019-05-29 13:41:50 -0400

    Updated setup script

--- app/setup.js
+++ app/setup.js
@@ -25,10 +25,9 @@ redis.mset(
 );
 
 // Create empty directories if they don't exist
-fs.ensureDirSync(root + "/blogs");
+fs.ensureDirSync(root + "/data");
 fs.ensureDirSync(root + "/tmp");
 fs.ensureDirSync(root + "/logs");
 fs.ensureDirSync(root + "/db");
-fs.ensureDirSync(root + "/static");
 
 process.exit();
