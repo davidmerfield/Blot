8d9454468be19875fa8fc68e5c1192746078ee9b
David Merfield <dmerfield@gmail.com>
2019-06-04 15:34:01 +0100

    Added comments

--- app/build/converters/markdown/ampersands.js
+++ app/build/converters/markdown/ampersands.js
@@ -1,6 +1,23 @@
-// Think about
-// - amerpsands in img srcs, do they still work?
-// - amerpsands in slugs, do they still work?
+// Why do we need to think about ampersands? Well, because ampersands (&) are invalid HTML unless
+// escaped as &amp;. Markdown converts transform an unescaped ampersand in text into an escaped 
+// ampersand. Pandoc, however, gets upset about unescaped ampersands in HTML snippets inside 
+// Markdown files. For example:
+
+// <a href="/?foo=bar&baz=bat">a</a> is returned as a literal string instead of HTML while:
+// <a href="/?foo=bar&amp;baz=bat">a</a> is properly processed. Read more here:
+
+// https://github.com/jgm/pandoc/issues/2410
+
+// So to get around this, we first escape all ampersands before passing the text to pandoc. Ideally
+// we would only do this for the HTML in the markdown but I'm not sure how to do that.
+
+// However, once we've done this, Pandoc doubly escapes our already escaped ampersands inside
+// code blocks, e.g. ```&``` becomes <pre>&amp;amp;</pre> so we run a second method after the
+// conversion in Pandoc to de-double escape the ampersands.
+
+// I think that while JGM is technically correct, this should all be the responsibility of the markdown
+// converter itself, but oh well...
+
 module.exports = {
   // This regex replaces unescaped amerpersands (and leaves escaped ones)
   // I found the regex here:
