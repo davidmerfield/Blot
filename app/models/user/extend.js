074b4fd8855b59b87d71223285db6e430c7d5a38
David Merfield <dmerfield@gmail.com>
2019-06-10 11:40:08 +0100

    More tweaks to emails

--- app/models/user/extend.js
+++ app/models/user/extend.js
@@ -20,6 +20,7 @@ module.exports = function extend (user) {
 
     if (subscription.plan) {
       user.pretty.amount = subscription.quantity;
+      user.pretty.s = subscription.quantity === 1 ? '' : 's';
       user.pretty.amount_in_words = amountInWords(subscription.quantity);
       user.pretty.expiry = helper.prettyDate(subscription.current_period_end * 1000);
       user.pretty.price = helper.prettyPrice(subscription.plan.amount * subscription.quantity);
