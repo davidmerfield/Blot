37aafd6357d7805365afd75ecf9f216210863c79
David Merfield <dmerfield@gmail.com>
2019-06-11 14:32:59 +0100

    Added test for metadata

--- app/models/template/tests/writeToFolder.js
+++ app/models/template/tests/writeToFolder.js
@@ -1,6 +1,7 @@
 describe("template", function() {
   var writeToFolder = require("../index").writeToFolder;
   var setView = require("../index").setView;
+  var setMetadata = require("../index").setMetadata;
   var fs = require("fs-extra");
 
   require("./setup")({ createTemplate: true });
@@ -20,6 +21,23 @@ describe("template", function() {
       writeToFolder(test.blog.id, test.template.id, function(err) {
         if (err) return done.fail(err);
         expect(fs.readFileSync(path, "utf-8")).toEqual(view.content);
+        done();
+      });
+    });
+  });
+
+  it("writes template metadata to package.json in a folder", function(done) {
+    var test = this;
+    var metadata = { locals: { foo: "bar" } };
+    var path =
+      test.clientDir + "/Templates/" + test.template.slug + "/package.json";
+
+    setMetadata(this.template.id, metadata, function(err) {
+      if (err) return done.fail(err);
+
+      writeToFolder(test.blog.id, test.template.id, function(err) {
+        if (err) return done.fail(err);
+        expect(fs.readJsonSync(path).locals).toEqual(metadata.locals);
         done();
       });
     });
