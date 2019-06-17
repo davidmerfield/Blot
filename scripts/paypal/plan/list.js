fd177989eb3c6e33a3bd98bbeea68630863787ed
David Merfield <dmerfield@gmail.com>
2019-06-17 13:58:02 -0400

    Adds scripts to create plans and products through PayPal's API

---
+++ scripts/paypal/plan/list.js
@@ -0,0 +1,29 @@
+var request = require("request");
+var config = require("config");
+
+function main(callback) {
+  require("../generate-access-token")(function(err, token) {
+    request(
+      {
+        uri: "https://api.sandbox.paypal.com/v1/billing/plans",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept-Language": "en_US",
+          Authorization: "Bearer " + token
+        }
+      },
+      function(err, res, data) {
+        if (err) return callback(err);
+        callback(null, JSON.parse(data));
+      }
+    );
+  });
+}
+
+module.exports = main;
+
+if (require.main === module)
+  main(function(err, res) {
+    if (err) throw err;
+    console.log(res);
+  });
