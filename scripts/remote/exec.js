4ff16d8f1f6d4b955f43a20bd7f97892386c0a47
David Merfield <dmerfield@gmail.com>
2019-06-21 14:32:27 -0400

    Removed shelljs

--- scripts/remote/exec.js
+++ scripts/remote/exec.js
@@ -1,11 +1,12 @@
-var HOST = require('./host');
+var HOST = require("./host");
+var exec = require("child_process").exec;
 
-require('shelljs/global');
-
-module.exports = function (command, callback) {
-
-  exec('ssh ' + HOST + ' \"' + command + '\"', {silent:true}, function(code, stdout, stderr){
-
+module.exports = function(command, callback) {
+  exec("ssh " + HOST + ' "' + command + '"', { silent: true }, function(
+    code,
+    stdout,
+    stderr
+  ) {
     if (code) return callback(code + stdout + stderr);
 
     return callback(null, stdout);
--- scripts/remote/fetchdir.js
+++ scripts/remote/fetchdir.js
@@ -1,33 +1,31 @@
-var helper =  require('../../app/helper');
-var fs = require('fs-extra');
+var helper = require("../../app/helper");
+var fs = require("fs-extra");
 var localRoot = helper.rootDir;
-var remoteRoot = require('./root');
+var remoteRoot = require("./root");
+var exec = require("child_process").exec;
 
-require('shelljs/global');
-
-function inside (dir, root) {
-
+function inside(dir, root) {
   if (dir.indexOf(root) !== 0) {
-    console.log('Root:', root);
-    console.log('Dir:', dir);
-    throw 'Directory is not inside the root';
+    console.log("Root:", root);
+    console.log("Dir:", dir);
+    throw "Directory is not inside the root";
   }
 }
 
-module.exports = function (remotedir, localdir, callback) {
-
+module.exports = function(remotedir, localdir, callback) {
   inside(localdir, localRoot);
   inside(remotedir, remoteRoot);
 
-  fs.ensureDir(localdir, function(err){
-
+  fs.ensureDir(localdir, function(err) {
     if (err) throw err;
 
-    exec('rsync -avL --progress -e ssh blot:' + remotedir + '/* ' + localdir, function(code, stdout, stderr){
+    exec(
+      "rsync -avL --progress -e ssh blot:" + remotedir + "/* " + localdir,
+      function(code, stdout, stderr) {
+        if (code) return callback(code + stdout + stderr);
 
-      if (code) return callback(code + stdout + stderr);
-
-      callback(null, stdout);
-    });
+        callback(null, stdout);
+      }
+    );
   });
 };
