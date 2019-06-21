e73ed79d59b12fda07d74170dbc7616daa8909e5
David Merfield <dmerfield@gmail.com>
2019-06-21 14:29:49 -0400

    Updated webhook script

--- scripts/webhook/index.js
+++ scripts/webhook/index.js
@@ -1,63 +1,73 @@
-require('../only_locally');
-require('shelljs/global');
+require("../only_locally");
 
-var config = require('../../config');
-var account_id = 'dbid:AAAsD4hYhV-hwl7Ti2jbK24ExD2EakNgyow';
+var exec = require("child_process").exec;
 
-var options = require('minimist')(process.argv.slice(2));
+var config = require("config");
+var account_id = "dbid:AAAsD4hYhV-hwl7Ti2jbK24ExD2EakNgyow";
 
-var url =  'https://blot.development/clients/dropbox/webhook';
+var options = require("minimist")(process.argv.slice(2));
+
+var url = "https://blot.development/clients/dropbox/webhook";
 var secret;
 
 if (options.f) {
-  url += '?full=true' ;
+  url += "?full=true";
   secret = config.dropbox.full.secret;
-
 } else {
-  url = 'https://blot.development/clients/dropbox/webhook';
+  url = "https://blot.development/clients/dropbox/webhook";
   secret = config.dropbox.app.secret;
 }
 
-var webhook = 'python scripts/webhook/dropbox_hook.py notify ' + url + ' --secret ' + secret + ' --account ' + account_id;
+var command =
+  "python scripts/webhook/dropbox_hook.py notify " +
+  url +
+  " --secret " +
+  secret +
+  " --account " +
+  account_id;
 
 if (options.c) {
-
   var interval = parseFloat(options.c) || 5;
 
-  console.log('Calling webhook every ' + interval + ' seconds...');
-
-  exec(webhook);
-
-  setInterval(function () {
-
-    exec(webhook);
-
+  console.log("Calling webhook every " + interval + " seconds...");
+  webhook(function(err) {
+    if (err) throw err;
+  });
+  setInterval(function() {
+    webhook(function(err) {
+      if (err) throw err;
+    });
   }, interval * 1000);
-
 } else if (options.s) {
-
   swarm();
-
 } else {
-
-  exec(webhook);
-
+  webhook(function(err) {
+    if (err) throw err;
+    process.exit();
+  });
 }
 
+function webhook(callback) {
+  console.log("Calling webhook...");
+  exec(command, function(err, stdout) {
+    if (err) return callback(err);
+    console.log(stdout.trim());
+    callback(null);
+  });
+}
+function swarm() {
+  var count = Math.round(Math.random() * 2) + 2;
 
-function swarm () {
-
-  var count = Math.round(Math.random()*2) + 2;
-
-  console.log("Herd of", count, 'webhooks');
+  console.log("Herd of", count, "webhooks");
 
   while (count > 0) {
-    exec(webhook);
+    webhook(function(err) {
+      if (err) throw err;
+    });
     count--;
   }
 
   var delay = Math.floor(Math.random() * 3000);
 
-  if (options.o === undefined)
-    setTimeout(swarm, delay);
+  if (options.o === undefined) setTimeout(swarm, delay);
 }
