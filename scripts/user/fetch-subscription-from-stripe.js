a7726ab4d1afecbe50b9c0ce65f702c4eb3d252d
David Merfield <dmerfield@gmail.com>
2019-06-22 15:44:42 -0400

    Updated script to fetch latest version of subscription from stripe

--- scripts/user/fetch-subscription-from-stripe.js
+++ scripts/user/fetch-subscription-from-stripe.js
@@ -1,21 +1,27 @@
 var User = require("user");
 var config = require("config");
 var stripe = require("stripe")(config.stripe.secret);
-
+var yesno = require("yesno");
+var each = require("../each/user");
 function main(user, callback) {
   stripe.customers.retrieveSubscription(
     user.subscription.customer,
     user.subscription.id,
     function(err, subscription) {
-      if (err) return callback(err);
-
-      if (!subscription) return callback(new Error("No subscription"));
+      if (err || !subscription)
+        return callback(err || new Error("No subscription"));
 
       User.set(user.uid, { subscription: subscription }, function(err) {
         if (err) return callback(err);
 
-        console.log("Fetched latest subscription for user " + user.email);
-        console.log(subscription);
+        console.log(
+          "User:",
+          user.uid,
+          user.email,
+          user.subscription.customer,
+          "updated subscription",
+          user.subscription.id
+        );
 
         callback(null, subscription);
       });
@@ -23,15 +29,15 @@ function main(user, callback) {
   );
 }
 
-if (require.main === module) {
-  function done(err) {
-    if (err) throw err;
-    console.log("Done!");
-    process.exit();
-  }
+function done(err) {
+  if (err) throw err;
+  console.log("Done!");
+  process.exit();
+}
 
+if (require.main === module) {
   if (process.argv[2]) {
-    User.getByEmail(email, function(err, user) {
+    User.getByEmail(process.argv[2], function(err, user) {
       if (err || !user) throw err || new Error("No user");
       main(user, done);
     });
@@ -40,6 +46,7 @@ if (require.main === module) {
       "Fetch latest subscription information from Stripe for all users? (y/N)",
       false,
       function(ok) {
+        if (!ok) return process.exit();
         each(main, done);
       }
     );
