48d91a597b07b7eb99bc0f38a36da538c393adef
David Merfield <dmerfield@gmail.com>
2019-06-22 15:33:58 -0400

    Added scripts to identify expired users

--- scripts/user/fetch-subscription-from-stripe.js
+++ scripts/user/fetch-subscription-from-stripe.js
@@ -2,35 +2,48 @@ var User = require("user");
 var config = require("config");
 var stripe = require("stripe")(config.stripe.secret);
 
-function main(email, callback) {
-  User.getByEmail(email, function(err, user) {
-    if (err) return callback(err);
+function main(user, callback) {
+  stripe.customers.retrieveSubscription(
+    user.subscription.customer,
+    user.subscription.id,
+    function(err, subscription) {
+      if (err) return callback(err);
 
-    stripe.customers.retrieveSubscription(
-      user.subscription.customer,
-      user.subscription.id,
-      function(err, subscription) {
+      if (!subscription) return callback(new Error("No subscription"));
+
+      User.set(user.uid, { subscription: subscription }, function(err) {
         if (err) return callback(err);
 
-        if (!subscription) return callback(new Error("No subscription"));
+        console.log("Fetched latest subscription for user " + user.email);
+        console.log(subscription);
 
-        User.set(user.uid, { subscription: subscription }, function(err) {
-          if (err) return callback(err);
-
-          callback(null, subscription);
-        });
-      }
-    );
-  });
+        callback(null, subscription);
+      });
+    }
+  );
 }
 
 if (require.main === module) {
-  main(process.argv[2], function(err, subscription) {
+  function done(err) {
     if (err) throw err;
-    console.log("Fetched latest subscription for user " + process.argv[2]);
-    console.log(subscription);
+    console.log("Done!");
     process.exit();
-  });
+  }
+
+  if (process.argv[2]) {
+    User.getByEmail(email, function(err, user) {
+      if (err || !user) throw err || new Error("No user");
+      main(user, done);
+    });
+  } else {
+    yesno.ask(
+      "Fetch latest subscription information from Stripe for all users? (y/N)",
+      false,
+      function(ok) {
+        each(main, done);
+      }
+    );
+  }
 }
 
 module.exports = main;
