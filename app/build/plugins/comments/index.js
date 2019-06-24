665fadd19e6972d020117f5bdaf76330d67e1f25
David Merfield <dmerfield@gmail.com>
2019-06-24 13:25:24 -0400

    Adds support for commento

---
+++ app/build/plugins/comments/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  description: 'Embed comments from Disqus or Commento',
+  isDefault: false,
+  category: 'external',
+  options: {
+    provider: {None: 'selected'},
+    id: ''
+  }
+};
--- app/build/plugins/disqus/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  description: 'Embed Disqus comments',
-  category: 'external',
-  isDefault: false,
-  options: {
-    shortname: ''
-  }
-};
