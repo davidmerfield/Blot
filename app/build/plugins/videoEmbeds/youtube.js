5b8c934b5c42cc67f258e8b2d34889c59c2acf4e
David Merfield <dmerfield@gmail.com>
2019-07-29 14:52:26 +0100

    Updates youtube embed plugin to handle videos with backslashes in their ID

--- app/build/plugins/videoEmbeds/youtube.js
+++ app/build/plugins/videoEmbeds/youtube.js
@@ -36,12 +36,18 @@ function fail() {
 function extractID(href) {
   var url = Url.parse(href, true);
   var id;
-
   if (url.host === "youtu.be") {
     id = url.pathname.slice(1);
   } else {
     id = url.query.v;
   }
+
+  // Url.parse maps backslashes (used to escape) to forward
+  // slashes, e.g. '/_' for some reason. We remove the forward
+  // slash here, since it breaks video embeds for videos with
+  // ids that contain escapable characters, e.g. 
+  // https://youtu.be/6orc\_lHvJKY
+  id = id.split('/').join('');
 
   return id;
 }
