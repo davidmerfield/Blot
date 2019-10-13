99c321df444026335e5922bc9516231cbf83eec3
David Merfield <dmerfield@gmail.com>
2019-10-13 15:52:19 -0400

    Allow request matching across nginx and node

--- app/index.js
+++ app/index.js
@@ -24,6 +24,56 @@ Blot.set("trust proxy", "loopback");
 // Prevent <iframes> embedding pages served by Blot
 Blot.use(helmet.frameguard("allow-from", config.host));
 
+Blot.use(function(req, res, next){
+  var init = Date.now();
+
+  console.log('[' + clfdate(new Date()) +']', req.headers['x-request-id'], req.protocol + '://' + req.host + req.originalUrl);
+
+  res.on('finish', function(){
+    console.log('[' + clfdate(new Date())+']', req.headers['x-request-id'], res.statusCode, Date.now() - init + 'ms', req.protocol + '://' + req.host + req.originalUrl);
+  });
+
+  next();
+});
+
+var CLF_MONTH = [
+  'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+  'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+];
+
+function pad2 (num) {
+  var str = String(num)
+
+  return (str.length === 1 ? '0' : '') + str
+}
+
+function rpad3 (num) {
+  var str = String(num)
+
+  if (str.length === 2)
+  return str + '0';
+
+  if (str.length === 1)
+  return str + '00';
+
+  return str;
+}
+
+function clfdate (dateTime) {
+  var date = dateTime.getUTCDate()
+  var hour = dateTime.getUTCHours()
+  var mins = dateTime.getUTCMinutes()
+  var secs = dateTime.getUTCSeconds()
+  var year = dateTime.getUTCFullYear()
+  var msecs = dateTime.getUTCMilliseconds();
+
+  var month = CLF_MONTH[dateTime.getUTCMonth()]
+
+  return pad2(date) + '/' + month + '/' + year +
+    ':' + pad2(hour) + ':' + pad2(mins) + ':' + pad2(secs) + '.' + rpad3(msecs)
+    ' +0000'
+}
+
 // Blot is composed of four sub applications.
 
 // The CDN
