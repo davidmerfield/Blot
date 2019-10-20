fd7a1415741a1e58bbccf4482065092a49232cc0
David Merfield <dmerfield@gmail.com>
2019-10-20 13:52:44 -0400

    Removes routing errors log

--- app/blog/error.js
+++ app/blog/error.js
@@ -1,7 +1,6 @@
 module.exports = function(server) {
   var helper = require("helper");
   var type = helper.type;
-  var log = require("./log");
   var Redirects = require("../models/redirects");
   var store404 = require("../models/404").set;
   var config = require("config");
@@ -28,7 +27,6 @@ module.exports = function(server) {
   });
 
   // 404s
-  server.use(log.four04);
   server.use(function(req, res, next) {
     res.addLocals({
       error: {
@@ -46,7 +44,6 @@ module.exports = function(server) {
   });
 
   // Errors
-  server.use(log.error);
   server.use(function(err, req, res, next) {
     // This reponse was partially finished
     // end it now and get over it...
