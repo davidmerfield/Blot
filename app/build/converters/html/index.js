f07c9395fe39a2db39103de6f470754d9d1ade32
David Merfield <dmerfield@gmail.com>
2019-10-26 11:40:02 -0400

    Adds limit to HTML file size

--- app/build/converters/html/index.js
+++ app/build/converters/html/index.js
@@ -18,6 +18,8 @@ function read(blog, path, options, callback) {
 
   fs.stat(localPath, function(err, stat) {
     if (err) return callback(err);
+    // Don't try and turn HTML files larger than 5mb into posts
+    if (stat && stat.size > 5 * 1000 * 1000) return callback(new Error('HTML File too big'));
 
     fs.readFile(localPath, "utf-8", function(err, contents) {
       if (err) return callback(err);
