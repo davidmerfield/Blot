6a195496c3d611d25ee166c6e31008b1bdb948f7
David Merfield <dmerfield@gmail.com>
2019-11-23 18:55:28 -0500

    Updates script to mount disk

--- scripts/production/mount_disk.sh
+++ scripts/production/mount_disk.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
-# I should write a script to rotate log files
 set -e
 
+# This is part of the upstart script for Blot
 DEVICE_NAME=nvme0n1
-CACHE_DIRECTORY=/cache
-BLOT_USER=ec2-user
 
 # Ensure we have mounted the file system
 mkfs -t xfs /dev/$DEVICE_NAME
 mount /dev/$DEVICE_NAME $CACHE_DIRECTORY
-chown -R $BLOT_USER:$BLOT_USER $CACHE_DIRECTORY
-
-# Restart the server
-stop blot
-start blot
+chown -R $BLOT_USER:$BLOT_USER $BLOT_CACHE_DIRECTORY
