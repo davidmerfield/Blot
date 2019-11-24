a22b6b9fde36ff3a03c6ab358057b8fea8661c70
David Merfield <dmerfield@gmail.com>
2019-11-23 19:30:58 -0500

    Updates mount script

--- scripts/production/mount_disk.sh
+++ scripts/production/mount_disk.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-
+# This is part of the upstart script for Blot so if 
+# you move it, make sure to update the upstart script
 set -e
+set -x 
 
-# This is part of the upstart script for Blot
 DEVICE_NAME=nvme0n1
 
 # Ensure we have mounted the file system
-mkfs -t xfs /dev/$DEVICE_NAME
-mount /dev/$DEVICE_NAME $CACHE_DIRECTORY
-chown -R $BLOT_USER:$BLOT_USER $BLOT_CACHE_DIRECTORY
+mkfs -t xfs /dev/$DEVICE_NAME || true
+mount /dev/$DEVICE_NAME $CACHE_DIRECTORY || true
+chown -R $BLOT_USER:$BLOT_USER $BLOT_CACHE_DIRECTORY || true
