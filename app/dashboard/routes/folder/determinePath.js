90671926f02bf244cc04acfb729ca0b8dfd782be
David Merfield <dmerfield@gmail.com>
2020-02-06 18:01:58 -0500

    Improves lookup for paths with unusual characters

--- app/dashboard/routes/folder/determinePath.js
+++ app/dashboard/routes/folder/determinePath.js
@@ -9,6 +9,13 @@ module.exports = function determinePath(req, res, next) {
   if (dir === "/") res.locals.folder.root = true;
 
   dir = decodeURIComponent(dir);
+  	
+  // Fixes an encoding bug I don't properly understand
+  // "ブ".length => 1
+  // "ブ".length => 2
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/normalize
+  dir = dir.normalize('NFC');
+
   req.dir = dir;
   res.locals.folder.redirect = req.path;
 
