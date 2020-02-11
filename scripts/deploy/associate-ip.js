1101ed10c49fe0b08d59d7a338a1ddf655a214e5
David Merfield <dmerfield@gmail.com>
2020-02-11 18:05:43 -0500

    Updates script to associate instance and IP

--- scripts/deploy/associate-ip.js
+++ scripts/deploy/associate-ip.js
@@ -12,14 +12,39 @@ var ec2 = new AWS.EC2({ region: process.env.AWS_REGION });
 // Configure Route 53 to point a host to this elastic IP
 // Create A records
 const AllocationId = process.env.AWS_DEPLOYMENT_ELASTIC_IP_ID;
-const InstanceId = process.env.AWS_DEPLOYMENT_INSTANCE_ID;
 
-var params = {
-	AllocationId,
-	InstanceId
-};
+ec2.describeInstances({}, function(err, data) {
+	if (err) throw err;
 
-ec2.associateAddress(params, function(err, data) {
-	if (err) console.log(err, err.stack);
-	else console.log(data);
+	let instances = [];
+
+	data.Reservations.forEach(function(res) {
+		instances = instances.concat(res.Instances);
+	});
+
+	const InstanceId = instances
+
+		// Find instances created by the deploy script
+		// (we add these tags in launch-instance.js)
+		.filter(({ Tags }) => {
+			return Tags.filter(tag => tag.Key === "deployed" && tag.Value === "true")
+				.length;
+		})
+
+		// Find instances that are not terminated
+		.filter(({ State: { Name } }) => {
+			return Name === "running";
+		})
+
+		.pop().InstanceId;
+
+	var params = {
+		AllocationId,
+		InstanceId
+	};
+
+	ec2.associateAddress(params, function(err, data) {
+		if (err) console.log(err, err.stack);
+		else console.log(data);
+	});
 });
