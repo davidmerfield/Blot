e6498ebc288f3c32e9965ea063030f22d56c823c
David Merfield <dmerfield@gmail.com>
2020-03-23 20:42:40 -0400

    Updates color picker

--- app/dashboard/routes/template-editor/load/color-inputs.js
+++ app/dashboard/routes/template-editor/load/color-inputs.js
@@ -2,8 +2,14 @@ module.exports = function(req, res, next) {
 	res.locals.colors = Object.keys(req.template.locals)
 		.filter(key => key.indexOf("_color") > -1)
 		.map(key => {
-			return { key, value: req.template.locals[key], label: key };
+			return { key, value: req.template.locals[key], label: desnake(key) };
 		});
 
 	return next();
 };
+
+function desnake(str) {
+	str = str.split("_").join(" ");
+	str = str[0].toUpperCase() + str.slice(1);
+	return str;
+}
