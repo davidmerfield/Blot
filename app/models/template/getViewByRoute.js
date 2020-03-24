33adcb1fb67ccfa7ed072f7422fe93b462ce0e14
David Merfield <dmerfield@gmail.com>
2020-03-24 18:49:38 -0400

    Updates method to get a view by route

--- app/models/template/getViewByRoute.js
+++ app/models/template/getViewByRoute.js
@@ -1,13 +1,33 @@
-var key = require("./key");
-var client = require("client");
-var Route = require("route-parser");
+const key = require("./key");
+const client = require("client");
+const Route = require("route-parser");
+const debug = require("debug")("template:getViewByRoute");
 
 module.exports = function getViewByRoute(templateID, url, callback) {
+	debug(templateID, url);
+
 	client.hgetall(key.routes(templateID), function(err, routes) {
+		if (err || !routes) {
+			debug("no routes set for template");
+			return callback(null);
+		}
+
+		debug(routes);
+
+		if (routes[url]) {
+			debug("matched exactly", routes[url]);
+			return callback(null, routes[url], {});
+		}
+
 		for (let route in routes) {
+			debug(route);
 			const match = new Route(route).match(url);
+
+			debug("match", match);
 			if (match) return callback(null, routes[route], match);
 		}
+		
+		debug("failed to find a match");
 		return callback(null);
 	});
 };
