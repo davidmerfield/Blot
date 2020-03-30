b953297a1ebe951a67b0d74e005da06becc680b2
David Merfield <dmerfield@gmail.com>
2020-03-30 19:46:32 -0400

    Add method to format an entry's updated and created dates

--- app/blog/render/load/augment.js
+++ app/blog/render/load/augment.js
@@ -14,6 +14,8 @@ module.exports = function(req, res) {
 
   return function(entry) {
     entry.formatDate = FormatDate(entry.dateStamp, req.blog.timeZone);
+    entry.formatUpdated = FormatDate(entry.updated, req.blog.timeZone);
+    entry.formatCreated = FormatDate(entry.created, req.blog.timeZone);
     entry.absoluteURL =
       req.blog.locals.blogURL +
       entry.url
