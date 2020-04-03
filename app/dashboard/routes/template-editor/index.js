ad326c2ea860d9649418a12f47d0ac919ee9357f
David Merfield <dmerfield@gmail.com>
2020-04-03 14:28:55 -0400

    Adds rename/create to template views

--- app/dashboard/routes/template-editor/index.js
+++ app/dashboard/routes/template-editor/index.js
@@ -5,7 +5,6 @@ const bodyParser = require("body-parser").urlencoded({ extended: false });
 const helper = require("helper");
 const formJSON = helper.formJSON;
 const Template = require("template");
-const Blog = require("blog");
 
 TemplateEditor.use(function(req, res, next) {
 	res.locals.partials.color = "template-editor/inputs/color";
@@ -54,77 +53,13 @@ TemplateEditor.route("/:templateSlug/settings")
 		res.render("template-editor/layout");
 	});
 
-TemplateEditor.route("/:templateSlug/source-code")
-	.all(require("./load/template-views"))
-	.get(function(req, res) {
-		if (res.locals.views[0] && res.locals.views[0].name) {
-			return res.redirect(
-				res.locals.base + "/source-code/" + res.locals.views[0].name + "/edit"
-			);
-		}
-
-		res.locals.partials.yield = "template-editor/source-code-edit";
-		res.locals.partials.sidebar = "template-editor/source-code-sidebar";
-		res.render("template-editor/layout");
-	});
-
-TemplateEditor.route("/:templateSlug/source-code/:viewSlug/edit/").get(function(
-	req,
-	res
-) {
-	res.locals.partials.yield = "template-editor/source-code-edit";
-	res.locals.partials.sidebar = "template-editor/source-code-sidebar";
-	res.render("template-editor/layout");
-});
-
-TemplateEditor.route("/:templateSlug/source-code/:viewSlug/preview/").get(
-	function(req, res) {
-		res.locals.partials.yield = "template-editor/source-code-preview";
-		res.locals.partials.sidebar = "template-editor/source-code-sidebar";
-		res.render("template-editor/layout");
-	}
-);
-
-TemplateEditor.route("/:templateSlug/source-code/:viewSlug/delete").post(
-	bodyParser,
-	function(req, res, next) {
-		Template.dropView(req.template.id, req.view.name, function(err) {
-			if (err) return next(err);
-			res.redirect(res.locals.base + "/source-code");
-		});
-	}
-);
-
-TemplateEditor.route("/:templateSlug/source-code/:viewSlug/save").post(
-	bodyParser,
-	function(req, res, next) {
-		var view = formJSON(req.body, Template.viewModel);
-
-		view.name = req.view.name;
-
-		Template.setView(req.template.id, view, function(err) {
-			if (err) return next(err);
-
-			var now = Date.now();
-			var changes = {
-				cacheID: now,
-				cssURL: "/style.css?" + now,
-				scriptURL: "/script.js?" + now,
-			};
-
-			Blog.set(req.blog.id, changes, function(err) {
-				if (err) return next(err);
-				res.send("Saved changes!");
-			});
-		});
-	}
-);
-
 TemplateEditor.use("/:templateSlug/delete", require("./deleteTemplate"));
 
 TemplateEditor.use(function(err, req, res, next) {
 	console.log(err);
 	return next(err);
 });
+
+TemplateEditor.use("/:templateSlug/source-code", require("./source-code"));
 
 module.exports = TemplateEditor;
