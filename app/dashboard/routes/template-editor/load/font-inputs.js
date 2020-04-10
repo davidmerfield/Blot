e670a5498e24b6489a7b225357bf3a35c1b2cb37
David Merfield <dmerfield@gmail.com>
2020-04-10 11:32:53 -0400

    Updates template editor routes to save font choices

--- app/dashboard/routes/template-editor/load/font-inputs.js
+++ app/dashboard/routes/template-editor/load/font-inputs.js
@@ -1,18 +1,35 @@
+const FONTS = require("../../../../blog/static/fonts");
+
 module.exports = function(req, res, next) {
-	res.locals.partials.font = 'template-editor/inputs/font';
+	res.locals.partials.font = "template-editor/inputs/font";
 	res.locals.fonts = Object.keys(req.template.locals)
-		.filter(key => key.indexOf("_font") > -1)
-		.map(key => {
-			return { key, value: req.template.locals[key], label: desnake(key) };
+		.filter((key) => key.indexOf("_font") > -1)
+		.map((key) => {
+			return {
+				key,
+				options: FONTS.map((option) => {
+					return {
+						selected:
+							req.template.locals[key].id &&
+							option.id === req.template.locals[key].id
+								? "selected"
+								: "",
+						name: option.name,
+						id: option.id,
+					};
+				}),
+				value: req.template.locals[key],
+				label: desnake(key),
+			};
 		});
 
 	return next();
 };
 
 // function (req, res, next) {
-// 	let fontChanges = 
-	
-// 	if (changes.)	
+// 	let fontChanges =
+
+// 	if (changes.)
 
 // 	return next()
 // }
--- app/dashboard/routes/template-editor/save/fonts.js
+++ app/dashboard/routes/template-editor/save/fonts.js
@@ -1,87 +1,20 @@
-module.exports = function(req, res, next) {
+const FONTS = require("../../../../blog/static/fonts");
 
+module.exports = function(req, res, next) {
+	console.log(req.locals);
 
 	for (let key in req.locals) {
 		if (key.indexOf("_font") === -1) continue;
 
 		let fontName = req.locals[key];
+		let match = FONTS.slice().filter(({ id }) => fontName === id)[0];
 
-		if (fonts[fontName]) {
-			req.locals[key + '_files'] = fonts[fontName].files;
-			req.locals[key] = fonts[fontName].stack;
-		}
+		if (match) {
+			req.locals[key] = match;
+		} 
 	}
 
 	console.log(req.locals);
-	console.log(req.partials);
 
 	next();
 };
-
-const fonts = {
-	Wremena: {
-		stack: "Wremena, serif",
-		files: `
-		@font-face {
-		  font-family: 'Wremena';
-		  src: url('/fonts/wremena/regular.otf'); /* IE9 Compat Modes */
-		  src: url('/fonts/wremena/regular.otf') format('embedded-opentype'), /* IE6-IE8 */
-		       url('/fonts/wremena/regular.woff') format('woff');
-		}
-
-		@font-face {
-		  font-family: 'Wremena';
-		  font-weight: 300;
-		  src: url('/fonts/wremena/light.otf'); /* IE9 Compat Modes */
-		  src: url('/fonts/wremena/light.otf') format('embedded-opentype'), /* IE6-IE8 */
-		       url('/fonts/wremena/light.woff') format('woff');
-		}
-
-		@font-face {
-		  font-family: 'Wremena';
-		  font-weight: bold;
-		  src: url('/fonts/wremena/bold.otf'); /* IE9 Compat Modes */
-		  src: url('/fonts/wremena/bold.otf') format('embedded-opentype'), /* IE6-IE8 */
-		       url('/fonts/wremena/bold.woff') format('woff');
-		}
-
-		`,
-	},
-};
-
-// Helvetica:
-//  stack: 'helvetica neue', helvetica, sans-serif
-
-// Avenir:
-// 	stack: 'avenir next', avenir, sans-serif
-
-// System Sans:
-// 	stack:
-// 	-apple-system, BlinkMacSystemFont, 'avenir next', avenir, 'helvetica neue', helvetica, ubuntu, roboto, noto, 'segoe ui', arial, sans-serif
-
-// Athelas:
-// 	stack: athelas,georgia,serif
-
-// Georgia:
-// 	stack: georgia,serif
-
-// Times:
-// 	stack: times,serif
-
-// Bodoni:
-// 	stack: 'Bodoni MT', serif
-
-// Calisto:
-// 	stack: 'Calisto MT', serif
-
-// Garamond:
-// 	stack: garamond, serif
-
-// Baskerville:
-// 	stack: baskerville,serif
-
-// Code:
-// 	stack: Consolas, monaco, monospace
-
-// Courier:
-// 	stack: 'Courier Next', courier, monospace
