03169ddb91d9c1627165feb8cf4f2fbafdfb0d0d
David Merfield <dmerfield@gmail.com>
2020-04-18 15:13:43 -0400

    Updated notes router

--- app/brochure/routes/notes.js
+++ app/brochure/routes/notes.js
@@ -1,16 +1,31 @@
 var Express = require("express");
 var notes = new Express.Router();
 var marked = require("marked");
-const fs = require("fs");
+const fs = require("fs-extra");
 const NOTES_DIRECTORY = require("helper").rootDir + "/notes";
 const path = require("path");
 
-const removeIgnorableItems = name => name[0] !== "." && name[0] !== "_";
+const HASH_TITLE_REGEX = /\# (.*)/;
+const DASH_TITLE_REGEX = /(.*)\n-+\n/;
 
-const extractName = filePath =>
-  fs.readFileSync(filePath, "utf-8").split("# ")[0];
+const removeIgnorableItems = (name) =>
+  name[0] !== "." && name[0] !== "_" && name !== "README";
 
-const withoutExtension = name => path.parse("/" + name).name;
+const extractName = (filePath) => {
+  const contents = fs.readFileSync(filePath, "utf-8");
+  const hashTitle = HASH_TITLE_REGEX.exec(contents);
+  const dashTitle = DASH_TITLE_REGEX.exec(contents);
+
+  if (hashTitle && hashTitle[1]) {
+    return hashTitle[1];
+  } else if (dashTitle && dashTitle[1]) {
+    return dashTitle[1];
+  } else {
+    return filePath.split("/").pop();
+  }
+};
+
+const withoutExtension = (name) => path.parse("/" + name).name;
 
 notes.use(function(req, res, next) {
   res.locals.base = "/notes";
@@ -19,32 +34,37 @@ notes.use(function(req, res, next) {
   next();
 });
 
+const TOC = fs
+  .readdirSync(NOTES_DIRECTORY)
+  .filter(removeIgnorableItems)
+  .map((section) => {
+    return {
+      name: section[0].toUpperCase() + section.slice(1),
+      id: section,
+      items: fs
+        .readdirSync(NOTES_DIRECTORY + "/" + section)
+        .filter(removeIgnorableItems)
+        .map((article) => {
+          return {
+            name: extractName(NOTES_DIRECTORY + "/" + section + "/" + article),
+            id: article,
+            slug:
+              "/notes/" +
+              withoutExtension(section) +
+              "/" +
+              withoutExtension(article),
+          };
+        }),
+      slug: "/notes/" + path.parse("/" + section).name,
+    };
+  });
+
 notes.param("section", function(req, res, next) {
   res.locals.selected[req.params.section] = "selected";
-  res.locals.toc = fs
-    .readdirSync(NOTES_DIRECTORY)
-    .filter(removeIgnorableItems)
-    .map(section => {
-      return {
-        name: section,
-        items: fs
-          .readdirSync(NOTES_DIRECTORY + "/" + section)
-          .filter(removeIgnorableItems)
-          .map(article => {
-            return {
-              name: extractName(
-                NOTES_DIRECTORY + "/" + section + "/" + article
-              ),
-              slug:
-                "/notes/" +
-                withoutExtension(section) +
-                "/" +
-                withoutExtension(article)
-            };
-          }),
-        slug: "/notes/" + path.parse("/" + section).name
-      };
-    });
+  res.locals.toc = TOC.map((section) => {
+    section.isSelected = req.params.section === section.id ? "selected" : false;
+    return section;
+  });
   res.locals.section = "/notes/" + req.params.section;
   next();
 });
@@ -56,12 +76,23 @@ notes.param("article", function(req, res, next) {
 
 notes.get("/", function(req, res, next) {
   res.locals.title = "Notes - Blot";
+  res.locals.toc = TOC;
+  next();
+});
+
+notes.get("/:section", function(req, res, next) {
+  res.locals.body = marked(
+    fs.readFileSync(
+      NOTES_DIRECTORY + "/" + req.params.section + "/README",
+      "utf-8"
+    )
+  );
   next();
 });
 
 notes.get("/:section/:article", function(req, res, next) {
   res.locals.body = marked(
-    require("fs").readFileSync(
+    fs.readFileSync(
       NOTES_DIRECTORY +
         "/" +
         req.params.section +
