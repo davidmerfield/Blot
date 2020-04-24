829b50239725e196dc9be851ceccc89dbf65657c
David Merfield <dmerfield@gmail.com>
2020-04-24 12:07:30 -0400

    Refactor transform gif

--- app/build/thumbnail/transform-gif.js
+++ app/build/thumbnail/transform-gif.js
@@ -1,20 +1,18 @@
 const { execFile } = require("child_process");
-var helper = require("helper");
-var callOnce = helper.callOnce;
-var extname = require("path").extname;
-var async = require("async");
-var gifsicle = require("gifsicle");
+const helper = require("helper");
+const callOnce = helper.callOnce;
+const extname = require("path").extname;
+const async = require("async");
+const gifsicle = require("gifsicle");
 const sharp = require("sharp");
-var thumbnails = require("./config").THUMBNAILS;
+const thumbnails = require("./config").THUMBNAILS;
 
 // Eventually remove this once Sharp supports animated gifs
 // https://github.com/lovell/sharp/issues/1372
 // https://github.com/lovell/sharp/pull/2012
 
 function main(path, outputDirectory, callback) {
-	var result = {};
-
-	console.log("here", path, outputDirectory);
+	let result = {};
 
 	callback = callOnce(callback);
 
@@ -27,9 +25,9 @@ function main(path, outputDirectory, callback) {
 			// I couldn't work out how to handle filenames like ex?yz.jpg
 			// Should I store the name URL-encoded (e.g. ex%3Fyz.jpg)...
 			// Now I just use the guuid + size + file extension.
-			var extension = extname(path).toLowerCase();
-			var fileName = name.toLowerCase() + extension;
-			var to = outputDirectory + "/" + fileName;
+			const extension = extname(path).toLowerCase();
+			const fileName = name.toLowerCase() + extension;
+			const to = outputDirectory + "/" + fileName;
 
 			transform(path, to, options, function(err, width, height) {
 				if (err) return next(err);
@@ -49,9 +47,8 @@ function main(path, outputDirectory, callback) {
 	);
 }
 
-function transform(path, to, options, callback) {
-	var size = options.size;
-	var args = [
+function transform(path, to, { size }, callback) {
+	const args = [
 		"--resize-fit-height",
 		size,
 		"--resize-fit-width",
@@ -64,8 +61,8 @@ function transform(path, to, options, callback) {
 		if (err) return callback(err);
 		sharp(to)
 			.metadata()
-			.then(function(metadata) {
-				callback(err, metadata.width, metadata.height);
+			.then(function({ width, height }) {
+				callback(err, width, height);
 			})
 			.catch(function(err) {
 				callback(err);
