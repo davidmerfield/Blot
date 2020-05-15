528f6c3f55a972391a25c83dfeebc52daf3faf0e
David Merfield <dmerfield@gmail.com>
2020-05-15 18:28:27 -0400

    Updates screenshot script

--- scripts/template/screenshots.js
+++ scripts/template/screenshots.js
@@ -3,6 +3,8 @@ const screenshot = helper.screenshot;
 const hash = helper.hash;
 const async = require("async");
 const config = require("config");
+const VIEW_DIRECTORY = helper.rootDir + "/app/brochure/views/templates";
+
 async.eachOfSeries(
 	{
 		diary: {
@@ -28,13 +30,19 @@ async.eachOfSeries(
 	},
 	function({ handle, pages }, template, next) {
 		const baseURL = `https://preview-of-${template}-on-${handle}.${config.host}`;
-		async.eachSeries(pages, function(page, next){
-			const url = baseURL + pages;
-			const path = `${__dirname}/${hash(url)}.png`;
-			console.log(url);
-			console.log(path);
-			screenshot(url, path, next);
-		}, next);
+		async.eachSeries(
+			pages,
+			function(page, next) {
+				const url = baseURL + pages;
+				const path = `${VIEW_DIRECTORY}/${template}/${handle}-${pages.indexOf(
+					page
+				)}.png`;
+				console.log(url);
+				console.log(path);
+				screenshot(url, path, next);
+			},
+			next
+		);
 	},
 	function(err) {
 		if (err) throw err;
