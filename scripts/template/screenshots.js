e49670cd2e4bca4ab8c161234c80cf6a9c219ded
David Merfield <dmerfield@gmail.com>
2020-05-16 15:17:13 -0400

    Rename screenshots

--- scripts/template/screenshots.js
+++ scripts/template/screenshots.js
@@ -4,36 +4,38 @@ const async = require("async");
 const config = require("config");
 const VIEW_DIRECTORY = helper.rootDir + "/app/brochure/views/templates";
 
-async.eachOfSeries(
-	{
-		diary: {
-			handle: "bjorn",
-			pages: ["/", "/search?q=fishing"],
-		},
-		essay: {
-			handle: "interviews",
-			pages: ["/", "/ingrid-newkirk"],
-		},
-		magazine: {
-			handle: "interviews",
-			pages: ["/", "/archives"],
-		},
-		picture: {
-			handle: "bjorn",
-			pages: ["/", "/archives"],
-		},
-		scrapbook: {
-			handle: "ferox",
-			pages: ["/", "/iems-l13-3"],
-		},
+const SCREENSHOTS = {
+	diary: {
+		handle: "bjorn",
+		pages: ["/", "/search?q=fishing"],
 	},
+	essay: {
+		handle: "interviews",
+		pages: ["/", "/ingrid-newkirk"],
+	},
+	magazine: {
+		handle: "interviews",
+		pages: ["/", "/archives"],
+	},
+	picture: {
+		handle: "bjorn",
+		pages: ["/", "/archives"],
+	},
+	scrapbook: {
+		handle: "ferox",
+		pages: ["/", "/iems-l13-3"],
+	},
+};
+
+async.eachOfSeries(
+	SCREENSHOTS,
 	function({ handle, pages }, template, next) {
 		const baseURL = `https://preview-of-${template}-on-${handle}.${config.host}`;
 		async.eachSeries(
 			pages,
 			function(page, next) {
 				const url = baseURL + page;
-				const path = `${VIEW_DIRECTORY}/${template}/${handle}-${pages.indexOf(
+				const path = `${VIEW_DIRECTORY}/${template}/${pages.indexOf(
 					page
 				)}.png`;
 				console.log(url);
