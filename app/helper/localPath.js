1a383679f08f870dbcf39d2abe89aa48356212e3
David Merfield <dmerfield@gmail.com>
2020-06-30 18:35:48 -0400

    Refactors local path to prevent access outside blog folder

--- app/helper/localPath.js
+++ app/helper/localPath.js
@@ -1,26 +1,29 @@
 var blogDir = require("./blogDir");
 var ensure = require("./ensure");
-var normalize = require("./pathNormalizer");
-var joinPath = require("path").join;
+var resolve = require("path").resolve;
+var join = require("path").join;
 
 // This takes a blog ID and a file
 // path and returns the path to the file
 // on the server.
 
 module.exports = function(blogID, path) {
-  ensure(blogID, "string").and(path, "string");
+	ensure(blogID, "string").and(path, "string");
 
-  if (!path) return "";
+	if (!path) path = "/";
 
-  path = path.trim();
+	const root = join(blogDir, blogID);
 
-  path = path.split("//").join("/");
+	path = path.trim();
+	path = path.split("//").join("/");
+	// Remove trailing slash
+	if (path.slice(-1) === "/") path = path.slice(0, -1);
 
-  // Remove trailing slash
-  if (path.slice(-1) === "/") path = path.slice(0, -1);
+	// Add leading slash
+	if (path[0] !== "/") path = "/" + path;
 
-  // Add leading slash
-  if (path[0] !== "/") path = "/" + path;
+	path = resolve(root, path);
+	path = join(root, path);
 
-  return joinPath(blogDir, blogID, path);
+	return path;
 };
