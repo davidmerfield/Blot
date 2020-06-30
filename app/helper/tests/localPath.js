963c7c950a9a182c8dde306207633d4bbb021ff4
David Merfield <dmerfield@gmail.com>
2020-06-30 18:35:01 -0400

    More test cases for localPath

--- app/helper/tests/localPath.js
+++ app/helper/tests/localPath.js
@@ -1,35 +1,51 @@
-describe("localPath", function(){
-
-	const localPath = require('../localPath');
+describe("localPath", function() {
+	const localPath = require("../localPath");
 	const BLOG_ID = "XYZ";
-		const BLOG_DIR = require("../blogDir");
+	const BLOG_DIR = require("../blogDir");
 
-	it("resolves a local path", function(){
-		expect(localPath(BLOG_ID, "/foo")).toEqual(`${BLOG_DIR}/${BLOG_ID}/foo`);
-	});
-
-	it("resolves root of blog folder with trailing slash", function(){
-		expect(localPath(BLOG_ID, "/")).toEqual(`${BLOG_DIR}/${BLOG_ID}/`);
-	});
-
-	it("resolves root of blog folder when passed an empty string", function(){
+	it("resolves root of blog folder when passed an empty string", function() {
 		expect(localPath(BLOG_ID, "")).toEqual(`${BLOG_DIR}/${BLOG_ID}/`);
 	});
 
-	it("resolves a local path without leading slash", function(){
+	it("resolves a local path", function() {
+		expect(localPath(BLOG_ID, "/foo")).toEqual(`${BLOG_DIR}/${BLOG_ID}/foo`);
+	});
+
+	it("resolves root of blog folder with trailing slash", function() {
+		expect(localPath(BLOG_ID, "/")).toEqual(`${BLOG_DIR}/${BLOG_ID}/`);
+	});
+
+	it("resolves paths with a dot and a slash", function() {
+		expect(localPath(BLOG_ID, "./bar")).toEqual(`${BLOG_DIR}/${BLOG_ID}/bar`);
+	});
+
+	it("resolves a local path without leading slash", function() {
 		expect(localPath(BLOG_ID, "foo")).toEqual(`${BLOG_DIR}/${BLOG_ID}/foo`);
 	});
 
-	it("resolves a local path with trailing slash", function(){
+	it("resolves a local path with trailing slash", function() {
 		expect(localPath(BLOG_ID, "foo/")).toEqual(`${BLOG_DIR}/${BLOG_ID}/foo`);
 	});
 
-	it("resolves a local path with dots inside the blog folder", function(){
-		expect(localPath(BLOG_ID, "/foo/bar/../baz")).toEqual(`${BLOG_DIR}/${BLOG_ID}/foo/baz`);
+	it("resolves a local path with dots inside the blog folder", function() {
+		expect(localPath(BLOG_ID, "/foo/bar/../baz")).toEqual(
+			`${BLOG_DIR}/${BLOG_ID}/foo/baz`
+		);
 	});
 
-	it("will not resolves a local path with dots outside the blog folder", function(){
+	it("will not resolves a local path with dots outside the blog folder", function() {
+		expect(localPath(BLOG_ID, "/foo/bar/../baz/../../../")).toEqual(
+			`${BLOG_DIR}/${BLOG_ID}/`
+		);
+	});
+
+	it("will not resolves a local path with dots outside the blog folder", function() {
 		expect(localPath(BLOG_ID, "/../../")).toEqual(`${BLOG_DIR}/${BLOG_ID}/`);
 	});
 
-})
+	it("will not resolves a local path outside the blog folder", function() {
+		expect(localPath(BLOG_ID, "/Users/David/Projects/blot/blogs")).toEqual(
+			`${BLOG_DIR}/${BLOG_ID}/Users/David/Projects/blot/blogs`
+		);
+	});
+});
