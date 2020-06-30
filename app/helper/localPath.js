44cf27da98d59469b0a58168f4d5613c81f2fd12
David Merfield <dmerfield@gmail.com>
2020-06-30 17:21:34 -0400

    Prevent the reading of paths outside the blog folder

--- app/helper/localPath.js
+++ app/helper/localPath.js
@@ -1,26 +1,32 @@
 var blogDir = require("./blogDir");
 var ensure = require("./ensure");
-var normalize = require("./pathNormalizer");
-var joinPath = require("path").join;
+var resolve = require("path").resolve;
 
 // This takes a blog ID and a file
 // path and returns the path to the file
 // on the server.
 
 module.exports = function(blogID, path) {
-  ensure(blogID, "string").and(path, "string");
+	ensure(blogID, "string").and(path, "string");
 
-  if (!path) return "";
+	const root = resolve(blogDir, blogID);
 
-  path = path.trim();
+	if (!path) return "";
 
-  path = path.split("//").join("/");
+	path = path.trim();
 
-  // Remove trailing slash
-  if (path.slice(-1) === "/") path = path.slice(0, -1);
+	path = path.split("//").join("/");
 
-  // Add leading slash
-  if (path[0] !== "/") path = "/" + path;
+	// Remove trailing slash
+	if (path.slice(-1) === "/") path = path.slice(0, -1);
 
-  return joinPath(blogDir, blogID, path);
+	// Add leading slash
+	if (path[0] !== "/") path = "/" + path;
+
+	path = resolve(blogDir, blogID, path);
+
+	// Ensure the resulting path is inside the blog folder!
+	if (path.indexOf(root) !== 0) path = root;
+
+	return path;
 };
