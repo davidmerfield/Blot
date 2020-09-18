9a8e20d5a68dd9899935c003e1a4e5634f0ada0d
David Merfield <dmerfield@gmail.com>
2020-09-18 13:20:27 -0400

    Adds features to folder listing property in templates

--- app/blog/render/retrieve/contents.js
+++ app/blog/render/retrieve/contents.js
@@ -1,22 +1,60 @@
 const fs = require("fs-extra");
 const helper = require("helper");
 const localPath = helper.localPath;
+const async = require("async");
+const Entry = require("entry");
+const Path = require('path');
 
-module.exports = function(req, callback) {
+module.exports = function (req, callback) {
 	let path = "/";
 
 	if (req.query.path) {
 		path = req.query.path;
 	}
 
-	fs.readdir(localPath(req.blog.id, path), function(err, contents) {
+	fs.readdir(localPath(req.blog.id, path), function (err, contents) {
 		// We should pass this error back to the template in future
 		if (err) return callback(null, []);
 
-		contents = contents.map((name) => {
-			return { name: name };
-		});
+		async.mapLimit(
+			contents,
+			5,
+			function (name, next) {
 
-		callback(null, contents);
+				let fullPathToItem = Path.join(path, name);
+
+				async.parallel(
+					[
+						function (done) {
+							fs.stat(localPath(req.blog.id, fullPathToItem), function (err, stat) {
+								if (err) return done(err);
+								done(null, { stat });
+							});
+						},
+						function (done) {
+							console.log(req.blog.id, path + "/" + name);
+
+							Entry.get(req.blog.id, path + "/" + name, function (entry) {
+								done(null, { entry });
+							});
+						},
+					],
+					function (err, results) {
+						if (err) return next(err);
+						next(null, {
+							name: name,
+							path: fullPathToItem,
+							stat: results[0].stat,
+							entry: results[1].entry,
+						});
+					}
+				);
+			},
+			function (err, contents) {
+				console.log(err, contents)
+				if (err) return callback(null, []);
+				callback(null, contents);
+			}
+		);
 	});
 };
