54ed1cdcc190a64d35a398f22d8944eb06b69ce6
David Merfield <dmerfield@gmail.com>
2020-10-01 12:48:07 -0400

    Minor tweak

--- app/clients/dropbox/routes/createFolder.js
+++ app/clients/dropbox/routes/createFolder.js
@@ -1,16 +1,16 @@
 var createClient = require("../util/createClient");
-var titleToFolder = require('./titleToFolder');
+var titleToFolder = require("./titleToFolder");
 
-module.exports = function(req, res, next) {
+module.exports = function (req, res, next) {
   if (req.unsavedAccount.full_access === false && !req.otherBlogsUseAppFolder)
     return next();
 
   var client = createClient(req.unsavedAccount.access_token);
-  var folder = titleToFolder(req.blog.title);
+  var folder = "/" + titleToFolder(req.blog.title);
 
   client
-    .filesCreateFolder({ path: "/" + folder, autorename: true })
-    .then(function(res) {
+    .filesCreateFolder({ path: folder, autorename: true })
+    .then(function (res) {
       req.unsavedAccount.folder = res.path_display;
       req.unsavedAccount.folder_id = res.id;
 
