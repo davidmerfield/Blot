6615bf9e4426009919aed6221d910eb674121b51
David Merfield <dmerfield@gmail.com>
2020-10-09 11:51:53 -0400

    Adds wikilinks plugin

---
+++ app/build/plugins/wikilinks/index.js
@@ -0,0 +1,13 @@
+function render($, callback) {
+
+	// for each text node
+
+  callback();
+}
+
+module.exports = {
+  render: render,
+  category: "Typography",
+  title: "Wikilinks",
+  description: "Turns Wikilinks into links"
+};
