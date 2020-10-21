663e8d73d7959246a91b79b1fc5ead72fc34d927
David Merfield <dmerfield@gmail.com>
2020-10-21 18:58:00 -0400

    Adds zooming view to pictures template

--- app/templates/latest/pictures/script.js
+++ app/templates/latest/pictures/script.js
@@ -1,1 +1,49 @@
 {{{appJS}}}
+
+
+// This mess works out the correct breakpoint at which the nav will fail
+
+// First, we temporarily reset the breakpoint width to make proper measurements further down
+for (var i = 0; i < document.styleSheets[0].rules.length; i++){
+  var rule = document.styleSheets[0].rules[i];
+  if (!rule.media) continue;
+  var mediaText = rule.media.mediaText;
+  if (mediaText.indexOf('(') === -1 || mediaText.indexOf(')') === -1) continue;
+  var condition = mediaText.slice(mediaText.indexOf('(') + 1, mediaText.lastIndexOf(')'));
+  var breakpoint = condition.slice(condition.indexOf(':') + 1).trim();
+  rule.media.mediaText = rule.media.mediaText.split(breakpoint).join('0px');
+  console.log(rule.media.mediaText);
+}
+
+var nav = document.querySelector('.navigation-container').cloneNode(true);
+
+var testerWrapper = document.createElement("div")
+testerWrapper.style.position = 'absolute';
+testerWrapper.style.bottom = '-100px';
+testerWrapper.style.left = '0';
+testerWrapper.style.width = '10000px';
+
+var tester = document.createElement("div")
+
+tester.style.position = 'absolute';
+tester.style.top = '0';
+tester.style.left = '0';
+
+tester.appendChild(nav);
+testerWrapper.appendChild(tester);
+document.body.appendChild(testerWrapper);
+
+var actual_breakpoint = tester.offsetWidth;
+document.body.removeChild(testerWrapper);
+
+var totalRules = document.styleSheets[0].rules.length;
+for (var i = 0; i < totalRules; i++){
+  var rule = document.styleSheets[0].rules[i];
+  if (!rule.media) continue;
+  var mediaText = rule.media.mediaText;
+  if (mediaText.indexOf('(') === -1 || mediaText.indexOf(')') === -1) continue;
+  var condition = mediaText.slice(mediaText.indexOf('(') + 1, mediaText.lastIndexOf(')'));
+  var breakpoint = condition.slice(condition.indexOf(':') + 1).trim();
+  rule.media.mediaText = rule.media.mediaText.split(breakpoint).join(actual_breakpoint + 'px');
+}
+
