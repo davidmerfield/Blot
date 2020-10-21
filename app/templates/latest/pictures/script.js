308a3436bb00774d8221c738461b87b9b8bc14d5
David Merfield <dmerfield@gmail.com>
2020-10-21 19:47:36 -0400

    Adds cursor-tracking hover to navigation links

--- app/templates/latest/pictures/script.js
+++ app/templates/latest/pictures/script.js
@@ -47,3 +47,13 @@ for (var i = 0; i < totalRules; i++){
   rule.media.mediaText = rule.media.mediaText.split(breakpoint).join(actual_breakpoint + 'px');
 }
 
+
+
+let btn = document.querySelector('.mouse-cursor-gradient-tracking');
+btn.onmousemove = function(e) {
+  let rect = e.target.getBoundingClientRect();
+  let x = e.clientX - rect.left;
+  let y = e.clientY - rect.top;
+  btn.style.setProperty('--x', x + 'px');
+  btn.style.setProperty('--y', y + 'px');
+}
