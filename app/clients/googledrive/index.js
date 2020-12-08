fd8da73192996c88085d8accfd983089e52cdaee
David Merfield <dmerfield@gmail.com>
2020-12-08 15:58:44 -0500

    Updates google drive client

--- app/clients/googledrive/index.js
+++ app/clients/googledrive/index.js
@@ -17,9 +17,15 @@ const app = new require('express')();
 app.use(function(req, res, next){
   res.locals.base = '/clients/googledrive';
 
+  // fake blog for testing, eventually this will be the real user's blog
+  req.blog = {
+    id: 'blog_0000000000000'
+  };
+
   res.render = function(view){
     res.send(require('mustache').render(require('fs').readFileSync(__dirname + '/views/' + view + '.html', 'utf-8'), res.locals));
   };
+
   next();
 });
 app.use('/clients/googledrive', require("./routes").dashboard);
--- app/clients/googledrive/routes/dashboard.js
+++ app/clients/googledrive/routes/dashboard.js
@@ -1,73 +1,168 @@
 // https://developers.google.com/drive/api/v3/quickstart/nodejs
 
-const	express = require('express');
+const { google } = require("googleapis");
+
+const REDIRECT_URL = "http://localhost:8822/clients/googledrive/authenticate";
+
+let creds;
+
+const SCOPE = ["https://www.googleapis.com/auth/drive"];
+
+const NO_ACCOUNT_ERROR =
+	"Google Drive client: Error no account in database for blog: ";
+const INVALID_ACCOUNT_STRING =
+	"Google Drive client: Error decoding JSON for account of blog ";
+
+const client = require("client");
+
+const database = {
+	accountKey: function (blogID) {
+		return "blog:" + blogID + ":googledrive:account";
+	},
+	getAccount: function (blogID, callback) {
+		const key = this.accountKey(blogID);
+
+		client.get(key, function (err, account) {
+			if (err) {
+				return callback(err);
+			}
+
+			if (!account) {
+				return callback(new Error(NO_ACCOUNT_ERROR + blogID));
+			}
+
+			try {
+				account = JSON.parse(account);
+			} catch (e) {
+				return callback(new Error(INVALID_ACCOUNT_STRING + blogID));
+			}
+
+			callback(null, account);
+		});
+	},
+	setAccount: function (blogID, changes, callback) {
+		const key = this.accountKey(blogID);
+
+		this.getAccount(blogID, function (err, account) {
+			// we don't care if the account doesn't exist
+			// if (err) return callback(err);
+
+			account = account || {};
+
+			for (var i in changes) {
+				account[i] = changes[i];
+			}
+
+			client.set(key, JSON.stringify(account), callback);
+		});
+	},
+};
+
+database.setAccount("123", { foo: "bar" }, function (err) {
+	if (err) throw err;
+	database.getAccount("123", function (err, account) {
+		if (err) throw err;
+		console.log("got account", account);
+	});
+});
+
+// Express web server
+const express = require("express");
+
 const dashboard = new express.Router();
 
-const {google} = require('googleapis');
-
-const oauth2Client = new google.auth.OAuth2(
-  process.env.GD_CLIENT_ID,
-  process.env.GD_CLIENT_SECRET,
-  'http://localhost:8822/clients/googledrive/authenticate'
-);
-
-dashboard.get('/', function(req, res){
-	res.redirect('/setup');
+dashboard.get("/", function (req, res) {
+	res.redirect(req.path + "setup");
 });
 
-dashboard.get('/setup', function(req, res){
-	res.render('setup');
+dashboard.get("/setup", function (req, res) {
+	res.render("setup");
 });
 
-dashboard.get('/redirect', function(req,  res){
-		const url = oauth2Client.generateAuthUrl({
-	  // 'online' (default) or 'offline' (gets refresh_token)
-	  access_type: 'offline',
+dashboard.get("/redirect", function (req, res) {
+	const oauth2Client = new google.auth.OAuth2(
+		process.env.BLOT_GOOGLEDRIVE_ID,
+		process.env.BLOT_GOOGLEDRIVE_SECRET,
+		REDIRECT_URL
+	);
 
-	  // If you only need one scope you can pass it as a string
-	  scope: "https://www.googleapis.com/auth/drive.metadata.readonly"
+	// 'online' (default) or 'offline' (gets refresh_token)
+	const url = oauth2Client.generateAuthUrl({
+		access_type: "offline",
+		scope: SCOPE,
 	});
-		res.redirect(url);
+
+	res.redirect(url);
 });
 
-dashboard.get('/authenticate', function(req, res){
+dashboard.get("/authenticate", function (req, res, next) {
+	const oauth2Client = new google.auth.OAuth2(
+		process.env.BLOT_GOOGLEDRIVE_ID,
+		process.env.BLOT_GOOGLEDRIVE_SECRET,
+		REDIRECT_URL
+	);
+
 	// This will provide an object with the access_token and refresh_token.
 	// Save these somewhere safe so they can be used at a later time.
-	oauth2Client.getToken(req.query.code).then(function(res){
-			
-		// we need to persist part of this response
-		// use it to generate new tokens in future
-		console.log(res);
+	oauth2Client
+		.getToken(req.query.code)
+		.then(function (account) {
+			console.log("setting credentials", account.tokens);
+			creds = account.tokens;
+			oauth2Client.setCredentials(account.tokens);
+			listFiles(oauth2Client);
 
-		oauth2Client.setCredentials(res.tokens);
+			database.setAccount(req.blog.id, account, function (err) {
+				if (err) return next(err);
+				res.send("Successfully saved account for " + req.blog.id);
+			});
+		})
+		.catch(next);
+});
 
-		listFiles(oauth2Client);
-		res.send('Success!');
-	}).catch(function(err){
-		console.log(err);
+dashboard.get("/list", function (req, res, next) {
+	createClient(req.blog.id, function (err, drive) {
+		if (err) return next(err);
+		drive.files.list(
+			{
+				pageSize: 10,
+				fields: "nextPageToken, files(id, name)",
+			},
+			(err, response) => {
+				if (err) return next(err);
+				res.locals.files = response.data.files;
+				res.render("list");
+			}
+		);
 	});
 });
 
-function listFiles(auth) {
-  const drive = google.drive({ version: "v3", auth });
-  drive.files.list(
-    {
-      pageSize: 10,
-      fields: "nextPageToken, files(id, name)"
-    },
-    (err, res) => {
-      if (err) return console.log("The API returned an error: " + err);
-      const files = res.data.files;
-      if (files.length) {
-        console.log("Files:");
-        files.map(file => {
-          console.log(`${file.name} (${file.id})`);
-        });
-      } else {
-        console.log("No files found.");
-      }
-    }
-  );
+// Google Drive helping wrapper
+
+function createClient(blogID, callback) {
+	database.getAccount(blogID, function (err, account) {
+		if (err) return callback(err);
+
+		const oauth2Client = new google.auth.OAuth2(
+			process.env.BLOT_GOOGLEDRIVE_ID,
+			process.env.BLOT_GOOGLEDRIVE_SECRET,
+			REDIRECT_URL
+		);
+
+		oauth2Client.on("tokens", (tokens) => {
+			console.log(
+				"Google drive client: Received new tokens for blog " + blogID
+			);
+			if (tokens.refresh_token) {
+				database.setAccount(blogID, { tokens }, function (err) {
+					if (err) console.log(err);
+				});
+			}
+		});
+		oauth2Client.setCredentials(account.tokens);
+		const drive = google.drive({ version: "v3", auth: oauth2Client });
+		return callback(null, drive);
+	});
 }
 
 module.exports = dashboard;
