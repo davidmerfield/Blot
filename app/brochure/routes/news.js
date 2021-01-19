8c54a76ec8fd1fda216108141845a1f1129e2402
David Merfield <dmerfield@gmail.com>
2021-01-19 11:12:57 -0500

    Tune up newsletter subscription UX

--- app/brochure/routes/news.js
+++ app/brochure/routes/news.js
@@ -13,34 +13,36 @@ var client = require("client");
 var listKey = "newsletter:list";
 var TTL = 60 * 60 * 24; // 1 day in seconds
 
-news.get("/", loadDone, loadToDo, function(req, res) {
+news.get("/", loadDone, loadToDo, function (req, res) {
   res.render("news");
 });
 
-news.get("/archive", function(req, res) {
+news.get("/archive", function (req, res) {
   res.render("news/archive");
 });
 
-news.get("/archive/:letter", function(req, res) {
+news.get("/archive/:letter", function (req, res) {
   res.render("news/archive");
 });
 
 // The rest of these pages should not be cached
-news.use(function(req, res, next) {
+news.use(function (req, res, next) {
   res.header("Cache-Control", "no-cache");
   next();
 });
 
-news.get("/sign-up", function(req, res) {
+news.get("/sign-up", function (req, res) {
   if (req.session && req.session.newsletter_email) {
     res.locals.email = req.session.newsletter_email;
     delete req.session.newsletter_email;
+  } else {
+    return res.redirect(req.baseUrl);
   }
 
   res.render("news/sign-up");
 });
 
-news.get("/cancel", function(req, res) {
+news.get("/cancel", function (req, res) {
   if (req.session && req.session.newsletter_email) {
     res.locals.email = req.session.newsletter_email;
     delete req.session.newsletter_email;
@@ -65,7 +67,13 @@ function cancellationLink(guid) {
   return "https://" + config.host + "/news/cancel/" + guid;
 }
 
-news.post("/cancel", parse, function(req, res, next) {
+// Removes guid from visible breadcrumbs
+news.param("guid", function (req, res, next) {
+  res.locals.breadcrumbs = res.locals.breadcrumbs.slice(0, -1);
+  next();
+});
+
+news.post("/cancel", parse, function (req, res, next) {
   var cancel, email, locals;
   var guid = uuid();
 
@@ -79,13 +87,13 @@ news.post("/cancel", parse, function(req, res, next) {
   cancel = cancellationLink(guid);
   locals = { email: email, cancel: cancel };
 
-  client.sismember(listKey, email, function(err, stat) {
+  client.sismember(listKey, email, function (err, stat) {
     if (err || !stat) return next(err || new Error("No subscription found"));
 
-    client.setex(cancellationKey(guid), TTL, email, function(err) {
+    client.setex(cancellationKey(guid), TTL, email, function (err) {
       if (err) return next(err);
 
-      helper.email.NEWSLETTER_CANCELLATION_CONFIRMATION(null, locals, function(
+      helper.email.NEWSLETTER_CANCELLATION_CONFIRMATION(null, locals, function (
         err
       ) {
         if (err) return next(err);
@@ -97,40 +105,41 @@ news.post("/cancel", parse, function(req, res, next) {
   });
 });
 
-news.get("/cancel/:guid", function(req, res, next) {
+news.get("/cancel/:guid", function (req, res, next) {
   var guid = decodeURIComponent(req.params.guid);
 
-  client.get(cancellationKey(guid), function(err, email) {
+  client.get(cancellationKey(guid), function (err, email) {
     if (err || !email) return next(err || new Error("No email"));
 
-    client.srem(listKey, email, function(err) {
+    client.srem(listKey, email, function (err, removed) {
       if (err) return next(err);
 
       var locals = { email: email };
 
-      helper.email.NEWSLETTER_CANCELLATION_CONFIRMED(null, locals, function(
-        err
-      ) {
-        if (err) return next(err);
+      res.locals.title = "Cancelled";
+      res.locals.email = email;
+      res.render("news/cancelled");
 
-        client.del(cancellationKey(guid), function(err) {
-          if (err) return next(err);
-
-          res.locals.email = email;
-          res.render("news/cancelled");
-        });
-      });
+      if (removed) {
+        helper.email.NEWSLETTER_CANCELLATION_CONFIRMED(
+          null,
+          locals,
+          function () {
+            // Email confirmation sent
+          }
+        );
+      }
     });
   });
 });
 
-news.get("/confirm/:guid", function(req, res, next) {
+news.get("/confirm/:guid", function (req, res, next) {
   var guid = decodeURIComponent(req.params.guid);
 
-  client.get(confirmationKey(guid), function(err, email) {
+  client.get(confirmationKey(guid), function (err, email) {
     if (err || !email) return next(err || new Error("No email"));
 
-    client.sadd(listKey, email, function(err) {
+    client.sadd(listKey, email, function (err, added) {
       if (err) return next(err);
 
       var locals = {
@@ -138,22 +147,27 @@ news.get("/confirm/:guid", function(req, res, next) {
         cancel: "https://" + config.host + "/news/cancel",
       };
 
-      helper.email.NEWSLETTER_SUBSCRIPTION_CONFIRMED(null, locals, function(
-        err
-      ) {
-        if (err) return next(err);
+      res.locals.title = "Confirmed";
+      res.locals.email = email;
+      res.render("news/confirmed");
 
-        client.del(confirmationKey(guid), function(err) {
-          if (err) return next(err);
-          res.locals.email = email;
-          res.render("news/confirmed");
-        });
-      });
+      // The first time the user clicks the confirmation
+      // link we send out a confirmation email, subsequent
+      // clicks they just see the confirmation page.
+      if (added) {
+        helper.email.NEWSLETTER_SUBSCRIPTION_CONFIRMED(
+          null,
+          locals,
+          function () {
+            // Email confirmation sent
+          }
+        );
+      }
     });
   });
 });
 
-news.post("/sign-up", parse, function(req, res, next) {
+news.post("/sign-up", parse, function (req, res, next) {
   var confirm, email, locals;
   var guid = uuid();
 
@@ -167,10 +181,10 @@ news.post("/sign-up", parse, function(req, res, next) {
   confirm = confirmationLink(guid);
   locals = { email: email, confirm: confirm };
 
-  client.setex(confirmationKey(guid), TTL, email, function(err) {
+  client.setex(confirmationKey(guid), TTL, email, function (err) {
     if (err) return next(err);
 
-    helper.email.NEWSLETTER_SUBSCRIPTION_CONFIRMATION(null, locals, function(
+    helper.email.NEWSLETTER_SUBSCRIPTION_CONFIRMATION(null, locals, function (
       err
     ) {
       if (err) return next(err);
@@ -182,22 +196,18 @@ news.post("/sign-up", parse, function(req, res, next) {
 });
 
 function loadToDo(req, res, next) {
-  fs.readFile(__dirname + "/../../../todo.txt", "utf-8", function(err, todo) {
+  fs.readFile(__dirname + "/../../../todo.txt", "utf-8", function (err, todo) {
     if (err) return next(err);
     res.locals.todo = marked(todo);
 
     var html = res.locals.todo;
     var $ = require("cheerio").load(html);
 
-    $("ul").each(function() {
+    $("ul").each(function () {
       var ul = $(this).html();
-      var p = $(this)
-        .prev()
-        .html();
+      var p = $(this).prev().html();
 
-      $(this)
-        .prev()
-        .remove();
+      $(this).prev().remove();
       $(this).replaceWith(
         "<details><summary>" + p + "</summary><ul>" + ul + "</ul></details>"
       );
@@ -209,57 +219,33 @@ function loadToDo(req, res, next) {
 }
 
 function loadDone(req, res, next) {
-  exec("git log -100", { cwd: helper.rootDir }, function(err, output) {
+  exec("git log -100", { cwd: helper.rootDir }, function (err, output) {
     if (err) return next(err);
 
     output = output.split("\n\n");
 
     var commits = [];
 
-    output.forEach(function(item, i) {
+    output.forEach(function (item, i) {
       if (i % 2 === 0) {
         var message = output[i + 1].trim();
 
         message = message[0].toUpperCase() + message.slice(1);
 
         // Ignores merge commits since they're not useful to readers
-        if (
-          message
-            .split(" ")
-            .join("")
-            .toLowerCase()
-            .indexOf("merge") > -1
-        )
+        if (message.split(" ").join("").toLowerCase().indexOf("merge") > -1)
           return;
 
         // Ignores commits mentioning 'commit' since they're not useful to readers
-        if (
-          message
-            .split(" ")
-            .join("")
-            .toLowerCase()
-            .indexOf("commit") > -1
-        )
+        if (message.split(" ").join("").toLowerCase().indexOf("commit") > -1)
           return;
 
         // Ignores commits to todo file since there are so many of them
-        if (
-          message
-            .split(" ")
-            .join("")
-            .toLowerCase()
-            .indexOf("todo") > -1
-        )
+        if (message.split(" ").join("").toLowerCase().indexOf("todo") > -1)
           return;
 
         // Ignores commits with links since they're ugly
-        if (
-          message
-            .split(" ")
-            .join("")
-            .toLowerCase()
-            .indexOf("://") > -1
-        )
+        if (message.split(" ").join("").toLowerCase().indexOf("://") > -1)
           return;
 
         commits.push({
