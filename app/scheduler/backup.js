7e20d630654fd909eaea1ded0f43b94373b2c752
David Merfield <dmerfield@gmail.com>
2021-01-21 13:39:56 -0500

    Refactor backup code and add removal of old backups

--- app/scheduler/backup.js
+++ app/scheduler/backup.js
@@ -1,14 +1,21 @@
+var fs = require("fs-extra");
 var config = require("config");
+var Moment = require("moment");
 var redis = require("redis").createClient();
 var helper = require("helper");
-var upload = helper.upload;
-var encrypt = helper.encrypter.encrypt;
-var fs = require("fs-extra");
-var DB_PATH = require("path").resolve(__dirname + "/../../db/dump.rdb");
-
 var tmp = helper.tempDir();
-var Moment = require("moment");
+var ensure = helper.ensure;
+var encrypt = helper.encrypter.encrypt;
 var joinpath = require("path").join;
+var DB_PATH = require("path").resolve(__dirname + "/../../db/dump.rdb");
+var BUCKET = config.backup.bucket;
+var AWS = require("aws-sdk");
+var DAYS_OF_BACKUPS_TO_KEEP = 7;
+
+AWS.config.update({
+  accessKeyId: config.aws.key,
+  secretAccessKey: config.aws.secret,
+});
 
 // called from command line
 if (require.main === module) {
@@ -26,25 +33,26 @@ function backup(callback) {
     var suffix = "-encrypted-dump.rdb";
     var filename = datestring + suffix;
     var encrypted_clone_path = joinpath(tmp, filename);
-    var options = {
-      bucket: config.backup.bucket,
-      remote: filename,
-    };
 
     console.log("Backup: Encrypting database dump");
     encrypt(DB_PATH, encrypted_clone_path, function (err) {
       if (err) return callback(err);
 
       console.log("Backup: Uploading encrypted copy of dump");
-      upload(encrypted_clone_path, options, function (err) {
+      upload(encrypted_clone_path, filename, function (err) {
         if (err) return callback(err);
 
         console.log("Backup: Removing local encrypted copy of dump");
         fs.remove(encrypted_clone_path, function (err) {
           if (err) return callback(err);
 
-          console.log("Backup: Complete");
-          callback();
+          console.log("Backup: Looking up old backup to remove");
+          wipeOldBackups(function (err) {
+            if (err) return callback(err);
+
+            console.log("Backup: Complete");
+            callback();
+          });
         });
       });
     });
@@ -71,6 +79,88 @@ function saveDBToDisk(callback) {
       });
     });
   });
+}
+
+function upload(path, filename, callback) {
+  ensure(path, "string").and(filename, "string").and(callback, "function");
+
+  var root = config.environment === "development" ? "_dev/" : "";
+  var remote = joinpath(root, filename);
+  var body = fs.createReadStream(path);
+
+  var params = {
+    Bucket: BUCKET,
+    Key: remote,
+  };
+
+  var s3Client = new AWS.S3({ params: params });
+
+  s3Client.upload({ Body: body }).send(function (err) {
+    if (err) return callback(err);
+    callback(null);
+  });
+}
+
+function wipeOldBackups(callback) {
+  var datestring = Moment.utc()
+    .subtract(DAYS_OF_BACKUPS_TO_KEEP, "days")
+    .format("YYYY-MM-DD-");
+
+  var root = config.environment === "development" ? "_dev/" : "";
+  var remote = joinpath(root, datestring);
+
+  console.log(
+    "Backup: Looking up backups made " +
+      DAYS_OF_BACKUPS_TO_KEEP +
+      " days ago on " +
+      datestring
+  );
+
+  var s3Client = new AWS.S3();
+  s3Client.listObjects(
+    {
+      Bucket: BUCKET,
+      Prefix: remote,
+    },
+    function (err, data) {
+      if (err) return callback(err);
+
+      if (!data || !data.Contents.length) {
+        console.log("Backup: No old backups to remove");
+        return callback();
+      }
+
+      var Objects = data.Contents.map((i) => {
+        return { Key: i.Key };
+      });
+
+      s3Client.deleteObjects(
+        {
+          Bucket: BUCKET,
+          Delete: {
+            Objects: Objects,
+            Quiet: false,
+          },
+        },
+        function (err, data) {
+          if (err) return callback(err);
+
+          if (data && data.Deleted && data.Deleted.length) {
+            console.log(
+              "Backup: Deleted " +
+                data.Deleted.length +
+                " dump made " +
+                DAYS_OF_BACKUPS_TO_KEEP +
+                " days ago: " +
+                data.Deleted.map((i) => i.Key).join(", ")
+            );
+          }
+
+          callback();
+        }
+      );
+    }
+  );
 }
 
 module.exports = backup;
