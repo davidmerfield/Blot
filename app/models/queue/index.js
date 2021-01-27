1b8c49b40455048eb9b545165bddbb9ae5afc673
David Merfield <dmerfield@gmail.com>
2021-01-27 11:44:30 -0500

    More refactoring

--- app/models/queue/index.js
+++ app/models/queue/index.js
@@ -3,33 +3,32 @@ const client = require("client");
 const async = require("async");
 const redis = require("redis");
 
+const NEW_TASK_MESSAGE = "new";
+
 module.exports = function Queue(prefix = "") {
 	const keys = {
 		// A list of blogs with outstanding tasks
-		blogs: "queue:" + prefix + "blogs",
+		blogs: `queue:${prefix}blogs`,
 
 		// A list of tasks specific to a blog
-		blog: (blogID) => "queue:" + prefix + "blog:" + blogID,
+		blog: (blogID) => `queue:${prefix}blog:${blogID}`,
 
 		// A list of blog:task pairs for all active builds.
-		processing: "queue:" + prefix + "proessing",
+		processing: `queue:${prefix}proessing`,
 
 		// A list of blog:task pairs for all completed builds.
-		completed: "queue:" + prefix + "completed",
+		completed: `queue:${prefix}completed`,
 
 		// A channel on which 'new task' and 'queue drained' events are emitted
-		channel: "queue:" + prefix + "channel",
+		channel: `queue:${prefix}channel`,
 
 		// A set of all keys created by this queue for easy cleanup
-		all: "queue:" + prefix + "all",
+		all: `queue:${prefix}all`,
 	};
 
-	const deserializeTask = (taskString) => [
-		taskString.slice(0, taskString.indexOf(":")),
-		JSON.parse(taskString.slice(taskString.indexOf(":") + 1)),
-	];
-
 	this.add = (blogID, tasks, callback = function () {}) => {
+		let serializedTasks;
+
 		// You can either pass a single task object or an array of tasks
 		if (!Array.isArray(tasks)) {
 			tasks = [tasks];
@@ -37,29 +36,38 @@ module.exports = function Queue(prefix = "") {
 
 		try {
 			// Now we attempt to serialize the tasks to store in Redis
-			tasks = tasks.map((task) => blogID + ":" + JSON.stringify(task));
+			serializedTasks = tasks.map(
+				(task) => blogID + ":" + JSON.stringify(task)
+			);
 		} catch (e) {
 			return callback(new TypeError("Queue: tasks must be valid object"));
 		}
 
 		client
 			.multi()
-			// Add the blog associated with this new task to the list
-			// of all blogs with tasks outstanding. Workers watch this.
+			// Add the blog associated with these new tasks to the list
+			// of all blogs with tasks outstanding.
 			.lpush(keys.blogs, blogID)
-			// Emit a notification to all subscribed clients that there
-			.publish(keys.channel, "new")
+			// Tell all subscribed clients there is a new task
+			.publish(keys.channel, NEW_TASK_MESSAGE)
 			// Add the tasks to the list of tasks associated with the blog
-			.lpush(keys.blog(blogID), tasks)
+			.lpush(keys.blog(blogID), serializedTasks)
 			// Add the task list for the blog to the list of keys associated
 			// with the queue. This allows us to quickly reset the queue.
 			.sadd(keys.all, keys.blog(blogID))
 			.exec(callback);
 	};
 
+	const deserializeTask = (taskString) => [
+		taskString.slice(0, taskString.indexOf(":")),
+		JSON.parse(taskString.slice(taskString.indexOf(":") + 1)),
+	];
+
 	this.inspect = (callback) => {
 		client.smembers(keys.all, function (err, queueKeys) {
-			var multi = client.multi();
+			if (err) return callback(err);
+
+			let multi = client.multi();
 
 			multi.lrange(keys.blogs, 0, -1);
 			multi.lrange(keys.processing, 0, -1);
@@ -70,7 +78,8 @@ module.exports = function Queue(prefix = "") {
 			});
 
 			multi.exec(function (err, res) {
-				var response = {
+				if (err) return callback(err);
+				let response = {
 					blogs: res[0],
 					processing: res[1],
 					completed: res[2],
@@ -81,7 +90,7 @@ module.exports = function Queue(prefix = "") {
 				res.slice(2).forEach(function (queue) {
 					queue.forEach(function (task) {
 						task = deserializeTask(task);
-						var blogID = task[0];
+						let blogID = task[0];
 						task = task[1];
 						response.queues[blogID] = response.queues[blogID] || [];
 						response.queues[blogID].push(task);
@@ -95,21 +104,19 @@ module.exports = function Queue(prefix = "") {
 	};
 
 	this.reset = (callback) => {
-		debug("resetting queue");
 		client.smembers(keys.all, function (err, queueKeys) {
-			var keysToDelete = [
-				keys.blogs,
-				keys.processing,
-				keys.completed,
-				keys.channel,
-				keys.all,
-			];
-
-			if (queueKeys && queueKeys.length) {
-				keysToDelete = keysToDelete.concat(queueKeys);
-			}
-
-			client.del(keysToDelete, callback);
+			if (err) return callback(err);
+			client.del(
+				[
+					keys.blogs,
+					keys.processing,
+					keys.completed,
+					keys.channel,
+					keys.all,
+					...queueKeys,
+				],
+				callback
+			);
 		});
 	};
 
@@ -125,17 +132,16 @@ module.exports = function Queue(prefix = "") {
 		this.reset(callback);
 	};
 
-	// filter this by message / channel queue prefix
 	this.internalClient.on("message", (channel, message) => {
-		if (message !== "new") return;
 		if (channel !== keys.channel) return;
+		if (message !== NEW_TASK_MESSAGE) return;
 		if (this.internalQueue) this.internalQueue.push({});
 	});
 
 	this.process = (processor) => {
 		// create a queue object with concurrency 1
 		this.internalQueue = async.queue(function attempt(task, done) {
-			var label = require("helper").hash(processor.toString()).slice(0, 6);
+			let label = require("helper").hash(processor.toString()).slice(0, 6);
 
 			debug("processor:", label, "handling task");
 			client.RPOPLPUSH(keys.blogs, keys.blogs, function (err, blogID) {
--- app/models/queue/tests/setup.js
+++ app/models/queue/tests/setup.js
@@ -2,15 +2,14 @@ module.exports = function () {
 	global.test.blog();
 
 	var Queue = require("../index");
-	// Resets the database for any tasks stored
+
 	beforeEach(function () {
-		var prefix = require("helper").hash(Date.now().toString()).slice(0, 10);
-		this.queue = new Queue(prefix);
-		console.log("queue", this.queue);
+		this.queue = new Queue(
+			require("helper").hash(Date.now().toString()).slice(0, 10)
+		);
 	});
 
 	afterEach(function (done) {
-		console.log("resetting queue");
 		this.queue.destroy(done);
 	});
 };
