47ba68a696443138281b762b22cad7445f094d6d
David Merfield <dmerfield@gmail.com>
2021-03-16 11:42:59 -0400

    Allow external redirects

--- app/dashboard/routes/settings/save/redirects.js
+++ app/dashboard/routes/settings/save/redirects.js
@@ -31,18 +31,6 @@ module.exports = function(req, res, next) {
 function urlNormalizer(url) {
   if (!url) return "";
 
-  try {
-    url = Url.parse(url).pathname;
-  } catch (e) {
-    return "";
-  }
-
-  if (url.slice(0, 1) !== "/") url = "/" + url;
-
-  if (url.slice(-1) === "/" && url.length > 1) url = url.slice(0, -1);
-
-  url = url.split("//").join("/");
-
   return url.toLowerCase();
 }
 
