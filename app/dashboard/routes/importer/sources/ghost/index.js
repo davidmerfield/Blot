4201660e65db527310dd5a1ec2f8e247c2f12f1a
David Merfield <dmerfield@gmail.com>
2021-05-18 18:42:03 -0400

    Fixes ghost importer

--- app/dashboard/routes/importer/sources/ghost/index.js
+++ app/dashboard/routes/importer/sources/ghost/index.js
@@ -3,13 +3,13 @@ var parse = require("./parse");
 var fs = require("fs-extra");
 
 if (require.main === module)
-  main(process.argv[2], process.argv[3], function (err) {
+  main(process.argv[2], process.argv[3], process.argv[4], function (err) {
     if (err) throw err;
 
     process.exit();
   });
 
-function main(source_file, output_directory, callback) {
+function main(source_file, output_directory, source_domain, callback) {
   if (!callback) throw new Error("Please pass a callback");
 
   if (!source_file)
@@ -22,13 +22,18 @@ function main(source_file, output_directory, callback) {
       new Error("Please pass an output directory as a third argument")
     );
 
+  if (!source_domain)
+    return callback(
+      new Error("Please pass a source domain as a fourth argument")
+    );
+
   load(source_file, function (err, blog) {
     if (err) return callback(err);
 
     fs.emptyDir(output_directory, function (err) {
       if (err) return callback(err);
 
-      parse(blog, output_directory, callback);
+      parse(blog, output_directory, source_domain, callback);
     });
   });
 }
