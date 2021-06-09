ab77196a0594b6612d53862d4ea142771cf80962
David Merfield <dmerfield@gmail.com>
2021-06-09 16:18:30 -0400

    Refactor local server

--- app/local.js
+++ app/local.js
@@ -1,19 +1,9 @@
 var config = require("config");
 var async = require("async");
-
-console.log("Capabilities:");
-console.log("- Twitter " + !!config.twitter.consumer_secret);
-console.log("- markdown with pandoc  " + !!config.pandoc_path);
-console.log("- .docx conversion  " + !!config.pandoc_path);
-console.log("- .odt conversion  " + !!config.pandoc_path);
-
 var Express = require("express");
 const User = require("models/user");
 const Blog = require("models/blog");
 const child_process = require("child_process");
-
-const NODE_VERSION = "v12.16.3";
-const NPM_VERSION = "6.14.4";
 
 // I was getting a warning message from npm when I 'cntrl-c' out
 // of the server without this.
@@ -21,23 +11,39 @@ process.on("SIGINT", () => {
   process.exit();
 });
 
-if (child_process.execSync("node -v").toString().trim() !== NODE_VERSION) {
-  throw new Error(
-    `Error: incorrect version of node installed. Please install and use node version: ${NODE_VERSION}`
+// A few sanity checks before we begin
+let nodeVersion, redisPing;
+
+const REQUIRED_NODE_VERSION = "v12.16.3";
+
+try {
+  nodeVersion = child_process.execSync("node -v").toString().trim();
+  redisPing = child_process.execSync("redis-cli ping").toString().trim();
+} catch (e) {}
+
+if (nodeVersion !== REQUIRED_NODE_VERSION) {
+  console.error(
+    `Error: required version of node unavailable.
+     Please install and use node version: ${REQUIRED_NODE_VERSION}
+     `
   );
+  return process.exit(1);
 }
 
-if (child_process.execSync("npm -v").toString().trim() !== NPM_VERSION) {
-  throw new Error(
-    `Error: incorrect version of npm installed. Please install and use npm version: ${NPM_VERSION}`
+if (redisPing !== "PONG") {
+  console.error(
+    `Error: redis server not running. Please install and run a redis server
+    `
   );
+  return process.exit(1);
 }
 
-if (child_process.execSync("redis-cli ping").toString().trim() !== "PONG") {
-  throw new Error(
-    `Error: redis server not running.  Please install and run a redis server`
-  );
-}
+console.log("Local server capabilities:");
+console.log("- twitter embeds " + !!config.twitter.consumer_secret);
+console.log("- markdown with pandoc  " + !!config.pandoc_path);
+console.log("- .docx conversion  " + !!config.pandoc_path);
+console.log("- .odt conversion  " + !!config.pandoc_path);
+console.log("- dropbox client " + !!config.dropbox.app.key);
 
 // Welcome to Blot. This is the Express application which listens on port 8080.
 // NGINX listens on port 80 in front of Express app and proxies requests to
