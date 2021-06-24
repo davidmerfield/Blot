498116a41722887e6b00c26834d25370ec356346
David Merfield <dmerfield@gmail.com>
2021-06-24 18:24:58 -0400

    Fixes bug with asset lambda

--- app/blog/render/retrieve/asset.js
+++ app/blog/render/retrieve/asset.js
@@ -1,15 +1,12 @@
 module.exports = function (req, callback) {
   return callback(null, function () {
     return function (text, render) {
-      var extension;
-      var ends_with = Ends_With(text);
-      var starts_with = Starts_With(text);
+      let extension;
 
-      if (ends_with(".css")) extension = ".css";
+      if (text.lastIndexOf(".") > -1 && text.lastIndexOf(".") < text.length + 2)
+        extension = text.slice(text.lastIndexOf("."));
 
-      if (ends_with(".js")) extension = ".js";
-
-      if (!starts_with("/")) text = "/" + text;
+      if (!text.startsWith("/")) text = "/" + text;
 
       if (extension)
         text = text + "?cache={{cacheID}}&amp;extension=" + extension;
@@ -18,24 +15,3 @@ module.exports = function (req, callback) {
     };
   });
 };
-
-var assert = require("assert");
-
-function Ends_With(str) {
-  return function (token) {
-    return str.slice(-token.length) === token;
-  };
-}
-
-function Starts_With(str) {
-  return function (token) {
-    return str.slice(0, token.length) === token;
-  };
-}
-assert(Ends_With("abc")("abc"));
-assert(Ends_With("abc")("ab") === false);
-assert(Ends_With("abc.txt")(".txt"));
-
-assert(Starts_With("abc")("abc"));
-assert(Starts_With("abc")("bc") === false);
-assert(Starts_With("")(""));
