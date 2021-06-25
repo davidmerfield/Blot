70165ffd343844c2c916eb2cd6cbe07219630030
David Merfield <dmerfield@gmail.com>
2021-06-25 15:33:22 -0400

    Finally fixes duplicate content moving clients

--- app/clients/dropbox/routes/writeExistingContents.js
+++ app/clients/dropbox/routes/writeExistingContents.js
@@ -6,6 +6,7 @@ var upload = require("../util/upload");
 var join = require("path").join;
 var Metadata = require("metadata");
 var Path = require("path");
+var Entry = require("models/entry");
 
 module.exports = function (req, res, next) {
   var walk, walked, queue, localFolder, dropboxFolder, token;
@@ -56,6 +57,7 @@ module.exports = function (req, res, next) {
 // want to clobber /foo/bar/baz.jpg if it already exists
 // so we run down a list of options. Returns the
 // name of the lowercased file.
+
 function ensureLowerCase(blogID, localFolder, path, name, callback) {
   if (name === name.toLowerCase()) return callback(null, name);
 
@@ -92,19 +94,27 @@ function ensureLowerCase(blogID, localFolder, path, name, callback) {
           return next();
         }
 
-        debug("successfully moved", currentPath, "to", join(directory, name));
+        let oldPath = join(path, originalName);
+        let newPath = join(path, name);
 
-        // The git client does not store the case-sensitive name
-        // since it allows for case-sensitive files. The Dropbox
-        // client does not however, so we save the original name in the db
-        if (originalName.toLowerCase() === name && name !== originalName) {
-          Metadata.add(blogID, join(path, name), originalName, function (err) {
-            debug("saved", originalName, "against", join(path, name));
+        renameEntry(blogID, oldPath, newPath, name, function (err) {
+          if (err) debug(err);
+
+          // we need to rename the entry otherwise we get duplicates
+          debug("successfully moved", currentPath, "to", join(directory, name));
+          // The git client does not store the case-sensitive name
+          // since it allows for case-sensitive files. The Dropbox
+          // client does not however, so we save the original name in the db
+          if (originalName.toLowerCase() === name && name !== originalName) {
+            Metadata.add(blogID, newPath, originalName, function (err) {
+              if (err) debug(err);
+              debug("saved", originalName, "against", join(path, name));
+              callback(null, name);
+            });
+          } else {
             callback(null, name);
-          });
-        } else {
-          callback(null, name);
-        }
+          }
+        });
       });
     },
     function () {
@@ -115,6 +125,27 @@ function ensureLowerCase(blogID, localFolder, path, name, callback) {
   );
 }
 
+// When we lowercase a file, we need to update its entry
+// as well, if one exists.
+function renameEntry(blogID, oldPath, newPath, newName, callback) {
+  debug("Attempting to rename", oldPath, "to", newPath);
+
+  Entry.get(blogID, oldPath, function (entry) {
+    if (!entry) {
+      debug("No entry from", oldPath);
+      return callback();
+    }
+
+    debug("Removing entry from", oldPath);
+    Entry.drop(blogID, oldPath, function (err) {
+      if (err) return callback(err);
+      entry.path = newPath;
+      entry.id = newPath;
+      entry.name = newName;
+      Entry.set(blogID, newPath, entry, callback);
+    });
+  });
+}
 function walker(blogID, localFolder, dropboxFolder, queue) {
   return function walk(path, callback) {
     debug("iterating", path);
