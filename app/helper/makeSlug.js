c68c0df61ae02d1d23a21620d4f471388d0bb9aa
David Merfield <dmerfield@gmail.com>
2021-08-30 14:28:29 -0400

    Actually fixes handling of spaces in make slug

--- app/helper/makeSlug.js
+++ app/helper/makeSlug.js
@@ -6,6 +6,7 @@ function makeSlug(string) {
   var words;
   var trimmed = "";
 
+  console.log
   ensure(string, "string");
 
   var slug = "";
@@ -27,10 +28,12 @@ function makeSlug(string) {
     .toLowerCase()
 
     // remove common punction, basically everything except & _ and - /
-
+    // Should we be stripping all &encoded; characters?
     .replace(/\%/g, "-percent")
     .replace(/&amp;/g, "and")
     .replace(/&nbsp;/g, " ")
+    .replace(/&thinsp;/g, " ")
+    .replace(/&mdash;/g, "-")
     .replace(/→/g, "to")
     .replace(/←/g, "from")
     .replace(/\./g, "-")
@@ -136,6 +139,7 @@ is("Peter Gregson – Bach recomposed: 6.6 Gigue","peter-gregson-bach-recomposed
 is("'xsb' command line error.", "xsb-command-line-error");
 is("Foo & bar", "foo-bar");
 is("Foo &amp; bar", "foo-and-bar");
+is("Foo&thinsp;bar", "foo-bar");
 is("China ← NYC → China", "china-from-nyc-to-china");
 is("China+()[] ← NYC! → China", "china-from-nyc-to-china");
 is("No more cd ../../", "no-more-cd");
