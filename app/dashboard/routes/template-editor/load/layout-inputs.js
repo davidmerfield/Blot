46719b3f1a4c5d00b7e83c056d9f87114730a61c
David Merfield <dmerfield@gmail.com>
2021-09-02 15:13:09 -0400

    Adds alignment and position options to template editor (#487)

--- app/dashboard/routes/template-editor/load/layout-inputs.js
+++ app/dashboard/routes/template-editor/load/layout-inputs.js
@@ -8,23 +8,59 @@ const MAP = {
 
 module.exports = function (req, res, next) {
   res.locals.partials.range = "template-editor/inputs/range";
+  res.locals.partials.position = "template-editor/inputs/position";
+  res.locals.partials.alignment = "template-editor/inputs/alignment";
   res.locals.layouts = Object.keys(req.template.locals)
     .filter(
       (key) =>
-        ["page_size", "nav_location", "thumbnail_size", "spacing_size"].indexOf(
-          key
-        ) > -1
+        ["page_size", "thumbnail_size", "spacing_size"].indexOf(key) > -1 ||
+        (key.indexOf("_position") > -1 && key.indexOf("_options") === -1) ||
+        (key.indexOf("_alignment") > -1 && key.indexOf("_options") === -1)
     )
     .map((key) => {
-      let range = req.template.locals[key + "_range"];
-      let min = (range && range[0]) || (MAP[key] && MAP[key].min) || 1;
-      let max = (range && range[1]) || (MAP[key] && MAP[key].max) || 60;
+      let min, max;
+
+      let options = req.template.locals[key + "_options"];
+
+      let isRange =
+        ["page_size", "thumbnail_size", "spacing_size"].indexOf(key) > -1;
+
+      let isAlignment =
+        !isRange &&
+        key.indexOf("_alignment") > -1 &&
+        options &&
+        options.constructor === Array;
+
+      let isPosition =
+        !isRange &&
+        !isAlignment &&
+        key.indexOf("_position") > -1 &&
+        options &&
+        options.constructor === Array;
+
+      if (isAlignment || isPosition) {
+        options = req.template.locals[key + "_options"].map((option) => {
+          return {
+            label: desnake(option),
+            selected: req.template.locals[key] === option ? "selected" : "",
+            value: option,
+          };
+        });
+      }
+
+      if (isRange) {
+        let range = req.template.locals[key + "_range"];
+        min = (range && range[0]) || (MAP[key] && MAP[key].min) || 1;
+        max = (range && range[1]) || (MAP[key] && MAP[key].max) || 60;
+      }
 
       return {
         key,
         value: req.template.locals[key],
-        isRange:
-          ["page_size", "thumbnail_size", "spacing_size"].indexOf(key) > -1,
+        isRange,
+        options,
+        isAlignment,
+        isPosition,
         label: (MAP[key] && MAP[key].label) || desnake(key),
         max,
         min,
