109e650a8ad9075fe860ff072fdc9a7be34d3c80
David Merfield <dmerfield@gmail.com>
2021-09-04 20:14:32 -0400

    Refactors createClient in Dropbox

--- app/clients/dropbox/util/createClient.js
+++ app/clients/dropbox/util/createClient.js
@@ -5,48 +5,52 @@
 const fetch = require("isomorphic-fetch");
 const Dropbox = require("dropbox").Dropbox;
 const database = require("../database");
+const config = require("config");
 
 module.exports = function (blogID, callback) {
-  if (typeof callback !== "function") {
-    throw new Error("Missing second parameter `callback` to createClient");
-  }
-
-  if (typeof blogID !== "string" || blogID.indexOf("blog_") !== 0) {
-    return callback(new Error("Pass blog ID to createClient"));
-  }
-
   database.get(blogID, function (err, account) {
-    const dbx = new Dropbox({
-      fetch: fetch,
-    });
+    const client = new Dropbox({ fetch });
 
-    dbx.auth.setAccessToken(account.access_token);
+    client.auth.setAccessToken(account.access_token);
 
+    // Legacy Dropbox authentications (pre September 2021)
+    // won't have refresh tokens.
+    // Refresh tokens are used to recieve new access tokens
+    // since new access tokens now expire after 4 hours
     if (!account.refresh_token) {
-      return callback(null, dbx, account);
+      return callback(null, client, account);
     }
 
-    console.log("Updating access token based on refresh token....");
-    dbx.auth.setRefreshToken(account.refresh_token);
-    dbx.auth
+    let clientId = account.full_access
+      ? config.dropbox.full.key
+      : config.dropbox.app.key;
+
+    let clientSecret = account.full_access
+      ? config.dropbox.full.secret
+      : config.dropbox.app.secret;
+
+    client.auth.setClientId(clientId);
+    client.auth.setClientSecret(clientSecret);
+    client.auth.setRefreshToken(account.refresh_token);
+
+    client.auth
       .checkAndRefreshAccessToken()
       .then(() => {
-        let latestToken = dbx.auth.getAccessToken();
+        let latestToken = client.auth.getAccessToken();
 
         if (latestToken === account.access_token) {
-          console.log("NO CHANGE to access_token based on refresh token....");
-          return callback(null, dbx, account);
+          return callback(null, client, account);
         }
 
-        console.log("CHANGED!!!! New access token based on refresh token....");
         account.access_token = latestToken;
 
         database.set(blogID, account, function (err) {
           if (err) return callback(err);
-          console.log("SAVED!!!! New access token based on refresh token....");
-          callback(null, dbx, account);
+          callback(null, client, account);
         });
       })
-      .catch();
+      .catch(function (err) {
+        callback(err);
+      });
   });
 };
