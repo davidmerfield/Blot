c9f46105a2438f390b178858510fb4be2fdaa81f
David Merfield <dmerfield@gmail.com>
2021-09-06 12:18:16 -0400

    Updates developer guide

--- app/brochure/routes/developers.js
+++ app/brochure/routes/developers.js
@@ -2,26 +2,21 @@ var Express = require("express");
 var developers = new Express.Router();
 var titleFromSlug = require("helper/titleFromSlug");
 
-
 developers.use(function (req, res, next) {
   res.locals.base = "/templates/developers";
   next();
 });
 
-developers.param("section", function (req, res, next) {
-  res.locals.selected[req.params.section] = "selected";
-  next();
-});
-
-developers.param("subsection", function (req, res, next) {
-  res.locals.selected[req.params.subsection] = "selected";
-  next();
-});
-
 developers.get(['/reference'], function(req, res, next){
     res.locals["show-on-this-page"] = true;
+    // These interfere with the reference template
+    // if we rename the reference template, you can
+    // remove these lines
+    delete res.locals.selected.reference;
+    delete res.locals.selected.referenceIndex;
     next();
 })
+
 developers.get("/", function (req, res) {
   res.locals.title = "Developer guide - Blot";
   res.render("templates/developers");
