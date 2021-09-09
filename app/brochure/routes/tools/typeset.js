ce9dad0212c2d2c6a7a0927f6cf5d046c2d7defb
David Merfield <dmerfield@gmail.com>
2021-09-09 18:01:24 -0400

    rollback

--- app/brochure/routes/tools/typeset.js
+++ app/brochure/routes/tools/typeset.js
@@ -1,24 +1,17 @@
 var Typeset = require("typeset");
 
-function action(string) {
-  var html = string instanceof Buffer ? string.toString() : string;
-
-  html = Typeset(html, { disable: ["hyphenate"], ignore: "textarea, input" });
-
-  return html;
-}
-
 module.exports = function typeset(req, res, next) {
   var send = res.send;
 
   res.send = function (string) {
-    req.trace("Starting typeset");
-    var html = action(string);
-    req.trace("finished typeset");
+    req.trace('Starting typeset');
+    var html = string instanceof Buffer ? string.toString() : string;
+
+    html = Typeset(html, { disable: ["hyphenate"], ignore: "textarea, input" });
+
+    req.trace('finished typeset');
     send.call(this, html);
   };
 
   next();
 };
-
-module.exports.action = action;
