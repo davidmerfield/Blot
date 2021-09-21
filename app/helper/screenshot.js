a9961cae84edc5e9b604aea1baac851b6fce5916
David Merfield <dmerfield@gmail.com>
2021-09-21 17:57:59 -0400

    Improves template section of site

--- app/helper/screenshot.js
+++ app/helper/screenshot.js
@@ -3,31 +3,30 @@ const imagemin = require("imagemin");
 const imageminPngquant = require("imagemin-pngquant");
 const dirname = require("path").dirname;
 
-async function main(site, path, callback) {
-  const width = 1260;
-  const height = 1260;
+async function main(site, path, options = {}) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+  if (options.mobile) {
+    const iPhone = puppeteer.devices["iPhone X"];
+    await page.emulate(iPhone);
+  } else {
+    const width = options.width || 1260;
+    const height = options.height || 778;
 
     await page.setViewport({
       width: width,
       height: height,
       deviceScaleFactor: 2,
     });
-
-    await page.goto(site);
-    await page.screenshot({ path: path });
-    await browser.close();
-    await imagemin([path], dirname(path), {
-      plugins: [imageminPngquant()],
-    });
-  } catch (e) {
-    return callback(e);
   }
 
-  callback();
+  await page.goto(site);
+  await page.screenshot({ path: path });
+  await browser.close();
+  await imagemin([path], dirname(path), {
+    plugins: [imageminPngquant()],
+  });
 }
 
 module.exports = main;
--- scripts/template/screenshots.js
+++ scripts/template/screenshots.js
@@ -28,7 +28,10 @@ const SCREENSHOTS = {
   },
   reference: {
     handle: "frances",
-    pages: ["/", "/marshfield-george-woodward-wickersham-house-cedarhurst-new-york-copy"],
+    pages: [
+      "/",
+      "/marshfield-george-woodward-wickersham-house-cedarhurst-new-york-copy",
+    ],
   },
 };
 
@@ -38,55 +41,36 @@ async.eachOfSeries(
     const baseURL = `https://preview-of-${template}-on-${handle}.${config.host}`;
     async.eachSeries(
       pages,
-      function (page, next) {
+      async function (page, next) {
         const url = baseURL + page;
-        const path = `${VIEW_DIRECTORY}/${template}/${pages.indexOf(page)}.png`;
-        const mediumPath = `${VIEW_DIRECTORY}/${template}/${pages.indexOf(
-          page
-        )}.medium.png`;
-        const smallPath = `${VIEW_DIRECTORY}/${template}/${pages.indexOf(
-          page
-        )}.small.png`;
+        const dir = `${VIEW_DIRECTORY}/${template}`;
+
+        const path = `${dir}/${pages.indexOf(page)}.png`;
+        const mobilePath = `${dir}/${pages.indexOf(page)}.mobile.png`;
+        const squarePath = `${dir}/${pages.indexOf(page)}.square.png`;
+
         console.log("screenshotting", url);
-        screenshot(url, path, (err) => {
-          if (err) return next(err);
+        await screenshot(url, path);
+        await screenshot(url, mobilePath, { mobile: true });
+        await screenshot(url, squarePath, { width: 1060, height: 1060 });
 
-          const resize = ({ label, width }) =>
-            sharp(path)
-              .resize({ width })
-              .toFile(
-                `${dirname(path)}/${basename(
-                  path,
-                  extname(path)
-                )}-${label}${extname(path)}`
-              );
+        const resize = ({ path, label, width }) =>
+          sharp(path)
+            .resize({ width })
+            .toFile(
+              `${dirname(path)}/${basename(
+                path,
+                extname(path)
+              )}-${label}${extname(path)}`
+            );
 
-          Promise.all(
-            [
-              { label: "medium", width: 1120 },
-              { label: "small", width: 306 },
-            ].map(resize)
-          ).then(() => {
-            console.log("complete");
-            next();
-          });
-
-          // console.log("resizing", path);
-          // sharp(path)
-          //   .resize({ width: 1120 }) // 2x the width of the image when viewed
-          //   .toFile(mediumPath)
-          //   .resize({ width: 306 }) // 2x the width of the image when viewed
-          //   .toFile(smallPath)
-          //   .then(() => {
-          //     console.log("minifying", smallPath);
-          //     next();
-          //     // imageminify(smallPath, (err) => {
-          //     //   if (err) return next(err);
-          //     //   console.log("minified", smallPath);
-          //     //   next();
-          //     // });
-          //   });
-        });
+        await Promise.all(
+          [
+            { path: path, label: "medium", width: 1120 },
+            { path: squarePath, label: "small", width: 306 },
+            { path: mobilePath, label: "medium", width: 560 },
+          ].map(resize)
+        );
       },
       next
     );
