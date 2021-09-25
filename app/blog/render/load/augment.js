d46157c499872c4d2b14e9bd55f2ee042316069c
David Merfield <dmerfield@gmail.com>
2021-09-25 11:02:11 -0400

    Makes blog.render.load async

--- app/blog/render/load/augment.js
+++ app/blog/render/load/augment.js
@@ -1,10 +1,12 @@
 var normalize = require("models/tags").normalize;
 var type = require("helper/type");
-
+var Entry = require("models/entry");
 var moment = require("moment");
+var async = require("async");
+
 require("moment-timezone");
 
-module.exports = function (req, res) {
+module.exports = function (req, res, entry, callback) {
   var blog = req.blog;
 
   // Can be either inherited from the properties of the blog
@@ -12,81 +14,88 @@ module.exports = function (req, res) {
   var hideDate = res.locals.hide_dates || false;
   var dateDisplay = res.locals.date_display || "MMMM D, Y";
 
-  return function (entry) {
-    entry.formatDate = FormatDate(entry.dateStamp, req.blog.timeZone);
-    entry.formatUpdated = FormatDate(entry.updated, req.blog.timeZone);
-    entry.formatCreated = FormatDate(entry.created, req.blog.timeZone);
+  entry.formatDate = FormatDate(entry.dateStamp, req.blog.timeZone);
+  entry.formatUpdated = FormatDate(entry.updated, req.blog.timeZone);
+  entry.formatCreated = FormatDate(entry.created, req.blog.timeZone);
 
-    entry.absoluteURL =
-      req.blog.locals.blogURL +
-      entry.url.split("/").map(encodeURIComponent).join("/");
+  entry.absoluteURL =
+    req.blog.locals.blogURL +
+    entry.url.split("/").map(encodeURIComponent).join("/");
 
-    entry.backlinks = entry.backlinks.map(function (linkUrl) {
-      return { label: linkUrl, url: linkUrl };
+  var tags = [];
+  var tagged = {};
+  var totalTags = entry.tags.length;
+
+  for (var i = 0; i < totalTags; i++) {
+    var tag = entry.tags[i];
+
+    // augment has already been called on this
+    // entry there is a bug in eachEntry
+    if (!type(tag, "string")) {
+      console.log(
+        "Error BAD TAG:",
+        req.blog.id,
+        req.originalHost,
+        req.url,
+        "has format date?",
+        type(entry.formatDate, "function")
+      );
+      console.log(tag);
+      continue;
+    }
+
+    if (!tag) continue;
+
+    var slug = normalize(tag);
+    var lower = tag.toLowerCase();
+
+    tagged[tag] = tagged[lower] = tagged[slug] = true;
+
+    tags.push({
+      name: tag,
+      tag: tag,
+      slug: slug,
+      first: i === 0,
+      last: i === totalTags - 1,
     });
+  }
 
-    var tags = [];
-    var tagged = {};
-    var totalTags = entry.tags.length;
+  for (var k in entry.thumbnail) {
+    entry.thumbnail[k].ratio =
+      (entry.thumbnail[k].height / entry.thumbnail[k].width) * 100 + "%";
+  }
 
-    for (var i = 0; i < totalTags; i++) {
-      var tag = entry.tags[i];
+  entry.tags = tags;
+  entry.tagged = tagged;
 
-      // augment has already been called on this
-      // entry there is a bug in eachEntry
-      if (!type(tag, "string")) {
-        console.log(
-          "Error BAD TAG:",
-          req.blog.id,
-          req.originalHost,
-          req.url,
-          "has format date?",
-          type(entry.formatDate, "function")
-        );
-        console.log(tag);
-        continue;
-      }
+  // We don't want to compute the entry's date
+  // string if the user explicitly told use to
+  // hide the dates. We also want to hide the
+  // dates for items in the menu, and items which
+  // are pages. Otherwise its weird.
+  if (!hideDate && !entry.menu && !entry.page) {
+    entry.date = moment
+      .utc(entry.dateStamp)
+      .tz(blog.timeZone)
+      .format(dateDisplay);
+  } else {
+    delete entry.date;
+  }
 
-      if (!tag) continue;
+  entry.backlinks = entry.backlinks || [];
 
-      var slug = normalize(tag);
-      var lower = tag.toLowerCase();
-
-      tagged[tag] = tagged[lower] = tagged[slug] = true;
-
-      tags.push({
-        name: tag,
-        tag: tag,
-        slug: slug,
-        first: i === 0,
-        last: i === totalTags - 1,
+  async.map(
+    entry.backlinks,
+    function (linkUrl, next) {
+      Entry.getByUrl(req.blog.id, linkUrl, function (entry) {
+        next(null, entry);
       });
+    },
+    function (err, backlinks) {
+      entry.backlinks = backlinks;
+      callback();
     }
-
-    for (var k in entry.thumbnail) {
-      entry.thumbnail[k].ratio =
-        (entry.thumbnail[k].height / entry.thumbnail[k].width) * 100 + "%";
-    }
-
-    entry.tags = tags;
-    entry.tagged = tagged;
-
-    // We don't want to compute the entry's date
-    // string if the user explicitly told use to
-    // hide the dates. We also want to hide the
-    // dates for items in the menu, and items which
-    // are pages. Otherwise its weird.
-    if (!hideDate && !entry.menu && !entry.page) {
-      entry.date = moment
-        .utc(entry.dateStamp)
-        .tz(blog.timeZone)
-        .format(dateDisplay);
-    } else {
-      delete entry.date;
-    }
-
-    return entry;
-  };
+  );
 };
 
 function FormatDate(dateStamp, zone) {
