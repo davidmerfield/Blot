7abe5d8a4363c2f85de2fe1cb30dec95efdf2343
David Merfield <dmerfield@gmail.com>
2021-10-01 21:51:10 -0400

    Updates sync

--- scripts/google-drive/sync.js
+++ scripts/google-drive/sync.js
@@ -3,10 +3,27 @@ const get = require("./get-blog");
 
 const args = Array.from(process.argv);
 
+if (!args.length) {
+  console.log("Sync every second (continuously):");
+  console.log("node scripts/google-drive/sync -c");
+  console.log();
+  console.log("Sync without making changes (dry run):");
+  console.log("node scripts/google-drive/sync -d");
+  console.log();
+  console.log("Sync changes after date (latestActivity) with dry-run:");
+  console.log("node scripts/google-drive/sync -d -l 2021-09-30T18:00:00.978Z");
+}
+
 get(function (err, user, blog) {
   if (err) throw err;
 
-  const options = { fromScratch: args.indexOf("-s") > -1 };
+  const options = {
+    dryRun: args.indexOf("-d") > -1,
+    fromScratch: args.indexOf("-s") > -1,
+    latestActivity: args.indexOf("-l") > -1 && args[args.indexOf("-l") + 1],
+  };
+
+  console.log("Syncing with options", options);
 
   sync(blog.id, options, function then(err) {
     if (err) throw err;
