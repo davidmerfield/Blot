f6d7c9c4450bcd8e005ea026aa8f03f117495f7a
David Merfield <dmerfield@gmail.com>
2022-02-17 14:07:09 +0000

    Updates micro.blog import script

--- app/dashboard/routes/importer/sources/micro.blog/index.js
+++ app/dashboard/routes/importer/sources/micro.blog/index.js
@@ -5,12 +5,15 @@ const yaml = require("yaml");
 const moment = require("moment");
 const walk = helper.walk;
 const basename = require("path").basename;
+const cheerio = require("cheerio");
 
 const input_directory = process.argv[2];
+const host = process.argv[3];
 const output_directory =
   (input_directory.endsWith("/")
     ? input_directory.slice(0, -1)
     : input_directory) + "-output";
+
 
 (async () => {
   const paths = walk(input_directory).filter(
@@ -49,11 +52,31 @@ const output_directory =
       ).valueOf();
       post.draft = post.page = false;
 
+      const $ = cheerio.load(post.html);
+
+      $("img").each(function () {
+        const src = $(this).attr("src");
+        const url = require("url").parse(src);
+
+        if (
+          url.hostname === host &&
+          fs.existsSync(input_directory + url.path)
+        ) {
+          const newPath = "/_assets/" + require("path").basename(url.path);
+          fs.copySync(input_directory + url.path, output_directory + newPath);
+          $(this).attr("src", newPath);
+        }
+      });
+
+      post.html = $.html();
+
       return post;
     })
   );
 
   // Writes each post in the post array,
   // downloads any images, pdfs
-  helper.process(output_directory, posts);
+  helper.process(output_directory, posts, {
+    preserve_output_directory: true,
+  });
 })();
