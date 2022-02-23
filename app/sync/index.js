0d7276a96d1a1d63eff32fe01bb205e274b5480f
David Merfield <dmerfield@gmail.com>
2022-02-23 10:54:31 +0000

    Updates sync messaging

--- app/sync/index.js
+++ app/sync/index.js
@@ -1,16 +1,26 @@
-var client = require("client");
-var buildFromFolder = require("template").buildFromFolder;
-var Blog = require("blog");
-var Update = require("./update");
-var localPath = require("helper/localPath");
-var clfdate = require("helper/clfdate");
-var uuid = require("uuid/v4");
-var renames = require("./renames");
-var debug = require("debug")("blot:blog:sync");
+const client = require("client");
+const buildFromFolder = require("template").buildFromFolder;
+const Blog = require("blog");
+const Update = require("./update");
+const localPath = require("helper/localPath");
+const clfdate = require("helper/clfdate");
+const uuid = require("uuid/v4");
+const renames = require("./renames");
+const debug = require("debug")("blot:blog:sync");
 const lockfile = require("proper-lockfile");
+const type = require("helper/type");
 
 function sync(blogID, callback) {
-  
+  if (!type(blogID, "string")) {
+    throw new TypeError("Expected blogID with type:String as first argument");
+  }
+
+  if (!type(callback, "function")) {
+    throw new TypeError(
+      "Expected callback with type:Function as second argument"
+    );
+  }
+
   Blog.get({ id: blogID }, async function (err, blog) {
     // It would be nice to get an error from Blog.get instead of this...
     if (err || !blog || !blog.id || blog.isDisabled) {
@@ -25,8 +35,8 @@ function sync(blogID, callback) {
       return callback(new Error("Failed to acquire lock to sync folder"));
     }
 
-    var syncID = "sync_" + uuid().slice(0, 7);
-    var log = function () {
+    const syncID = "sync_" + uuid().slice(0, 7);
+    const log = function () {
       console.log.apply(null, [
         clfdate(),
         blogID.slice(0, 12),
@@ -35,7 +45,7 @@ function sync(blogID, callback) {
       ]);
     };
 
-    var folder = {
+    const folder = {
       path: localPath(blogID, "/"),
       update: new Update(blog, log),
       log,
@@ -51,11 +61,13 @@ function sync(blogID, callback) {
     // This function is to be called when we are finished
     // with the lock on the user's folder.
     folder.log("Started");
-    client.publish("sync:status:" + blogID, "Sync started");
+    client.publish("sync:status:" + blogID, "Syncing");
 
+    // Pass methods to trigger folder updates back to the
+    // function which wanted to modify the blog's folder.
     callback(null, folder, function (syncError, callback) {
       folder.log("Released lock");
-      client.publish("sync:status:" + blogID, "Sync complete");
+      client.publish("sync:status:" + blogID, "Synced");
 
       if (typeof syncError === "function")
         throw new Error("Pass an error or null as first argument to done");
@@ -64,24 +76,35 @@ function sync(blogID, callback) {
         throw new Error("Pass a callback to done");
 
       renames(blogID, async function (err) {
-        if (err) return callback(err);
-
-        // We could do these next two things in parallel
-        // but it's a little bit of refactoring...
-        await release();
+        if (err) {
+          await release();
+          return callback(err);
+        }
 
         // What is the appropriate order for this?
-        buildFromFolder(blogID, function (err) {
-          if (err) return callback(err);
+        buildFromFolder(blogID, async function (err) {
+          if (err) {
+            await release();
+            return callback(err);
+          }
+
+          const cacheID = Date.now();
 
           // Passing in cacheID manually busts the cache.
           // Since Blog.set and Blog.flushCache depend on each other
           // we can't put this there. Ideally we would expose a single function to
           // wipe the cache. So fix that eventually...
-          Blog.set(blogID, { cacheID: Date.now() }, function (err) {
-            if (err) return callback(err);
+          Blog.set(blogID, { cacheID }, async function (err) {
+            if (err) {
+              await release();
+              return callback(err);
+            }
 
-            folder.log("Finished");
+            // We could do these next two things in parallel
+            // but it's a little bit of refactoring...
+            await release();
+
+            folder.log("Finished sync");
             callback(syncError);
           });
         });
