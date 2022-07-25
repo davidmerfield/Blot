14f14c25a3e3e1bd5303336bf1e9a7edfcca640b
David Merfield <dmerfield@gmail.com>
2022-07-25 22:03:12 +0100

    Fixes sitemap on brochure

--- app/brochure/routes/sitemap.js
+++ app/brochure/routes/sitemap.js
@@ -1,25 +1,36 @@
 var config = require("config");
 
 module.exports = function (req, res) {
-  res.locals.layout = "";
+  res.locals.layout = "sitemap.html";
   res.locals.protocol = req.protocol;
   res.locals.host = config.host;
   res.locals.urls = [
     "/",
-    "/sign-up",
-    "/log-in",
+    "/about",
+    "/about/source-code",
+    "/about/news",
+    "/about/status",
+    "/about/notes",
     "/how",
-    "/how/templates",
     "/how/pages",
     "/how/public-files",
     "/how/metadata",
-    "/how/clients",
-    "/how/guides",
-    "/developers",
+    "/how/sync",
+    "/how/posts",
+    "/how/configure",
+    "/sign-up",
+    "/log-in",
+    "/templates",
+    "/templates/developers",
+    "/questions",
     "/contact",
-    "/about",
-    "/news",
-  ];
+    "/privacy",
+    "/terms",
+  ].map((i) => {
+    return {
+      url: config.protocol + config.host + i,
+    };
+  });
   res.setHeader("Content-Type", "text/xml");
   res.render("sitemap");
 };
