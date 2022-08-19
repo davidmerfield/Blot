f8cb13f9e1e7e070747bcc03d22ac650af0aa30a
David Merfield <dmerfield@gmail.com>
2022-08-19 18:41:14 +0100

    Reorganize

---
+++ app/clients/dropbox/routes/dashboard.js
@@ -0,0 +1,155 @@
+const express = require("express");
+const dashboard = express.Router();
+const disconnect = require("clients/dropbox/disconnect");
+const views = __dirname + "/../../views/";
+const prepare = require("./prepare");
+const { Dropbox } = require("dropbox");
+const config = require("config");
+const fetch = require("node-fetch");
+const Database = require("clients/dropbox/database");
+const join = require("path").join;
+const moment = require("moment");
+
+dashboard.use(function loadDropboxAccount(req, res, next) {
+  Database.get(req.blog.id, function (err, account) {
+    if (err) return next(err);
+
+    if (!account) return next();
+
+    var last_sync = account.last_sync;
+    var error_code = account.error_code;
+
+    res.locals.account = req.account = account;
+
+    if (last_sync) {
+      res.locals.account.last_sync = moment.utc(last_sync).fromNow();
+    }
+
+    if (error_code) {
+      res.locals.account.folder_missing = error_code === 409;
+      res.locals.account.revoked = error_code === 401;
+    }
+
+    var dropboxBreadcrumbs = [];
+    var folder;
+
+    if (res.locals.account.full_access) {
+      folder = join("Dropbox", res.locals.account.folder);
+    } else {
+      folder = join("Dropbox", "Apps", "Blot", res.locals.account.folder);
+    }
+
+    dropboxBreadcrumbs = folder.split("/").map(function (name) {
+      return { name: name };
+    });
+
+    dropboxBreadcrumbs[dropboxBreadcrumbs.length - 1].last = true;
+    res.locals.dropboxBreadcrumbs = dropboxBreadcrumbs;
+
+    return next();
+  });
+});
+
+// The settings page for a Dropbox account
+dashboard.get("/", function (req, res) {
+  // Ask to user to authenticate with Dropbox if they have not yet
+  if (!req.account) {
+    var query = "";
+    if (req.query.setup) query = "?setup=true";
+    return res.redirect(req.baseUrl + "/setup" + query);
+  }
+
+  res.locals.status = req.session.dropbox && req.session.dropbox.status;
+  res.locals.stages = JSON.stringify(STAGES);
+
+  res.render(views + "index");
+});
+
+// Explains to the user what will happen when they authenticate
+// then provides them with a link to the dropbox redirect
+dashboard.get("/setup", function (req, res) {
+  res.render(views + "authenticate");
+});
+
+// Redirects the user to the OAuth page on Dropbox.com
+dashboard.get("/redirect", function (req, res) {
+  var redirectUri, key, secret;
+
+  redirectUri =
+    req.protocol + "://" + req.get("host") + "/clients/dropbox/authenticate";
+
+  req.session.blogToAuthenticate = req.blog.handle;
+
+  if (req.query.full_access) {
+    key = config.dropbox.full.key;
+    secret = config.dropbox.full.secret;
+    redirectUri += "?full_access=true";
+  } else {
+    key = config.dropbox.app.key;
+    secret = config.dropbox.app.secret;
+  }
+
+  const dbconfig = {
+    fetch,
+    clientId: key,
+    clientSecret: secret,
+  };
+
+  const dbx = new Dropbox(dbconfig);
+
+  // what are these mystery params
+  dbx.auth
+    .getAuthenticationUrl(
+      redirectUri,
+      null,
+      "code",
+      "offline",
+      null,
+      "none",
+      false
+    )
+    .then((authUrl) => {
+      res.writeHead(302, { Location: authUrl });
+      res.end();
+    });
+
+  // res.redirect(authentication_url);
+});
+
+// Explains to the user what happens when they change the
+// permission they grant to Blot per access to their Dropbox
+dashboard.get("/permission", function (req, res) {
+  res.render(views + "permission");
+});
+
+// This route recieves the user back from
+// Dropbox when they have accepted or denied
+// the request to access their folder.
+dashboard.get("/authenticate", function (req, res) {
+  // the user has reloaded this page
+  if (req.session.dropbox && req.session.dropbox.preparing === true) {
+    return res.redirect(req.baseUrl);
+  }
+
+  const { code, full_access } = req.query;
+
+  // this the first time the user has visited this page
+  req.session.dropbox = { code, full_access, preparing: true };
+
+  prepare(req, res);
+
+  res.redirect(req.baseUrl);
+});
+
+// Will remove the Dropbox account from the client's database
+// and revoke the token if needed.
+dashboard
+  .route("/disconnect")
+  .get(function (req, res) {
+    res.render(views + "disconnect");
+  })
+  .post(function (req, res, next) {
+    disconnect(req.blog.id, next);
+  });
+
+module.exports = dashboard;
--- app/clients/dropbox/routes/site/index.js
+++ app/clients/dropbox/routes/site/index.js
@@ -1,14 +1,39 @@
 const express = require("express");
 const site = express.Router();
-
-// This is called by Dropbox when changes
-// are made to the folder of a Blot user.
-site.use("/webhook", require("./webhook"));
+const session = require("dashboard/session");
 
 // This is called by Dropbox when the user
 // authorizes Blot's access to their folder
 // as part of the OAUTH flow. We then redirect
 // them to a dashboard route within the Dropbox client
-site.use("/authenticate", require("./authenticate"));
+// Customers are sent back to:
+// blot.im/clients/dropbox/authenticate
+// when they have authorized (or declined to authorize)
+// Blot's access to their folder. This is a public-facing
+// route without access to the customer's session by default.
+// We need to work out which blog they were
+// authenticating based on a value stored in their session
+// before they were sent out to Dropbox. Unfortunately we
+// can't pass a blog username in the URL, since it needs to
+// be the same URL every time, e.g. this would not work:
+// blot.im/clients/dropbox/authenticate?handle=david
+site.get("/authenticate", session, function (req, res, next) {
+  const handle = req.session.blogToAuthenticate;
+  if (!handle) return next(new Error("No blog to authenticate"));
+  delete req.session.blogToAuthenticate;
+  let redirect =
+    "/dashboard/" +
+    handle +
+    "/client/dropbox/authenticate?code=" +
+    req.query.code;
+
+  if (req.query.full_access) redirect += "&full_access=true";
+
+  res.redirect(redirect);
+});
+
+// This is called by Dropbox when changes
+// are made to the folder of a Blot user.
+site.use("/webhook", require("./webhook"));
 
 module.exports = site;
