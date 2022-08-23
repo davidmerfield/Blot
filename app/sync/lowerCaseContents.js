045a4815b75875c543c02ddb934647df1da4c92d
David Merfield <dmerfield@gmail.com>
2022-08-23 15:32:36 +0100

    Adds rename method to sync

--- app/sync/lowerCaseContents.js
+++ app/sync/lowerCaseContents.js
@@ -1,1 +1,116 @@
-lowerCaseContents.js
+const fs = require("fs-extra");
+const { join } = require("path");
+
+// Takes a file or folder whose name is not fully
+// lowercase and to make it lowercase. For example:
+//
+//      /foo/BaR.jpg --> /foo/bar.jpg
+//
+// We also need to think about updating files inside
+// folders with a case-sensitive name...
+// think about things e.g. previews which would trigger folder writes...
+
+async function lowerCase(localFolder, update) {
+  const renamedDirectories = {};
+
+  const walk = async (dir) => {
+    const contents = await fs.readdir(join(localFolder, dir));
+
+    for (const item of contents) {
+      const stat = await fs.stat(join(localFolder, join(dir, item)));
+      const directory = stat.isDirectory();
+      const file = !directory;
+      const hasNameWithCapitals = item !== item.toLowerCase();
+      const formerParentDirectory = renamedDirectories[dir];
+      const inMovedDirectory = formerParentDirectory !== undefined;
+
+      // this directory has a case-sensitive name
+      // and it is inside a directory which has moved
+      if (directory && hasNameWithCapitals && inMovedDirectory) {
+        const blotPath = join(formerParentDirectory, item);
+        const newPath = join(dir, item.toLowerCase());
+        const pathOnDisk = join(localFolder, dir, item);
+
+        renamedDirectories[newPath] = blotPath;
+
+        await fs.rename(pathOnDisk, join(localFolder, newPath));
+        await update(blotPath);
+        await update(newPath);
+        await walk(newPath);
+      }
+
+      // this directory has a case-sensitive name
+      // but it is not inside a directory which has moved
+      if (directory && hasNameWithCapitals && !inMovedDirectory) {
+        const path = join(dir, item);
+        const newPath = join(dir, item.toLowerCase());
+
+        renamedDirectories[newPath] = path;
+
+        await fs.rename(join(localFolder, path), join(localFolder, newPath));
+        await update(path);
+        await update(newPath);
+        await walk(newPath);
+      }
+
+      // this directory does not have a case-sensitive name
+      // but it is inside a directory which has moved
+      if (directory && !hasNameWithCapitals && inMovedDirectory) {
+        const blotPath = join(formerParentDirectory, item);
+        const path = join(dir, item);
+
+        renamedDirectories[path] = blotPath;
+
+        await update(blotPath);
+        await update(path);
+        await walk(path);
+      }
+
+      // this directory does not have a case-sensitive name
+      // and it is not inside a directory which has moved
+      if (directory && !hasNameWithCapitals && !inMovedDirectory) {
+        await walk(join(dir, item));
+      }
+
+      // this file has a case-sensitive name
+      // and it is inside a directory which has moved
+      if (file && hasNameWithCapitals && inMovedDirectory) {
+        const blotPath = join(formerParentDirectory, item);
+        const newPath = join(dir, item.toLowerCase());
+        const pathOnDisk = join(localFolder, dir, item);
+        await fs.rename(pathOnDisk, join(localFolder, newPath));
+        await update(blotPath);
+        await update(newPath);
+      }
+
+      // this file has a case-sensitive name
+      // but it is not inside a directory which has moved
+      if (file && hasNameWithCapitals && !inMovedDirectory) {
+        const blotPath = join(dir, item);
+        const path = join(dir, item.toLowerCase());
+        await fs.rename(join(localFolder, blotPath), join(localFolder, path));
+        await update(blotPath);
+        await update(path);
+      }
+
+      // this file does not have a case-sensitive name
+      // but it is inside a directory which has moved
+      if (file && !hasNameWithCapitals && inMovedDirectory) {
+        const blotPath = join(formerParentDirectory, item);
+        const path = join(dir, item);
+        await update(blotPath);
+        await update(path);
+      }
+
+      // this file has neither a case-sensitive name
+      // nor is it inside a directory which has moved
+      if (file && !hasNameWithCapitals && !inMovedDirectory) {
+        // do nothing
+      }
+    }
+  };
+
+  await walk("/");
+}
+
+module.exports = lowerCase;
--- app/sync/tests/rename.js
+++ app/sync/tests/rename.js
@@ -1,1 +1,50 @@
-rename.js
+describe("rename", function () {
+  const syncWithCallback = require("../index");
+  var fs = require("fs-extra");
+  const { promisify } = require("util");
+  const { join } = require("path");
+  const rename = require("../rename");
+
+  syncWithCallback[promisify.custom] = (blogID) =>
+    new Promise((resolve, reject) => {
+      syncWithCallback(blogID, (err, folder, done) => {
+        if (err) {
+          reject(err);
+        } else {
+          folder.update = promisify(folder.update);
+          resolve({ folder, done: promisify(done) });
+        }
+      });
+    });
+
+  const sync = promisify(syncWithCallback);
+
+  // Set up a test blog before each test
+  global.test.blog();
+
+  // Expose methods for creating fake files, paths, etc.
+  beforeEach(function () {
+    this.fake = global.test.fake;
+    this.rename = promisify(rename(this.blog, console.log));
+  });
+
+  beforeEach(function () {
+    this.checkEntry = promisify(global.test.CheckEntry(this.blog.id));
+  });
+
+  it("renames an entry ", async function () {
+    const oldPath = this.fake.path(".txt");
+    const path = this.fake.path(".txt");
+    const content = this.fake.file();
+    const { folder, done } = await sync(this.blog.id);
+
+    await fs.outputFile(join(folder.path, oldPath), content, "utf-8");
+    await folder.update(oldPath);
+
+    await fs.move(join(folder.path, oldPath), join(folder.path, path));
+    await this.rename(path, oldPath, {});
+
+    // await this.checkEntry({ path: path });
+    await done(null);
+  });
+});
