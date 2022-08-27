4bcb22a0eff2eb7ae92a5e8916005f96501d9682
David Merfield <dmerfield@gmail.com>
2022-08-27 11:11:26 +0100

    Updates status tests

--- app/models/blog/getStatuses.js
+++ app/models/blog/getStatuses.js
@@ -1,14 +1,37 @@
 const key = require("./key");
 const client = require("client");
+const ensure = require("helper/ensure");
 
-module.exports = function (blogID, callback) {
-  const offset = 0;
-  const limit = 100;
+module.exports = function (blogID, options, callback) {
+  if (typeof options === "function" && callback === undefined) {
+    callback = options;
+    options = {};
+  }
 
-  client.lrange(key.status(blogID), offset, offset + limit, function (
-    err,
-    items
-  ) {
+  ensure(blogID, "string").and(options, "object").and(callback, "function");
+
+  // Fetch the first page by default
+  const page = options.page === undefined ? 1 : options.page;
+
+  // Use a page size of 100 by default
+  const pageSize = options.pageSize === undefined ? 100 : options.pageSize;
+
+  const offset = (page - 1) * pageSize;
+
+  // We remove one because, per the redis docs:
+  // > Note that if you have a list of numbers from 0 to 100,
+  // > LRANGE list 0 10 will return 11 elements, that is, the
+  // > rightmost item is included. This may or may not be
+  // > consistent with behavior of range-related functions in
+  // > your programming language of choice.
+  const limit = offset + pageSize - 1;
+
+  ensure(offset, "number")
+    .and(limit, "number")
+    .and(page, "number")
+    .and(pageSize, "number");
+
+  client.lrange(key.status(blogID), offset, limit, function (err, items) {
     if (err) return callback(err);
     items = items.map(JSON.parse);
     callback(null, items);
--- app/models/blog/tests/getStatuses.js
+++ app/models/blog/tests/getStatuses.js
@@ -11,7 +11,30 @@ describe("Blog.setStatus", function () {
     const messages = ["Hey", "You", "David"];
     for (const message of messages) await setStatus(this.blog.id, { message });
     const statuses = await getStatuses(this.blog.id);
-    console.log(statuses);
-    expect(statuses.length).toEqual(3);
+    expect(statuses.length).toEqual(messages.length);
+  });
+
+  it("gets pages of statuses", async function () {
+    const totalMessages = 1000;
+    const messages = [];
+    while (messages.length < totalMessages)
+      messages.push(global.test.fake.lorem.sentence());
+
+    for (const message of messages) await setStatus(this.blog.id, { message });
+
+    const statuses = await getStatuses(this.blog.id);
+
+    expect(statuses.length).toEqual(100);
+    expect(statuses[0].message).toEqual(messages.at(-1));
+
+    const secondPage = await getStatuses(this.blog.id, {page: 2});
+
+    expect(secondPage.length).toEqual(100);
+
+    const shortPage = await getStatuses(this.blog.id, {page: 1, pageSize: 10});
+
+    expect(shortPage.length).toEqual(10);
+    expect(shortPage[0].message).toEqual(messages.at(-1));
+    expect(shortPage.at(-1).message).toEqual(messages.at(-10));
   });
 });
