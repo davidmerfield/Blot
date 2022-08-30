c0a7197c3b1ea8f420f3c0f95e157692c6097470
David Merfield <dmerfield@gmail.com>
2022-08-30 15:46:45 +0100

    Makes it possible to reset password before deleting account

--- app/dashboard/routes/account/password.js
+++ app/dashboard/routes/account/password.js
@@ -65,7 +65,7 @@ function save(req, res, next) {
 
     User.set(req.user.uid, { passwordHash: passwordHash }, function (err) {
       if (err) return next(err);
-      res.message("/account/password", "Saved your new password");
+      res.message("/dashboard", "Saved your new password");
     });
   });
 }
