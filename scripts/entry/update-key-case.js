2a0c3ed52246172d02a68532a3e0fc1ac3e4ce8b
David Merfield <dmerfield@gmail.com>
2023-02-08 16:57:23 +0000

    Finishes script to fix case of keys

--- scripts/entry/update-key-case.js
+++ scripts/entry/update-key-case.js
@@ -1,61 +1,74 @@
-const eachEntry = require("../each/entry");
 const eachBlog = require("../each/blog");
 const ensure = require("helper/ensure");
 const options = require("minimist")(process.argv.slice(2));
 const client = require("client");
+const async = require("async");
 
 const pathNormalizer = require("helper/pathNormalizer");
 
-const handle = (user, blog, entry, next) => {
-  const oldKey = "blog:" + blog.id + ":entry:" + oldPathNormalize(entry.path);
-  const newKey = "blog:" + blog.id + ":entry:" + pathNormalizer(entry.path);
-
-  const oldDepKey =
-    "blog:" + blog.id + ":dependents:" + oldPathNormalize(entry.path);
-  const newDepKey =
-    "blog:" + blog.id + ":dependents:" + pathNormalizer(entry.path);
-
-  if (oldKey === newKey && oldDepKey === newDepKey) return next();
-
-  console.log("updating keys:");
-
-  const multi = client.multi();
-
-  multi.rename(oldKey, newKey);
-  multi.rename(oldDepKey, newDepKey);
-
-  multi.exec(function (err, stat) {
-    if (err) throw err;
-    console.log("Stat:", stat);
-    next();
-  });
-};
-
 const handleBlog = (user, blog, next) => {
-  client.hgetall("blog:" + blog.id + ":ignored_files", function (
-    err,
-    ignoredFilePaths
-  ) {
+  client.zrevrange("blog:" + blog.id + ":all", 0, -1, function (err, ids) {
     if (err) throw err;
-    console.log(ignoredFilePaths);
-    next();
-  });
-};
+    const multi = client.multi();
 
-eachEntry(
-  handle,
-  function (err) {
-    if (err) throw err;
-    console.log("Finished each entry");
-    eachBlog(
-      handleBlog,
+    async.eachSeries(
+      ids,
+      (path, next) => {
+        const oldKey = "blog:" + blog.id + ":entry:" + oldPathNormalize(path);
+        const newKey = "blog:" + blog.id + ":entry:" + pathNormalizer(path);
+
+        const oldDepKey =
+          "blog:" + blog.id + ":dependents:" + oldPathNormalize(path);
+        const newDepKey =
+          "blog:" + blog.id + ":dependents:" + pathNormalizer(path);
+
+        if (oldKey === newKey && oldDepKey === newDepKey) return next();
+
+        client.exists(oldKey, function (err, exists) {
+          if (exists) {
+            console.log("updating:", oldKey, "=>", newKey);
+            multi.rename(oldKey, newKey);
+          } else {
+          }
+
+          client.exists(oldDepKey, function (err, exists) {
+            if (exists) {
+              console.log("updating:", oldDepKey, "=>", newDepKey);
+              multi.rename(oldDepKey, newDepKey);
+            } else {
+            }
+
+            next();
+          });
+        });
+      },
       function (err) {
         if (err) throw err;
-        console.log("Finished each blog");
-        process.exit();
-      },
-      options
+
+        multi.exec(function (err, stat) {
+          if (err) throw err;
+          console.log("Stat:", stat);
+
+          client.hgetall("blog:" + blog.id + ":ignored_files", function (
+            err,
+            ignoredFilePaths
+          ) {
+            if (err) throw err;
+            console.log(ignoredFilePaths);
+            next();
+          });
+        });
+      }
     );
+  });
+};
+
+eachBlog(
+  handleBlog,
+  function (err) {
+    if (err) throw err;
+    console.log("Finished each blog");
+    process.exit();
   },
   options
 );
