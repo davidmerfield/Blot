8dafc71a4976712e017924a66f03fd64a6ce592c
David Merfield <dmerfield@gmail.com>
2023-02-09 09:09:58 +0000

    Fix use of hashfile in sync.update (#612)

--- app/sync/tests/update.js
+++ app/sync/tests/update.js
@@ -14,6 +14,41 @@ describe("update", function () {
     this.checkEntry = global.test.CheckEntry(this.blog.id);
   });
 
+  it("rebuilds an entry if the source file changes", function (testDone) {
+    const path = this.fake.path(".txt");
+    let content = 1;
+    const checkEntry = this.checkEntry;
+
+    sync(this.blog.id, function (err, folder, done) {
+      if (err) testDone.fail(err);
+
+      fs.outputFileSync(folder.path + path, content.toString(), "utf-8");
+
+      const poll = setInterval(function () {
+        content = content + 1;
+        console.log("setting content to", content.toString());
+        fs.outputFileSync(folder.path + path, content.toString(), "utf-8");
+        if (content > 300) {
+          console.log("cleaning interval at:", content.toString());
+          clearInterval(poll);
+        }
+      }, 10);
+
+      folder.update(path, function (err) {
+        if (err) testDone.fail(err);
+
+        checkEntry(
+          { path: path, html: `<p>${content.toString()}</p>` },
+          function (err) {
+            if (err) testDone.fail(err);
+            done(null, testDone);
+          }
+        );
+      });
+    });
+  });
+
+
   it("creates an entry from a new file", function (testDone) {
     var path = this.fake.path(".txt");
     var content = this.fake.file();
