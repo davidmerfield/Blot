d0af6169940bb411207ee80364d7021cfc084161
David Merfield <dmerfield@gmail.com>
2023-02-14 14:50:27 +0000

    Adds script to lower-case folder contents

---
+++ scripts/dropbox/lowercase-folder.js
@@ -0,0 +1,40 @@
+const sync = require("sync");
+const get = require("../get/blog");
+const each = require("../each/blog");
+
+function main(blog, callback) {
+  sync(blog.id, async function (err, folder, done) {
+    if (err) return callback(err);
+
+    try {
+      await folder.lowerCaseContents();
+    } catch (e) {
+      return done(e, callback);
+    }
+
+    done(null, callback);
+  });
+}
+
+if (process.argv[2]) {
+  each(
+    function (user, blog, next) {
+      main(blog, next);
+    },
+    function (err) {
+      if (err) throw err;
+      console.log("Processed all blogs");
+      process.exit();
+    }
+  );
+} else {
+  get(process.argv[2], function (err, user, blog) {
+    if (err) throw err;
+
+    main(blog, function (err) {
+      if (err) throw err;
+      console.log("Processed blog");
+      process.exit();
+    });
+  });
+}
