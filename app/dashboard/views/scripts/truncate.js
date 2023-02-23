d09cc133ea74aafc795d5ce7b5f43c52a35e33e6
David Merfield <dmerfield@gmail.com>
2023-02-23 16:15:39 +0000

    Updates truncate script

--- app/dashboard/views/scripts/truncate.js
+++ app/dashboard/views/scripts/truncate.js
@@ -1,70 +1,100 @@
+function truncateAll(node) {
+  var nodes = Array.from(node.children);
+
+  const {
+    width
+  } = node.getBoundingClientRect();
+
+  const styles = window.getComputedStyle(node);
+
+  const paddingWidth = parseFloat(styles.paddingLeft) +
+    parseFloat(styles.paddingRight);
+
+  const borderWidth = parseFloat(styles.borderLeftWidth) +
+    parseFloat(styles.borderRightWidth);
+
+  const trueWidth = width - paddingWidth - borderWidth;
+
+  console.log('trueWidth of parent:', trueWidth);
+  
+  nodes.forEach(node=>node.style.maxWidth=Math.floor(trueWidth/nodes.length) + 'px');
+  nodes.forEach(n => truncate(n, trueWidth / nodes.length));
+}
+
 // crops any given text to the size of the given target
 function truncate(target) {
-
-  const storedText =  target.getAttribute('data-text');
-  
-  if (storedText) {
-    target.innerHTML = storedText;
-  } else {
-    target.setAttribute('data-text', target.innerHTML);
-  }
-
   const text = target.innerHTML;
-
   const fontStyle = window.getComputedStyle(target, null).getPropertyValue('font');
+  const measure = getTextWidth(fontStyle);
+  const ellipsisWidth = measure('...');
+  const textWidth = measure(text);
+
+  const {
+    width
+  } = target.getBoundingClientRect();
+
+  const styles = window.getComputedStyle(target);
+
+  const paddingWidth = parseFloat(styles.paddingLeft) +
+    parseFloat(styles.paddingRight);
+
+  const marginWidth = parseFloat(styles.marginLeft) +
+    parseFloat(styles.marginRight);
+
+  const borderWidth = parseFloat(styles.borderLeftWidth) +
+    parseFloat(styles.borderRightWidth);
+
+  const trueWidth = width - paddingWidth - borderWidth;
+
+  maxWidth =  trueWidth - ellipsisWidth;
+
+  console.log('text',text, maxWidth)
+
+  if (target.clientWidth === 0) {
+    return;
+  }
+
+  if (Math.round(textWidth) <= Math.round(target.clientWidth)) {
+    return;
+  }
+
+  let cutPoint;
+  let trimmedText = text;
+  let trimmedTextWidth;
+
+  do {
+    cutPoint = Math.floor(trimmedText.length / 2);
+    trimmedText = trimmedText.slice(0, cutPoint) + trimmedText.slice(cutPoint + 1, trimmedText.length);
+    trimmedTextWidth = measure(trimmedText);
+  } while (trimmedTextWidth >= maxWidth);
+
+  const result = trimmedText.slice(0, cutPoint) + '...' + trimmedText.slice(cutPoint, trimmedText.length);
 
 
-  /** recursively splices text from the middle by one char at a time 
-   * until text fits to given maxWidth
-   * then inserts the ellipsis in the middle
-   **/
-  sliceIfNotFitting(target, text, fontStyle);
+  target.innerHTML = result;
 }
 
-function sliceIfNotFitting(target, text, fontStyle, cutPoint) {
-
-  const ellipsisWidth = getTextWidth('...', fontStyle);
-  const maxWidth = Math.floor(target.clientWidth - ellipsisWidth);
-  var textWidth = getTextWidth(text, fontStyle);
-
-  if (target.clientWidth === 0 || 
-     cutPoint === undefined && Math.round(textWidth) <= Math.round(target.clientWidth)) {
-    return target.innerHTML = text;
-  }
-
-  if (cutPoint === undefined) {
-    cutPoint = Math.floor(text.length / 2)
-  }
-
-  if (textWidth > maxWidth) {
-    cutPoint = Math.floor(text.length / 2);
-    text = text.slice(0, cutPoint) + text.slice(cutPoint + 1, text.length);
-    // console.log('cropping', textWidth, text);
-    sliceIfNotFitting(target, text, fontStyle, cutPoint);
-  } else {
-    //  console.log('now it is good', text, textWidth)
-    // now add ellipsis we should all be good now
-    console.log('satisfied with', text, maxWidth);
-    target.innerHTML = text.slice(0, cutPoint) + '...' + text.slice(cutPoint, text.length);
-  }
-
-}
 
 // uses canvas to measure given text width with given font styles
-function getTextWidth(text, font) {
+function getTextWidth(fontStyle) {
   // re-use canvas object for better performance
   const canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
   const context = canvas.getContext("2d");
-  context.font = font;
-  const {width} = context.measureText(text);
-  return width;
+  context.font = fontStyle;
+  return function measure(text) {
+    const {
+      width
+    } = context.measureText(text);
+    return width;
+
+  };
 }
 
-// you can change the input width from the css
-document.querySelectorAll('.truncate').forEach(truncate);
 
-window.addEventListener('resize', function(event) {
-  document.querySelectorAll('.truncate').forEach(truncate);
-}, true);
+document.querySelectorAll('.truncate').forEach(function(node){
+  truncate(node);
+});
 
-
+document.querySelectorAll('.truncate-all').forEach(function(node){
+  truncateAll(node);
+});
