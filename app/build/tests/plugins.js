3976eac31a34183736059c1f5dbb8a97dbc4212c
David Merfield <dmerfield@gmail.com>
2023-03-08 18:10:17 +0000

    Improvements to Wikilinks (#622)
    * Run wikilinks conversion before typeset plugin
    
    * Adds support for relative and absolute wikilinks
    
    * Refactors build tests
    
    * Fixes wikilink tests
    
    * Adds other test for data attribute
    
    * Adds new test for typeset compatibility
    
    * Adds wikilink lookup by URL and title
    
    * Adds support for fuzzy lookup by path
    
    * Removes comments
    
    * Adds tests for absolute paths
    
    * Further cleanup
    
    * Updates todo
    
    * Adds to wikilink documentation
    
    * Updates todo

--- app/build/tests/plugins.js
+++ app/build/tests/plugins.js
@@ -1,6 +1,6 @@
 describe("build", function () {
-  var build = require("../index");
-  var fs = require("fs-extra");
+  const build = require("build");
+  const fs = require("fs-extra");
 
   global.test.blog();
 
@@ -18,68 +18,10 @@ describe("build", function () {
   it("will use a title to generate an image caption over the alt text", function (done) {
     const contents = `![Alt text here](foo.jpg "Title here")`;
     const path = "/hello.txt";
-    const html = '<p><img src="/foo.jpg" title="Title here" alt="Alt text here"><span class="caption">Title here</span></p>';
+    const html =
+      '<p><img src="/foo.jpg" title="Title here" alt="Alt text here"><span class="caption">Title here</span></p>';
 
     this.blog.plugins.imageCaption = { enabled: true, options: {} };
-    this.buildAndCheck({ path, contents }, { html }, done);
-  });
-
-  it("will convert wikilinks if plugin is enabled", function (done) {
-    const contents = "A [[wikilink]]";
-    const path = "/hello.txt";
-    const html = '<p>A <a href="wikilink" class="wikilink">wikilink</a></p>';
-
-    this.blog.plugins.wikilinks = { enabled: true, options: {} };
-    this.buildAndCheck({ path, contents }, { html }, done);
-  });
-
-  it("will convert multiple wikilinks on one line", function (done) {
-    const contents = "A [[wikilink]] and [[another|one]]";
-    const path = "/hello.txt";
-    const html =
-      '<p>A <a href="wikilink" class="wikilink">wikilink</a> and <a href="another" class="wikilink">one</a></p>';
-
-    this.blog.plugins.wikilinks = { enabled: true, options: {} };
-    this.buildAndCheck({ path, contents }, { html }, done);
-  });
-
-  it("will ignore wikilinks spanning multiple lines", function (done) {
-    const contents = "[[wiki\n\nhey]]";
-    const path = "/hello.txt";
-    const html =
-      '<p>[[wiki</p>\n<p>hey]]</p>';
-
-    this.blog.plugins.wikilinks = { enabled: true, options: {} };
-    this.buildAndCheck({ path, contents }, { html }, done);
-  });
-
-  it("will convert wikilinks inside other nodes", function (done) {
-    const contents = "- A **[[wikilink]]** in a list";
-    const path = "/hello.txt";
-    const html =
-      '<ul>\n<li>A <strong><a href="wikilink" class="wikilink">wikilink</a></strong> in a list</li>\n</ul>';
-
-    this.blog.plugins.wikilinks = { enabled: true, options: {} };
-    this.buildAndCheck({ path, contents }, { html }, done);
-  });
-
-  it("will convert wikilinks next to ignored nodes", function (done) {
-    const contents = "<script>console.log('hey');</script>\n\nA **[[wikilink elsewhere]]** ";
-    const path = "/hello.txt";
-    const html =
-      `<script>console.log('hey');</script>\n<p>A <strong><a href="wikilink elsewhere" class="wikilink">wikilink elsewhere</a></strong></p>`;
-
-    this.blog.plugins.wikilinks = { enabled: true, options: {} };
-    this.buildAndCheck({ path, contents }, { html }, done);
-  });
-
-  it("will convert wikilinks whose path contains square brackets", function (done) {
-    const contents = "[[../[snips]/wikilink]]";
-    const path = "/hello.txt";
-    const html =
-      '<p><a href="../[snips]/wikilink" class="wikilink">../[snips]/wikilink</a></p>';
-
-    this.blog.plugins.wikilinks = { enabled: true, options: {} };
     this.buildAndCheck({ path, contents }, { html }, done);
   });
 
@@ -95,7 +37,8 @@ describe("build", function () {
   it("will turn titles with nested children into title case if plugin is enabled", function (done) {
     const contents = "# Title *goes [with](/here)* children";
     const path = "/hello.txt";
-    const html = '<h1 id="title-goes-with-children">Title <em>Goes <a href="/here">With</a></em> Children</h1>';
+    const html =
+      '<h1 id="title-goes-with-children">Title <em>Goes <a href="/here">With</a></em> Children</h1>';
 
     this.blog.plugins.titlecase = { enabled: true, options: {} };
     this.buildAndCheck({ path, contents }, { html }, done);
