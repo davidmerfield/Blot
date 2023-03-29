024daeeb41689b8e8d160a63381da7eafcbbba23
David Merfield <dmerfield@gmail.com>
2023-03-29 14:20:06 +0100

    Updates compressino

--- app/helper/express-disk-cache/compress.js
+++ app/helper/express-disk-cache/compress.js
@@ -1,18 +1,16 @@
 var zopfli = require("node-zopfli");
-
+const gzip = require("util").promisify(zopfli.gzip);
 const fs = require("fs-extra");
 
 module.exports = async function compress(path) {
-  fs.createReadStream(path)
-    .pipe(
-      zopfli.createGzip({
-        verbose: false,
-        verbose_more: false,
-        numiterations: 15,
-        blocksplitting: true,
-        blocksplittinglast: false,
-        blocksplittingmax: 15,
-      })
-    )
-    .pipe(fs.createWriteStream(path + "gzip"));
+  const input = await fs.readFile(path);
+  const output = await gzip(input, {
+    verbose: false,
+    verbose_more: false,
+    numiterations: 15,
+    blocksplitting: true,
+    blocksplittinglast: false,
+    blocksplittingmax: 15,
+  });
+  await fs.outputFile(path + "gzip", output);
 };
