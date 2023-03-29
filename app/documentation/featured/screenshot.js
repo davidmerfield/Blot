99be9f8de91d65681bf32a4d1777886313a414ed
David Merfield <dmerfield@gmail.com>
2023-03-29 18:24:17 +0100

    Updates tool to screenshot featured sites

--- app/documentation/featured/screenshot.js
+++ app/documentation/featured/screenshot.js
@@ -1,12 +1,17 @@
-// ratio 1.60781421
 const screenshot = require("helper/screenshot");
+const root = require("helper/rootDir");
+const { join } = require("path");
 
 (async function (sites) {
   for (const { link, host } of sites) {
-    console.log('screenshotting', link);
-    await screenshot(link, __dirname + "/data/" + host + ".png", {
+    const path = join(root, "app/views/images/sites", host + ".png");
+
+    // ratio 1.60781421
+    const options = {
       width: 1400,
       height: 870,
-    });
+    };
+    
+    await screenshot(link, path, options);
   }
 })(require("./featured-checked.json"));
