f4851bcdf04d172bb348d9629306a238040addab
David Merfield <dmerfield@gmail.com>
2023-03-30 10:02:39 +0100

    Wraps disk cache

--- app/helper/express-disk-cache/compress.js
+++ app/helper/express-disk-cache/compress.js
@@ -3,14 +3,16 @@ const gzip = require("util").promisify(zopfli.gzip);
 const fs = require("fs-extra");
 
 module.exports = async function compress(path) {
-  const input = await fs.readFile(path);
-  const output = await gzip(input, {
-    verbose: false,
-    verbose_more: false,
-    numiterations: 15,
-    blocksplitting: true,
-    blocksplittinglast: false,
-    blocksplittingmax: 15,
-  });
-  await fs.outputFile(path + "gzip", output);
+  try {
+    const input = await fs.readFile(path);
+    const output = await gzip(input, {
+      verbose: false,
+      verbose_more: false,
+      numiterations: 15,
+      blocksplitting: true,
+      blocksplittinglast: false,
+      blocksplittingmax: 15,
+    });
+    await fs.outputFile(path + "gzip", output);
+  } catch (e) {}
 };
