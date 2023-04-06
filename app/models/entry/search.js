edde20444d8e6b3285edead840307ca51695359d
David Merfield <dmerfield@gmail.com>
2023-04-06 12:26:08 +0100

    Improves blog search

---
+++ app/models/entry/search.js
@@ -0,0 +1,39 @@
+const transliterate = require("transliteration");
+const ensure = require("helper/ensure");
+const client = require("models/client");
+const { promisify } = require("util");
+const get = promisify((blogID, entryIDs, callback) =>
+  require("./get")(blogID, entryIDs, function (entries) {
+    callback(null, entries);
+  })
+);
+
+module.exports = function (blogID, query, callback) {
+  ensure(blogID, "string").and(query, "string").and(callback, "function");
+
+  const results = [];
+
+  query = transliterate(query);
+
+  // this will not search pages or deleted entries
+  const key = "blog:" + blogID + ":entries";
+
+  client.zrevrange(key, 0, -1, async function (err, entryIDs) {
+    const chunked = [...chunks(entryIDs, 100)];
+
+    for (const page of chunked) {
+      const entries = await get(blogID, page);
+      for (const entry of entries) {
+        if (entry.html.includes(query)) results.push(entry);
+      }
+    }
+
+    callback(null, results);
+  });
+};
+
+function* chunks(arr, n) {
+  for (let i = 0; i < arr.length; i += n) {
+    yield arr.slice(i, i + n);
+  }
+}
---
+++ scripts/db/remove-search-keys.js
@@ -0,0 +1,46 @@
+const client = require("redis").createClient();
+const multi = client.multi();
+const yesno = require("yesno");
+const Keys = require("./keys");
+
+const keysToDelete = [];
+
+Keys("blog:*:search:*", handle, done);
+
+function handle(keys, next) {
+  if (keysToDelete.length > 100 && process.argv[2] !== "-f") return done();
+
+  keys
+    // we need to perform this filtering because
+    // the pattern will match some keys incorrectly
+    // since the * will include ':', e.g.
+    // blog:123:entry:search.txt <- should not be deleted
+    // but will appear in the list of keys
+    .filter(function (key) {
+      const blogID = key.split(":")[1];
+      return key.startsWith(`blog:${blogID}:search:`);
+    })
+    .forEach((key) => keysToDelete.push(key));
+
+  next();
+}
+
+function done(err) {
+  if (err) throw err;
+  if (!keysToDelete.length) {
+    console.log("No keys to delete");
+    process.exit();
+  }
+  console.log(JSON.stringify(keysToDelete, null, 2));
+  yesno.ask("Delete " + keysToDelete.length + " keys? (y/n)", false, function (
+    ok
+  ) {
+    if (!ok) return process.exit();
+    multi.del(keysToDelete);
+    multi.exec(function (err) {
+      if (err) throw err;
+      console.log("Deleted " + keysToDelete.length + " keys");
+      process.exit();
+    });
+  });
+}
