5dc8c4895b537eafffa02ca164d9a67c077be12f
David Merfield <dmerfield@gmail.com>
2023-04-06 11:40:40 +0100

    Updates search

--- app/models/entry/search.js
+++ app/models/entry/search.js
@@ -1,6 +1,10 @@
 const transliterate = require("transliteration");
+const ensure = require("helper/ensure");
 
 module.exports = function (blogID, query, callback) {
+  ensure(blogID, "string").and(query, "string").and(callback, "function");
+
   query = transliterate(query);
+
   callback(null, []);
 };
