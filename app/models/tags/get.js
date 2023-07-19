0acf9f48f8104b6195773263afc3b19b12db255b
David Merfield <dmerfield@gmail.com>
2023-07-19 17:38:12 +0100

    Fixes tag lookup when involving accents

--- app/models/tags/get.js
+++ app/models/tags/get.js
@@ -2,7 +2,24 @@ var normalize = require("./normalize");
 var get = require("./_get");
 
 // This is the method exposed publicly
-// it normalized the tag first...
+// It checks a few variants of the tag
+// until it finds one that works. 
+// It first tries the normalized tag,
+// then the tag as it was entered,
+// then the tag as it was entered but decoded, e.g.
+// "Hello%20World" -> "Hello World" 
 module.exports = function (blogID, tag, callback) {
-  return get(blogID, normalize(tag), callback);
+  get(blogID, normalize(tag), function (err, entryIDs, prettyTag) {
+    if (entryIDs && entryIDs.length) {
+      return callback(null, entryIDs, prettyTag);
+    }
+
+    get(blogID, tag, function (err, entryIDs, prettyTag) {
+      if (entryIDs && entryIDs.length) {
+        return callback(null, entryIDs, prettyTag);
+      }
+
+      get(blogID, decodeURIComponent(tag), callback);
+    });
+  });
 };
