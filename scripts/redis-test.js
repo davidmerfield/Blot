7265476db45585741c9bb9d84d397ec0128604a0
David Merfield <dmerfield@gmail.com>
2023-07-29 16:04:22 +0100

    Update redis script

--- scripts/redis-test.js
+++ scripts/redis-test.js
@@ -1,5 +1,4 @@
 var config = require("config");
-const { ConsoleReporter } = require("jasmine");
 var redis = require("redis");
 
 var clientA = redis.createClient(config.redis.port);
@@ -11,10 +10,10 @@ clientA.on("error", function (err) {
   if (err.stack) console.log(err.stack);
 });
 
-console.time('clientA.ping');
+console.time("clientA.ping");
 clientA.ping(function (err, reply) {
   if (err) throw err;
-  console.timeEnd('clientA.ping');
+  console.timeEnd("clientA.ping");
   console.log("A: " + reply);
 });
 
@@ -22,7 +21,7 @@ const remoteRedisURL = "redis://" + process.argv[2] + ":6379";
 
 console.log("remoteRedisURL", remoteRedisURL);
 
-var clientB = redis.createClient({url: remoteRedisURL});
+var clientB = redis.createClient({ url: remoteRedisURL });
 
 clientB.on("error", function (err) {
   console.log("Redis Error:");
@@ -31,9 +30,9 @@ clientB.on("error", function (err) {
   if (err.stack) console.log(err.stack);
 });
 
-console.time('clientB.ping');
+console.time("clientB.ping");
 clientB.ping(function (err, reply) {
   if (err) throw err;
-  console.timeEnd('clientB.ping');
+  console.timeEnd("clientB.ping");
   console.log("B: " + reply);
 });
