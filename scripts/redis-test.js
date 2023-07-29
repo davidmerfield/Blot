492e8f6b243fbe4cb0c7372350ad990af8079156
David Merfield <dmerfield@gmail.com>
2023-07-29 15:59:44 +0100

    Update redis script

--- scripts/redis-test.js
+++ scripts/redis-test.js
@@ -3,18 +3,32 @@ var redis = require("redis");
 
 var clientA = redis.createClient(config.redis.port);
 
+clientA.on("error", function (err) {
+  console.log("Redis Error:");
+  console.log(err);
+  if (err.trace) console.log(err.trace);
+  if (err.stack) console.log(err.stack);
+});
+
+clientA.ping(function (err, reply) {
+  if (err) throw err;
+  console.log("A: " + reply);
+});
+
 const remoteRedisURL = "redis://" + process.argv[2] + ":6379";
 
 console.log("remoteRedisURL", remoteRedisURL);
 
 var clientB = redis.createClient(remoteRedisURL);
 
+clientB.on("error", function (err) {
+  console.log("Redis Error:");
+  console.log(err);
+  if (err.trace) console.log(err.trace);
+  if (err.stack) console.log(err.stack);
+});
+
 clientB.ping(function (err, reply) {
   if (err) throw err;
   console.log("B: " + reply);
-});
-
-clientA.ping(function (err, reply) {
-  if (err) throw err;
-  console.log("A: " + reply);
 });
