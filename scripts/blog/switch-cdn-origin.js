7eff4adf6e4f3301318654de79997659a451c3c4
David Merfield <dmerfield@gmail.com>
2023-08-02 13:56:58 +0100

    Switch from blotcdn.com to cdn.blot.im (#675)
    * Simplfies CDN setup
    
    * Updates cdn conf
    
    * Update cdn origin switching script
    
    * tweaks redis script
    
    * Finishes switch CDN script
    
    * Revert nginx changes
    
    * Removes blot cdn conf

--- scripts/blog/switch-cdn-origin.js
+++ scripts/blog/switch-cdn-origin.js
@@ -1,5 +1,7 @@
-const OLD_ORIGIN = "https://blotcdn.com";
-const NEW_ORIGIN = "//cdn.blot.im";
+const each = require("../each/blog");
+
+const OLD_ORIGIN = "https://blotcdn.com/";
+const NEW_ORIGIN = "https://cdn.blot.im/";
 
 const keys = require("../db/keys");
 const get = require("../get/blog");
@@ -7,23 +9,48 @@ const client = require("client");
 
 const async = require("async");
 
-get(process.argv[2], function (err, user, blog) {
-  if (err) throw err;
+if (process.argv[2]) {
+  get(process.argv[2], function (err, user, blog) {
+    if (err) throw err;
+    main(blog, function (err) {
+      if (err) throw err;
+      console.log("Processed!");
+      process.exit();
+    });
+  });
+} else {
+  each(
+    (user, blog, next) => {
+      if (!blog) return next();
+      if (blog.isDisabled) return next();
+      main(blog, next);
+    },
+    (err) => {
+      if (err) throw err;
+      console.log("All blogs processed!");
+      process.exit();
+    }
+  );
+}
+
+function main(blog, callback) {
+  if (!blog.id) return callback(new Error("Please pass a blog"));
 
   const searchParam = "*" + blog.id + "*";
+
+  console.log("searching '" + searchParam + "'");
 
   keys(
     searchParam,
     function (keys, next) {
       if (!keys.length) return next();
 
-      //   console.log("Found " + keys.length + " keys associated with this blog");
+      const multi = client.multi();
 
       // For each key in series
       async.eachSeries(
         keys,
         function (key, next) {
-          //   console.log("Checking key", key);
           client.type(key, function (err, type) {
             if (err) return next(err);
 
@@ -32,13 +59,12 @@ get(process.argv[2], function (err, user, blog) {
                 if (err) return next(err);
 
                 if (value.indexOf(OLD_ORIGIN) > -1) {
-                  console.log("Found string key with old origin", key);
-                  console.log(value);
+                  console.log("Modifying", key);
                   value = value.split(OLD_ORIGIN).join(NEW_ORIGIN);
-                  client.set(key, value, next);
-                } else {
-                  next();
+                  multi.set(key, value);
                 }
+
+                next();
               });
             } else if (type === "hash") {
               client.hgetall(key, function (err, value) {
@@ -47,30 +73,27 @@ get(process.argv[2], function (err, user, blog) {
                 Object.keys(value).forEach(function (hashKey) {
                   const hashValue = value[hashKey];
                   if (hashValue.indexOf(OLD_ORIGIN) > -1) {
-                    console.log("Found hash key with old origin", key, hashKey);
-                    console.log(hashValue);
-                    //   value[hashKey] = hashValue.replace(OLD_ORIGIN, NEW_ORIGIN);
-                    //   client.hset(key, hashKey, value[hashKey], next);
-                  } else {
+                    console.log("Modifying", key, hashKey);
+                    value = value.split(OLD_ORIGIN).join(NEW_ORIGIN);
+                    multi.hset(key, hashKey, value[hashKey]);
                   }
                 });
 
                 next();
               });
             } else {
-              console.log("key", key, "is unsupported type: ", type);
+              //   console.log("key", key, "is unsupported type: ", type);
               next();
             }
           });
         },
-        next
+        (err) => {
+          if (err) return next(err);
+
+            multi.exec(next);
+        }
       );
     },
-    function (err) {
-      if (err) throw err;
-
-      console.log("Done!");
-      process.exit();
-    }
+    callback
   );
-});
+}
