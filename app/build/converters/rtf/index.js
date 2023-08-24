c5ec8847e4252cc0e7e3bb5e11c33dd7ed2b12ea
David Merfield <dmerfield@gmail.com>
2023-08-24 15:45:15 +0100

    Fixes bug with metadata in RTF files

--- app/build/converters/rtf/index.js
+++ app/build/converters/rtf/index.js
@@ -3,7 +3,9 @@ var ensure = require("helper/ensure");
 var LocalPath = require("helper/localPath");
 var time = require("helper/time");
 var extname = require("path").extname;
-
+var Metadata = require("build/metadata");
+var cheerio = require("cheerio");
+var extend = require("helper/extend");
 var convert = require("./convert");
 
 function is(path) {
@@ -34,6 +36,47 @@ function read(blog, path, options, callback) {
 
       convert(blog, text, options, function (err, html) {
         if (err) return callback(err);
+        var metadata = {};
+        var $ = cheerio.load(html, { decodeEntities: false });
+
+        $("p").each(function (i) {
+          
+          if (i === 0 && $(this).prev().length) {
+            return false;
+          }
+
+          var text = $(this).text();
+
+          if (text.indexOf(":") === -1) return false;
+
+          var key = text.slice(0, text.indexOf(":"));
+
+          // Key has space
+          if (/\s/.test(key.trim())) return false;
+
+          var parsed = Metadata(text);
+
+          if (parsed.html === text) return false;
+
+          extend(metadata).and(parsed.metadata);
+
+          if (parsed.html) {
+            $(this).html(parsed.html.trim());
+          } else {
+            $(this).remove();
+          }
+        });
+
+        html = $.html().trim();
+
+        var metadataString = "<!--";
+
+        for (var i in metadata) metadataString += "\n" + i + ": " + metadata[i];
+
+        if (metadataString !== "<!--") {
+          metadataString += "\n-->\n";
+          html = metadataString + html;
+        }
 
         callback(null, html, stat);
       });
