c138ef330ebf1274f35b090e3057df603b9d426c
David Merfield <dmerfield@gmail.com>
2023-10-02 17:46:06 +0100

    Make tags.get more tolerant of bad inputs (#696)
    * Make get more tolerant of bad inputs
    
    * Linting

--- app/models/tags/get.js
+++ app/models/tags/get.js
@@ -3,13 +3,28 @@ var get = require("./_get");
 
 // This is the method exposed publicly
 // It checks a few variants of the tag
-// until it finds one that works. 
+// until it finds one that works.
 // It first tries the normalized tag,
 // then the tag as it was entered,
 // then the tag as it was entered but decoded, e.g.
-// "Hello%20World" -> "Hello World" 
+// "Hello%20World" -> "Hello World"
 module.exports = function (blogID, tag, callback) {
-  get(blogID, normalize(tag), function (err, entryIDs, prettyTag) {
+  let normalizedTag = tag;
+  let decodedTag = tag;
+
+  try {
+    normalizedTag = normalize(tag);
+  } catch (e) {
+    // do nothing if normalization fails
+  }
+
+  try {
+    decodedTag = decodeURIComponent(tag);
+  } catch (e) {
+    // do nothing if decoding fails
+  }
+
+  get(blogID, normalizedTag, function (err, entryIDs, prettyTag) {
     if (entryIDs && entryIDs.length) {
       return callback(null, entryIDs, prettyTag);
     }
@@ -19,7 +34,7 @@ module.exports = function (blogID, tag, callback) {
         return callback(null, entryIDs, prettyTag);
       }
 
-      get(blogID, decodeURIComponent(tag), callback);
+      get(blogID, decodedTag, callback);
     });
   });
 };
