9291899418b881139c3f12b058ba8073406d9cc0
David Merfield <dmerfield@gmail.com>
2023-10-02 17:39:24 +0100

    Make get more tolerant of bad inputs

--- app/models/tags/get.js
+++ app/models/tags/get.js
@@ -1,25 +1,40 @@
-var normalize = require("./normalize");
-var get = require("./_get");
+var normalize = require('./normalize')
+var get = require('./_get')
 
 // This is the method exposed publicly
 // It checks a few variants of the tag
-// until it finds one that works. 
+// until it finds one that works.
 // It first tries the normalized tag,
 // then the tag as it was entered,
 // then the tag as it was entered but decoded, e.g.
-// "Hello%20World" -> "Hello World" 
+// "Hello%20World" -> "Hello World"
 module.exports = function (blogID, tag, callback) {
-  get(blogID, normalize(tag), function (err, entryIDs, prettyTag) {
+  let normalizedTag = tag
+  let decodedTag = tag
+
+  try {
+    normalizedTag = normalize(tag)
+  } catch (e) {
+    // do nothing if normalization fails
+  }
+
+  try {
+    decodedTag = decodeURIComponent(tag)
+  } catch (e) {
+    // do nothing if decoding fails
+  }
+
+  get(blogID, normalizedTag, function (err, entryIDs, prettyTag) {
     if (entryIDs && entryIDs.length) {
-      return callback(null, entryIDs, prettyTag);
+      return callback(null, entryIDs, prettyTag)
     }
 
     get(blogID, tag, function (err, entryIDs, prettyTag) {
       if (entryIDs && entryIDs.length) {
-        return callback(null, entryIDs, prettyTag);
+        return callback(null, entryIDs, prettyTag)
       }
 
-      get(blogID, decodeURIComponent(tag), callback);
-    });
-  });
-};
+      get(blogID, decodedTag, callback)
+    })
+  })
+}
