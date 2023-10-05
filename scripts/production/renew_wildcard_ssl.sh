19c967dd6d4d7c37855b87d5bf061ef6ebbe2aea
David Merfield <dmerfield@gmail.com>
2023-10-05 17:43:21 +0100

    Tweaks script to renew wildcard ssl cert

--- scripts/production/renew_wildcard_ssl.sh
+++ scripts/production/renew_wildcard_ssl.sh
@@ -22,20 +22,13 @@ echo "[`date -u +%Y-%m-%dT%T.%3NZ`] Beginning renewal of wildcard certificate"
 $ACME --no-reload-nginx --dns-provider route53 -d "*.$BLOT_HOST" -d "$BLOT_HOST" 
 echo "[`date -u +%Y-%m-%dT%T.%3NZ`] Finished renewal of wildcard certificate" 
 
-# We need to store the key and pem in redis so that the openresty
-# reload script can fetch them
+# We need to store the key and pem in redis so that the openresty reload script can fetch them
 echo "[`date -u +%Y-%m-%dT%T.%3NZ`] Beginning storage of key and pem in redis"
-cp /etc/ssl/private/letsencrypt-domain.key /tmp/letsencrypt-domain.key
-cp /etc/ssl/private/letsencrypt-domain.pem /tmp/letsencrypt-domain.pem
 
 # store the contents of /tmp/letsencrypt-domain.key in redis at the key 'blot:openresty:ssl:key'
 # use redis-cli
-cat /tmp/letsencrypt-domain.key | redis-cli -h $BLOT_REDIS_HOST set 'blot:openresty:ssl:key'
-cat /tmp/letsencrypt-domain.pem | redis-cli -h $BLOT_REDIS_HOST set 'blot:openresty:ssl:pem'
-
-# clean up the temporary files
-rm /tmp/letsencrypt-domain.key
-rm /tmp/letsencrypt-domain.pem
+cat /etc/ssl/private/letsencrypt-domain.key | redis-cli -h $BLOT_REDIS_HOST -x set 'blot:openresty:ssl:key'
+cat /etc/ssl/private/letsencrypt-domain.pem | redis-cli -h $BLOT_REDIS_HOST -x set 'blot:openresty:ssl:pem'
 
 redis-cli -h $BLOT_REDIS_HOST set 'blot:openresty:ssl:updated' $(date -u +%s)
 
