f2973362efb20dfbf681432673f4ef36e26dc6ea
David Merfield <dmerfield@gmail.com>
2023-10-30 17:00:08 +0000

    updates workflows

--- config/openresty/conf/cacher/tests/start-openresty.sh
+++ config/openresty/conf/cacher/tests/start-openresty.sh
@@ -7,12 +7,22 @@ if [ -z "$PIDS" ]; then
   echo "No nginx processes running"
 else
   
-  # for each PID, kill it
-    for PID in $PIDS
+# for each PID, kill it
+for PID in $PIDS
     do
       echo "Killing nginx process $PID"
       sudo kill -9 $PID
     done
 fi
 
-sudo openresty -c $1
+# determine the path to the openresty executable
+# on my mac it will be present in the PATH already, but on github actions 
+# we need to use: /usr/local/openresty/bin/openresty
+OPENRESTY=$(which openresty)
+
+if [ -z "$OPENRESTY" ]; then
+  OPENRESTY="/usr/local/openresty/bin/openresty"
+  exit 1
+fi
+
+sudo $OPENRESTY -c $1
