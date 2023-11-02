0d8fd8ac1372353c6858c25035f78008863bbf74
David Merfield <dmerfield@gmail.com>
2023-11-02 18:13:27 +0000

    Adjusts setup script

--- app/setup.js
+++ app/setup.js
@@ -1,29 +1,28 @@
-const config = require('config')
-const root = require('helper/rootDir')
-const fs = require('fs-extra')
+const config = require("config");
+const root = require("helper/rootDir");
+const fs = require("fs-extra");
 
-const redis = require('models/redis')
-const client = new redis()
-const { join } = require('path')
-const documentation = require('./documentation/build')
-const templates = require('./templates')
-const async = require('async')
-const clfdate = require('helper/clfdate')
+const redis = require("models/redis");
+const client = new redis();
+const documentation = require("./documentation/build");
+const templates = require("./templates");
+const async = require("async");
+const clfdate = require("helper/clfdate");
 
 const log = (...arguments) =>
-  console.log.apply(null, [clfdate(), 'Setup:', ...arguments])
+  console.log.apply(null, [clfdate(), "Setup:", ...arguments]);
 
 function main (callback) {
   async.series(
     [
       async function () {
-        log('Creating required directories')
-        await fs.ensureDir(config.blog_folder_dir)
-        await fs.ensureDir(config.blog_static_files_dir)
-        await fs.ensureDir(root + '/tmp')
-        await fs.ensureDir(root + '/data')
-        await fs.ensureDir(root + '/logs')
-        log('Created required directories')
+        log("Creating required directories");
+        await fs.ensureDir(config.blog_folder_dir);
+        await fs.ensureDir(config.blog_static_files_dir);
+        await fs.ensureDir(root + "/tmp");
+        await fs.ensureDir(root + "/data");
+        await fs.ensureDir(root + "/logs");
+        log("Created required directories");
       },
 
       function (callback) {
@@ -34,50 +33,60 @@ function main (callback) {
         // to the apex domain, but we need to generate a cert to do this.
         // Typically, domain keys like domain:example.com store a blog's ID
         // but since the homepage is not a blog, we just use a placeholder 'X'
-        log('Creating SSL key for redis')
+        log("Creating SSL key for redis");
         client.msetnx(
-          ['domain:' + config.host, 'X', 'domain:www.' + config.host, 'X'],
+          ["domain:" + config.host, "X", "domain:www." + config.host, "X"],
           function (err) {
             if (err) {
               console.error(
-                'Unable to set domain flag for host' +
+                "Unable to set domain flag for host" +
                   config.host +
-                  '. SSL may not work on site.'
-              )
-              console.error(err)
+                  ". SSL may not work on site."
+              );
+              console.error(err);
             }
 
-            log('Created SSL key for redis')
-            callback()
+            log("Created SSL key for redis");
+            callback();
           }
-        )
+        );
       },
       async function () {
-        log('Building documentation')
-        await documentation({ watch: config.environment === 'development' })
-        log('Built documentation')
+        // we only want to build the documentation in development
+        // in production we run node app/setup.js to build the documentation
+        // before starting the server
+        if (config.environment === "development" && require.main !== module) {
+          log("Building documentation");
+          await documentation({ watch: config.environment === "development" });
+          log("Built documentation");
+        }
       },
       function (callback) {
-        log('Building templates')
-        templates(
-          { watch: config.environment === 'development' },
-          function (err) {
-            if (err) throw err
-            log('Built templates')
-            callback()
-          }
-        )
+        // we only want to build the templates in development
+        // in production we run node app/setup.js to build the documentation
+        // before starting the server
+        if (config.environment === "development" && require.main !== module) {
+          log("Building templates");
+          templates(
+            { watch: config.environment === "development" },
+            function (err) {
+              if (err) throw err;
+              log("Built templates");
+              callback();
+            }
+          );
+        }
       }
     ],
     callback
-  )
+  );
 }
 
 if (require.main === module) {
   main(function (err) {
-    if (err) throw err
-    process.exit()
-  })
+    if (err) throw err;
+    process.exit();
+  });
 }
 
-module.exports = main
+module.exports = main;
