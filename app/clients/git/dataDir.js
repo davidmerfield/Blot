d8e491bc06a72ab5d24199aeadbbbf6bc06c8e61
David Merfield <dmerfield@gmail.com>
2023-11-02 16:59:07 +0000

    Updates git client data directory

--- app/clients/git/dataDir.js
+++ app/clients/git/dataDir.js
@@ -1,7 +1,12 @@
-require("fs-extra").ensureDirSync(__dirname + "/data");
+const config = require("config");
+const { blot_directory } = config;
+const { join } = require("path");
+const git_data_directory = join(blot_directory, "data/git");
+const fs = require("fs-extra");
+
+fs.ensureDirSync(git_data_directory);
 
 // consolidated reference to location of bareRepoDirectory in
 // tests and code so we can move this in future painlessly:
 // basically look for '/data'
-
-module.exports = __dirname + "/data";
+module.exports = git_data_directory;
