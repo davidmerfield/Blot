39374d424aa69414b8bb4c8debef0d882e558c87
David Merfield <dmerfield@gmail.com>
2023-11-09 14:32:25 +0000

    Updates script

--- scripts/user/identify-overdue-users.js
+++ scripts/user/identify-overdue-users.js
@@ -1,6 +1,9 @@
 const each = require("../each/user");
 const child_process = require("child_process");
 const { blog_static_files_dir, blog_folder_dir } = require("config");
+
+let rolling_total = 0;
+
 each(
   function (user, next) {
     if (user.isDisabled) {
@@ -17,24 +20,29 @@ each(
 
       // user.blogs is an array of blogIDs
       for (const blogID of user.blogs) {
-        console.log("blogID", blogID);
+        // console.log("blogID", blogID);
         const static_space_used = child_process
-          .execSync(`du -sh ${blog_static_files_dir}/${blogID}`)
+          .execSync(`du -s ${blog_static_files_dir}/${blogID}`)
           .toString();
         console.log("static space used:", static_space_used);
         const folder_space_used = child_process
-          .execSync(`du -sh ${blog_folder_dir}/${blogID}`)
+          .execSync(`du -s ${blog_folder_dir}/${blogID}`)
           .toString();
+        const total_in_bytes =
+          parseInt(static_space_used) + parseInt(folder_space_used);
         console.log("folder space used:", folder_space_used);
+
+        rolling_total += total_in_bytes;
       }
 
-      console.log(user.subscription);
+      //   console.log(user.subscription);
     }
     next();
   },
   function (err) {
     if (err) throw err;
     console.log("Done!");
+    console.log("Total space to be deleted:", rolling_total, "bytes");
     process.exit();
   }
 );
