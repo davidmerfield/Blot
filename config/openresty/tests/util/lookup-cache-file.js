2bbc506afa3fcd81ebebeba3c096a403182755b5
David Merfield <dmerfield@gmail.com>
2023-11-22 18:06:58 +0000

    Updates inspect cache

---
+++ config/openresty/tests/util/lookup-cache-file.js
@@ -0,0 +1,34 @@
+const fs = require("fs-extra");
+const fetch = require("node-fetch");
+const main = async cache_key_hash => {
+  const cache_directory = "/var/www/cache";
+  // the cache file path is in the following format: $x/$y/$cache_key_hash
+  // where x is the last character of the cache_key_hash
+  // and y are the two characters before x
+  const cache_file_path = `${cache_directory}/${
+    cache_key_hash[cache_key_hash.length - 1]
+  }/${cache_key_hash[cache_key_hash.length - 3]}/${cache_key_hash}`;
+
+  const stream = fs.createReadStream(cache_file_path, { encoding: "utf8" });
+  let data = "";
+  for await (const chunk of stream) {
+    data += chunk;
+    if (data.length > 1024 * 1024) break;
+    if (data.includes("KEY: ")) break;
+  }
+  const host_line = data.split("\n").find(line => line.includes("KEY: "));
+  const uri = host_line.split("KEY: ")[1];
+
+  console.log(uri);
+
+  // we fetch the uri and check the response headers for 'Blot-Cache' which will be HIT or MISS
+  const response = await fetch(uri);
+  const cache_status = response.headers.get("Blot-Cache");
+  console.log("cache_status", cache_status);
+};
+
+module.exports = main;
+
+if (require.main === module) {
+  main(process.argv[2]);
+}
