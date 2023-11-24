b0b65565f7969de9fb27c4b169902b406fbc9883
David Merfield <dmerfield@gmail.com>
2023-11-24 16:09:01 +0000

    Further fixes

--- app/cdn.js
+++ app/cdn.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const cdn = new express.Router();
 const { blot_directory } = require("config");
-const compression = require("compression");
 
 // Use express static and try to match the request to files in the following directories:
 // $blot_directory/data/static/$uri;
 // $blot_directory/app/blog/static/$uri;
-// we also want to use maximum gzip compression and set the max age to 1 year
 // we also want to add the header 'access-control-allow-origin' to all responses
-
-cdn.use(compression({ level: 9 }));
 
 cdn.use(
   express.static(blot_directory + "/data/static", {
