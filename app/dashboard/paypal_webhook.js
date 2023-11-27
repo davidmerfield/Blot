c55adcecc72a728bdbc3c0d44f6de044559a17eb
David Merfield <dmerfield@gmail.com>
2023-11-27 17:18:26 +0000

    Update config

--- app/dashboard/paypal_webhook.js
+++ app/dashboard/paypal_webhook.js
@@ -6,7 +6,6 @@ paypal.post("/", parser.json(), (req, res) => {
   console.log("UNTRUSTED WEBHOOK PAYLOAD", req.body);
 
   // we basically fetch the subscription from paypal and restore it in the db
-
   res.status(200).send("OK");
 });
 
