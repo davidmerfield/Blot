6ad67d556ed31b32363ec33e2d7691c8549f1f79
David Merfield <dmerfield@gmail.com>
2023-12-01 16:20:02 +0000

    Fixes delete blog

--- app/dashboard/account/delete.js
+++ app/dashboard/account/delete.js
@@ -7,94 +7,10 @@ var logout = require("./util/logout");
 var async = require("async");
 
 var Blog = require("models/blog");
-var pretty = require("helper/prettyPrice");
-
-var User = require("models/user");
 var config = require("config");
 var stripe = require("stripe")(config.stripe.secret);
 
 const parse = require("dashboard/parse");
-
-Delete.route("/blog/:handle")
-
-  // Verify the blog to be closed is owned
-  // by the current user and load it into locals.
-  // Work out how much we need to change the subscription
-  // by after the blog has been deleted.
-  .all(loadBlogToDelete, calculateSubscriptionChange)
-
-  .get(function (req, res) {
-    res.locals.title = "Delete " + req.blogToDelete.pretty.label;
-    res.locals.breadcrumb = "Delete  " + req.blogToDelete.pretty.label;
-    res.render("account/delete-blog", { host: process.env.BLOT_HOST });
-  })
-
-  // Save any changes to the user's subscription
-  // then remove the blogID from the list of blogs
-  // owned by the user.
-  // Finally delete contents of the blog's folder on s3
-  // which contains cached images/avatars. Also delete
-  // the contents blog's folder on the server.
-  // Delete the credentials used to sync the blog's folder
-  .post(
-    parse,
-    checkPassword,
-    function (req, res, next) {
-      Blog.remove(req.blogToDelete.id, next);
-    },
-    calculateSubscriptionChange,
-    decreaseSubscription,
-    function (req, res) {
-      res.message(req.baseUrl, "Deleted " + req.blogToDelete.title);
-    }
-  );
-
-function loadBlogToDelete (req, res, next) {
-  Blog.get({ handle: req.params.handle }, function (err, blog) {
-    if (err) {
-      return next(err);
-    }
-
-    if (!blog || blog.owner !== req.user.uid) {
-      return next(new Error("There is no blog to delete"));
-    }
-
-    req.blogToDelete = Blog.extend(blog);
-    res.locals.blogToDelete = blog;
-
-    next();
-  });
-}
-
-function calculateSubscriptionChange (req, res, next) {
-  var subscription = req.user.subscription;
-
-  // The user does not have an active subscription
-  // so proceed to the next middleware
-  if (!subscription || !subscription.status || subscription.status !== "active")
-    return next();
-
-  var currentQuantity = req.user.subscription.quantity;
-  var newQuantity = req.user.blogs.length - 1;
-
-  // Quantity cannot go below 1
-  // You must pay for at least one blog to keep an account open
-  // We only decrease the quantity on the Stripe plan if you are
-  // paying for all of your blogs.
-  if (newQuantity < 1) newQuantity = 1;
-
-  // You can't increase your bill by deleting a blog
-  // Some early users have free blogs for various
-  // reasons. Handle this case here.
-  if (newQuantity >= currentQuantity) return next();
-
-  res.locals.reduction = pretty(
-    (currentQuantity - newQuantity) * req.user.subscription.plan.amount
-  );
-  req.newQuantity = newQuantity;
-
-  return next();
-}
 
 Delete.route("/")
 
@@ -127,32 +43,6 @@ function deleteBlogs (req, res, next) {
 
 function deleteUser (req, res, next) {
   User.remove(req.user.uid, next);
-}
-
-function decreaseSubscription (req, res, next) {
-  var subscription = req.user.subscription;
-  var quantity = req.newQuantity;
-
-  if (!quantity || !subscription) return next();
-
-  stripe.customers.updateSubscription(
-    subscription.customer,
-    subscription.id,
-    { quantity: quantity, prorate: false },
-    function (err, subscription) {
-      if (err) return next(err);
-
-      if (!subscription) return next(new Error("No subscription"));
-
-      User.set(req.user.uid, { subscription: subscription }, function (err) {
-        if (err) return next(err);
-
-        Email.SUBSCRIPTION_DECREASE(req.user.uid);
-
-        next();
-      });
-    }
-  );
 }
 
 async function deleteSubscription (req, res, next) {
