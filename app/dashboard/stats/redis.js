20bdab37698e52e26b01ac8c7e759b9b786c3cd6
David Merfield <dmerfield@gmail.com>
2023-12-01 18:39:22 +0000

    Updates stats

---
+++ app/dashboard/stats/redis.js
@@ -0,0 +1,65 @@
+const moment = require("moment");
+const fs = require("fs-extra");
+const STATS_DIRECTORY = require("./statsDirectory") + "/redis";
+const client = require("client");
+
+async function main () {
+  const stats = await getAllStats();
+
+  let hour;
+  let hourData = [];
+  let hourPath = STATS_DIRECTORY + "/" + hour + ".json";
+
+  for (let i = 0; i < stats.length; i++) {
+    const minute = date.format("YYYY-MM-DD-HH-mm");
+    const minuteData = {
+      ...stat,
+      date: moment(minute, "YYYY-MM-DD-HH-mm").valueOf()
+    };
+    const minutePath = STATS_DIRECTORY + "/" + minute + ".json";
+
+    const stat = stats[i];
+    const date = moment(stat.timestamp * 1000);
+
+    // initialize the current minute and hour
+    if (!hour) hour = date.format("YYYY-MM-DD-HH");
+
+    // write this minute
+    await fs.outputJSON(minutePath, minuteData);
+
+    hourData.push(minuteData);
+
+    // we are in a new hour
+    if (hour !== date.format("YYYY-MM-DD-HH")) {
+      // write the last hour if and only if we have gathered more minutes than those already written
+      const existingHour =
+        ((await fs.exists(hourPath)) && (await fs.readJSON(hourPath)).length) ||
+        0;
+
+      if (hourData.length > existingHour) {
+        await fs.outputJSON(hourPath, hourData);
+      }
+      hourData = [];
+    }
+  }
+}
+
+async function getAllStats () {
+  return new Promise((resolve, reject) => {
+    const key = "blot:stats";
+    client.lrange(key, 0, -1, (err, data) => {
+      if (err) reject(err);
+      resolve(data.map(JSON.parse).reverse());
+    });
+  });
+}
+
+if (require.main === module) {
+  getAllStats()
+    .then(stats => {
+      console.log(stats);
+    })
+    .catch(console.log);
+}
+
+module.exports = main;
