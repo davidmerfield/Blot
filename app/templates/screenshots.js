fe7367a0be8ee825ba5a626fff501464a4c2eef4
David Merfield <dmerfield@gmail.com>
2023-12-18 19:01:08 +0000

    Updates screenshots script

--- app/templates/screenshots.js
+++ app/templates/screenshots.js
@@ -122,6 +122,9 @@ module.exports = main;
 
 if (require.main === module) {
   main()
-    .then(() => console.log("Done!"))
+    .then(() => {
+      console.log("Done!");
+      process.exit();
+    })
     .catch(console.error);
 }
