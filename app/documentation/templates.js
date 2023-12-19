f7db1187509c31d1c632b721fb2ceb5fce0a8d6f
David Merfield <dmerfield@gmail.com>
2023-12-19 07:42:34 +0000

    Updates folders

--- app/documentation/templates.js
+++ app/documentation/templates.js
@@ -13,49 +13,49 @@ const folderForTemplate = {
 const templatesDirectory = __dirname + "/../templates/latest";
 const foldersDirectory = __dirname + "/../templates/folders";
 
-module.exports = async (req, res, next) => {
-  res.locals.allTemplates = fs
-    .readdirSync(templatesDirectory)
-    .filter(i => !i.startsWith(".") && !i.endsWith(".js") && !i.endsWith(".md"))
-    .map(i => {
-      return {
-        name: i[0].toUpperCase() + i.slice(1),
-        slug: i,
-        preview:
-          config.protocol +
-          "preview-of-" +
-          req.params.template +
-          "-on-" +
-          i +
-          "." +
-          config.host,
-        folder: folderForTemplate[i]
-      };
-    });
+const folders = fs
+  .readdirSync(foldersDirectory)
+  .filter(i => !i.startsWith(".") && !i.endsWith(".js") && !i.endsWith(".md"))
+  .map(i => {
+    return {
+      name: i[0].toUpperCase() + i.slice(1),
+      slug: i,
+      folder: folderForTemplate[i]
+    };
+  });
 
-  res.locals.allFolders = fs
-    .readdirSync(foldersDirectory)
-    .filter(i => !i.startsWith(".") && !i.endsWith(".js") && !i.endsWith(".md"))
-    .map(i => {
-      return {
-        name: i[0].toUpperCase() + i.slice(1),
-        slug: i,
-        preview:
-          config.protocol +
-          "preview-of-" +
-          req.params.template +
-          "-on-" +
-          i +
-          "." +
-          config.host,
-        folder: folderForTemplate[i]
-      };
-    });
+const templates = fs
+  .readdirSync(templatesDirectory)
+  .filter(i => !i.startsWith(".") && !i.endsWith(".js") && !i.endsWith(".md"))
+  .map(i => {
+    return {
+      name: i[0].toUpperCase() + i.slice(1),
+      slug: i,
+      folder: folderForTemplate[i]
+    };
+  });
+
+module.exports = async (req, res, next) => {
+  res.locals.allTemplates = templates;
+
+  // only include folders that are not used as templates
+  res.locals.allFolders = folders.filter(
+    i => !Object.values(folderForTemplate).includes(i.slug)
+  );
 
   if (req.params.template) {
-    res.locals.template = res.locals.allTemplates.find(
-      i => i.slug === req.params.template
-    );
+    res.locals.template = {
+      ...templates.find(i => i.slug === req.params.template)
+    };
+
+    res.locals.template.preview =
+      config.protocol +
+      "preview-of-" +
+      req.params.template +
+      "-on-" +
+      res.locals.template.folder +
+      "." +
+      config.host;
 
     try {
       const markdown = await fs.readFile(
@@ -68,9 +68,7 @@ module.exports = async (req, res, next) => {
   }
 
   if (req.params.folder) {
-    res.locals.folder = res.locals.allFolders.find(
-      i => i.slug === req.params.folder
-    );
+    res.locals.folder = { ...folders.find(i => i.slug === req.params.folder) };
   }
 
   next();
