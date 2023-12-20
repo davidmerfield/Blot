52ed4b051f16048c6d7b0a5ea351ae72673d9a9f
David Merfield <dmerfield@gmail.com>
2023-12-20 21:25:05 +0000

    More tweaks to dashboard

--- app/dashboard/import/index.js
+++ app/dashboard/import/index.js
@@ -9,7 +9,6 @@ const sse = require("helper/sse")({ channel });
 
 Import.use((req, res, next) => {
   res.locals.importBase = res.locals.base + "/import";
-  res.locals.breadcrumbs.add("Services", "services");
   res.locals.breadcrumbs.add("Import", "import");
   next();
 });
