ed575acdb7a3bf1806ab4a0a32d50b08bb4e4a40
David Merfield <dmerfield@gmail.com>
2024-02-08 10:35:46 +0000

    Fixes ordering of thumnbail candidates (#751)

--- app/build/thumbnail/candidates.js
+++ app/build/thumbnail/candidates.js
@@ -1,10 +1,17 @@
 const cheerio = require("cheerio");
 const URL = require("url");
 
+const YOUTUBE_HOSTS = [
+  "www.youtube.com",
+  "youtube.com",
+  "www.youtube-nocookie.com",
+  "youtube-nocookie.com",
+  "m.youtube.com",
+  "youtu.be"
+];
+
 module.exports = function (metadata, html) {
   const candidates = [];
-
-  const $ = cheerio.load(html, { decodeEntities: false });
 
   // Would be nice to resolve this relative to
   // the location of the entry so we could
@@ -13,45 +20,58 @@ module.exports = function (metadata, html) {
     candidates.push(metadata.thumbnail);
   }
 
-  $(".videoContainer iframe").each(function () {
+  const $ = cheerio.load(html, { decodeEntities: false });
+
+  $("img, .videoContainer iframe").each(function () {
     try {
-      // handle vimeo
-      const thumbnail = $(this).attr("data-thumbnail");
+      // handle images
+      if ($(this).is("img")) {
+        const src = $(this).attr("src");
 
-      if (thumbnail) {
-        candidates.push(thumbnail);
-        return;
+        // The img lacks an src attribute – it happens!
+        if (!src) return;
+
+        // We've already added this image as a candidate
+        if (candidates.indexOf(src) > -1) return;
+
+        candidates.push(src);
+      } else {
+        // handle vimeo
+        const thumbnail = $(this).attr("data-thumbnail");
+
+        if (thumbnail) {
+          candidates.push(thumbnail);
+          return;
+        }
+
+        // handle youtube
+        const { hostname, pathname } = URL.parse($(this).attr("src"));
+
+        // We only want to handle youtube thumbnails
+        if (
+          YOUTUBE_HOSTS.indexOf(hostname) === -1 ||
+          pathname.indexOf("/embed/") !== 0
+        ) {
+          return;
+        }
+
+        const id = pathname.slice("/embed/".length);
+
+        if (!id) {
+          return;
+        }
+
+        // see here for a discussion of youtube thumbnail URLs:
+        // https://stackoverflow.com/questions/2068344/how-do-i-get-a-youtube-video-thumbnail-from-the-youtube-api
+        const highResThumbnail = `https://i.ytimg.com/vi/${id}/maxresdefault.jpg`;
+        const fallbackThumbnail = `http://img.youtube.com/vi/${id}/mqdefault.jpg`;
+
+        if (candidates.indexOf(highResThumbnail) === -1) {
+          candidates.push(highResThumbnail);
+          candidates.push(fallbackThumbnail);
+        }
       }
-
-      // handle youtube
-      const { hostname, pathname } = URL.parse($(this).attr("src"));
-
-      if (hostname !== "www.youtube-nocookie.com") return;
-
-      const id = pathname.slice("/embed/".length);
-
-      if (!id) return;
-
-      candidates.push(`https://i.ytimg.com/vi/${id}/maxresdefault.jpg`);
-      candidates.push(`http://img.youtube.com/vi/${id}/mqdefault.jpg`);
-
-      // see here for a discussion of youtube thumbnail URLs:
-      // https://stackoverflow.com/questions/2068344/how-do-i-get-a-youtube-video-thumbnail-from-the-youtube-api
-    } catch (e) {
-      return;
-    }
-  });
-
-  $("img").each(function () {
-    const src = $(this).attr("src");
-
-    // The img lacks an src attribute – it happens!
-    if (!src) return;
-
-    // We've already added this image as a candidate
-    if (candidates.indexOf(src) > -1) return;
-
-    candidates.push(src);
+    } catch (e) {}
   });
 
   return candidates;
