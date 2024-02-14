1b30482b6b523ccbe634ce6a388d936a2007ac9b
David Merfield <dmerfield@gmail.com>
2024-02-14 11:32:42 +0000

    Updates wikimedia importer

--- app/dashboard/import/sources/wikimedia_commons/index.js
+++ app/dashboard/import/sources/wikimedia_commons/index.js
@@ -1,1 +1,22 @@
-https://commons.wikimedia.org/wiki/Leonardo_da_Vinci
+// https://commons.wikimedia.org/wiki/Leonardo_da_Vinci
+
+const fs = require("fs-extra");
+const DATA_DIR = __dirname + "/data";
+
+const main = async label => {
+  if (!label) {
+    console.log("No label provided");
+    process.exit(1);
+  }
+
+  const folder = `${DATA_DIR}/${label}`;
+
+  if (!fs.existsSync(folder)) {
+    console.log(`No data directory found for ${label} at ${folder}`);
+    process.exit(1);
+  }
+};
+
+if (require.main === module) {
+  main(process.argv[2]);
+}
