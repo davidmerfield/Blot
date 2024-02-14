24ebb04f8f56f7a27fbcf6b7db2c2d7b64fc49c3
David Merfield <dmerfield@gmail.com>
2024-02-14 12:54:48 +0000

    Fixes thumbnail quote bug (#757)
    * Fixes thumbnail quote bug
    
    * revert changes
    
    * Adds failing test
    
    * Fixes bug

--- app/build/thumbnail/tests/index.js
+++ app/build/thumbnail/tests/index.js
@@ -6,6 +6,7 @@ describe("thumbnail", function () {
   global.test.blog();
   global.test.tmp();
 
+  var he = require("he");
   var localPath = require("helper/localPath");
   var fs = require("fs-extra");
 
@@ -60,6 +61,27 @@ describe("thumbnail", function () {
 
     fs.copyFileSync(
       __dirname + "/images/" + imagePath,
+      localPath(this.blog.id, imagePath)
+    );
+
+    thumbnail(this.blog, path, metadata, html, function (err, result) {
+      expect(err).toBe(null);
+      expect(result).toEqual(jasmine.any(Object));
+      expect(result.small).toEqual(jasmine.any(Object));
+
+      done();
+    });
+  });
+
+  it("creates a thumbnail for photos whose name has been HTML encoded", function (done) {
+    var thumbnail = require("../index");
+    var metadata = {};
+    var imagePath = "/portrait's.jpg";
+    var html = `<img src="${he.encode(imagePath)}">`;
+    var path = "/post.txt";
+
+    fs.copyFileSync(
+      __dirname + "/images/portrait.jpg",
       localPath(this.blog.id, imagePath)
     );
 
