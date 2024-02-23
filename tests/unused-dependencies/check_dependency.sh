03e07382b66c05247c0ee13531d18bc71f05a6e3
David Merfield <dmerfield@gmail.com>
2024-02-23 16:20:41 +0000

    Updates package

--- tests/unused-dependencies/check_dependency.sh
+++ tests/unused-dependencies/check_dependency.sh
@@ -5,6 +5,6 @@ APP_ROOT=$(dirname $(dirname $(dirname $PATH_TO_THIS_FILE)))/app
 
 # prevent exit error if grep finds no match
 set +e
-grep -rnwl app -e "require([\"'].*$1.*[\"'])" --exclude-dir=importer --include=\*.js
+grep -rnwl $APP_ROOT -e "require([\"'].*$1.*[\"'])" --exclude-dir=importer --include=\*.js
 set -e
 
