11a88985fd61238da079a750b5ec1e42bc4b774a
David Merfield <dmerfield@gmail.com>
2024-03-18 15:59:14 +0000

    Adds opt-in search for pages (#770)

--- app/models/entry/tests/setup.js
+++ app/models/entry/tests/setup.js
@@ -5,19 +5,19 @@ module.exports = function () {
   const set = require("../set");
   const search = require("../search");
   const drop = require("../drop");
-  
+
   global.test.blog();
 
   beforeEach(function () {
-    this.get = async (path) => {
-      return new Promise((resolve) => {
-        get(this.blog.id, path, (entry) => {
+    this.get = async path => {
+      return new Promise(resolve => {
+        get(this.blog.id, path, entry => {
           resolve(entry);
         });
       });
     };
 
-    this.search = async (query) => {
+    this.search = async query => {
       return new Promise((resolve, reject) => {
         search(this.blog.id, query, (err, ids) => {
           if (err) reject(err);
@@ -26,10 +26,21 @@ module.exports = function () {
       });
     };
 
-    this.drop = async (path) => {
-      return new Promise((resolve) => {
+    this.drop = async path => {
+      return new Promise(resolve => {
         drop(this.blog.id, path, () => {
           resolve();
+        });
+      });
+    };
+
+    this.remove = async path => {
+      return new Promise((resolve, reject) => {
+        fs.remove(this.blogDirectory + path, err => {
+          if (err) reject(err);
+          drop(this.blog.id, path, () => {
+            resolve();
+          });
         });
       });
     };
@@ -39,9 +50,9 @@ module.exports = function () {
         fs.outputFileSync(this.blogDirectory + path, contents);
         build(this.blog, path, {}, (err, entry) => {
           if (err) return reject(err);
-          set(this.blog.id, path, entry, (err) => {
+          set(this.blog.id, path, entry, err => {
             if (err) return reject(err);
-            get(this.blog.id, path, (entry) => {
+            get(this.blog.id, path, entry => {
               resolve(entry);
             });
           });
