38d12dae9da9e07873fa81e1f27dc908dc6f60c2
David Merfield <dmerfield@gmail.com>
2024-03-18 17:35:42 +0000

    Adds more rendering tests

---
+++ app/blog/tests/template.js
@@ -0,0 +1,41 @@
+describe("template engine", function () {
+  require("./setup")();
+
+  it("lists entries in reverse chronological", async function () {
+    await this.write("/first.txt", "Foo");
+    await this.write("/second.txt", "Bar");
+
+    await this.template({
+      "entries.html":
+        "{{#entries}}<p><a href='{{{url}}}'>{{title}}</a></p>{{/entries}}"
+    });
+
+    const res = await this.fetch(`/`);
+
+    expect((await res.text()).trim()).toEqual(
+      "<p><a href='/second'>second</a></p><p><a href='/first'>first</a></p>"
+    );
+  });
+
+  it("augments entry.next and entry.previous", async function () {
+    await this.write("/first.txt", "Link: first\n\nFoo");
+    await this.write("/second.txt", "Tags: foo\nLink: second\n\nSecond");
+
+    await this.template({
+      "entry.html": `
+        {{#entry}}
+          {{{html}}} 
+          {{^next.tagged.foo}}
+          {{#next}}
+          <p>Next: <a href='{{{url}}}'>{{title}}</a></p>
+          {{/next}}
+          {{/next.tagged.foo}}
+        {{/entry}}
+      `
+    });
+
+    const res = await this.fetch(`/first`);
+
+    expect(await res.text()).not.toContain("<a href='/second'>");
+  });
+});
