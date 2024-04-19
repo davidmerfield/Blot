9da47c4f4b4626cd54f2df318ff0c194d06b28d1
David Merfield <dmerfield@gmail.com>
2024-04-19 16:13:51 +0100

    Adds script to identify card testers

---
+++ app/scheduler/daily/check-card-testers.js
@@ -0,0 +1,49 @@
+// Iterate over Stripe customers and check if they have created a blog or nor
+// If there are users without a blog, log their information to the console
+// so we can investigate further as to why they haven't created a blog yet
+const User = require('models/user');
+const Blog = require('models/blog');
+const config = require("config");
+const stripe = require("stripe")(config.stripe.secret);
+
+
+const getByCustomerID = async (customerId) => {
+    return new Promise((resolve, reject) => {
+        User.getByCustomerID(customerId, (err, user) => {
+            resolve(user);
+        });
+    });
+};
+
+module.exports = async function (startingAfter = null) {
+    
+    console.log('listing 100 customers starting after', startingAfter || 'beginning');
+
+    const response = await stripe.customers.list({ limit: 100, starting_after: startingAfter });
+
+    for (const customer of response.data) {
+        const user = await getByCustomerID(customer.id);
+    
+        if (!user) {
+            console.log(`No user found for customer ${customer.id}`);
+            continue;
+
+         }
+
+         if (!user.blogs || user.blogs.length === 0) {
+             console.log(`User ${user.email} has no blog`);
+         }
+
+    }
+
+    if (!response.has_more) {
+        console.log("No more customers to fetch");
+        return true;
+    }
+
+    return module.exports(response.data[response.data.length - 1].id);
+}
+
+if (require.main === module) {
+    module.exports();
+}
