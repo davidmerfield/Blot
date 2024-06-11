9b348418ef63a4e0d40cd3a7827a597cbcab9ede
David Merfield <dmerfield@gmail.com>
2024-06-11 12:13:06 +0100

    More tests

--- app/site/tests/index.js
+++ app/site/tests/index.js
@@ -12,5 +12,31 @@ describe("Blot's site'", function () {
         expect(res.status).toEqual(200);
     });
     
+    it("lets user log in", async function () {
+      const email = this.user.email;
+      const password = this.user.fakePassword;
+
+      const res = await fetch(this.origin + "/sites/log-in", {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+        headers: { "Content-Type": "application/json" }
+      });
+
+      const cookie = res.headers.get("set-cookie");
+
+      expect(cookie).toBeTruthy();
+      
+      // load the dashboard using the cookie
+
+      const res2 = await fetch(this.origin + "/sites", {
+        headers: { cookie }
+      });
+
+      const text = await res2.text();
+
+      expect(res2.status).toEqual(200);
+
+    });
+
   });
   
