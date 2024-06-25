40ee281bebb20dce91da641561fe33330136a581
David Merfield <dmerfield@gmail.com>
2024-06-25 16:33:46 +0100

    Updates site

--- app/dashboard/index.js
+++ app/dashboard/index.js
@@ -101,6 +101,9 @@ dashboard.use(function (err, req, res, next) {
     };
   }
 
+  console.error(err.message);
+  console.error(err.stack);
+
   res.locals.layout = "";
   res.status(status);
   res.render("error");
