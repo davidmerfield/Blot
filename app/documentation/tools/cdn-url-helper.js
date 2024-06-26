b77c4d75f287af0c1d4b7a2faff790c1def1149f
David Merfield <dmerfield@gmail.com>
2024-06-26 09:27:07 +0100

    Updates CDN URL helper

--- app/documentation/tools/cdn-url-helper.js
+++ app/documentation/tools/cdn-url-helper.js
@@ -3,19 +3,30 @@ const hash = require("helper/hash");
 const config = require("config");
 const { join } = require("path");
 
-module.exports = ({cacheID, viewDirectory}) => () => (text, render) => {
+// Create a cache object to store the results
+const cache = {};
+
+module.exports = ({ cacheID, viewDirectory }) => {
+  return () => (text, render) => {
     const path = render(text);
-  
-    let identifier = cacheID;
-  
-    try {
-      const contents = fs.readFileSync(join(viewDirectory, path), "utf8");
-      identifier = hash(contents).slice(0, 8);
-      // console.log('hashed', path, identifier);
-    } catch (e) {
-      console.log('failed to hash', path, e);
-      // if the file doesn't exist, we'll use the cacheID
+
+    let identifier = cache[cacheID]; // Check if the result is already cached
+
+    if (!identifier) {
+      try {
+        const contents = fs.readFileSync(join(viewDirectory, path), "utf8");
+        identifier = hash(contents).slice(0, 8);
+        // console.log('hashed', path, identifier);
+
+        // Cache the result for future use
+        cache[cacheID] = identifier;
+      } catch (e) {
+        console.log("failed to hash", path, e);
+        // if the file doesn't exist, we'll use the cacheID
+        identifier = cacheID;
+      }
     }
-  
+
     return `${config.cdn.origin}/documentation/v-${identifier}${path}`;
   };
+};
