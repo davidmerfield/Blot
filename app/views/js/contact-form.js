beb01f5b7cac90f9c8c6d58f0e174e22bbb0b758
David Merfield <dmerfield@gmail.com>
2024-11-22 11:48:34 +0100

    Updates contact form js

--- app/views/js/contact-form.js
+++ app/views/js/contact-form.js
@@ -2,20 +2,27 @@
 const contactForm = document.getElementById('contact-form');
 
 if (contactForm) {
-    // when the email checkbox is checked, show the email input
-contactForm.querySelector('input[type="checkbox"][name="emailToggle"]').addEventListener('change', function(e) {
-  var emailInput = document.querySelector('input[type="email"]');
-  emailInput.parentElement.style.display = e.target.checked ? 'block' : 'none';
-  // focus the email input if it's shown
-  if (e.target.checked) {
-    emailInput.focus();
-  }
-});
 
-// if the input is checked on page load, show the email input
-if (contactForm.querySelector('input[type="checkbox"][name="emailToggle"]').checked) {
-  contactForm.querySelector('input[type="email"]').parentElement.style.display = 'block';
-}
+  const emailInput = contactForm.querySelector('input[type="email"]');
+  const emailToggle = contactForm.querySelector('input[type="checkbox"][name="emailToggle"]');
+
+  if (emailToggle){ 
+
+  // when the email checkbox is checked, show the email input
+  emailToggle.addEventListener('change', function(e) {
+    emailInput.parentElement.style.display = e.target.checked ? 'block' : 'none';
+    // focus the email input if it's shown
+    if (e.target.checked) {
+      emailInput.focus();
+    }
+  });
+  
+  // if the input is checked on page load, show the email input
+  if (emailToggle.checked) {
+    emailInput.parentElement.style.display = 'block';
+  }
+    
+  }
 
 // when the form button is clicked, submit the form in the background and replace the form html 
 // with a success message or error message. ensure the server recieves the same data if js is 
