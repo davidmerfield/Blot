cb1d9ba9339165e86916ea78cea6054912d2db88
David Merfield <dmerfield@gmail.com>
2024-12-19 13:48:31 +0100

    Initial commit

---
+++ app/build/plugins/flickr/index.js
@@ -0,0 +1,77 @@
+/* from flickr 
+
+oEmbed Endpoint
+
+The official oEmbed endpoint for Bluesky posts is 
+
+https://www.flickr.com/services/oembed/
+
+    url (required): flickr.com or flic.kr url to a post or gallery
+    format (optional): json
+*/
+
+const each = require("../eachEl");
+const Url = require("url");
+const fetch = require("node-fetch");
+
+function render($, callback) {
+  console.log("flickr plugin", "render", $.html());
+
+  each(
+    $,
+    "a",
+    function (el, next) {
+      var href, host, text, id;
+
+      try {
+        href = $(el).attr("href");
+        text = $(el).text();
+        host = Url.parse(href).host;
+      } catch (e) {
+        return next();
+      }
+
+      // Ensure we managed to extract everything from the url
+      if (!href || !text || !host) return next();
+
+      // Look for bare links
+      if (href !== text) return next();
+
+      // which point to a post on flickr
+      if (host !== "flickr.com" && host !== "flic.kr") return next();
+
+      var params = {
+        url: href,
+        format: "json"
+      };
+
+      var oembedUrl = "https://www.flickr.com/services/oembed/?" +
+        new URLSearchParams(params).toString();
+
+
+      fetch(oembedUrl)
+        .then((res) => res.json())
+        .then((data) => {
+          if (!data || !data.html) return next();
+
+          var html = data.html;
+
+          $(el).replaceWith(html);
+          next();
+        })
+        .catch(() => {
+          return next();
+        });
+    },
+    function () {
+      callback();
+    }
+  );
+}
+
+module.exports = {
+  render: render,
+  category: "external",
+  title: "Flickr",
+  description: "Embed Flickr posts and galleries",
+};
---
+++ app/build/plugins/flickr/tests.js
@@ -0,0 +1,46 @@
+describe("bluesky plugin", function () {
+    const replaceURLsWithEmbeds = require("./index.js").render;
+    const cheerio = require("cheerio");
+  
+    it("works", function (done) {
+      // html bare link to a post on bluesky
+      const html =
+        '<a href="https://bsky.app/profile/logicallyjc.bsky.social/post/3lbretguxqk2b">https://bsky.app/profile/logicallyjc.bsky.social/post/3lbretguxqk2b</a>';
+  
+      const $ = cheerio.load(html);
+  
+      replaceURLsWithEmbeds($, function () {
+  
+          console.log('html:', $.html());
+        expect($("a[href='https://bsky.app/profile/logicallyjc.bsky.social/post/3lbretguxqk2b']").length).toBe(0);
+        expect($("blockquote").length).toBe(1);
+        done();
+      });
+    });
+  
+    it("does not error when there are no links", function (done) {
+      const html = "<p>hello</p>";
+  
+      const $ = cheerio.load(html);
+  
+      replaceURLsWithEmbeds($, function () {
+        expect($("a").length).toBe(0);
+        expect($("blockquote").length).toBe(0);
+        done();
+      });
+    });
+  
+    // if the bluesky link is invalid or poorly formatted, it should not be replaced
+    it("does not error when the link is invalid", function (done) {
+      const html = '<a href="https://bsky.app">https://bsky.app</a>';
+  
+      const $ = cheerio.load(html);
+  
+      replaceURLsWithEmbeds($, function () {
+        expect($("a").length).toBe(1);
+        expect($("blockquote").length).toBe(0);
+        done();
+      });
+    });
+  });
+  
