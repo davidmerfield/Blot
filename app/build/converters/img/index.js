d3c438cdba5b612cea90b63144ae10e1f2cafdfc
David Merfield <dmerfield@gmail.com>
2025-01-16 14:54:10 +0100

    Adds support for reading exif data

--- app/build/converters/img/index.js
+++ app/build/converters/img/index.js
@@ -6,10 +6,11 @@ const LocalPath = require("helper/localPath");
 const hash = require("helper/hash");
 const sharp = require("sharp");
 const config = require("config");
+const exifReader = require('exif-reader');
+const YAML = require("yaml");
 
 const EXTENSIONS_TO_CONVERT = [".tif", ".tiff", ".webp", ".avif"];
 const SUPPORTED_EXTENSIONS = [".png", ".jpg", ".jpeg", ".gif", ...EXTENSIONS_TO_CONVERT];
-
 
 function is(path) {
   return SUPPORTED_EXTENSIONS.includes(extname(path).toLowerCase());
@@ -22,16 +23,31 @@ function read(blog, path, options, callback) {
     .and(callback, "function");
 
   const localPath = LocalPath(blog.id, path);
+
   // if we need to convert the image to another format, store the converted image in the asset directory
   const assetDirectory = join(config.blog_static_files_dir, blog.id);
   
   fs.stat(localPath, async (err, stat) => {
     if (err) return callback(err);
-
     const name = options.name || basename(path);
     const pathForTitle = options.pathDisplay || join(dirname(path), name);
     const title = titlify(pathForTitle);
     const isRetina = path.toLowerCase().includes("@2x") ? 'data-2x="true"' : "";
+
+    let imagePath = path;
+    let image = sharp(localPath);
+
+    let metadata = {};
+    try {
+      const metadataRaw = await image.metadata();
+      if (metadataRaw.exif) {
+      metadata.exif = exifReader(metadataRaw.exif);
+      }
+    } catch (e) {
+      return callback(e);
+    }
+
+    const metadataString = Object.keys(metadata).length === 0 ? "" : `---\n${YAML.stringify(metadata)}\n---\n\n`;
 
     if (EXTENSIONS_TO_CONVERT.includes(extname(path).toLowerCase())) {
       const convertedPath = join("/_assets", hash(path), `${name}.png`);
@@ -40,15 +56,14 @@ function read(blog, path, options, callback) {
       // remove any existing converted image
       await fs.remove(join(assetDirectory, convertedPath));
       try {
-        await sharp(localPath).png().toFile(join(assetDirectory, convertedPath));
+      await image.png().toFile(join(assetDirectory, convertedPath));
+      imagePath = convertedPath;
       } catch (e) {
-        return callback(e);
+      return callback(e);
       }
-
-      path = convertedPath;
     }
 
-    const contents = `<img src="${encodeURI(path)}" title="${title}" alt="${title}" ${isRetina}/>`;
+    const contents = `${metadataString}<img src="${encodeURI(imagePath)}" title="${title}" alt="${title}" ${isRetina}/>`;
     
     callback(null, contents, stat);
   });
