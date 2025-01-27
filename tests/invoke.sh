1acd020ec04857f2ba1a6bc7fefbf9932bba83c0
David Merfield <dmerfield@gmail.com>
2025-01-27 11:59:21 +0100

    Updates test invocation

--- tests/invoke.sh
+++ tests/invoke.sh
@@ -3,7 +3,10 @@
 # Set the optional test path
 TEST_PATH=$1
 
-echo "Running tests in path: $TEST_PATH"
+# Set the optional test seed
+TEST_SEED=$2
+
+echo "Running tests in path: $TEST_PATH with seed $TEST_SEED"
 
 # Run the tests in the specified path using docker-compose
-TEST_PATH=$TEST_PATH docker-compose  -f tests/docker-compose.yml up --build --abort-on-container-exit --remove-orphans
+TEST_PATH=$TEST_PATH TEST_SEED=$TEST_SEED docker-compose  -f tests/docker-compose.yml up --build --abort-on-container-exit --remove-orphans
