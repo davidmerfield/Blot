4a930b270440acc4825131f1cb23474dee04786f
David Merfield <dmerfield@gmail.com>
2025-01-27 12:36:18 +0100

    Updates test

--- app/helper/screenshot/tests/index.js
+++ app/helper/screenshot/tests/index.js
@@ -1,18 +1,24 @@
 describe("screenshot plugin", function () {
     const screenshot = require("../index.js");
     const fs = require("fs-extra");
-    
+    const hashFile = require('helper/hashFile');
     it("works", async function () {
         const site = "https://example.com";
         const path = __dirname + "/data/screenshot.png";
         const expectedPath = __dirname + "/expected.png";
+        const expectedHash = await hashFile(expectedPath);
 
         await screenshot(site, path);
     
         expect(fs.existsSync(path)).toBe(true);
-        expect(fs.readFileSync(path)).toEqual(fs.readFileSync(expectedPath));
+
+        const hash = await hashFile(path);
+
+        if (hash !== expectedHash) {
+            throw new Error(`Hash mismatch: ${hash} !== ${expectedHash}`);
+        }
 
         // cleanup
-        fs.unlinkSync(path);
-    });
+        // fs.unlinkSync(path);
+    }, 15000); // 10 seconds timeout
 });
