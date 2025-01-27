ec1c4beb9c0eed58209c30ddf4c79ba0964fe1ab
David Merfield <dmerfield@gmail.com>
2025-01-27 10:45:15 +0100

    Fixes question test

--- app/models/question/tests/setup.js
+++ app/models/question/tests/setup.js
@@ -15,4 +15,21 @@ module.exports = function setup(options) {
       }
     });
   });
+
+  // remove all keys from the redis-db with the prefix 'blot:questions
+  afterEach(function (done) {
+    const client = require("models/client");
+
+    client.keys("blot:questions:*", function (err, keys) {
+      if (err) return done(err);
+      if (keys.length > 0) {
+        client.del(keys, function (err, response) {
+          if (err) return done(err);
+          done();
+        });
+      } else {
+        done();
+      }
+    });
+  });
 };
