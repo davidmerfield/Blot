162cdfe3ac7ee45a3b0d477f595cdefda3c122d8
David Merfield <dmerfield@gmail.com>
2025-01-28 12:10:05 +0100

    Fix invalid docker compose name

--- tests/benchmark/invoke.sh
+++ tests/benchmark/invoke.sh
@@ -3,4 +3,4 @@
 echo "Running benchmark"
 
 # Run the tests in the specified path using docker-compose
-docker-compose  -f tests/benchmark/docker-compose.yml up --build --abort-on-container-exit --remove-orphans
+docker-compose -p blot-benchmark -f tests/benchmark/docker-compose.yml up --build --abort-on-container-exit --remove-orphans
