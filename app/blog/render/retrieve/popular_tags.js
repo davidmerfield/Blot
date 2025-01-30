bc5586fd5a45dd6b60712d463b23a77abb575d5a
David Merfield <dmerfield@gmail.com>
2025-01-31 00:13:48 +0100

    Revert changes to popular tags

--- app/blog/render/retrieve/popular_tags.js
+++ app/blog/render/retrieve/popular_tags.js
@@ -3,11 +3,9 @@ var Entry = require("models/entry");
 var async = require("async");
 
 module.exports = function (req, callback) {
-  req.log('Listing tags');
   Tags.list(req.blog.id, function (err, tags) {
     // In future, we might want to expose
     // other options for this sorting...
-    req.log('Sorting tags');
     tags = tags.sort(function (a, b) {
       if (a.entries.length > b.entries.length) return -1;
 
@@ -16,27 +14,21 @@ module.exports = function (req, callback) {
       return 0;
     });
 
-    tags = tags.map((tag) => {
-      tag.tag = tag.name;
-      tag.total = tag.entries.length;
-      return tag;
-    });
+    async.each(
+      tags,
+      function (tag, next) {
+        // so we can do {{tag}} since I like it.
+        tag.tag = tag.name;
+        tag.total = tag.entries.length;
 
-    req.log('Listed tags');
-    callback(null, tags);
-    // async.each(
-    //   tags,
-    //   function (tag, next) {
-    //     // so we can do {{tag}} since I like it.
-    //     next();
-    //     // Entry.get(req.blog.id, tag.entries, function (entries) {
-    //     //   tag.entries = entries;
-    //     //   next();
-    //     // });
-    //   },
-    //   function () {
-    //     req.log('Listed tags');
-    //   }
-    // );
+        Entry.get(req.blog.id, tag.entries, function (entries) {
+          tag.entries = entries;
+          next();
+        });
+      },
+      function () {
+        callback(null, tags);
+      }
+    );
   });
 };
