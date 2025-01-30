a55e66503551b92018d0632f773dc044f3e6b7b1
David Merfield <dmerfield@gmail.com>
2025-01-30 23:21:54 +0100

    Updates popular tags

--- app/blog/render/retrieve/popular_tags.js
+++ app/blog/render/retrieve/popular_tags.js
@@ -3,9 +3,11 @@ var Entry = require("models/entry");
 var async = require("async");
 
 module.exports = function (req, callback) {
+  req.log('Listing tags');
   Tags.list(req.blog.id, function (err, tags) {
     // In future, we might want to expose
     // other options for this sorting...
+    req.log('Sorting tags');
     tags = tags.sort(function (a, b) {
       if (a.entries.length > b.entries.length) return -1;
 
@@ -14,22 +16,27 @@ module.exports = function (req, callback) {
       return 0;
     });
 
-    async.each(
-      tags,
-      function (tag, next) {
-        // so we can do {{tag}} since I like it.
-        tag.tag = tag.name;
-        tag.total = tag.entries.length;
-        next();
-        // Entry.get(req.blog.id, tag.entries, function (entries) {
-        //   tag.entries = entries;
+    tags = tags.map((tag) => {
+      tag.tag = tag.name;
+      tag.total = tag.entries.length;
+      return tag;
+    });
 
-        //   next();
-        // });
-      },
-      function () {
-        callback(null, tags);
-      }
-    );
+    req.log('Listed tags');
+    callback(null, tags);
+    // async.each(
+    //   tags,
+    //   function (tag, next) {
+    //     // so we can do {{tag}} since I like it.
+    //     next();
+    //     // Entry.get(req.blog.id, tag.entries, function (entries) {
+    //     //   tag.entries = entries;
+    //     //   next();
+    //     // });
+    //   },
+    //   function () {
+    //     req.log('Listed tags');
+    //   }
+    // );
   });
 };
