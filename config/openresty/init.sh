775cab0e1a6e24c5f9be11802cd84edf5285b7dc
David Merfield <dmerfield@gmail.com>
2025-02-05 14:26:42 +0100

    Fixes localhost

--- config/openresty/init.sh
+++ config/openresty/init.sh
@@ -1,17 +1,19 @@
 cat /etc/nginx/openssl.cnf
 
-mkdir -p /etc/ssl/private /etc/ssl/certs
+if [ ! -f /etc/ssl/private/wildcard.key ] || [ ! -f /etc/ssl/certs/wildcard.crt ]; then
+    mkdir -p /etc/ssl/private /etc/ssl/certs
 
-openssl req \
-    -newkey rsa:2048 \
-    -x509 \
-    -nodes \
-    -keyout /etc/ssl/private/wildcard.key \
-    -new \
-    -out /etc/ssl/certs/wildcard.crt \
-    -config /etc/nginx/openssl.cnf \
-    -sha256 \
-    -days 3650
+    openssl req \
+        -newkey rsa:2048 \
+        -x509 \
+        -nodes \
+        -keyout /etc/ssl/private/wildcard.key \
+        -new \
+        -out /etc/ssl/certs/wildcard.crt \
+        -config /etc/nginx/openssl.cnf \
+        -sha256 \
+        -days 3650
+fi
 
 # Start nginx
 nginx -g "daemon off;"
