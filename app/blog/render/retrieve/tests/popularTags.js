4906cc9e9c1a7949aa837125fad6f0cd337e12bb
David Merfield <dmerfield@gmail.com>
2025-02-08 12:00:55 +0100

    Updates popular tags test

--- app/blog/render/retrieve/tests/popularTags.js
+++ app/blog/render/retrieve/tests/popularTags.js
@@ -21,5 +21,34 @@ describe("popular tags", function () {
         expect(body.trim()).toEqual('<ul><li>def</li><li>abc</li></ul>');
     });
 
+    it("lists popular tags with many posts", async function () {
+        const tags = Array.from({ length: 100 }, (_, i) => `tag${i}`);
+        const tagUsage = {};
+    
+        for (let i = 0; i < 200; i++) {
+            const numTags = (i % 5) + 1; // Deterministically assign the number of tags
+            const postTags = tags.slice(0, numTags); // Use the first `numTags` tags
+            for (const tag of postTags) {
+                tagUsage[tag] = (tagUsage[tag] || 0) + 1;
+            }
+            await this.blog.write({ path: `/post${i}.txt`, content: `Tags: ${postTags.join(', ')}\n\nContent ${i}` });
+        }
+    
+        await this.blog.rebuild();
+    
+        await this.template({
+            'entries.html': `<ul>{{#popular_tags}}<li>{{tag}} {{entries.length}}</li>{{/popular_tags}}</ul>`
+        });
+    
+        const res = await this.get('/');
+        const body = await res.text();
+    
+        const sortedTags = Object.keys(tagUsage).sort((a, b) => tagUsage[b] - tagUsage[a]);
+        const expectedHtml = `<ul>${sortedTags.map(tag => `<li>${tag} ${tagUsage[tag]}</li>`).join('')}</ul>`;
+    
+        expect(res.status).toEqual(200);
+        expect(body.trim()).toEqual(expectedHtml);
+    }, 30000);
+
 
 });
