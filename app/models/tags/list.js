14dd8e76996e6815acf2154f4bdb460496c16ece
David Merfield <dmerfield@gmail.com>
2025-02-12 16:26:22 +0100

    Revert "Adds debugging to tags list"
    This reverts commit 3c393d29f72406f17d269fe7576fa0f38f880f35.

--- app/models/tags/list.js
+++ app/models/tags/list.js
@@ -1,13 +1,10 @@
 const client = require("models/client");
 const ensure = require("helper/ensure");
 const key = require("./key");
-const clfdate = require("helper/clfdate");
 
 module.exports = async function getAll(blogID, callback) {
   try {
     ensure(blogID, "string").and(callback, "function");
-
-    console.log(clfdate(), "Fetching all tags for", blogID);
 
     // Fetch all tags using SMEMBERS
     const allTags = await new Promise((resolve, reject) => {
@@ -17,8 +14,6 @@ module.exports = async function getAll(blogID, callback) {
       });
     });
 
-    console.log(clfdate(), "Found", allTags.length, "tags for", blogID);
-
     if (allTags.length === 0) {
       return callback(null, []); // No tags to process
     }
@@ -26,7 +21,6 @@ module.exports = async function getAll(blogID, callback) {
     // Iterate over tags and fetch their details
     const tags = [];
     for (const tag of allTags) {
-      console.log(clfdate(), "Fetching tag", tag, "for", blogID);
       const [entries, name] = await Promise.all([
         new Promise((resolve, reject) => {
           client.smembers(key.tag(blogID, tag), (err, result) => {
@@ -42,7 +36,6 @@ module.exports = async function getAll(blogID, callback) {
         }),
       ]);
 
-      console.log(clfdate(), "Fetched", entries.length, "entries for tag", tag, "for", blogID);
       if (entries.length > 0) {
         tags.push({
           name,
@@ -52,7 +45,6 @@ module.exports = async function getAll(blogID, callback) {
       }
     }
 
-    console.log(clfdate(), "Fetched all tags for", blogID);
     return callback(null, tags);
   } catch (error) {
     return callback(error);
