08cde114ed6b861c8f5cf0df0f448320db7537b6
David Merfield <dmerfield@gmail.com>
2025-02-17 11:32:03 +0100

    Update deploy script and screenshots workflow

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -89,8 +89,8 @@ DOCKER_RUN_COMMAND="docker run --pull=always -d \
   ghcr.io/davidmerfield/blot:$GIT_COMMIT_HASH"
 
 # Configurable health check timeout
-timeout=${HEALTH_CHECK_TIMEOUT:-60}  # Default to 60 seconds
-interval=2  # Interval between health checks
+timeout=${HEALTH_CHECK_TIMEOUT:-90}  # Default to 90 seconds
+interval=5  # Interval between health checks
 
 # Function to run a command over SSH
 ssh_blot() {
