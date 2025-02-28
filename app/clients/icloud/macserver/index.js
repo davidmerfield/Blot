3d42a2d73aed991185bd3f6a7a9f8d855abf347d
David Merfield <dmerfield@gmail.com>
2025-02-28 19:36:31 +0100

    Update icloud server

--- app/clients/icloud/macserver/index.js
+++ app/clients/icloud/macserver/index.js
@@ -89,7 +89,7 @@ const getLimiterForBlogID = (blogID) => {
 const handleFileEvent = async (event, filePath) => {
   try {
     // handle paths with special characters e.g. umlauts
-    const normalizedFilePath = filePath;//.normalize("NFC");
+    const normalizedFilePath = filePath.normalize("NFC");
     const relativePath = normalizedFilePath.replace(`${iCloudDriveDirectory}/`, "");
     const [blogID, ...restPath] = relativePath.split("/");
     const path = restPath.join("/");
