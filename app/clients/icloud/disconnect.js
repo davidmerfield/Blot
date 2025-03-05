b239ada03ab3cd7602d0ac960fc506ab0fb47119
David Merfield <dmerfield@gmail.com>
2025-03-05 15:16:17 +0100

    Updates iCloud client

--- app/clients/icloud/disconnect.js
+++ app/clients/icloud/disconnect.js
@@ -1,11 +1,28 @@
 const Blog = require("models/blog");
 const database = require("./database");
 
-module.exports = async (blogID, callback) => {
-  
-    await database.delete(blogID);
+const config = require("config");
 
-    Blog.set(blogID, { client: "" }, async function (err) {
-        callback();
-    });
+const MACSERVER_URL = config.icloud.server_address; // The Macserver base URL from config
+const MACSERVER_AUTH = config.icloud.secret; // The Macserver Authorization secret from config
+
+module.exports = async (blogID, callback) => {
+  await database.delete(blogID);
+
+  const response = await fetch(`${MACSERVER_URL}/disconnect`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: MACSERVER_AUTH,
+      blogID: blogID,
+    },
+  });
+
+  if (!response.ok) {
+    console.error(`Macserver /disconnect request failed: ${response.status}`);
+  }
+
+  Blog.set(blogID, { client: "" }, async function (err) {
+    callback();
+  });
 };
--- app/clients/icloud/macserver/index.js
+++ app/clients/icloud/macserver/index.js
@@ -302,6 +302,31 @@ const startServer = () => {
     next();
   });
 
+  app.post("/disconnect", async (req, res) => {
+    const blogID = req.header("blogID");
+
+    if (!blogID) {
+      return res.status(400).send("Missing blogID header");
+    }
+
+    // ensure the blogID doesn't container any characters other than
+    // letters, numbers and underscores
+    if (!/^[a-zA-Z0-9_]+$/.test(blogID)) {
+      return res.status(400).send("Invalid blogID");
+    }
+
+    console.log(`Received disconnect request for blogID: ${blogID}`);
+
+    // remove the blogid folder and the limiter
+    limiters.delete(blogID);
+    await fs.remove(path.join(iCloudDriveDirectory, blogID));
+
+    console.log(`Disconnected blogID: ${blogID}`);
+    
+    res.sendStatus(200);
+  });
+
+
   app.post("/setup", async (req, res) => {
     const blogID = req.header("blogID");
     
