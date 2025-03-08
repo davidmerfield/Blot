cf63bcfb1079046521f474ed5b7f9efc782892f8
David Merfield <dmerfield@gmail.com>
2025-03-08 13:53:13 +0100

    Updates iCloud client

--- scripts/macserver/deploy.sh
+++ scripts/macserver/deploy.sh
@@ -4,7 +4,6 @@
 set -e
 
 REMOTE_CODE_DIRECTORY=/Users/admin/blot
-REMOTE_MACSERVER_CODE_DIRECTORY=app/clients/icloud/macserver
 
 # Function to run a command over SSH
 ssh_macserver() {
@@ -13,6 +12,6 @@ ssh_macserver() {
 
 # Restart the application using PM2 with nvm loaded
 echo "Restarting the macserver process with PM2..."
-ssh_macserver "export NVM_DIR=\"\$HOME/.nvm\" && [ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\" && cd $REMOTE_CODE_DIRECTORY && git pull && cd $REMOTE_MACSERVER_CODE_DIRECTORY && npm i && pm2 restart macserver"
+ssh_macserver "export NVM_DIR=\"\$HOME/.nvm\" && [ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\" && cd $REMOTE_CODE_DIRECTORY && git pull && pm2 restart macserver"
 
 echo "Deployment completed successfully!"
