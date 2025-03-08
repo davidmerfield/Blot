7be11828d1f51a202ffe60d30eba0aa03778e3e5
David Merfield <dmerfield@gmail.com>
2025-03-08 14:37:23 +0100

    Updates iCloud client

--- app/clients/icloud/init.js
+++ app/clients/icloud/init.js
@@ -1,12 +1,21 @@
 const clfdate = require("helper/clfdate");
 
 const email = require("helper/email");
+const prettySize = require("helper/prettySize");
 const config = require("config");
 const MAC_SERVER_ADDRESS = config.icloud.server_address;
 const MACSERVER_AUTH = config.icloud.secret; // The Macserver Authorization secret from config
 
-module.exports = async () => {
+const DISK_SPACE_WARNING_THRESHOLD = 1e9; // 1GB
+const ICLOUD_SPACE_WARNING_THRESHOLD = 1e9; // 1GB
 
+const DISK_SPACE_LIMIT = 1e8; // 100MB
+const ICLOUD_SPACE_LIMIT = 1e8; // 100MB
+
+// map to keep track of which notifications have been sent
+const notificationsSent = {};
+
+module.exports = async () => {
   setInterval(async () => {
     console.log(clfdate(), "Checking Mac server stats");
     try {
@@ -14,15 +23,54 @@ module.exports = async () => {
       const res = await fetch(MAC_SERVER_ADDRESS + "/stats", {
         headers: { Authorization: MACSERVER_AUTH },
       });
+
       const stats = await res.json();
 
-      // email.WARNING_LOW_DISK_SPACE(null, { disks }, function (err) {
-      //   if (err) console.log(clfdate(), err);
-      // });
+      if (!stats || !stats.disk_bytes_available || !stats.icloud_bytes_available) {
+        throw new Error("No stats returned");
+      }
+
+      stats.disk_bytes_available_human = prettySize(stats.disk_bytes_available);
+      stats.icloud_bytes_available_human = prettySize(stats.icloud_bytes_available);
 
       console.log(clfdate(), "Mac server stats: ", stats);
+
+      if (stats.disk_bytes_available < DISK_SPACE_LIMIT) {
+        console.log(clfdate(), "Disk space is low");
+        if (!notificationsSent.disk_space_low) {
+          email.ICLOUD_DISK_LIMIT(null, { stats });
+          notificationsSent.disk_space_low = true;
+        }
+      } else if (stats.disk_bytes_available < DISK_SPACE_WARNING_THRESHOLD) {
+        console.log(clfdate(), "Disk space is running out");
+        if (!notificationsSent.disk_space_warning) {
+          email.ICLOUD_APPROACHING_DISK_LIMIT(null, { stats });
+          notificationsSent.disk_space_warning = true;
+        }
+      }
+
+      if (stats.icloud_bytes_available < ICLOUD_SPACE_LIMIT) {
+        console.log(clfdate(), "iCloud drive space is low");
+        if (!notificationsSent.icloud_space_low) {
+          email.ICLOUD_QUOTA_LIMIT(null, { stats });
+          notificationsSent.icloud_space_low = true;
+        }
+      } else if (stats.icloud_bytes_available < ICLOUD_SPACE_WARNING_THRESHOLD) {
+        console.log(clfdate(), "iCloud drive space is running out");
+        if (!notificationsSent.icloud_space_warning) {
+          email.ICLOUD_APPROACHING_QUOTA_LIMIT(null, { stats });
+          notificationsSent.icloud_space_warning = true;
+        }
+      }
+
+
     } catch (error) {
+
       console.log(clfdate(), "Error connecting to mac server: ", error);
+      if (!notificationsSent.icloud_server_down) {
+        email.ICLOUD_SERVER_DOWN();
+        notificationsSent.icloud_server_down = true;
+      }
     }
   }, 1000 * 15); // 15 seconds
 };
