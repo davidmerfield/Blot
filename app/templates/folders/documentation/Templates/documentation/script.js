0e10af59e0c3e3d222660637c1d1f416a7d1e7c5
David Merfield <dmerfield@gmail.com>
2025-03-12 17:02:56 +0100

    Updates documentation folder

--- app/templates/folders/documentation/Templates/documentation/script.js
+++ app/templates/folders/documentation/Templates/documentation/script.js
@@ -1,3 +1,14 @@
 // Plugin JavaScript for analytics embed code
 {{{appJS}}}
 {{> flickity-pkgd}}
+
+tocbot.init({
+    // Where to render the table of contents.
+    tocSelector: '#toc',
+    // Where to grab the headings to build the table of contents.
+    contentSelector: '.entry',
+    // Which headings to grab inside of the contentSelector element.
+    headingSelector: 'h1, h2, h3',
+    // For headings inside relative or absolute positioned containers within content.
+    hasInnerContainers: true,
+  });
