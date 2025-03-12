b2bcba427aba331905a7c3d982e9f099a973ae12
David Merfield <dmerfield@gmail.com>
2025-03-12 15:19:45 +0100

    Simplify tests

--- app/templates/tests/index.js
+++ app/templates/tests/index.js
@@ -1,21 +1,10 @@
 describe("Templates", function () {
-  const buildTemplates = require("templates");
   const config = require("config");
 
   // Set timeout to 5 minutes
   global.test.timeout(5 * 60 * 1000);
 
   global.test.site();
-
-  it(
-    "build without error",
-    function (done) {
-      buildTemplates({ watch: false }, function (err) {
-        if (err) return done.fail(err);
-        done();
-      });
-    },
-  );
 
   it("has no broken links for the blog template", async function () {
     await this.checkBrokenLinks("https://preview-of-blog-on-" + this.blog.handle + "." + config.host);
