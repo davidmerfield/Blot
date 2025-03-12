2345a6e669975b24e0efdf0f8fb731980ff5c606
David Merfield <dmerfield@gmail.com>
2025-03-12 15:24:13 +0100

    Adds test for broken links to all templates

--- app/templates/tests/index.js
+++ app/templates/tests/index.js
@@ -6,7 +6,24 @@ describe("Templates", function () {
 
   global.test.site();
 
-  it("has no broken links for the blog template", async function () {
-    await this.checkBrokenLinks("https://preview-of-blog-on-" + this.blog.handle + "." + config.host);
+  const templates = require("fs")
+    .readdirSync(__dirname + "/../latest")
+    .concat(require("fs").readdirSync(__dirname + "/../past"))
+    .filter((i) => i.indexOf(".") === -1);
+
+  templates.forEach((template) => {
+    it(
+      "has no broken links for the " + template + " template",
+      async function () {
+        await this.checkBrokenLinks(
+          "https://preview-of-" +
+            template +
+            "-on-" +
+            this.blog.handle +
+            "." +
+            config.host 
+        );
+      }
+    );
   });
 });
