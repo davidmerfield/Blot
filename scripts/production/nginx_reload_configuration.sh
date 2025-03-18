d99ae9da59365df0f2fe8bec35181fc5bd082957
David Merfield <dmerfield@gmail.com>
2025-03-18 18:26:02 +0100

    Updates scripts

--- scripts/production/nginx_reload_configuration.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-set -x
-
-openresty -s reload
--- scripts/remote/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  download: require("./download"),
-  exec: require("./exec"),
-  fetchdir: require("./fetchdir"),
-  host: require("./host"),
-  root: require("./root"),
-};
